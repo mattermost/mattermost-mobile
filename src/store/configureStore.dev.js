d4dbf0f70b718a4086ec1cb7d8dbaeed3ed8d4e6
Harrison Healey <harrisonmhealey@gmail.com>
2016-10-11 10:36:34 -0400

    Updated remote-redux-devtools configuration in store

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import {applyMiddleware, compose, createStore} from 'redux';
-import devTools from 'remote-redux-devtools';
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'remote-redux-devtools';
 import rootReducer from 'reducers/index.js';
 import thunk from 'redux-thunk';
 
@@ -10,13 +10,12 @@ export default function configureStore(preloadedState) {
     const store = createStore(
         rootReducer,
         preloadedState,
-        compose(
-            applyMiddleware(thunk),
-            devTools({
-                name: 'Mattermost',
-                hostname: 'localhost',
-                port: 5678
-            })
+        composeWithDevTools({
+            name: 'Mattermost',
+            hostname: 'localhost',
+            port: 5678
+        })(
+            applyMiddleware(thunk)
         )
     );
 
@@ -27,10 +26,6 @@ export default function configureStore(preloadedState) {
             store.replaceReducer(nextRootReducer);
         });
     }
-
-    // If you have other enhancers & middlewares
-    // update the store after creating / changing to allow devTools to use them
-    devTools.updateStore(store);
 
     return store;
 }
