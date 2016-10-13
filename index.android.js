231a3ffcf77514ba1e926670b016cbae91b23812
Thomas Hopkins <thomas@fullstacklabs.co>
2016-10-13 11:33:50 -0700

    Update index.android.js to match index.ios.js

--- index.android.js
+++ index.android.js
@@ -3,46 +3,20 @@
 
 import React from 'react';
 
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import {AppRegistry} from 'react-native';
+import configureStore from 'store/configureStore.js';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5
-    }
-});
+import {Provider} from 'react-redux';
+import RootContainer from 'containers/root_container.js';
+
+const store = configureStore();
 
 class Mattermost extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    {'Welcome to React Native!'}
-                </Text>
-                <Text style={styles.instructions}>
-                    {'To get started, edit index.android.jsx'}
-                </Text>
-                <Text style={styles.instructions}>
-                    {'Double tap R on your keyboard to reload\nShake or press menu button for dev menu'}
-                </Text>
-            </View>
+            <Provider store={store}>
+                <RootContainer/>
+            </Provider>
         );
     }
 }
