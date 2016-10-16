8d1e3e13a011e913e8290892e1bbed7e2e9ca6e5
Thomas Hopkins <thomas@fullstacklabs.co>
2016-10-16 11:17:39 -0700

    Handle failure to fetch teams

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import _ from 'lodash';
 import {requestData, requestSuccess, requestFailure} from './helpers.js';
 import Client from 'client/client_instance';
 import {TeamsTypes as types} from 'constants';
@@ -9,16 +10,32 @@ export function fetchTeams() {
     return async (dispatch) => {
         try {
             dispatch(requestData(types.FETCH_TEAMS_REQUEST));
-            const response = await fetch(url, {
             const url = `${Client.getTeamsRoute()}/all_team_listings`;
+            const resp = await fetch(url, {
                 headers: {
                     Authorization: 'Bearer bpfzjdtxybddtnfptmt78hghqo'
                 }
             });
-            const data = await response.json();
-            dispatch(requestSuccess(types.FETCH_TEAMS_SUCCESS, data));
+            let data;
+            const contentType = _.first(resp.headers.map['content-type']) || 'unknown';
+            if (contentType === 'application/json') {
+                data = await resp.json();
+            } else {
+                data = await resp.text();
+            }
+            if (resp.ok) {
+                dispatch(requestSuccess(types.FETCH_TEAMS_SUCCESS, data));
+            } else {
+                let msg;
+                if (contentType === 'application/json') {
+                    msg = data.message;
+                } else {
+                    msg = data;
+                }
+                throw new Error(msg);
+            }
         } catch (err) {
-            dispatch(requestFailure(types.FETCH_TEAMS_FAILURE, {msg: 'failed to load local storage'}));
+            dispatch(requestFailure(types.FETCH_TEAMS_FAILURE, err));
         }
     };
 }
