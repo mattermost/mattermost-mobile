d0f4cfc8a8a4e4d1dbe98b83a31a155ba7b52b1c
Thomas Hopkins <thomas@fullstacklabs.co>
2016-10-17 12:18:21 -0700

    Fix disabling of fetch-mock

--- test/fetch_mock.js
+++ test/fetch_mock.js
@@ -1,20 +1,21 @@
 /* eslint-disable no-empty-function */
 
 // set this to false to allow requests to server in test suite
-const MOCK_FETCH = false;
+const MOCK_FETCH = true;
 
-let fetchMock = {
-    mock: () => {},
-    delete: () => {},
-    get: () => {},
-    head: () => {},
-    patch: () => {},
-    post: () => {},
-    put: () => {}
-};
+let fetchMock = require('fetch-mock');
 
-if (MOCK_FETCH) {
-    fetchMock = require('fetch-mock');
+if (!MOCK_FETCH) {
+    fetchMock.restore();
+    fetchMock = {
+        mock: () => {},
+        delete: () => {},
+        get: () => {},
+        head: () => {},
+        patch: () => {},
+        post: () => {},
+        put: () => {}
+    };
 }
 
 export default fetchMock;
