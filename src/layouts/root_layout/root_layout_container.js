a9d7fdb10fc5b09252185b2189c037af050c24c6
Harrison Healey <harrisonmhealey@gmail.com>
2016-12-08 10:09:27 -0500

    Simplified mapStateToProps for RootLayoutContainer

--- src/layouts/root_layout/root_layout_container.js
+++ src/layouts/root_layout/root_layout_container.js
@@ -8,9 +8,12 @@ import Config from 'config';
 import RootLayout from './root_layout';
 
 function mapStateToProps(state, ownProps) {
+    const users = state.entities.users;
+    const currentUserId = users.currentId;
+
     let locale = Config.DefaultLocale;
-    if (state.entities.users.currentId && state.entities.users.profiles[state.entities.users.currentId]) {
-        locale = state.entities.users.profiles[state.entities.users.currentId].locale;
+    if (currentUserId && users.profiles[currentUserId]) {
+        locale = users.profiles[currentUserId].locale;
     }
 
     return {
