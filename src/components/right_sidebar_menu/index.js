ef855bf5251dac3a489c09a17126f7ffdc497822
Harrison Healey <harrisonmhealey@gmail.com>
2016-12-08 10:07:46 -0500

    Used a separate index.js for component containers

--- src/components/right_sidebar_menu/index.js
+++ src/components/right_sidebar_menu/index.js
@@ -1,26 +1,6 @@
 // Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import RightSidebarMenuContainer from './right_sidebar_menu_container';
 
-import {goToFlaggedPosts, goToRecentMentions} from 'actions/navigation';
-import {logout} from 'actions/users';
-
-import RightSidebarMenu from './right_sidebar_menu';
-
-function mapStateToProps(state, ownProps) {
-    return ownProps;
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators({
-            goToFlaggedPosts,
-            goToRecentMentions,
-            logout
-        }, dispatch)
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(RightSidebarMenu);
+export default RightSidebarMenuContainer;
