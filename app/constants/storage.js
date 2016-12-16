15b093bae94825c63af1131a0122edbb91589ca0
Harrison Healey <harrisonmhealey@gmail.com>
2016-12-16 12:53:45 -0500

    Fixed merge conflict

--- app/constants/storage.js
+++ app/constants/storage.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import keymirror from 'keymirror';
+import keyMirror from 'service/utils/key_mirror';
 
-const StorageTypes = keymirror({
+const StorageTypes = keyMirror({
     SAVE_TO_STORAGE: null,
     SAVE_TO_STORAGE_ERROR: null,
     LOAD_FROM_STORAGE: null,
