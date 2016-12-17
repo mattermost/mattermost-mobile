e29067dc5eb987922372855610391c5fd3c16126
Elias Nahum <nahumhbl@gmail.com>
2016-12-17 12:18:26 -0300

    Fix login flow

--- app/scenes/root/root.js
+++ app/scenes/root/root.js
@@ -10,6 +10,7 @@ export default class Root extends React.Component {
     static propTypes = {
         credentials: React.PropTypes.object,
         logoutRequest: React.PropTypes.object,
+        loginRequest: React.PropTypes.object,
         actions: React.PropTypes.shape({
             goToSelectServer: React.PropTypes.func,
             goToSelectTeam: React.PropTypes.func,
@@ -23,6 +24,20 @@ export default class Root extends React.Component {
     componentDidMount() {
         // Any initialization logic for navigation, setting up the client, etc should go here
 
+        this.init();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.logoutRequest.status === RequestStatus.STARTED &&
+            nextProps.logoutRequest.status === RequestStatus.SUCCESS) {
+            this.props.actions.removeStorage();
+        } else if (this.props.logoutRequest.status === RequestStatus.SUCCESS &&
+            nextProps.logoutRequest.status === RequestStatus.NOT_STARTED) {
+            this.init();
+        }
+    }
+
+    init = () => {
         if (this.props.logoutRequest.status === RequestStatus.SUCCESS) {
             this.props.actions.removeStorage().then(() => {
                 setTimeout(this.loadStoreAndScene, 1000);
@@ -30,12 +45,18 @@ export default class Root extends React.Component {
         } else {
             this.loadStoreAndScene();
         }
-    }
+    };
 
     loadStoreAndScene = () => {
         this.props.actions.loadStorage().then(() => {
             if (this.props.credentials.token && this.props.credentials.url) {
-                this.props.actions.setStoreFromLocalData(this.props.credentials).then(this.props.actions.goToSelectTeam);
+                this.props.actions.setStoreFromLocalData(this.props.credentials).then(() => {
+                    if (this.props.loginRequest.status === RequestStatus.SUCCESS) {
+                        this.props.actions.goToSelectTeam();
+                    } else {
+                        this.props.actions.goToSelectServer();
+                    }
+                });
             } else {
                 this.props.actions.goToSelectServer();
             }
