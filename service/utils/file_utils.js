bbefb1f717c74dc4ffa40f2aee1c6eaaa6752037
Thomas Hopkins <thomas@fullstacklabs.co>
2017-02-13 16:52:48 -0800

    Refactor getFileType in file utils

--- service/utils/file_utils.js
+++ service/utils/file_utils.js
@@ -28,7 +28,7 @@ export function fileSizeToString(bytes) {
 }
 
 export function getFileIconPath(fileInfo) {
-    const fileType = getFileType(fileInfo.extension);
+    const fileType = getFileType(fileInfo);
 
     let icon;
     if (fileType in Constants.ICON_FROM_TYPE) {
@@ -40,43 +40,22 @@ export function getFileIconPath(fileInfo) {
     return icon;
 }
 
-function getFileType(extin) {
-    const ext = extin.toLowerCase();
-    if (Constants.IMAGE_TYPES.indexOf(ext) > -1) {
-        return 'image';
-    }
-
-    if (Constants.AUDIO_TYPES.indexOf(ext) > -1) {
-        return 'audio';
-    }
-
-    if (Constants.VIDEO_TYPES.indexOf(ext) > -1) {
-        return 'video';
-    }
-
-    if (Constants.SPREADSHEET_TYPES.indexOf(ext) > -1) {
-        return 'spreadsheet';
-    }
-
-    if (Constants.CODE_TYPES.indexOf(ext) > -1) {
-        return 'code';
-    }
-
-    if (Constants.WORD_TYPES.indexOf(ext) > -1) {
-        return 'word';
-    }
-
-    if (Constants.PRESENTATION_TYPES.indexOf(ext) > -1) {
-        return 'presentation';
-    }
-
-    if (Constants.PDF_TYPES.indexOf(ext) > -1) {
-        return 'pdf';
-    }
-
-    if (Constants.PATCH_TYPES.indexOf(ext) > -1) {
-        return 'patch';
-    }
-
-    return 'other';
+function getFileType(file) {
+    const fileExt = file.extension.toLowerCase();
+    const fileTypes = [
+        'image',
+        'audio',
+        'video',
+        'spreadsheet',
+        'code',
+        'word',
+        'presentation',
+        'pdf',
+        'patch'
+    ];
+    return fileTypes.find((fileType) => {
+        const constForFileTypeExtList = `${fileType}_types`.toUpperCase();
+        const fileTypeExts = Constants[constForFileTypeExtList];
+        return fileTypeExts.indexOf(fileExt) > -1;
+    }) || 'other';
 }
