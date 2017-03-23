9fec29aa927bbb1c920bc8f8ca0bf9d277e18698
enahum <nahumhbl@gmail.com>
2017-03-23 17:25:33 -0300

    PLT-5501 Confirm server version is compatible with the app version (#385)

--- app/components/logout/index.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
-
-import LogoutContainer from './logout_container';
-
-export default LogoutContainer;
