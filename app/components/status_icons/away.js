3220cfb303203bf67066e84631ec81e1c58dd00f
Elias Nahum <nahumhbl@gmail.com>
2017-03-23 09:01:38 -0300

    channel drawer spacing of icons made equal

--- app/components/status_icons/away.js
+++ app/components/status_icons/away.js
@@ -2,6 +2,7 @@
 // See License.txt for license information.
 
 import React from 'react';
+import {View} from 'react-native';
 import Svg, {
     Ellipse,
     G,
@@ -16,30 +17,33 @@ export default class AwayStatus extends React.Component {
     };
 
     render() {
+        const {color, height, width} = this.props;
         return (
-            <Svg
-                width={this.props.width}
-                height={this.props.height}
-                viewBox='-299 391 12 12'
-            >
-                <G>
-                    <Ellipse
-                        cx='-294.6'
-                        cy='394'
-                        rx='2.5'
-                        ry='2.5'
-                        fill={this.props.color}
-                    />
+            <View style={{height, width, alignItems: 'flex-start'}}>
+                <Svg
+                    width={width}
+                    height={height}
+                    viewBox='-299 391 12 12'
+                >
+                    <G>
+                        <Ellipse
+                            cx='-294.6'
+                            cy='394'
+                            rx='2.5'
+                            ry='2.5'
+                            fill={color}
+                        />
+                        <Path
+                            d='M-293.8,399.4c0-0.4,0.1-0.7,0.2-1c-0.3,0.1-0.6,0.2-1,0.2c-2.5,0-2.5-2-2.5-2s-1,0.1-1.2,0.5c-0.4,0.6-0.6,1.7-0.7,2.5 c0,0.1-0.1,0.5,0,0.6c0.2,1.3,2.2,2.3,4.4,2.4c0,0,0.1,0,0.1,0c0,0,0.1,0,0.1,0c0.7,0,1.4-0.1,2-0.3 C-293.3,401.5-293.8,400.5-293.8,399.4z'
+                            fill={color}
+                        />
+                    </G>
                     <Path
-                        d='M-293.8,399.4c0-0.4,0.1-0.7,0.2-1c-0.3,0.1-0.6,0.2-1,0.2c-2.5,0-2.5-2-2.5-2s-1,0.1-1.2,0.5c-0.4,0.6-0.6,1.7-0.7,2.5 c0,0.1-0.1,0.5,0,0.6c0.2,1.3,2.2,2.3,4.4,2.4c0,0,0.1,0,0.1,0c0,0,0.1,0,0.1,0c0.7,0,1.4-0.1,2-0.3 C-293.3,401.5-293.8,400.5-293.8,399.4z'
-                        fill={this.props.color}
+                        d='M-287,400c0,0.1-0.1,0.1-0.1,0.1l-4.9,0c-0.1,0-0.1-0.1-0.1-0.1v-1.6c0-0.1,0.1-0.1,0.1-0.1l4.9,0c0.1,0,0.1,0.1,0.1,0.1 V400z'
+                        fill={color}
                     />
-                </G>
-                <Path
-                    d='M-287,400c0,0.1-0.1,0.1-0.1,0.1l-4.9,0c-0.1,0-0.1-0.1-0.1-0.1v-1.6c0-0.1,0.1-0.1,0.1-0.1l4.9,0c0.1,0,0.1,0.1,0.1,0.1 V400z'
-                    fill={this.props.color}
-                />
-            </Svg>
+                </Svg>
+            </View>
         );
     }
 }
