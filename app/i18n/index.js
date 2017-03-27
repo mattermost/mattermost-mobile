e81b1b951e6c5d050254fe1f3b3edf5e350f28e5
Elias Nahum <nahumhbl@gmail.com>
2017-03-27 01:06:29 -0300

    add supported translations

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -3,14 +3,32 @@
 
 import 'intl';
 
+import de from 'assets/i18n/de.json';
 import en from 'assets/i18n/en.json';
 import es from 'assets/i18n/es.json';
+import fr from 'assets/i18n/fr.json';
+import ja from 'assets/i18n/ja.json';
+import ko from 'assets/i18n/ko.json';
+import nl from 'assets/i18n/nl.json';
+import ptBR from 'assets/i18n/pt-BR.json';
+import ru from 'assets/i18n/ru.json';
+import zhCN from 'assets/i18n/zh-CN.json';
+import zhTW from 'assets/i18n/zh-TW.json';
 
 const DEFAULT_LOCALE = 'en';
 
 const TRANSLATIONS = {
+    de,
     en,
-    es
+    es,
+    fr,
+    ja,
+    ko,
+    nl,
+    ptBR,
+    ru,
+    zhCN,
+    zhTW
 };
 
 export function getTranslations(locale) {
