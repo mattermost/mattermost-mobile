65a2b7d06cc9c07e36517a3905fd1588b2813d68
Elias Nahum <nahumhbl@gmail.com>
2017-03-29 14:49:00 -0300

    PLT-6027 fix handle Push notifications for DM/GM

--- app/components/push_notification/push_notification.js
+++ app/components/push_notification/push_notification.js
@@ -16,11 +16,16 @@ export default class PushNotification extends PureComponent {
     static propTypes = {
         currentTeamId: PropTypes.string,
         currentChannelId: PropTypes.string,
+        teams: PropTypes.object,
         actions: PropTypes.shape({
             goToNotification: PropTypes.func.isRequired,
             queueNotification: PropTypes.func.isRequired,
             setDeviceToken: PropTypes.func.isRequired
         }).isRequired
+    };
+
+    static defaultProps: {
+        teams: {}
     };
 
     constructor(props) {
@@ -88,9 +93,9 @@ export default class PushNotification extends PureComponent {
     };
 
     onNotificationTapped = (notification) => {
-        const {currentTeamId, currentChannelId} = this.props;
+        const {currentTeamId, currentChannelId, teams} = this.props;
 
-        if (currentTeamId && currentChannelId) {
+        if (currentTeamId && currentChannelId && Object.keys(teams).length) {
             // this means that the store has the necessary data
             this.props.actions.goToNotification(notification);
         } else {
