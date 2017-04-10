43e4c5e2f2bcdc28d9c88fb712aa64cd002b9f39
Harrison Healey <harrisonmhealey@gmail.com>
2017-04-10 10:33:35 -0400

    PLT-5860 Updated copyright date (#476)

--- app/scenes/channel/channel_post_list/index.js
+++ app/scenes/channel/channel_post_list/index.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
 import ChannelPostListContainer from './channel_post_list_container';
