ea8d236f7f9e3aeadcca515115e174b70ac33b90
Jarred Witt <me@jarredwitt.com>
2017-05-30 11:38:11 -0500

    Add component unit testing

--- test/mocks.js
+++ test/mocks.js
@@ -1,4 +1,8 @@
 import {addMock} from 'mocha-react-native';
 
+addMock('react-native-device-info', {
+    getBuildNumber: () => true,
+    getVersion: () => true
+});
 addMock('react-native-linear-gradient', {});
 addMock('UIManager', {});
