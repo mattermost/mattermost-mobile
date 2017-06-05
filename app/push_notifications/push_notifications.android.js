05b124057375d1c1d7aac87be45cb979b0266fb4
Elias Nahum <nahumhbl@gmail.com>
2017-06-05 11:08:58 -0400

    Fix push notifications

---
+++ app/push_notifications/push_notifications.android.js
@@ -0,0 +1,87 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import {AppState} from 'react-native';
+import {NotificationsAndroid, PendingNotifications} from 'react-native-notifications';
+
+class PushNotification {
+    constructor() {
+        this.onRegister = null;
+        this.onNotification = null;
+
+        NotificationsAndroid.setNotificationReceivedListener((notification) => {
+            const data = notification.getData();
+
+            if (notification) {
+                this.handleNotification(data, false);
+            }
+        });
+
+        NotificationsAndroid.setNotificationOpenedListener((notification) => {
+            const data = notification.getData();
+
+            if (notification) {
+                this.handleNotification(data, true);
+            }
+        });
+    }
+
+    handleNotification = (data, userInteraction) => {
+        const deviceNotification = {
+            data,
+            foreground: !userInteraction && AppState.currentState === 'active',
+            message: data.message,
+            userInfo: data.userInfo,
+            userInteraction
+        };
+
+        if (this.onNotification) {
+            this.onNotification(deviceNotification);
+        }
+    };
+
+    configure(options) {
+        this.onRegister = options.onRegister;
+        this.onNotification = options.onNotification;
+
+        NotificationsAndroid.refreshToken();
+        NotificationsAndroid.setRegistrationTokenUpdateListener((deviceToken) => {
+            if (this.onRegister) {
+                this.onRegister({token: deviceToken});
+            }
+        });
+
+        if (options.popInitialNotification) {
+            PendingNotifications.getInitialNotification().
+            then((notification) => {
+                if (notification) {
+                    const data = notification.getData();
+                    if (data) {
+                        this.handleNotification(data, true);
+                    }
+                }
+            }).
+            catch((err) => {
+                console.log('Android getInitialNotifiation() failed', err); //eslint-disable-line no-console
+            });
+        }
+    }
+
+    localNotificationSchedule(notification) {
+        if (notification.date) {
+            notification.fireDate = notification.date.getTime();
+            Reflect.deleteProperty(notification, 'date');
+            NotificationsAndroid.scheduleLocalNotification(notification);
+        }
+    }
+
+    cancelAllLocalNotifications() {
+        NotificationsAndroid.cancelAllLocalNotifications();
+    }
+
+    setApplicationIconBadgeNumber(number) {
+        NotificationsAndroid.setBadgesCount(number);
+    }
+}
+
+export default new PushNotification();
--- ios/Mattermost/AppDelegate.m
+++ ios/Mattermost/AppDelegate.m
@@ -8,11 +8,11 @@
  */
 
 #import "AppDelegate.h"
-#import "RCTPushNotificationManager.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "Orientation.h"
 #import "RCCManager.h"
+#import "RNNotifications.h"
 
 @implementation AppDelegate
 
@@ -22,20 +22,6 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-//  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-//                                                      moduleName:@"Mattermost"
-//                                               initialProperties:nil
-//                                                   launchOptions:launchOptions];
-//  
-//  [RCTSplashScreen open:rootView withImageNamed:@"splash"];
-//  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-//
-//  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-//  UIViewController *rootViewController = [UIViewController new];
-//  rootViewController.view = rootView;
-//  self.window.rootViewController = rootViewController;
-//  [self.window makeKeyAndVisible];
-
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
@@ -43,35 +29,34 @@
   return YES;
 }
 
-// Required to register for notifications
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
-{
-  [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings];
-}
-// Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-// Required for the notification event. You must call the completion handler after handling the remote notification.
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  [RCTPushNotificationManager didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-// Required for the registrationError event.
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
-  [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error];
-}
-// Required for the localNotification event.
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
-{
-  [RCTPushNotificationManager didReceiveLocalNotification:notification];
-}
-
 // Required for orientation
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
   return [Orientation getOrientation];
+}
+
+// Required to register for notifications
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+  [RNNotifications didRegisterUserNotificationSettings:notificationSettings];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  [RNNotifications didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  [RNNotifications didFailToRegisterForRemoteNotificationsWithError:error];
+}
+
+// Required for the notification event.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification {
+  [RNNotifications didReceiveRemoteNotification:notification];
+}
+
+// Required for the localNotification event.
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+  [RNNotifications didReceiveLocalNotification:notification];
 }
 @end
