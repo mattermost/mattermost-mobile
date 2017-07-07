d9df54bad997bd07d7302b79f144b8a5febb1954
Jarred Witt <me@jarredwitt.com>
2017-07-07 18:03:06 -0500

    RN-38 Clicking on an at mention should open user profile

--- app/components/at_mention/at_mention.js
+++ app/components/at_mention/at_mention.js
@@ -4,39 +4,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Text} from 'react-native';
+import {injectIntl, intlShape} from 'react-intl';
 
 import CustomPropTypes from 'app/constants/custom_prop_types';
 
-export default class AtMention extends React.PureComponent {
+class AtMention extends React.PureComponent {
     static propTypes = {
+        intl: intlShape,
         mentionName: PropTypes.string.isRequired,
         mentionStyle: CustomPropTypes.Style,
+        navigator: PropTypes.object.isRequired,
         textStyle: CustomPropTypes.Style,
+        theme: PropTypes.object.isRequired,
         usersByUsername: PropTypes.object.isRequired
     };
 
     constructor(props) {
         super(props);
 
+        const userDetails = this.getUserDetailsFromMentionName(props);
         this.state = {
-            username: this.getUsernameFromMentionName(props)
+            username: userDetails.username,
+            id: userDetails.id
         };
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.mentionName !== this.props.mentionName || nextProps.usersByUsername !== this.props.usersByUsername) {
+            const userDetails = this.getUserDetailsFromMentionName(nextProps);
             this.setState({
-                username: this.getUsernameFromMentionName(nextProps)
+                username: userDetails.username,
+                id: userDetails.id
             });
         }
     }
 
-    getUsernameFromMentionName(props) {
+    goToUserProfile = () => {
+        const {intl, navigator, theme} = this.props;
+        navigator.push({
+            screen: 'UserProfile',
+            title: intl.formatMessage({id: 'mobile.routes.user_profile', defaultMessage: 'Profile'}),
+            animated: true,
+            backButtonTitle: '',
+            passProps: {
+                userId: this.state.id
+            },
+            navigatorStyle: {
+                navBarTextColor: theme.sidebarHeaderTextColor,
+                navBarBackgroundColor: theme.sidebarHeaderBg,
+                navBarButtonColor: theme.sidebarHeaderTextColor,
+                screenBackgroundColor: theme.centerChannelBg
+            }
+        });
+    };
+
+    getUserDetailsFromMentionName(props) {
         let mentionName = props.mentionName;
 
         while (mentionName.length > 0) {
             if (props.usersByUsername[mentionName]) {
-                return props.usersByUsername[mentionName].username;
+                const user = props.usersByUsername[mentionName];
+                return {
+                    username: user.username,
+                    id: user.id
+                };
             }
 
             // Repeatedly trim off trailing punctuation in case this is at the end of a sentence
@@ -47,21 +78,27 @@ export default class AtMention extends React.PureComponent {
             }
         }
 
-        return '';
+        return {
+            username: ''
+        };
     }
 
     render() {
+        const {mentionName, mentionStyle, textStyle} = this.props;
         const username = this.state.username;
 
         if (!username) {
-            return <Text style={this.props.textStyle}>{'@' + this.props.mentionName}</Text>;
+            return <Text style={textStyle}>{'@' + mentionName}</Text>;
         }
 
         const suffix = this.props.mentionName.substring(username.length);
 
         return (
-            <Text style={this.props.textStyle}>
-                <Text style={this.props.mentionStyle}>
+            <Text
+                style={textStyle}
+                onPress={this.goToUserProfile}
+            >
+                <Text style={mentionStyle}>
                     {'@' + username}
                 </Text>
                 {suffix}
@@ -69,3 +106,5 @@ export default class AtMention extends React.PureComponent {
         );
     }
 }
+
+export default injectIntl(AtMention);
