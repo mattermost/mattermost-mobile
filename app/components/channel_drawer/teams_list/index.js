6eeb55eaa9108687e1afb47bee940c19b5e65e92
Harrison Healey <harrisonmhealey@gmail.com>
2017-09-28 10:17:22 -0400

    Added selector for getting sorted teams list (#954)

--- app/components/channel_drawer/teams_list/index.js
+++ app/components/channel_drawer/teams_list/index.js
@@ -5,32 +5,22 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import {getCurrentUrl} from 'mattermost-redux/selectors/entities/general';
-import {getCurrentTeamId, getJoinableTeams, getMyTeams, getTeamMemberships} from 'mattermost-redux/selectors/entities/teams';
-import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentTeamId, getJoinableTeams, getTeamMemberships} from 'mattermost-redux/selectors/entities/teams';
 
 import {handleTeamChange} from 'app/actions/views/select_team';
 import {getTheme} from 'app/selectors/preferences';
+import {getMySortedTeams} from 'app/selectors/teams';
 import {removeProtocol} from 'app/utils/url';
 
 import TeamsList from './teams_list';
 
 function mapStateToProps(state, ownProps) {
-    const user = getCurrentUser(state);
-
-    function sortTeams(locale, a, b) {
-        if (a.display_name !== b.display_name) {
-            return a.display_name.toLowerCase().localeCompare(b.display_name.toLowerCase(), locale, {numeric: true});
-        }
-
-        return a.name.toLowerCase().localeCompare(b.name.toLowerCase(), locale, {numeric: true});
-    }
-
     return {
         canCreateTeams: false,
         joinableTeams: getJoinableTeams(state),
         currentTeamId: getCurrentTeamId(state),
         currentUrl: removeProtocol(getCurrentUrl(state)),
-        teams: getMyTeams(state).sort(sortTeams.bind(null, (user.locale))),
+        teams: getMySortedTeams(state),
         theme: getTheme(state),
         myTeamMembers: getTeamMemberships(state),
         ...ownProps
