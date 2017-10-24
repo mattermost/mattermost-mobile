0002b7b9bf34a4c4caa3d7c85b6b758821aebeb0
Jarred Witt <me@jarredwitt.com>
2017-10-24 08:29:07 -0700

    Skip await for loadMe if we have the currentUser

--- app/screens/root/index.js
+++ app/screens/root/index.js
@@ -5,13 +5,14 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import {loadMe} from 'mattermost-redux/actions/users';
-
+import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
 
 import Root from './root';
 
 function mapStateToProps(state) {
     return {
+        currentUser: getCurrentUser(state),
         credentials: state.entities.general.credentials,
         theme: getTheme(state)
     };
