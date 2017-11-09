3ae2439845b1e504ff6b2a62a6f7e4732107c21f
Harrison Healey <harrisonmhealey@gmail.com>
2017-11-09 10:16:19 -0500

    RN-471 Fixed null pointer from Swiper scroll view

--- app/components/swiper.js
+++ app/components/swiper.js
@@ -71,7 +71,9 @@ export default class Swiper extends PureComponent {
         if (this.runOnLayout) {
             if (Platform.OS === 'ios') {
                 setTimeout(() => {
-                    this.scrollView.scrollTo({x: this.props.width * this.state.index, animated: false});
+                    if (this.scrollView) {
+                        this.scrollView.scrollTo({x: this.props.width * this.state.index, animated: false});
+                    }
                 }, 100);
             } else {
                 this.scrollView.setPageWithoutAnimation(this.state.index);
