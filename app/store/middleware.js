b7fe369fabfb4cc46788a74bb2c9bf77274c1fc0
enahum <nahumhbl@gmail.com>
2017-11-12 08:33:42 -0300

    Apply data retention policy (#1122)

--- app/store/middleware.js
+++ app/store/middleware.js
@@ -4,7 +4,7 @@
 import DeviceInfo from 'react-native-device-info';
 
 import {ViewTypes} from 'app/constants';
-import Config from 'assets/config';
+import initialState from 'app/initial_state';
 
 export function messageRetention() {
     return (next) => (action) => {
@@ -23,6 +23,7 @@ export function messageRetention() {
 
             // Keep only the last 60 messages for the last 5 viewed channels in each team
             // and apply data retention on those posts if applies
+
             return next(cleanupState(action));
         } else if (action.type === ViewTypes.DATA_CLEANUP) {
             const nextAction = cleanupState(action, true);
@@ -36,8 +37,22 @@ export function messageRetention() {
 function resetStateForNewVersion(action) {
     const {payload} = action;
     const lastChannelForTeam = getLastChannelForTeam(payload);
-    let users = {};
 
+    let general = initialState.entities.general;
+    if (payload.entities.general) {
+        general = payload.entities.general;
+    }
+
+    let teams = initialState.entities.teams;
+    if (payload.entities.teams) {
+        teams = {
+            currentTeamId: payload.entities.teams.currentTeamId,
+            teams: payload.entities.teams.teams,
+            myMembers: payload.entities.teams.myMembers
+        };
+    }
+
+    let users = initialState.entities.users;
     if (payload.entities.users) {
         const currentUserId = payload.entities.users.currentUserId;
         if (currentUserId) {
@@ -50,38 +65,74 @@ function resetStateForNewVersion(action) {
         }
     }
 
+    let preferences = initialState.entities.preferences;
+    if (payload.entities.preferences) {
+        preferences = payload.entities.preferences;
+    }
+
+    let search = initialState.entities.search;
+    if (payload.entities.search && payload.entities.search.recent) {
+        search = {
+            recent: payload.entities.search.recent
+        };
+    }
+
+    let channelDrafts = initialState.views.channel.drafts;
+    if (payload.views.channel && payload.views.channel.drafts) {
+        channelDrafts = payload.views.channel.drafts;
+    }
+
+    let i18n = initialState.views.i18n;
+    if (payload.views.i18n) {
+        i18n = payload.views.i18n;
+    }
+
+    let fetchCache = initialState.views.fetchCache;
+    if (payload.views.fetchCache) {
+        fetchCache = payload.views.fetchCache;
+    }
+
+    let lastTeamId = initialState.views.team.lastTeamId;
+    if (payload.views.team && payload.views.team.lastTeamId) {
+        lastTeamId = payload.views.team.lastTeamId;
+    }
+
+    let threadDrafts = initialState.views.thread.drafts;
+    if (payload.views.thread && payload.views.thread.drafts) {
+        threadDrafts = payload.views.thread.drafts;
+    }
+
+    let selectServer = initialState.views.selectServer;
+    if (payload.views.selectServer) {
+        selectServer = payload.views.selectServer;
+    }
+
     const nextState = {
         app: {
             build: DeviceInfo.getBuildNumber(),
             version: DeviceInfo.getVersion()
         },
         entities: {
-            general: payload.entities.general,
-            teams: {
-                currentTeamId: payload.entities.teams.currentTeamId,
-                teams: payload.entities.teams.teams,
-                myMembers: payload.entities.teams.myMembers
-            },
+            general,
+            teams,
             users,
-            preferences: payload.entities.preferences,
-            search: {
-                recent: payload.entities.search ? payload.entities.search.recent : {}
-            }
+            preferences,
+            search
         },
         views: {
             channel: {
-                drafts: payload.views.channel.drafts
+                drafts: channelDrafts
             },
-            i18n: payload.views.i18n,
-            fetchCache: payload.views.fetchCache,
+            i18n,
+            fetchCache,
             team: {
-                lastTeamId: payload.views.team.lastTeamId,
+                lastTeamId,
                 lastChannelForTeam
             },
             thread: {
-                drafts: payload.views.thread.drafts
+                drafts: threadDrafts
             },
-            selectServer: payload.views.selectServer
+            selectServer
         }
     };
 
@@ -126,7 +177,12 @@ function cleanupState(action, keepCurrent = false) {
         }
     };
 
-    const retentionPeriod = Config.EnableMessageRetention ? Config.MessageRetentionPeriod + 1 : 0;
+    let retentionPeriod = 0;
+    if (resetPayload.entities.general && resetPayload.entities.general.dataRetentionPolicy &&
+        resetPayload.entities.general.dataRetentionPolicy.message_deletion_enabled) {
+        retentionPeriod = resetPayload.entities.general.dataRetentionPolicy.message_retention_cutoff;
+    }
+
     const postIdsToKeep = Object.values(lastChannelForTeam).reduce((array, channelIds) => {
         const ids = channelIds.reduce((result, id) => {
             // we need to check that the channel id is not already included
@@ -152,9 +208,7 @@ function cleanupState(action, keepCurrent = false) {
         const post = payload.entities.posts.posts[postId];
 
         if (post) {
-            const skip = keepCurrent && currentChannelId === post.channel_id;
-
-            if (!skip && retentionPeriod && (Date.now() - post.create_at) / (1000 * 3600 * 24) > retentionPeriod) {
+            if (retentionPeriod && post.create_at < retentionPeriod) {
                 const postsInChannel = nextEntitites.posts.postsInChannel[post.channel_id] || [];
                 const index = postsInChannel.indexOf(postId);
                 if (index !== -1) {
