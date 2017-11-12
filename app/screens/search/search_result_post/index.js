8e960f0257fdac5f3c3444d8bf69df625573d5a4
Harrison Healey <harrisonmhealey@gmail.com>
2017-11-12 06:34:39 -0500

    RN-472 Improved handling of deleted search results (#1119)

---
+++ app/screens/search/search_result_post/index.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import {connect} from 'react-redux';
+
+import {Posts} from 'mattermost-redux/constants';
+import {getPost} from 'mattermost-redux/selectors/entities/posts';
+
+import SearchResultPost from './search_result_post';
+
+function mapStateToProps(state, ownProps) {
+    const post = getPost(state, ownProps.postId);
+
+    return {
+        isDeleted: post && post.state === Posts.POST_DELETED
+    };
+}
+
+export default connect(mapStateToProps)(SearchResultPost);
