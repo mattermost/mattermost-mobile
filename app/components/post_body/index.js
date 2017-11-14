a5c374246dbe7e07fdd3fc45c5cf5fe382b97920
Martin Kraft <martinkraft@gmail.com>
2017-11-14 11:18:57 -0500

    RN-359: Adds '(edited)' indicator to post.

--- app/components/post_body/index.js
+++ app/components/post_body/index.js
@@ -8,7 +8,7 @@ import {flagPost, unflagPost} from 'mattermost-redux/actions/posts';
 import {Posts} from 'mattermost-redux/constants';
 import {getPost} from 'mattermost-redux/selectors/entities/posts';
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
-import {isPostEphemeral, isSystemMessage} from 'mattermost-redux/utils/post_utils';
+import {isEdited, isPostEphemeral, isSystemMessage} from 'mattermost-redux/utils/post_utils';
 
 import PostBody from './post_body';
 
@@ -20,6 +20,7 @@ function mapStateToProps(state, ownProps) {
         postProps: post.props || {},
         fileIds: post.file_ids,
         hasBeenDeleted: post.state === Posts.POST_DELETED,
+        hasBeenEdited: isEdited(post),
         hasReactions: post.has_reactions,
         isFailed: post.failed,
         isPending: post.id === post.pending_post_id,
