d5256c2d4f3fdb0f01a4fe9e7b7c2c052ddc855a
Harrison Healey <harrisonmhealey@gmail.com>
2017-11-22 11:21:22 -0500

    Made VectorIcon use a PureComponent

--- app/components/vector_icon.js
+++ app/components/vector_icon.js
@@ -1,62 +1,64 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, {PureComponent} from 'react';
 import {Text} from 'react-native';
-import IonIcon from 'react-native-vector-icons/Ionicons';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import FoundationIcon from 'react-native-vector-icons/Foundation';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-export default function vectorIcon(props) {
-    const {name, type, style, size} = props;
+export default class VectorIcon extends PureComponent {
+    static propTypes = {
+        name: PropTypes.string,
+        type: PropTypes.string,
+        size: PropTypes.number,
+        style: Text.propTypes.style
+    };
 
-    switch (type) {
-    case 'fontawesome':
-        return (
-            <FontAwesomeIcon
-                name={name}
-                style={style}
-                size={size}
-            />
-        );
-    case 'foundation':
-        return (
-            <FoundationIcon
-                name={name}
-                style={style}
-                size={size}
-            />
-        );
-    case 'ion':
-        return (
-            <IonIcon
-                name={name}
-                style={style}
-                size={size}
-            />
-        );
-    case 'material':
-        return (
-            <MaterialIcon
-                name={name}
-                style={style}
-                size={size}
-            />
-        );
+    static defaultProps = {
+        size: 14
+    };
+
+    render() {
+        const {name, type, style, size} = this.props;
+
+        switch (type) {
+        case 'fontawesome':
+            return (
+                <FontAwesomeIcon
+                    name={name}
+                    style={style}
+                    size={size}
+                />
+            );
+        case 'foundation':
+            return (
+                <FoundationIcon
+                    name={name}
+                    style={style}
+                    size={size}
+                />
+            );
+        case 'ion':
+            return (
+                <IonIcon
+                    name={name}
+                    style={style}
+                    size={size}
+                />
+            );
+        case 'material':
+            return (
+                <MaterialIcon
+                    name={name}
+                    style={style}
+                    size={size}
+                />
+            );
+        }
+
+        return null;
     }
-
-    return null;
 }
-
-vectorIcon.propTypes = {
-    name: PropTypes.string,
-    type: PropTypes.string,
-    size: PropTypes.number,
-    style: Text.propTypes.style
-};
-
-vectorIcon.defaultProps = {
-    size: 14
-};
