3f6c38d872cd1b299bf39555836b352cbff2264a
Harrison Healey <harrisonmhealey@gmail.com>
2017-11-28 17:49:43 -0500

    Applied correct text colour to (edited) indicator on Android (#1219)

--- app/utils/theme.js
+++ app/utils/theme.js
@@ -17,7 +17,7 @@ export function makeStyleSheetFromTheme(getStyleFromTheme) {
     };
 }
 
-export function changeOpacity(oldColor, opacity) {
+function normalizeColor(oldColor) {
     let color = oldColor;
     if (color.length && color[0] === '#') {
         color = color.slice(1);
@@ -32,11 +32,44 @@ export function changeOpacity(oldColor, opacity) {
         color += tempColor[2] + tempColor[2];
     }
 
+    return color;
+}
+
+export function changeOpacity(oldColor, opacity) {
+    const color = normalizeColor(oldColor);
+
     const r = parseInt(color.substring(0, 2), 16);
     const g = parseInt(color.substring(2, 4), 16);
     const b = parseInt(color.substring(4, 6), 16);
 
-    return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')';
+    return `rgba(${r},${g},${b},${opacity})`;
+}
+
+function blendComponent(background, foreground, opacity) {
+    return ((1 - opacity) * background) + (opacity * foreground);
+}
+
+export function blendColors(background, foreground, opacity) {
+    const backgroundNormalized = normalizeColor(background);
+    const foregroundNormalized = normalizeColor(foreground);
+
+    const r = blendComponent(
+        parseInt(backgroundNormalized.substring(0, 2), 16),
+        parseInt(foregroundNormalized.substring(0, 2), 16),
+        opacity
+    );
+    const g = blendComponent(
+        parseInt(backgroundNormalized.substring(2, 4), 16),
+        parseInt(foregroundNormalized.substring(2, 4), 16),
+        opacity
+    );
+    const b = blendComponent(
+        parseInt(backgroundNormalized.substring(4, 6), 16),
+        parseInt(foregroundNormalized.substring(4, 6), 16),
+        opacity
+    );
+
+    return `rgb(${r},${g},${b})`;
 }
 
 export function concatStyles(...styles) {
