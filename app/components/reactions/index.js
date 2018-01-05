20ffe9482ede3aab4009c01ec876b357c5c9eeab
Saturnino Abril <saturnino.abril@gmail.com>
2018-01-05 22:39:07 +0800

    add reaction when user click other user's reaction from a post

--- app/components/reactions/index.js
+++ app/components/reactions/index.js
@@ -4,11 +4,12 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import {addReaction, getReactionsForPost, removeReaction} from 'mattermost-redux/actions/posts';
+import {getReactionsForPost, removeReaction} from 'mattermost-redux/actions/posts';
 import {makeGetReactionsForPost} from 'mattermost-redux/selectors/entities/posts';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
-
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+
+import {addReaction} from 'app/actions/views/emoji';
 
 import Reactions from './reactions';
 
