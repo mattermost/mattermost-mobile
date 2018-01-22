6132b72adb69bb680a246844c1884b9795027cef
enahum <nahumhbl@gmail.com>
2018-01-22 16:18:42 -0300

    iOS request permissions if needed to save files to the photo library (#1368)
    * iOS request permissions if needed to save files to the photo library
    
    * Fixing capital letter in permission description

---
+++ app/constants/permissions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+export default {
+    AUTHORIZED: 'authorized',
+    DENIED: 'denied',
+    UNDETERMINED: 'undetermined'
+};
