c1487ccdd4541b76e56c9c6159b22bbe15faf2bb
Elias Nahum <nahumhbl@gmail.com>
2018-01-26 14:11:24 -0300

    Fix Android Share Extension crash

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 // See License.txt for license information.
 
 /* eslint-disable no-unused-vars */
+import {AppRegistry, Platform} from 'react-native';
+
 import Mattermost from 'app/mattermost';
+import ShareExtension from 'share_extension/android';
+
+if (Platform.OS === 'android') {
+    AppRegistry.registerComponent('MattermostShare', () => ShareExtension);
+}
 
 const app = new Mattermost();
