768d843e377e8c2852bf05b0dc72b98ac3f62a20
enahum <nahumhbl@gmail.com>
2018-02-01 17:37:20 -0300

    Hotfix for release-1.5 (#1405)
    * Request data retention policies only when the user is logged in (#1351)
    
    * Bump App version to 1.5.3
    
    * Bump app build version to 83

--- app/actions/views/login.js
+++ app/actions/views/login.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
+
+import {getDataRetentionPolicy} from 'mattermost-redux/actions/general';
 import {GeneralTypes} from 'mattermost-redux/action_types';
 import {Client, Client4} from 'mattermost-redux/client';
 
@@ -25,8 +27,10 @@ export function handlePasswordChanged(password) {
 
 export function handleSuccessfulLogin() {
     return async (dispatch, getState) => {
+        const {config, license} = getState().entities.general;
         const token = Client4.getToken();
         const url = Client4.getUrl();
+
         dispatch({
             type: GeneralTypes.RECEIVED_APP_CREDENTIALS,
             data: {
@@ -37,6 +41,13 @@ export function handleSuccessfulLogin() {
 
         Client.setToken(token);
         Client.setUrl(url);
+
+        if (config.DataRetentionEnableMessageDeletion && config.DataRetentionEnableMessageDeletion === 'true' &&
+            license.IsLicensed === 'true' && license.DataRetention === 'true') {
+            getDataRetentionPolicy()(dispatch, getState);
+        } else {
+            dispatch({type: GeneralTypes.RECEIVED_DATA_RETENTION_POLICY, data: {}});
+        }
 
         return true;
     };
