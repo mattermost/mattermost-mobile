7ff733967460e12379222f613bfa9fa23d90aab4
Jesús Espino <jespinog@gmail.com>
2018-02-21 07:43:23 +0100

    MM-9620: Add reactions permissions (#1451)

--- app/components/post_body/index.js
+++ app/components/post_body/index.js
@@ -8,6 +8,10 @@ import {flagPost, unflagPost} from 'mattermost-redux/actions/posts';
 import {Posts} from 'mattermost-redux/constants';
 import {getPost} from 'mattermost-redux/selectors/entities/posts';
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {hasNewPermissions} from 'mattermost-redux/selectors/entities/general';
+import {haveIChannelPermission} from 'mattermost-redux/selectors/entities/roles';
+import {getChannel} from 'mattermost-redux/selectors/entities/channels';
+import Permissions from 'mattermost-redux/constants/permissions';
 import {isEdited, isPostEphemeral, isSystemMessage} from 'mattermost-redux/utils/post_utils';
 
 import PostBody from './post_body';
@@ -16,6 +20,17 @@ const POST_TIMEOUT = 20000;
 
 function mapStateToProps(state, ownProps) {
     const post = getPost(state, ownProps.postId);
+    const channel = getChannel(state, post.channel_id) || {};
+    const teamId = channel.team_id;
+
+    let canAddReaction = true;
+    if (hasNewPermissions(state)) {
+        canAddReaction = haveIChannelPermission(state, {
+            team: teamId,
+            channel: post.channel_id,
+            permission: Permissions.ADD_REACTION
+        });
+    }
 
     let isFailed = post.failed;
     let isPending = post.id === post.pending_post_id;
@@ -37,7 +52,8 @@ function mapStateToProps(state, ownProps) {
         isPostEphemeral: isPostEphemeral(post),
         isSystemMessage: isSystemMessage(post),
         message: post.message,
-        theme: getTheme(state)
+        theme: getTheme(state),
+        canAddReaction
     };
 }
 
