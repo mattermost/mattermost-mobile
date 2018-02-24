e51aef1dee82a817e16128061b7364470e52b91b
Michał Odziemczyk <koxen@users.noreply.github.com>
2018-02-24 07:52:40 +0100

    eslint

--- app/screens/index.js
+++ app/screens/index.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import {Navigation} from 'react-native-navigation';
 
-
 import IntlWrapper from 'app/components/root';
 
 function wrapWithContextProvider(Comp, excludeEvents = true) {
