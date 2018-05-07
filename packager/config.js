91a07618a67db17bea8fc7a6264e0baa15be9f95
Elias Nahum <nahumhbl@gmail.com>
2018-05-07 08:40:47 -0300

    RN CLI packager config

---
+++ packager/config.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+/* eslint-disable no-console */
+const modulePaths = require('./modulePaths');
+const resolve = require('path').resolve;
+const fs = require('fs');
+
+const platformRegex = /\.(android\.js|ios\.js)/g;
+const modulesRegex = /\/(node_modules)/;
+
+const config = {
+    getTransformOptions: (entryFile, {platform}) => {
+        console.log('Building modules for', platform);
+        const moduleMap = {};
+        modulePaths.forEach((path) => {
+            let realPath = path;
+            if (platform && platformRegex.test(realPath)) {
+                realPath = path.replace(platformRegex, `.${platform}.js`);
+            }
+
+            let fsFile = realPath;
+            if (path.match(modulesRegex).length > 1) {
+                fsFile = path.replace(modulesRegex, '');
+            }
+
+            if (fs.existsSync(fsFile)) {
+                moduleMap[resolve(realPath)] = true;
+            }
+        });
+        return {
+            preloadedModules: moduleMap,
+            transform: {
+                inlineRequires: {
+                    blacklist: moduleMap,
+                },
+            },
+        };
+    },
+};
+
+module.exports = config;
