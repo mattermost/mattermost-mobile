39e2863f451acdf4b54c0b149671ced25bd0961e
Saturnino Abril <saturnino.abril@gmail.com>
2018-06-19 01:52:47 +0800

    move makeGetProfilesByIdsAndUsernames to mattermost-redux

--- app/components/combined_system_message/index.js
+++ app/components/combined_system_message/index.js
@@ -3,51 +3,16 @@
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {createSelector} from 'reselect';
 
 import {getMissingProfilesByIds, getMissingProfilesByUsernames} from 'mattermost-redux/actions/users';
 import {Preferences} from 'mattermost-redux/constants';
 import {getBool, getTeammateNameDisplaySetting} from 'mattermost-redux/selectors/entities/preferences';
-import {getCurrentUser, getProfiles, getUsersByUsername} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentUser, makeGetProfilesByIdsAndUsernames} from 'mattermost-redux/selectors/entities/users';
 
 import CombinedSystemMessage from './combined_system_message';
 
-function makeGetUserProfiles() {
-    return createSelector(
-        getProfiles,
-        getUsersByUsername,
-        (state, props) => props.allUserIds,
-        (state, props) => props.allUsernames,
-        (allProfilesById, allProfilesByUsername, allUserIds, allUsernames) => {
-            const userProfiles = [];
-
-            if (allUserIds.length > 0) {
-                const profilesById = allUserIds.
-                    filter((userId) => allProfilesById[userId]).
-                    map((userId) => allProfilesById[userId]);
-
-                if (profilesById && profilesById.length > 0) {
-                    userProfiles.push(...profilesById);
-                }
-            }
-
-            if (allUsernames.length > 0) {
-                const profilesByUsername = allUsernames.
-                    filter((username) => allProfilesByUsername[username]).
-                    map((username) => allProfilesByUsername[username]);
-
-                if (profilesByUsername && profilesByUsername.length > 0) {
-                    userProfiles.push(...profilesByUsername);
-                }
-            }
-
-            return userProfiles;
-        }
-    );
-}
-
 function makeMapStateToProps() {
-    const getUserProfiles = makeGetUserProfiles();
+    const getProfilesByIdsAndUsernames = makeGetProfilesByIdsAndUsernames();
 
     return (state, ownProps) => {
         const currentUser = getCurrentUser(state);
@@ -57,7 +22,7 @@ function makeMapStateToProps() {
             currentUsername: currentUser.username,
             showJoinLeave: getBool(state, Preferences.CATEGORY_ADVANCED_SETTINGS, Preferences.ADVANCED_FILTER_JOIN_LEAVE, true),
             teammateNameDisplay: getTeammateNameDisplaySetting(state),
-            userProfiles: getUserProfiles(state, {allUserIds, allUsernames}),
+            userProfiles: getProfilesByIdsAndUsernames(state, {allUserIds, allUsernames}),
         };
     };
 }
