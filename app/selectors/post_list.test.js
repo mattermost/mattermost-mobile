267bf91fe9d1b5dc4c9ec1cc6c3d094dbfdfc553
Jesse Hallam <jesse.hallam@gmail.com>
2018-07-09 14:44:38 -0400

    unit test makePreparePostIdsForSearchPosts

--- app/selectors/post_list.test.js
+++ app/selectors/post_list.test.js
@@ -6,6 +6,7 @@ import assert from 'assert';
 import {
     DATE_LINE,
     makePreparePostIdsForPostList,
+    makePreparePostIdsForSearchPosts,
     START_OF_NEW_MESSAGES,
 } from 'app/selectors/post_list';
 
@@ -441,6 +442,89 @@ describe('Selectors.PostList', () => {
                 '1003', 
                 '1001',
                 'date-Thu Jan 01 1970 01:00:00 GMT+0000 (GMT)',
+            ]);
+        });
+    });
+
+    describe('makePreparePostIdsForSearchPosts', () => {
+        it('should return an empty array if there are no posts specified', () => {
+            const preparePostIdsForSearchPosts = makePreparePostIdsForSearchPosts();
+
+            const state = {
+                entities: {
+                    posts: {
+                        posts: {
+                            1001: {id: '1001', create_at: 0, type: ''},
+                            1002: {id: '1002', create_at: 1, type: ''},
+                        },
+                    },
+                    users: {
+                        currentUserId: '1234',
+                        profiles: {
+                            1234: {id: '1234', username: 'user'},
+                        },
+                    },
+                },
+            };
+
+            const postIds = [];
+            const actual = preparePostIdsForSearchPosts(state, postIds);
+            assert.deepEqual(actual, []);
+        });
+
+        it('should return an empty array if there are no matching posts', () => {
+            const preparePostIdsForSearchPosts = makePreparePostIdsForSearchPosts();
+
+            const state = {
+                entities: {
+                    posts: {
+                        posts: {},
+                    },
+                    users: {
+                        currentUserId: '1234',
+                        profiles: {
+                            1234: {id: '1234', username: 'user'},
+                        },
+                    },
+                },
+            };
+
+            const postIds = ['1002', '1001'];
+            const actual = preparePostIdsForSearchPosts(state, postIds);
+            assert.deepEqual(actual, []);
+        });
+
+        it('should return results when there are posts', () => {
+            const preparePostIdsForSearchPosts = makePreparePostIdsForSearchPosts();
+
+            const state = {
+                entities: {
+                    posts: {
+                        posts: {
+                            1001: {id: '1001', create_at: 0, type: ''},
+                            1002: {id: '1002', create_at: 1000, type: ''},
+                            // Same timestamp as 1002
+                            1003: {id: '1003', create_at: 1000, type: ''},
+                        },
+                    },
+                    users: {
+                        currentUserId: '1234',
+                        profiles: {
+                            1234: {id: '1234', username: 'user'},
+                        },
+                    },
+                },
+            };
+
+            const postIds = ['1003', '1002', '1001'];
+            const actual = preparePostIdsForSearchPosts(state, postIds);
+            assert.deepEqual(actual, [
+                'date-Thu Jan 01 1970 00:00:01 GMT+0000 (GMT)-index-0',
+                '1003',
+                'date-Thu Jan 01 1970 00:00:01 GMT+0000 (GMT)-index-1',
+                '1002', 
+                'date-Thu Jan 01 1970 00:00:00 GMT+0000 (GMT)-index-2',
+                '1001',
             ]);
         });
     });
