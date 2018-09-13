a873640e4172fec7d1e9a311042694923fca371a
Elias Nahum <nahumhbl@gmail.com>
2018-09-13 09:57:54 -0300

    Fix Option modal items tap effect off center (#2109)
    * Fix Option modal items tap effect off center
    
    * Add snapshot

---
+++ app/screens/options_modal/option_modal_list.test.js
@@ -0,0 +1,43 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import OptionModalListIOS from './options_modal_list.ios';
+import OptionModalListAndroid from './options_modal_list.android';
+
+describe('OptionModalList', () => {
+    const baseProps = {
+        items: [{
+            action: jest.fn(),
+            text: {
+                id: 'mobile.file_upload.camera',
+                defaultMessage: 'Take Photo or Video',
+            },
+            icon: 'camera',
+        }, {
+            action: jest.fn(),
+            text: {
+                id: 'mobile.file_upload.library',
+                defaultMessage: 'Photo Library',
+            },
+            icon: 'photo',
+        }],
+        onCancelPress: jest.fn(),
+        title: 'test',
+    };
+
+    test('should match snapshot for iOS', async () => {
+        const wrapper = shallow(
+            <OptionModalListIOS {...baseProps}/>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot for Android', async () => {
+        const wrapper = shallow(
+            <OptionModalListAndroid {...baseProps}/>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
