aba9aad5960c879055a3729cf9474a4b599d3922
Jarred Witt <me@jarredwitt.com>
2018-10-17 14:28:28 -0700

    Create network listener component

---
+++ app/components/network_indicator/index.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import {startPeriodicStatusUpdates, stopPeriodicStatusUpdates, logout} from 'mattermost-redux/actions/users';
+import {init as initWebSocket, close as closeWebSocket} from 'mattermost-redux/actions/websocket';
+
+import {connection} from 'app/actions/device';
+import {getConnection, isLandscape} from 'app/selectors/device';
+
+import NetworkIndicator from './network_indicator';
+
+function mapStateToProps(state) {
+    const {websocket} = state.requests.general;
+    const webSocketStatus = websocket.status;
+    const isConnecting = websocket.error > 1;
+
+    return {
+        isConnecting,
+        isLandscape: isLandscape(state),
+        isOnline: getConnection(state),
+        webSocketStatus,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            closeWebSocket,
+            connection,
+            initWebSocket,
+            logout,
+            startPeriodicStatusUpdates,
+            stopPeriodicStatusUpdates,
+        }, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NetworkIndicator);
---
+++ app/components/network_indicator/network_indicator.js
@@ -0,0 +1,390 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {intlShape} from 'react-intl';
+import {
+    ActivityIndicator,
+    Alert,
+    Animated,
+    AppState,
+    Platform,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
+import FormattedText from 'app/components/formatted_text';
+import {ViewTypes} from 'app/constants';
+import mattermostBucket from 'app/mattermost_bucket';
+import networkConnectionListener, {checkConnection} from 'app/utils/network';
+import {t} from 'app/utils/i18n';
+import LocalConfig from 'assets/config';
+
+import {RequestStatus} from 'mattermost-redux/constants';
+
+const HEIGHT = 38;
+const OFFLINE = 'offline';
+const CONNECTING = 'connecting';
+const CONNECTED = 'connected';
+const {
+    ANDROID_TOP_LANDSCAPE,
+    ANDROID_TOP_PORTRAIT,
+    IOS_TOP_LANDSCAPE,
+    IOS_TOP_PORTRAIT,
+    IOSX_TOP_PORTRAIT,
+    STATUS_BAR_HEIGHT,
+} = ViewTypes;
+
+export default class NetworkIndicator extends PureComponent {
+    static propTypes = {
+        actions: PropTypes.shape({
+            closeWebSocket: PropTypes.func.isRequired,
+            connection: PropTypes.func.isRequired,
+            initWebSocket: PropTypes.func.isRequired,
+            logout: PropTypes.func.isRequired,
+            startPeriodicStatusUpdates: PropTypes.func.isRequired,
+            stopPeriodicStatusUpdates: PropTypes.func.isRequired,
+        }).isRequired,
+        isConnecting: PropTypes.bool,
+        isLandscape: PropTypes.bool,
+        isOnline: PropTypes.bool,
+        webSocketStatus: PropTypes.string,
+    };
+
+    static defaultProps = {
+        isOnline: true,
+    };
+
+    static contextTypes = {
+        intl: intlShape.isRequired,
+    };
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        const {isLandscape, networkStatus} = prevState;
+
+        if (nextProps.isLandscape !== isLandscape && networkStatus) {
+            const navBar = this.getNavBarHeight(nextProps.isLandscape);
+            const top = new Animated.Value(navBar - HEIGHT);
+
+            return {
+                navBar,
+                top,
+            };
+        }
+
+        return null;
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.isX = DeviceInfo.getModel().includes('iPhone X');
+        const navBar = this.getNavBarHeight(props.isLandscape);
+
+        this.state = {
+            isLandscape: props.isLandscape,
+            networkStatus: null,
+            navBar,
+            top: new Animated.Value(navBar - HEIGHT),
+        };
+
+        this.backgroundColor = new Animated.Value(0);
+
+        this.networkListener = networkConnectionListener(this.handleConnectionChange);
+    }
+
+    componentDidMount() {
+        this.mounted = true;
+    }
+
+    componentDidUpdate(prevProps) {
+        const {webSocketStatus} = this.props;
+
+        if (this.props.isOnline) {
+            if (this.state.networkStatus && webSocketStatus === RequestStatus.STARTED && this.props.webSocketStatus === RequestStatus.SUCCESS) {
+                // Show the connected animation only if we had a previous network status
+                this.connected();
+            } else if (webSocketStatus === RequestStatus.STARTED && this.props.webSocketStatus === RequestStatus.FAILURE && this.props.isConnecting) {
+                // Show the connecting bar if it failed to connect at least twice
+                this.connecting();
+            }
+        } else if (prevProps.isOnline && !this.props.isOnline) {
+            this.offline();
+        }
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+
+        const {closeWebSocket, stopPeriodicStatusUpdates} = this.props.actions;
+
+        this.networkListener.removeEventListener();
+
+        AppState.removeEventListener('change', this.handleAppStateChange);
+
+        closeWebSocket();
+        stopPeriodicStatusUpdates();
+    }
+
+    connect = () => {
+        // need to change this to use the non-listener function
+        checkConnection((result) => {
+            this.setLocalState({networkStatus: CONNECTING}, () => {
+                if (result) {
+                    this.props.actions.connection(true);
+                    this.initializeWebSocket();
+                } else {
+                    this.setLocalState({networkStatus: OFFLINE});
+                }
+            });
+        });
+    };
+
+    connected = () => {
+        this.setState({networkStatus: CONNECTED});
+        Animated.sequence([
+            Animated.timing(
+                this.backgroundColor, {
+                    toValue: 1,
+                    duration: 100,
+                }
+            ),
+            Animated.timing(
+                this.state.top, {
+                    toValue: (this.state.navBar - HEIGHT),
+                    duration: 300,
+                    delay: 500,
+                }
+            ),
+        ]).start(() => {
+            this.backgroundColor.setValue(0);
+            this.setLocalState({networkStatus: null});
+        });
+    };
+
+    connecting = () => {
+        const prevState = this.state.networkStatus;
+        this.setLocalState({networkStatus: CONNECTING}, () => {
+            if (prevState !== OFFLINE) {
+                this.show();
+            }
+        });
+    };
+
+    getNavBarHeight = (isLandscape) => {
+        if (Platform.OS === 'android') {
+            if (isLandscape) {
+                return ANDROID_TOP_LANDSCAPE;
+            }
+
+            return ANDROID_TOP_PORTRAIT;
+        }
+
+        if (this.isX && isLandscape) {
+            return IOS_TOP_LANDSCAPE;
+        } else if (this.isX) {
+            return IOSX_TOP_PORTRAIT;
+        } else if (isLandscape) {
+            return IOS_TOP_LANDSCAPE + STATUS_BAR_HEIGHT;
+        }
+
+        return IOS_TOP_PORTRAIT;
+    };
+
+    handleWebSocket = (open) => {
+        const {actions} = this.props;
+        const {
+            closeWebSocket,
+            startPeriodicStatusUpdates,
+            stopPeriodicStatusUpdates,
+        } = actions;
+
+        if (open) {
+            this.initializeWebSocket();
+            startPeriodicStatusUpdates();
+        } else {
+            closeWebSocket(true);
+            stopPeriodicStatusUpdates();
+        }
+    };
+
+    handleAppStateChange = async (appState) => {
+        this.handleWebSocket(appState === 'active');
+    };
+
+    handleConnectionChange = (isConnected) => {
+        const {connection} = this.props.actions;
+
+        // Prevent for being called more than once.
+        if (this.isConnected !== isConnected) {
+            this.isConnected = isConnected;
+            this.handleWebSocket(isConnected);
+            connection(isConnected);
+        }
+    };
+
+    initializeWebSocket = async () => {
+        const {formatMessage} = this.context.intl;
+        const {actions} = this.props;
+        const {closeWebSocket, initWebSocket} = actions;
+        const platform = Platform.OS;
+        let certificate = null;
+        if (platform === 'ios') {
+            certificate = await mattermostBucket.getPreference('cert', LocalConfig.AppGroupId);
+        }
+
+        initWebSocket(platform, null, null, null, {certificate}).catch(() => {
+            // we should dispatch a failure and show the app as disconnected
+            Alert.alert(
+                formatMessage({id: 'mobile.authentication_error.title', defaultMessage: 'Authentication Error'}),
+                formatMessage({
+                    id: 'mobile.authentication_error.message',
+                    defaultMessage: 'Mattermost has encountered an error. Please re-authenticate to start a new session.',
+                }),
+                [{
+                    text: formatMessage({
+                        id: 'navbar_dropdown.logout',
+                        defaultMessage: 'Logout',
+                    }),
+                    onPress: actions.logout,
+                }],
+                {cancelable: false}
+            );
+            closeWebSocket(true);
+        });
+    };
+
+    offline = () => {
+        this.setLocalState({networkStatus: OFFLINE}, () => {
+            this.show();
+        });
+    };
+
+    setLocalState = (state, callback) => {
+        if (!this.mounted) {
+            return;
+        }
+
+        this.setState(state, callback);
+    };
+
+    show = () => {
+        Animated.timing(
+            this.state.top, {
+                toValue: this.state.navBar,
+                duration: 300,
+            }
+        ).start();
+    };
+
+    render() {
+        if (!this.state.networkStatus) {
+            return null;
+        }
+
+        const background = this.backgroundColor.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['#939393', '#629a41'],
+        });
+
+        let i18nId;
+        let defaultMessage;
+        let action;
+        switch (this.state.networkStatus) {
+        case OFFLINE:
+            i18nId = t('mobile.offlineIndicator.offline');
+            defaultMessage = 'Cannot connect to the server';
+            action = (
+                <TouchableOpacity
+                    onPress={this.connect}
+                    style={[styles.actionContainer, styles.actionButton]}
+                >
+                    <IonIcon
+                        color='#FFFFFF'
+                        name='ios-refresh'
+                        size={20}
+                    />
+                </TouchableOpacity>
+            );
+            break;
+        case CONNECTING:
+            i18nId = t('mobile.offlineIndicator.connecting');
+            defaultMessage = 'Connecting...';
+            action = (
+                <View style={styles.actionContainer}>
+                    <ActivityIndicator
+                        color='#FFFFFF'
+                        size='small'
+                    />
+                </View>
+            );
+            break;
+        case CONNECTED:
+        default:
+            i18nId = t('mobile.offlineIndicator.connected');
+            defaultMessage = 'Connected';
+            action = (
+                <View style={styles.actionContainer}>
+                    <IonIcon
+                        color='#FFFFFF'
+                        name='md-checkmark'
+                        size={20}
+                    />
+                </View>
+            );
+            break;
+        }
+
+        return (
+            <Animated.View style={[styles.container, {top: this.state.top, backgroundColor: background}]}>
+                <Animated.View style={styles.wrapper}>
+                    <FormattedText
+                        defaultMessage={defaultMessage}
+                        id={i18nId}
+                        style={styles.message}
+                    />
+                    {action}
+                </Animated.View>
+            </Animated.View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: HEIGHT,
+        width: '100%',
+        zIndex: 9,
+        position: 'absolute',
+    },
+    wrapper: {
+        alignItems: 'center',
+        flex: 1,
+        height: HEIGHT,
+        flexDirection: 'row',
+        paddingLeft: 12,
+        paddingRight: 5,
+    },
+    message: {
+        color: '#FFFFFF',
+        fontSize: 14,
+        fontWeight: '600',
+        flex: 1,
+    },
+    actionButton: {
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#FFFFFF',
+    },
+    actionContainer: {
+        alignItems: 'flex-end',
+        height: 24,
+        justifyContent: 'center',
+        paddingRight: 10,
+        width: 60,
+    },
+});
