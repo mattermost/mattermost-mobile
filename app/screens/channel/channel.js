67fa7be6d08c2f84ae33a8f848565f6a662962ea
Jarred Witt <me@jarredwitt.com>
2018-10-17 14:29:05 -0700

    Remove unnecessary network/websocket logic

--- app/screens/channel/channel.js
+++ app/screens/channel/channel.js
@@ -5,8 +5,6 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {intlShape} from 'react-intl';
 import {
-    Alert,
-    AppState,
     Dimensions,
     Platform,
     StyleSheet,
@@ -20,14 +18,12 @@ import ChannelLoader from 'app/components/channel_loader';
 import MainSidebar from 'app/components/sidebars/main';
 import SettingsSidebar from 'app/components/sidebars/settings';
 import KeyboardLayout from 'app/components/layout/keyboard_layout';
-import OfflineIndicator from 'app/components/offline_indicator';
+import NetworkIndicator from 'app/components/network_indicator';
 import SafeAreaView from 'app/components/safe_area_view';
 import StatusBar from 'app/components/status_bar';
-import {DeviceTypes, ViewTypes} from 'app/constants';
-import mattermostBucket from 'app/mattermost_bucket';
+import {ViewTypes} from 'app/constants';
 import {preventDoubleTap} from 'app/utils/tap';
 import PostTextbox from 'app/components/post_textbox';
-import networkConnectionListener from 'app/utils/network';
 import tracker from 'app/utils/time_tracker';
 import LocalConfig from 'assets/config';
 
@@ -47,17 +43,11 @@ let ClientUpgradeListener;
 export default class Channel extends PureComponent {
     static propTypes = {
         actions: PropTypes.shape({
-            connection: PropTypes.func.isRequired,
             loadChannelsIfNecessary: PropTypes.func.isRequired,
             loadProfilesAndTeamMembersForDMSidebar: PropTypes.func.isRequired,
-            logout: PropTypes.func.isRequired,
             selectDefaultTeam: PropTypes.func.isRequired,
             selectInitialChannel: PropTypes.func.isRequired,
-            initWebSocket: PropTypes.func.isRequired,
-            closeWebSocket: PropTypes.func.isRequired,
             recordLoadTime: PropTypes.func.isRequired,
-            startPeriodicStatusUpdates: PropTypes.func.isRequired,
-            stopPeriodicStatusUpdates: PropTypes.func.isRequired,
         }).isRequired,
         currentChannelId: PropTypes.string,
         channelsRequestFailed: PropTypes.bool,
@@ -82,8 +72,6 @@ export default class Channel extends PureComponent {
     componentWillMount() {
         EventEmitter.on('leave_team', this.handleLeaveTeam);
 
-        this.networkListener = networkConnectionListener(this.handleConnectionChange);
-
         if (this.props.currentTeamId) {
             this.loadChannels(this.props.currentTeamId);
         } else {
@@ -92,8 +80,6 @@ export default class Channel extends PureComponent {
     }
 
     componentDidMount() {
-        AppState.addEventListener('change', this.handleAppStateChange);
-
         if (tracker.initialLoad) {
             this.props.actions.recordLoadTime('Start time', 'initialLoad');
         }
@@ -129,15 +115,7 @@ export default class Channel extends PureComponent {
     }
 
     componentWillUnmount() {
-        const {closeWebSocket, stopPeriodicStatusUpdates} = this.props.actions;
-
         EventEmitter.off('leave_team', this.handleLeaveTeam);
-        this.networkListener.removeEventListener();
-
-        AppState.removeEventListener('change', this.handleAppStateChange);
-
-        closeWebSocket();
-        stopPeriodicStatusUpdates();
     }
 
     attachPostTextBox = (ref) => {
@@ -211,71 +189,8 @@ export default class Channel extends PureComponent {
         }
     });
 
-    handleWebSocket = (open) => {
-        const {actions} = this.props;
-        const {
-            closeWebSocket,
-            startPeriodicStatusUpdates,
-            stopPeriodicStatusUpdates,
-        } = actions;
-
-        if (open) {
-            this.initializeWebSocket();
-            startPeriodicStatusUpdates();
-        } else {
-            closeWebSocket(true);
-            stopPeriodicStatusUpdates();
-        }
-    };
-
-    handleAppStateChange = async (appState) => {
-        this.handleWebSocket(appState === 'active');
-    };
-
-    handleConnectionChange = (isConnected) => {
-        const {connection} = this.props.actions;
-
-        // Prevent for being called more than once.
-        if (this.isConnected !== isConnected) {
-            this.isConnected = isConnected;
-            this.handleWebSocket(isConnected);
-            connection(isConnected);
-        }
-    };
-
     handleLeaveTeam = () => {
         this.props.actions.selectDefaultTeam();
-    };
-
-    initializeWebSocket = async () => {
-        const {formatMessage} = this.context.intl;
-        const {actions} = this.props;
-        const {initWebSocket} = actions;
-        const platform = Platform.OS;
-        let certificate = null;
-        if (platform === 'ios') {
-            certificate = await mattermostBucket.getPreference('cert', LocalConfig.AppGroupId);
-        }
-
-        initWebSocket(platform, null, null, null, {certificate}).catch(() => {
-            // we should dispatch a failure and show the app as disconnected
-            Alert.alert(
-                formatMessage({id: 'mobile.authentication_error.title', defaultMessage: 'Authentication Error'}),
-                formatMessage({
-                    id: 'mobile.authentication_error.message',
-                    defaultMessage: 'Mattermost has encountered an error. Please re-authenticate to start a new session.',
-                }),
-                [{
-                    text: formatMessage({
-                        id: 'navbar_dropdown.logout',
-                        defaultMessage: 'Logout',
-                    }),
-                    onPress: actions.logout,
-                }],
-                {cancelable: false}
-            );
-            this.props.actions.closeWebSocket(true);
-        });
     };
 
     loadChannels = (teamId) => {
@@ -359,7 +274,7 @@ export default class Channel extends PureComponent {
                 >
                     <SafeAreaView navigator={navigator}>
                         <StatusBar/>
-                        <OfflineIndicator/>
+                        <NetworkIndicator/>
                         <ChannelNavBar
                             navigator={navigator}
                             openChannelDrawer={this.openChannelSidebar}
