78e5b99884528b1243b5ff6668660f2ecab4d063
Harrison Healey <harrisonmhealey@gmail.com>
2018-10-18 14:12:25 -0400

    MM-5957 Add mention highlighting on mobile  (#2277)
    * Switch pullOutImages to use a NodeWalker
    
    * Add markdown transformer to combine adjacent text nodes
    
    * MM-5957 Add mention highlighting on mobile

--- app/components/markdown/index.js
+++ app/components/markdown/index.js
@@ -5,12 +5,14 @@ import {connect} from 'react-redux';
 
 import {getAutolinkedUrlSchemes} from 'mattermost-redux/selectors/entities/general';
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {getCurrentUserMentionKeys} from 'mattermost-redux/selectors/entities/users';
 
 import Markdown from './markdown';
 
 function mapStateToProps(state) {
     return {
         autolinkedUrlSchemes: getAutolinkedUrlSchemes(state),
+        mentionKeys: getCurrentUserMentionKeys(state),
         theme: getTheme(state),
     };
 }
