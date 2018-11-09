921674a50e62ddd62f1aa7e96880f486b7151ff7
Harrison Healey <harrisonmhealey@gmail.com>
2018-11-09 11:41:48 -0500

    MM-12901 Create showSearchModal action and use for hashtags

--- app/components/markdown/hashtag/index.js
+++ app/components/markdown/hashtag/index.js
@@ -1,22 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-
-import {clearSearch} from 'mattermost-redux/actions/search';
+import {bindActionCreators} from 'redux';
 
 import {showSearchModal} from 'app/actions/views/search';
 
-import ChannelSearchButton from './channel_search_button';
+import Hashtag from './hashtag';
 
 function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators({
-            clearSearch,
             showSearchModal,
         }, dispatch),
     };
 }
 
-export default connect(null, mapDispatchToProps)(ChannelSearchButton);
+export default connect(null, mapDispatchToProps)(Hashtag);
