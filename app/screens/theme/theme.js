c81187f9a569221648cc66af43ac6fe0ec4ae3da
Dan Seawel <dseawel@hotmail.com>
2018-11-25 18:30:05 -0600

    MM-12906 - Add thumbnails to theme selection

--- app/screens/theme/theme.js
+++ app/screens/theme/theme.js
@@ -2,17 +2,25 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {View, FlatList} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import {intlShape} from 'react-intl';
 
 import StatusBar from 'app/components/status_bar';
 import Section from 'app/screens/settings/section';
 import SectionItem from 'app/screens/settings/section_item';
+import ThemeTile from './theme_tile';
 import FormattedText from 'app/components/formatted_text';
 
 import {changeOpacity, makeStyleSheetFromTheme, setNavigatorStyles} from 'app/utils/theme';
 import Preferences from 'mattermost-redux/constants/preferences';
+
+const thumbnailImages = {
+    default: require('assets/images/themes/mattermost.png'),
+    organization: require('assets/images/themes/organization.png'),
+    mattermostDark: require('assets/images/themes/mattermost_dark.png'),
+    windows10: require('assets/images/themes/windows_dark.png'),
+};
 
 export default class Theme extends React.PureComponent {
     static propTypes = {
@@ -21,6 +29,8 @@ export default class Theme extends React.PureComponent {
         }).isRequired,
         allowedThemes: PropTypes.arrayOf(PropTypes.object),
         customTheme: PropTypes.object,
+        isLandscape: PropTypes.bool.isRequired,
+        isTablet: PropTypes.bool.isRequired,
         navigator: PropTypes.object.isRequired,
         teamId: PropTypes.string.isRequired,
         theme: PropTypes.object.isRequired,
@@ -63,57 +73,67 @@ export default class Theme extends React.PureComponent {
         }]);
     }
 
-    renderThemeRow = ({item, title}) => {
+    renderAllowedThemeTiles = () => {
+        const {theme, allowedThemes, isLandscape, isTablet} = this.props;
+
+        return allowedThemes.map((allowedTheme) => (
+            <ThemeTile
+                key={allowedTheme.key}
+                label={(
+                    <FormattedText
+                        id={`user.settings.display.${allowedTheme.type}`}
+                        defaultMessage={allowedTheme.type}
+                    />
+                )}
+                action={this.setTheme}
+                actionValue={allowedTheme.key}
+                selected={allowedTheme.type.toLowerCase() === theme.type.toLowerCase()}
+                theme={theme}
+                imageSrc={thumbnailImages[allowedTheme.key]}
+                isLandscape={isLandscape}
+                isTablet={isTablet}
+            />
+        ));
+    };
+
+    renderCustomThemeRow = ({item}) => {
         const {theme} = this.props;
-        const style = getStyleSheet(theme);
 
         return (
-            <React.Fragment>
-                <SectionItem
-                    label={(
-                        <FormattedText
-                            id={`user.settings.display.${item.type}`}
-                            defaultMessage={title || item.type}
-                        />
-                    )}
-                    action={this.setTheme}
-                    actionType='select'
-                    actionValue={item.key}
-                    selected={item.type.toLowerCase() === theme.type.toLowerCase()}
-                    theme={theme}
-                />
-                <View style={style.divider}/>
-            </React.Fragment>
+            <SectionItem
+                label={(
+                    <FormattedText
+                        id={`user.settings.display.${item.type}`}
+                        defaultMessage={'Custom Theme'}
+                    />
+                )}
+                action={this.setTheme}
+                actionType='select'
+                actionValue={item.key}
+                selected={item.type.toLowerCase() === theme.type.toLowerCase()}
+                theme={theme}
+            />
         );
     };
 
-    keyExtractor = (item) => item.key;
-
     render() {
-        const {theme, allowedThemes} = this.props;
+        const {theme} = this.props;
         const {customTheme} = this.state;
         const style = getStyleSheet(theme);
         return (
             <View style={style.container}>
                 <StatusBar/>
                 <View style={style.wrapper}>
-                    <Section
-                        disableHeader={true}
-                        theme={theme}
-                    >
-                        <FlatList
-                            data={allowedThemes}
-                            renderItem={this.renderThemeRow}
-                            keyExtractor={this.keyExtractor}
-                        />
-                    </Section>
+                    <View style={style.tilesContainer}>
+                        {this.renderAllowedThemeTiles()}
+                    </View>
 
                     {customTheme &&
                         <Section
                             disableHeader={true}
                             theme={theme}
                         >
-                            {this.renderThemeRow({item: customTheme, title: 'Custom'})}
+                            {this.renderCustomThemeRow({item: customTheme})}
                         </Section>
                     }
                 </View>
@@ -133,9 +153,16 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
             flex: 1,
             paddingTop: 35,
         },
-        divider: {
-            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
-            height: 1,
+        tilesContainer: {
+            marginBottom: 30,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            backgroundColor: theme.centerChannelBg,
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            borderTopColor: changeOpacity(theme.centerChannelColor, 0.1),
+            borderBottomColor: changeOpacity(theme.centerChannelColor, 0.1),
         },
     };
 });
