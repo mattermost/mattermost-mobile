e96624a99027a99031e4a13726a401ee1219d4a4
Elias Nahum <nahumhbl@gmail.com>
2018-11-27 19:45:44 -0300

    Fetch team channels on extension init (#2383)

--- share_extension/android/extension_post/index.js
+++ share_extension/android/extension_post/index.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import {getCurrentChannel, getDefaultChannel} from 'mattermost-redux/selectors/entities/channels';
@@ -8,6 +9,7 @@ import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
+import {getTeamChannels} from 'share_extension/android/actions';
 import {getAllowedServerMaxFileSize} from 'app/utils/file';
 
 import ExtensionPost from './extension_post';
@@ -17,12 +19,12 @@ function mapStateToProps(state) {
     const {credentials} = state.entities.general;
     const {token, url} = credentials;
     let channel = getCurrentChannel(state);
-    if (channel.delete_at !== 0) {
+    if (channel && channel.delete_at !== 0) {
         channel = getDefaultChannel(state);
     }
 
     return {
-        channelId: channel.id,
+        channelId: channel?.id,
         currentUserId: getCurrentUserId(state),
         maxFileSize: getAllowedServerMaxFileSize(config),
         teamId: getCurrentTeamId(state),
@@ -31,4 +33,12 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(ExtensionPost);
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            getTeamChannels,
+        }, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExtensionPost);
