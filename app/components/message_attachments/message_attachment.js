ccaa1a1c11c8f479ad2a6608a91f0c7c44a7ecc2
Elias Nahum <nahumhbl@gmail.com>
2018-11-30 11:12:07 -0300

    More fixes to the post layout (#2408)

--- app/components/message_attachments/message_attachment.js
+++ app/components/message_attachments/message_attachment.js
@@ -65,7 +65,7 @@ export default class MessageAttachment extends PureComponent {
         }
 
         return (
-            <View>
+            <React.Fragment>
                 <AttachmentPreText
                     baseTextStyle={baseTextStyle}
                     blockStyles={blockStyles}
@@ -75,9 +75,7 @@ export default class MessageAttachment extends PureComponent {
                     textStyles={textStyles}
                     value={attachment.pretext}
                 />
-                <View
-                    style={[style.container, style.border, borderStyle]}
-                >
+                <View style={[style.container, style.border, borderStyle]}>
                     <AttachmentAuthor
                         icon={attachment.author_icon}
                         link={attachment.author_link}
@@ -125,7 +123,7 @@ export default class MessageAttachment extends PureComponent {
                         theme={theme}
                     />
                 </View>
-            </View>
+            </React.Fragment>
         );
     }
 }
