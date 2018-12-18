640cdf0ed202dd798835b2aacf3a6c374a9053a2
Miguel Alatzar <this.migbot@gmail.com>
2018-12-17 21:30:55 -0800

    Use updated getJoinableTeams function from mattermost-redux

--- app/screens/select_team/index.js
+++ app/screens/select_team/index.js
@@ -12,24 +12,12 @@ import {logout} from 'mattermost-redux/actions/users';
 import {getCurrentChannelId} from 'mattermost-redux/selectors/entities/channels';
 import {getJoinableTeams} from 'mattermost-redux/selectors/entities/teams';
 
-import {getCurrentLocale} from 'app/selectors/i18n';
-
 import SelectTeam from './select_team.js';
 
 function mapStateToProps(state) {
-    const locale = getCurrentLocale(state);
-
-    function sortTeams(a, b) {
-        const options = {
-            numeric: true,
-            sensitivity: 'base',
-        };
-        return a.display_name.localeCompare(b.display_name, locale, options);
-    }
-
     return {
         teamsRequest: state.requests.teams.getTeams,
-        teams: Object.values(getJoinableTeams(state)).sort(sortTeams),
+        teams: getJoinableTeams(state),
         currentChannelId: getCurrentChannelId(state),
         joinTeamRequest: state.requests.teams.joinTeam,
     };
