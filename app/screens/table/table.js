cb9319ad512b91fc9bc7ef9f0bfb1c317138fcc2
Harrison Healey <harrisonmhealey@gmail.com>
2018-12-18 13:48:31 -0500

    Fix horizontal scrolling on Android

--- app/screens/table/table.js
+++ app/screens/table/table.js
@@ -3,7 +3,10 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {
+    Platform,
+    ScrollView,
+} from 'react-native';
 
 export default class Table extends React.PureComponent {
     static propTypes = {
@@ -12,10 +15,27 @@ export default class Table extends React.PureComponent {
     };
 
     render() {
-        return (
-            <ScrollView contentContainerStyle={{width: this.props.tableWidth}}>
-                {this.props.renderRows()}
-            </ScrollView>
-        );
+        const content = this.props.renderRows();
+
+        let container;
+        if (Platform.OS === 'android') {
+            // On Android, ScrollViews can only handle one direction at once, so use two ScrollViews that go in
+            // different directions. This prevents diagonal scrolling, so only do it on Android when totally necessary.
+            container = (
+                <ScrollView>
+                    <ScrollView horizontal={true}>
+                        {content}
+                    </ScrollView>
+                </ScrollView>
+            );
+        } else {
+            container = (
+                <ScrollView contentContainerStyle={{width: this.props.tableWidth}}>
+                    {content}
+                </ScrollView>
+            );
+        }
+
+        return container;
     }
 }
