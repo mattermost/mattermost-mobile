c1b08f30f007dfee673792203d24407cb179abef
Elias Nahum <nahumhbl@gmail.com>
2019-03-19 21:13:58 -0300

    iOS Share Extension fix post and dm/gm list (#2659)
    * MM-14629 DMs and GMs for the iOS extension to match sidebar
    
    * MM-14631 ios extension logging and fix race condition to post after uploading

--- ios/Mattermost/AppDelegate.m
+++ ios/Mattermost/AppDelegate.m
@@ -22,13 +22,16 @@
 #import <UploadAttachments/UploadAttachments-Swift.h>
 #import <UserNotifications/UserNotifications.h>
 #import "Mattermost-Swift.h"
+#import <os/log.h>
 
 @implementation AppDelegate
 
 NSString* const NotificationClearAction = @"clear";
 
 -(void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)(void))completionHandler {
+  os_log(OS_LOG_DEFAULT, "Mattermost will attach session from handleEventsForBackgroundURLSession!! identifier=%{public}@", identifier);
   [[UploadSession shared] attachSessionWithIdentifier:identifier completionHandler:completionHandler];
+  os_log(OS_LOG_DEFAULT, "Mattermost session ATTACHED from handleEventsForBackgroundURLSession!! identifier=%{public}@", identifier);
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -58,6 +61,9 @@ NSString* const NotificationClearAction = @"clear";
   self.window.backgroundColor = [UIColor whiteColor];
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
   [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error: nil];
+
+  os_log(OS_LOG_DEFAULT, "Mattermost started!!");
+
 
   return YES;
 }
@@ -123,7 +129,7 @@ NSString* const NotificationClearAction = @"clear";
       // This is to notify the NotificationCenter that something has changed.
       completionHandler(UIBackgroundFetchResultNewData);
     }];
-    
+
     return;
   } else if (state == UIApplicationStateInactive) {
     // When the notification is opened
