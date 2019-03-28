ad3443f716f17305ab28a540263b143848aa82ba
JoramWilander <jwawilander@gmail.com>
2019-03-28 14:07:22 -0400

    Generate random channel URL name from display names that don't clean into a usable name

--- app/actions/views/create_channel.js
+++ app/actions/views/create_channel.js
@@ -6,6 +6,17 @@ import {createChannel} from 'mattermost-redux/actions/channels';
 import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {cleanUpUrlable} from 'mattermost-redux/utils/channel_utils';
+import {generateId} from 'mattermost-redux/utils/helpers';
+
+export function generateChannelNameFromDisplayName(displayName) {
+    let name = cleanUpUrlable(displayName);
+
+    if (name === '') {
+        name = generateId();
+    }
+
+    return name;
+}
 
 export function handleCreateChannel(displayName, purpose, header, type) {
     return async (dispatch, getState) => {
@@ -14,7 +25,7 @@ export function handleCreateChannel(displayName, purpose, header, type) {
         const teamId = getCurrentTeamId(state);
         const channel = {
             team_id: teamId,
-            name: cleanUpUrlable(displayName),
+            name: generateChannelNameFromDisplayName(displayName),
             display_name: displayName,
             purpose,
             header,
