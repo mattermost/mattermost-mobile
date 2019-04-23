b11cf8e51c9bcee966c7a33f75d42977de31f7fb
Miguel Alatzar <migbot@users.noreply.github.com>
2019-04-22 19:17:03 -0700

    [MM-14669] Return null dimensions when height or width is falsy (#2681)
    * Return 50x50 dimensions when height or width is 0
    
    * Refactor image constants
    
    * Return null dimensions when height or width is falsy

---
+++ app/constants/image.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export const IMAGE_MAX_HEIGHT = 350;
+export const IMAGE_MIN_DIMENSION = 50;
