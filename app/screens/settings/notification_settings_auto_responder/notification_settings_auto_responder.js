79ab55e332ebefdd0e5e05c26e364ba9057edd29
Miguel Alatzar <this.migbot@gmail.com>
2019-06-12 14:27:17 -0700

    Save user notify props on componentWillUnmount

--- app/screens/settings/notification_settings_auto_responder/notification_settings_auto_responder.js
+++ app/screens/settings/notification_settings_auto_responder/notification_settings_auto_responder.js
@@ -62,7 +62,7 @@ export default class NotificationSettingsAutoResponder extends PureComponent {
         this.navigationEventListener = Navigation.events().bindComponent(this);
     }
 
-    componentDidDisappear() {
+    componentWillUnmount() {
         this.saveUserNotifyProps();
     }
 
