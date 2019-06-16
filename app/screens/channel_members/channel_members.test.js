630a3db2a6fdaf4de1e77393119a0cd06dd1b4b8
Miguel Alatzar <migbot@users.noreply.github.com>
2019-06-16 11:15:50 -0700

    [MM-15668] Member list items are selectable and enabled only when canManagerUsers is true (#2844)
    * Member list items are selectable only when canManagerUsers is true
    
    * Address review comment

---
+++ app/screens/channel_members/channel_members.test.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import React from 'react';
+
+import Preferences from 'mattermost-redux/constants/preferences';
+
+import {shallowWithIntl} from 'test/intl-test-helper';
+
+import CustomList from 'app/components/custom_list';
+import ChannelMembers from './channel_members';
+
+describe('ChannelMembers', () => {
+    const navigator = {
+        setOnNavigatorEvent: jest.fn(),
+        setButtons: jest.fn(),
+    };
+
+    const baseProps = {
+        theme: Preferences.THEMES.default,
+        currentUserId: 'current-user-id',
+        currentChannelId: 'current-channel-id',
+        canManageUsers: false,
+        actions: {
+            getProfilesInChannel: jest.fn().mockImplementation(() => Promise.resolve()),
+            handleRemoveChannelMembers: jest.fn(),
+            searchProfiles: jest.fn(),
+        },
+        navigator,
+    };
+
+    test('should match snapshot', () => {
+        const wrapper = shallowWithIntl(
+            <ChannelMembers {...baseProps}/>,
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    test('should use renderUnselectableItem when canManagerUsers is false', () => {
+        const props = {...baseProps, canManageUsers: false};
+
+        const wrapper = shallowWithIntl(
+            <ChannelMembers {...props}/>,
+        );
+
+        const renderItem = wrapper.find(CustomList).props().renderItem;
+        expect(renderItem).toEqual(wrapper.instance().renderUnselectableItem);
+    });
+
+    test('should use renderSelectableItem when canManagerUsers is true', () => {
+        const props = {...baseProps, canManageUsers: true};
+
+        const wrapper = shallowWithIntl(
+            <ChannelMembers {...props}/>,
+        );
+
+        const renderItem = wrapper.find(CustomList).props().renderItem;
+        expect(renderItem).toEqual(wrapper.instance().renderSelectableItem);
+    });
+});
