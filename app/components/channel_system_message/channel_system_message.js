ce57b171c63ec9b261c1715d6d3f322d91cddbce
Igor Shmukler <igor.shmukler@gmail.com>
2019-07-03 11:50:15 +0300

    uninlined functions

--- app/components/channel_system_message/channel_system_message.js
+++ app/components/channel_system_message/channel_system_message.js
@@ -17,10 +17,6 @@ export default class MiscSystemMessage extends React.PureComponent {
 
         const style = getStyleSheet(theme);
 
-        const renderUsername = (name) => {
-            return (name[0] === '@') ? name : `@${name}`;
-        }
-
         const systemMessageRenderers = {
 //            [Posts.POST_TYPES.JOIN_CHANNEL]: renderJoinChannelMessage,
 //            [Posts.POST_TYPES.LEAVE_CHANNEL]: renderLeaveChannelMessage,
@@ -38,168 +34,7 @@ export default class MiscSystemMessage extends React.PureComponent {
 //            [Posts.POST_TYPES.ME]: renderMeMessage,
         };
 
-        const renderChannelDeletedMessage = (post) => {
-            if (!post.props.username) {
-                return null;
-            }
-            const username = renderUsername(post.props.username);
-            return (
-                <FormattedMarkdownText
-                    id='api.channel.delete_channel.archived'
-                    defaultMessage='{username} has archived the channel.'
-                    values={{username}}
-                    style={style.baseText}
-                    navigator={navigator}
-                    textStyles={textStyles}
-                    theme={theme}
-                />
-            );
-        };
-
-        const renderHeaderChangeMessage = (post) => {
-            if (!post.props.username) {
-                return null;
-            }
-            const headerOptions = {
-                singleline: true,
-                channelNamesMap: post.props && post.props.channel_mentions,
-            };
-            const username = renderUsername(post.props.username);
-            const oldHeader = post.props.old_header ? renderFormattedText(post.props.old_header, headerOptions) : null;
-            const newHeader = post.props.new_header ? renderFormattedText(post.props.new_header, headerOptions) : null;
-            if (post.props.new_header) {
-                if (post.props.old_header) {
-                    return (
-                        <FormattedMarkdownText
-                            id='api.channel.post_update_channel_header_message_and_forget.updated_from'
-                            defaultMessage='{username} updated the channel header from: {old} to: {new}'
-                            navigator={navigator}
-                            textStyles={textStyles}
-                            theme={theme}
-                            values={{
-                                username,
-                                old: oldHeader,
-                                new: newHeader,
-                            }}
-                        />
-                    );
-                }
-                return (
-                    <FormattedMarkdownText
-                        id='api.channel.post_update_channel_header_message_and_forget.updated_to'
-                        defaultMessage='{username} updated the channel header to: {new}'
-                        values={{
-                            username,
-                            new: newHeader,
-                        }}
-                        navigator={navigator}
-                        textStyles={textStyles}
-                        theme={theme}
-                        style={style.baseText}
-                    />
-                );
-            } else if (post.props.old_header) {
-                return (
-                    <FormattedMarkdownText
-                        id='api.channel.post_update_channel_header_message_and_forget.removed'
-                        defaultMessage='{username} removed the channel header (was: {old})'
-                        style={style.baseText}
-                        navigator={navigator}
-                        textStyles={textStyles}
-                        theme={theme}
-                        values={{
-                            username,
-                            old: oldHeader,
-                        }}
-                    />
-                );
-            }
-            return null;
-        };
-
-        const renderDisplayNameChangeMessage = (post) => {
-            if (!(post.props.username && post.props.old_displayname && post.props.new_displayname)) {
-                return null;
-            }
-            const username = renderUsername(post.props.username);
-            const oldDisplayName = post.props.old_displayname;
-            const newDisplayName = post.props.new_displayname;
-            return (
-                <FormattedMarkdownText
-                    id='api.channel.post_update_channel_displayname_message_and_forget.updated_from'
-                    defaultMessage='{username} updated the channel display name from: {old} to: {new}'
-                    style={style.baseText}
-                    navigator={navigator}
-                    textStyles={textStyles}
-                    theme={theme}
-                    values={{
-                        username,
-                        old: oldDisplayName,
-                        new: newDisplayName,
-                    }}
-                />
-            );
-        };
-
-        function renderPurposeChangeMessage(post) {
-            if (!post.props.username) {
-                return null;
-            }
-            const username = renderUsername(post.props.username);
-            const oldPurpose = post.props.old_purpose;
-            const newPurpose = post.props.new_purpose;
-            if (post.props.new_purpose) {
-                if (post.props.old_purpose) {
-                    return (
-                        <FormattedMarkdownText
-                            id='app.channel.post_update_channel_purpose_message.updated_from'
-                            defaultMessage='{username} updated the channel purpose from: {old} to: {new}'
-                            style={style.baseText}
-                            navigator={navigator}
-                            textStyles={textStyles}
-                            theme={theme}
-                            values={{
-                                username,
-                                old: oldPurpose,
-                                new: newPurpose,
-                            }}
-                        />
-                    );
-                }
-                return (
-                    <FormattedMarkdownText
-                        id='app.channel.post_update_channel_purpose_message.updated_to'
-                        defaultMessage='{username} updated the channel purpose to: {new}'
-                        style={style.baseText}
-                        navigator={navigator}
-                        theme={theme}
-                        textStyles={textStyles}
-                        values={{
-                            username,
-                            new: newPurpose,
-                        }}
-                    />
-                );
-            } else if (post.props.old_purpose) {
-                return (
-                    <FormattedMarkdownText
-                        id='app.channel.post_update_channel_purpose_message.removed'
-                        defaultMessage='{username} removed the channel purpose (was: {old})'
-                        style={style.baseText}
-                        navigator={navigator}
-                        textStyles={textStyles}
-                        theme={theme}
-                        values={{
-                            username,
-                            old: oldPurpose,
-                        }}
-                    />
-                );
-            }
-            return null;
-        }
-
-        return post.type ? systemMessageRenderers[post.type](post) : null;
+        return post.type ? systemMessageRenderers[post.type](post, navigator, style, textStyles, theme) : null;
     }
 }
 
@@ -235,3 +70,168 @@ const getStyleSheet = makeStyleSheetFromTheme(theme => {
         },
     };
 });
+
+const renderUsername = (name) => {
+    return (name[0] === '@') ? name : `@${name}`;
+};
+
+const renderDisplayNameChangeMessage = (post, navigator, style, textStyles, theme) => {
+    if (!(post.props.username && post.props.old_displayname && post.props.new_displayname)) {
+        return null;
+    }
+    const username = renderUsername(post.props.username);
+    const oldDisplayName = post.props.old_displayname;
+    const newDisplayName = post.props.new_displayname;
+    return (
+        <FormattedMarkdownText
+            id='api.channel.post_update_channel_displayname_message_and_forget.updated_from'
+            defaultMessage='{username} updated the channel display name from: {old} to: {new}'
+            style={style.baseText}
+            navigator={navigator}
+            textStyles={textStyles}
+            theme={theme}
+            values={{
+                username,
+                old: oldDisplayName,
+                new: newDisplayName,
+            }}
+        />
+    );
+};
+
+const renderPurposeChangeMessage = (post, navigator, style, textStyles, theme) => {
+    if (!post.props.username) {
+        return null;
+    }
+    const username = renderUsername(post.props.username);
+    const oldPurpose = post.props.old_purpose;
+    const newPurpose = post.props.new_purpose;
+    if (post.props.new_purpose) {
+        if (post.props.old_purpose) {
+            return (
+                <FormattedMarkdownText
+                    id='app.channel.post_update_channel_purpose_message.updated_from'
+                    defaultMessage='{username} updated the channel purpose from: {old} to: {new}'
+                    style={style.baseText}
+                    navigator={navigator}
+                    textStyles={textStyles}
+                    theme={theme}
+                    values={{
+                        username,
+                        old: oldPurpose,
+                        new: newPurpose,
+                    }}
+                />
+            );
+        }
+        return (
+            <FormattedMarkdownText
+                id='app.channel.post_update_channel_purpose_message.updated_to'
+                defaultMessage='{username} updated the channel purpose to: {new}'
+                style={style.baseText}
+                navigator={navigator}
+                theme={theme}
+                textStyles={textStyles}
+                values={{
+                    username,
+                    new: newPurpose,
+                }}
+            />
+        );
+    } else if (post.props.old_purpose) {
+        return (
+            <FormattedMarkdownText
+                id='app.channel.post_update_channel_purpose_message.removed'
+                defaultMessage='{username} removed the channel purpose (was: {old})'
+                style={style.baseText}
+                navigator={navigator}
+                textStyles={textStyles}
+                theme={theme}
+                values={{
+                    username,
+                    old: oldPurpose,
+                }}
+            />
+        );
+    }
+    return null;
+}
+
+const renderChannelDeletedMessage = (post, navigator, style, textStyles, theme) => {
+    if (!post.props.username) {
+        return null;
+    }
+    const username = renderUsername(post.props.username);
+    return (
+        <FormattedMarkdownText
+            id='api.channel.delete_channel.archived'
+            defaultMessage='{username} has archived the channel.'
+            values={{username}}
+            style={style.baseText}
+            navigator={navigator}
+            textStyles={textStyles}
+            theme={theme}
+        />
+    );
+};
+
+const renderHeaderChangeMessage = (post, navigator, style, textStyles, theme) => {
+    if (!post.props.username) {
+        return null;
+    }
+    const headerOptions = {
+        singleline: true,
+        channelNamesMap: post.props && post.props.channel_mentions,
+    };
+    const username = renderUsername(post.props.username);
+    const oldHeader = post.props.old_header ? renderFormattedText(post.props.old_header, headerOptions) : null;
+    const newHeader = post.props.new_header ? renderFormattedText(post.props.new_header, headerOptions) : null;
+    if (post.props.new_header) {
+        if (post.props.old_header) {
+            return (
+                <FormattedMarkdownText
+                    id='api.channel.post_update_channel_header_message_and_forget.updated_from'
+                    defaultMessage='{username} updated the channel header from: {old} to: {new}'
+                    navigator={navigator}
+                    textStyles={textStyles}
+                    theme={theme}
+                    values={{
+                        username,
+                        old: oldHeader,
+                        new: newHeader,
+                    }}
+                />
+            );
+        }
+        return (
+            <FormattedMarkdownText
+                id='api.channel.post_update_channel_header_message_and_forget.updated_to'
+                defaultMessage='{username} updated the channel header to: {new}'
+                values={{
+                    username,
+                    new: newHeader,
+                }}
+                navigator={navigator}
+                textStyles={textStyles}
+                theme={theme}
+                style={style.baseText}
+            />
+        );
+    } else if (post.props.old_header) {
+        return (
+            <FormattedMarkdownText
+                id='api.channel.post_update_channel_header_message_and_forget.removed'
+                defaultMessage='{username} removed the channel header (was: {old})'
+                style={style.baseText}
+                navigator={navigator}
+                textStyles={textStyles}
+                theme={theme}
+                values={{
+                    username,
+                    old: oldHeader,
+                }}
+            />
+        );
+    }
+    return null;
+};
