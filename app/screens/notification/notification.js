3e4174e3925b04362a4618114218df99276aa54a
krjn <51929404+krjn@users.noreply.github.com>
2019-07-05 15:42:47 -0300

    [MM-16389] Enable emojis for profile icons from webhooks
    Removes borders from profile icons when it is from a custom URL.

--- app/screens/notification/notification.js
+++ app/screens/notification/notification.js
@@ -17,6 +17,7 @@ import FormattedText from 'app/components/formatted_text';
 import ProfilePicture from 'app/components/profile_picture';
 import {changeOpacity} from 'app/utils/theme';
 
+import {Client4} from 'mattermost-redux/client';
 import {isDirectChannel} from 'mattermost-redux/utils/channel_utils';
 import EventEmitter from 'mattermost-redux/utils/event_emitter';
 import {displayUsername} from 'mattermost-redux/utils/user_utils';
@@ -73,7 +74,8 @@ export default class Notification extends PureComponent {
         );
 
         if (data.from_webhook && config.EnablePostIconOverride === 'true' && data.use_user_icon !== 'true') {
-            const wsIcon = data.override_icon_url ? {uri: data.override_icon_url} : webhookIcon;
+            const overrideIconURL = Client4.getAbsoluteUrl(data.override_icon_url); // eslint-disable-line camelcase
+            const wsIcon = data.override_icon_url ? {uri: overrideIconURL} : webhookIcon;
             icon = (
                 <Image
                     source={wsIcon}
