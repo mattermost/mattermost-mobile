d8d7003c6ee048c576b9882933882868f2ff1732
Elias Nahum <nahumhbl@gmail.com>
2019-07-14 12:56:35 -0700

    Remove comments

--- app/utils/bottom_sheet/bottom_sheet.android.js
+++ app/utils/bottom_sheet/bottom_sheet.android.js
@@ -1,9 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-// import RNBottomSheet from 'react-native-bottom-sheet';
-// export default RNBottomSheet;
-
 import {Navigation} from 'react-native-navigation';
 
 export default {
