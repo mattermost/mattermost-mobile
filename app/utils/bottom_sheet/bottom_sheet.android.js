b8af8a386072454273a383435ff502a0c72f89cd
Elias Nahum <nahumhbl@gmail.com>
2019-07-17 11:06:21 -0400

    Lazy android native modules (#2973)
    * Lazy loading Android modules
    
    * Remove react-native-bottom-sheet
    
    * Remove comments
    
    * npm audit fix
    
    * Update rnn-v2 modals
    
    * audit fix
    
    * Fix tests

--- app/utils/bottom_sheet/bottom_sheet.android.js
+++ app/utils/bottom_sheet/bottom_sheet.android.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import RNBottomSheet from 'react-native-bottom-sheet';
-export default RNBottomSheet;
+import {store} from 'app/mattermost';
+import {showModalOverCurrentContext} from 'app/actions/navigation';
+
+export default {
+    showBottomSheetWithOptions: (options, callback) => {
+        function itemAction(index) {
+            callback(index);
+        }
+
+        const items = options.options.splice(0, options.cancelButtonIndex).map((o, index) => ({
+            action: () => itemAction(index),
+            text: o,
+        }));
+
+        store.dispatch(showModalOverCurrentContext('OptionsModal', {title: '', items}));
+    },
+};
