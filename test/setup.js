08516d360a744b34592d29f0cbb9302737fb2cb2
Miguel Alatzar <this.migbot@gmail.com>
2019-07-19 14:22:11 -0700

    Include in-app notifications in iOS badge number

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,13 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import MockAsyncStorage from 'mock-async-storage';
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
+
+const mockImpl = new MockAsyncStorage();
+jest.mock('@react-native-community/async-storage', () => mockImpl);
 
 /* eslint-disable no-console */
 
