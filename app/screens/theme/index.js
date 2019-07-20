1295f64737e4c5c0cb48ebbe47ce668f23354861
Miguel Alatzar <this.migbot@gmail.com>
2019-07-19 19:21:40 -0700

    Apply new theme to all navigation components

--- app/screens/theme/index.js
+++ app/screens/theme/index.js
@@ -3,12 +3,15 @@
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
 import {savePreferences} from 'mattermost-redux/actions/preferences';
+
 import {getAllowedThemes, getCustomTheme} from 'app/selectors/theme';
 import {isLandscape, isTablet} from 'app/selectors/device';
+import {applyTheme} from 'app/actions/navigation';
 
 import Theme from './theme';
 
@@ -25,6 +28,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
     actions: bindActionCreators({
         savePreferences,
+        applyTheme,
     }, dispatch),
 });
 
