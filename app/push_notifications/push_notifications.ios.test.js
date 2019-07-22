4f8174865d8cf22393a54f02a6ed9c359a4e7c0d
Miguel Alatzar <this.migbot@gmail.com>
2019-07-22 10:46:31 -0700

    Make foreground notifications key a const

--- app/push_notifications/push_notifications.ios.test.js
+++ app/push_notifications/push_notifications.ios.test.js
@@ -3,7 +3,7 @@
 
 import AsyncStorage from '@react-native-community/async-storage';
 import NotificationsIOS from 'react-native-notifications';
-import PushNotification from './push_notifications.ios';
+import PushNotification, {FOREGROUND_NOTIFICATIONS_KEY} from './push_notifications.ios';
 
 jest.mock('react-native-notifications', () => {
     let badgesCount = 0;
@@ -32,25 +32,24 @@ jest.mock('react-native-notifications', () => {
 describe('PushNotification', () => {
     const channel1ID = 'channel-1-id';
     const channel2ID = 'channel-2-id';
-    const {foregroundNotificationsKey: key} = PushNotification;
     const notification = {
         getData: jest.fn(),
         getMessage: jest.fn(),
     };
 
     it('should track foreground notifications for channel', async () => {
-        let item = await AsyncStorage.getItem(key);
+        let item = await AsyncStorage.getItem(FOREGROUND_NOTIFICATIONS_KEY);
         expect(item).toBe(null);
 
         await PushNotification.trackForegroundNotification(channel1ID);
-        item = await AsyncStorage.getItem(key);
+        item = await AsyncStorage.getItem(FOREGROUND_NOTIFICATIONS_KEY);
         expect(item).not.toBe(null);
         let foregroundNotifications = JSON.parse(item);
         expect(foregroundNotifications[channel1ID]).toBe(1);
         expect(foregroundNotifications[channel2ID]).toBe(undefined);
 
         await PushNotification.trackForegroundNotification(channel1ID);
-        item = await AsyncStorage.getItem(key);
+        item = await AsyncStorage.getItem(FOREGROUND_NOTIFICATIONS_KEY);
         expect(item).not.toBe(null);
         foregroundNotifications = JSON.parse(item);
         expect(foregroundNotifications[channel1ID]).toBe(2);
@@ -104,7 +103,7 @@ describe('PushNotification', () => {
             [channel1ID]: 1,
             [channel2ID]: 1,
         };
-        await AsyncStorage.setItem(key, JSON.stringify(foregroundNotifications));
+        await AsyncStorage.setItem(FOREGROUND_NOTIFICATIONS_KEY, JSON.stringify(foregroundNotifications));
 
         const notificationCount = deliveredNotifications.length + Object.values(foregroundNotifications).reduce((a, b) => a + b);
         expect(notificationCount).toBe(7);
@@ -123,7 +122,7 @@ describe('PushNotification', () => {
             expect(channel1DeliveredNotifications.length).toBe(0);
             expect(channel2DeliveredNotifications.length).toBe(2);
 
-            const item = await AsyncStorage.getItem(key);
+            const item = await AsyncStorage.getItem(FOREGROUND_NOTIFICATIONS_KEY);
             const foregroundNotifs = JSON.parse(item);
             const channel1ForegroundNotifications = foregroundNotifs[channel1ID];
             const channel2ForegroundNotifications = foregroundNotifs[channel2ID];
