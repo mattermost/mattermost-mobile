a0b021d21d2efd50dc9b0a6a867c8f0c2969f5d5
Mattermost Build <build@mattermost.com>
2019-07-23 11:22:43 +0200

    Automated cherry pick of #3019 (#3020)
    * Fixing guest login
    
    * Fixing eslint

--- app/actions/views/channel.js
+++ app/actions/views/channel.js
@@ -98,7 +98,7 @@ export function loadProfilesAndTeamMembersForDMSidebar(teamId) {
         const directChannels = Object.values(channels).filter((c) => (isDirectChannel(c) || isGroupChannel(c)));
         directChannels.forEach((channel) => {
             const member = myMembers[channel.id];
-            if (isDirectChannel(channel) && !isDirectChannelVisible(currentUserId, myPreferences, channel) && member.mention_count > 0) {
+            if (isDirectChannel(channel) && !isDirectChannelVisible(currentUserId, myPreferences, channel) && member && member.mention_count > 0) {
                 const teammateId = getUserIdFromChannelName(currentUserId, channel.name);
                 let pref = dmPrefs.get(teammateId);
                 if (pref) {
@@ -108,7 +108,7 @@ export function loadProfilesAndTeamMembersForDMSidebar(teamId) {
                 }
                 dmPrefs.set(teammateId, pref);
                 prefs.push(pref);
-            } else if (isGroupChannel(channel) && !isGroupChannelVisible(myPreferences, channel) && (member.mention_count > 0 || member.msg_count < channel.total_msg_count)) {
+            } else if (isGroupChannel(channel) && !isGroupChannelVisible(myPreferences, channel) && member && (member.mention_count > 0 || member.msg_count < channel.total_msg_count)) {
                 const id = channel.id;
                 let pref = gmPrefs.get(id);
                 if (pref) {
@@ -352,9 +352,8 @@ export function selectDefaultChannel(teamId) {
             // Handle case when the default channel cannot be found
             // so we need to get the first available channel of the team
             const channels = Object.values(channelsInTeam);
-            const firstChannel = channels.length ? channels[0].id : {id: ''};
-
-            channelId = firstChannel.id;
+            const firstChannel = channels.length ? channels[0].id : '';
+            channelId = firstChannel;
         }
 
         if (channelId) {
