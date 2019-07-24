ac791a5f60c842b520a54fb6c1997989805dda7c
Elias Nahum <nahumhbl@gmail.com>
2019-07-24 14:21:21 -0400

    Fix assigning credentials during app init

--- app/screens/entry/entry.js
+++ app/screens/entry/entry.js
@@ -21,7 +21,6 @@ import {
 } from 'app/mattermost';
 import {ViewTypes} from 'app/constants';
 import PushNotifications from 'app/push_notifications';
-import {stripTrailingSlashes} from 'app/utils/url';
 import {wrapWithContextProvider} from 'app/utils/wrap_context_provider';
 
 import ChannelLoader from 'app/components/channel_loader';
@@ -64,7 +63,6 @@ export default class Entry extends PureComponent {
         initializeModules: PropTypes.func,
         actions: PropTypes.shape({
             autoUpdateTimezone: PropTypes.func.isRequired,
-            setDeviceToken: PropTypes.func.isRequired,
         }).isRequired,
     };
 
@@ -148,33 +146,18 @@ export default class Entry extends PureComponent {
     };
 
     setAppCredentials = () => {
-        const {
-            actions: {
-                setDeviceToken,
-            },
-        } = this.props;
         const {getState} = store;
         const state = getState();
 
-        const {credentials} = state.entities.general;
         const {currentUserId} = state.entities.users;
 
-        if (app.deviceToken) {
-            setDeviceToken(app.deviceToken);
-        }
-
-        if (credentials.token && credentials.url) {
-            Client4.setToken(credentials.token);
-            Client4.setUrl(stripTrailingSlashes(credentials.url));
-        } else if (app.waitForRehydration) {
+        if (app.waitForRehydration) {
             app.waitForRehydration = false;
         }
 
         if (currentUserId) {
             Client4.setUserId(currentUserId);
         }
-
-        app.setAppCredentials(app.deviceToken, currentUserId, credentials.token, credentials.url);
     };
 
     setStartupThemes = () => {
--- app/screens/entry/index.js
+++ app/screens/entry/index.js
@@ -3,7 +3,6 @@
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-import {setDeviceToken} from 'mattermost-redux/actions/general';
 import {autoUpdateTimezone} from 'mattermost-redux/actions/timezone';
 import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
 import {isTimezoneEnabled} from 'mattermost-redux/selectors/entities/timezone';
@@ -31,7 +30,6 @@ function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators({
             autoUpdateTimezone,
-            setDeviceToken,
         }, dispatch),
     };
 }
