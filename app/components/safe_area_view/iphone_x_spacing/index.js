ec70d8221f7a8b102180cd8105106f1df5ee93f2
Claude Wild <imisshtml@gmail.com>
2019-07-29 13:31:06 -0400

    MM-10193 Updated Modifier for iphone_x_spacing
    Updated Modifier for iphone_x_spacing

--- app/components/safe_area_view/iphone_x_spacing/index.js
+++ app/components/safe_area_view/iphone_x_spacing/index.js
@@ -8,15 +8,15 @@ const paddingHorizontal = (isLandscape, modifier = 0) => {
 };
 
 const paddingLeft = (isLandscape, modifier = 0) => {
-    return DeviceTypes.IS_IPHONE_X && isLandscape ? {paddingLeft: ViewTypes.IOS_HORIZONTAL_LANDSCAPE} : null;
+    return DeviceTypes.IS_IPHONE_X && isLandscape ? {paddingLeft: ViewTypes.IOS_HORIZONTAL_LANDSCAPE + modifier} : null;
 };
 
 const paddingRight = (isLandscape, modifier = 0) => {
-    return DeviceTypes.IS_IPHONE_X && isLandscape ? {paddingRight: ViewTypes.IOS_HORIZONTAL_LANDSCAPE} : null;
+    return DeviceTypes.IS_IPHONE_X && isLandscape ? {paddingRight: ViewTypes.IOS_HORIZONTAL_LANDSCAPE + modifier} : null;
 };
 
 const marginHorizontal = (isLandscape, modifier = 0) => {
-    return DeviceTypes.IS_IPHONE_X && isLandscape ? {marginHorizontal: ViewTypes.IOS_HORIZONTAL_LANDSCAPE} : null;
+    return DeviceTypes.IS_IPHONE_X && isLandscape ? {marginHorizontal: ViewTypes.IOS_HORIZONTAL_LANDSCAPE + modifier} : null;
 };
 
 export {paddingHorizontal, paddingLeft, paddingRight, marginHorizontal};
