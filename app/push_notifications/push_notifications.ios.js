390c30af009e7f49890b229693e5d429ff280692
Mattermost Build <build@mattermost.com>
2019-08-13 16:18:55 +0200

    Do not track foreground notifications when notification is opened (#3111)

--- app/push_notifications/push_notifications.ios.js
+++ app/push_notifications/push_notifications.ios.js
@@ -54,7 +54,9 @@ class PushNotification {
             this.onNotification(this.deviceNotification);
         }
 
-        this.trackForegroundNotification(data.channel_id);
+        if (foreground) {
+            this.trackForegroundNotification(data.channel_id);
+        }
     };
 
     handleReply = (action, completed) => {
