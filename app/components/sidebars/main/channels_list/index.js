6cc63c24bb79e326f1044235b3eb42d5b67f3aed
Elias Nahum <nahumhbl@gmail.com>
2019-08-13 19:43:27 -0400

    Convert sidebar channel list to not connected component

--- app/components/sidebars/main/channels_list/index.js
+++ app/components/sidebars/main/channels_list/index.js
@@ -1,17 +1,288 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {connect} from 'react-redux';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {
+    Dimensions,
+    Platform,
+    View,
+} from 'react-native';
+import {intlShape} from 'react-intl';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
-import {isLandscape} from 'app/selectors/device';
-import ChannelsList from './channels_list';
+import {paddingHorizontal as padding} from 'app/components/safe_area_view/iphone_x_spacing';
+import SearchBar from 'app/components/search_bar';
+import {ViewTypes} from 'app/constants';
 
-function mapStateToProps(state) {
-    return {
-        theme: getTheme(state),
-        isLandscape: isLandscape(state),
+import {changeOpacity, makeStyleSheetFromTheme} from 'app/utils/theme';
+
+import List from './list';
+import SwitchTeamsButton from './switch_teams_button';
+
+const {ANDROID_TOP_PORTRAIT} = ViewTypes;
+let FilteredList = null;
+
+export default class ChannelsList extends PureComponent {
+    static propTypes = {
+        currentTeamId: PropTypes.string.isRequired,
+        drawerOpened: PropTypes.bool,
+        isLandscape: PropTypes.bool,
+        onJoinChannel: PropTypes.func.isRequired,
+        onSearchEnds: PropTypes.func.isRequired,
+        onSearchStart: PropTypes.func.isRequired,
+        onSelectChannel: PropTypes.func.isRequired,
+        onShowTeams: PropTypes.func.isRequired,
+        previewChannel: PropTypes.func,
+        teamsCount: PropTypes.number.isRequired,
+        theme: PropTypes.object.isRequired,
     };
+
+    static contextTypes = {
+        intl: intlShape.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searching: false,
+            term: '',
+        };
+
+        MaterialIcon.getImageSource('close', 20, this.props.theme.sidebarHeaderTextColor).then((source) => {
+            this.closeButton = source;
+        });
+    }
+
+    componentDidMount() {
+        Dimensions.addEventListener('change', this.handleDimensions);
+    }
+
+    componentWillUnmount() {
+        Dimensions.removeEventListener('change', this.handleDimensions);
+    }
+
+    cancelSearch = () => {
+        if (this.refs.search_bar) {
+            this.refs.search_bar.cancel();
+        }
+    };
+
+    handleDimensions = ({window}) => {
+        const {width, height} = window;
+        const isLandscape = width > height;
+
+        this.setState({isLandscape});
+    };
+
+    onSelectChannel = (channel, currentChannelId) => {
+        if (channel.fake) {
+            this.props.onJoinChannel(channel, currentChannelId);
+        } else {
+            this.props.onSelectChannel(channel, currentChannelId);
+        }
+
+        this.cancelSearch();
+    };
+
+    onSearch = (term) => {
+        this.setState({term});
+    };
+
+    onSearchFocused = () => {
+        if (!FilteredList) {
+            FilteredList = require('./filtered_list').default;
+        }
+        this.setState({searching: true});
+        this.props.onSearchStart();
+    };
+
+    onSearchCancel = () => {
+        this.props.onSearchEnds();
+        this.setState({searching: false});
+        this.onSearch('');
+    };
+
+    render() {
+        const {intl} = this.context;
+        const {
+            currentTeamId,
+            isLandscape,
+            onShowTeams,
+            teamsCount,
+            theme,
+            previewChannel,
+        } = this.props;
+
+        const {searching, term} = this.state;
+        const styles = getStyleSheet(theme);
+
+        let list;
+        if (searching) {
+            // TODO: Migrate to realm
+            // list = (
+            //     <FilteredList
+            //         currentTeamId={currentTeamId}
+            //         isLandscape={isLandscape}
+            //         onSelectChannel={this.onSelectChannel}
+            //         styles={styles}
+            //         term={term}
+            //         previewChannel={previewChannel}
+            //         theme={theme}
+            //     />
+            // );
+        } else {
+            list = (
+                <List
+                    currentTeamId={currentTeamId}
+                    isLandscape={isLandscape}
+                    onSelectChannel={this.onSelectChannel}
+                    styles={styles}
+                    previewChannel={previewChannel}
+                    theme={theme}
+                />
+            );
+        }
+
+        const searchBarInput = {
+            backgroundColor: changeOpacity(theme.sidebarHeaderTextColor, 0.2),
+            color: theme.sidebarHeaderTextColor,
+            fontSize: 15,
+            ...Platform.select({
+                android: {
+                    marginBottom: -5,
+                },
+            }),
+        };
+
+        const title = (
+            <View style={[styles.searchContainer, padding(isLandscape)]}>
+                <SearchBar
+                    ref='search_bar'
+                    placeholder={intl.formatMessage({id: 'mobile.channel_drawer.search', defaultMessage: 'Jump to...'})}
+                    cancelTitle={intl.formatMessage({id: 'mobile.post.cancel', defaultMessage: 'Cancel'})}
+                    inputCollapsedMargin={0}
+                    backgroundColor='transparent'
+                    inputHeight={33}
+                    inputStyle={searchBarInput}
+                    placeholderTextColor={changeOpacity(theme.sidebarHeaderTextColor, 0.5)}
+                    tintColorSearch={changeOpacity(theme.sidebarHeaderTextColor, 0.5)}
+                    tintColorDelete={changeOpacity(theme.sidebarHeaderTextColor, 0.5)}
+                    titleCancelColor={theme.sidebarHeaderTextColor}
+                    selectionColor={changeOpacity(theme.sidebarHeaderTextColor, 0.5)}
+                    onSearchButtonPress={this.onSearch}
+                    onCancelButtonPress={this.onSearchCancel}
+                    onChangeText={this.onSearch}
+                    onFocus={this.onSearchFocused}
+                    searchIconCollapsedMargin={5}
+                    searchIconExpandedMargin={5}
+                    value={term}
+                    leftComponent={(
+                        <SwitchTeamsButton
+                            currentTeamId={currentTeamId}
+                            onShowTeams={onShowTeams}
+                            teamsCount={teamsCount}
+                            theme={theme}
+                        />
+                    )}
+                />
+            </View>
+        );
+
+        return (
+            <View
+                style={styles.container}
+            >
+                <View style={styles.headerContainer}>
+                    {title}
+                </View>
+                {list}
+            </View>
+        );
+    }
 }
 
-export default connect(mapStateToProps, null, null, {forwardRef: true})(ChannelsList);
+const getStyleSheet = makeStyleSheetFromTheme((theme) => {
+    return {
+        container: {
+            backgroundColor: theme.sidebarBg,
+            flex: 1,
+        },
+        headerContainer: {
+            alignItems: 'center',
+            paddingLeft: 10,
+            backgroundColor: theme.sidebarBg,
+            flexDirection: 'row',
+            borderBottomWidth: 1,
+            borderBottomColor: changeOpacity(theme.sidebarHeaderTextColor, 0.10),
+            ...Platform.select({
+                android: {
+                    height: ANDROID_TOP_PORTRAIT,
+                },
+                ios: {
+                    height: 44,
+                },
+            }),
+        },
+        header: {
+            color: theme.sidebarHeaderTextColor,
+            flex: 1,
+            fontSize: 17,
+            fontWeight: 'normal',
+            paddingLeft: 16,
+        },
+        switchContainer: {
+            position: 'relative',
+            top: -1,
+        },
+        titleContainer: { // These aren't used by this component, but they are passed down to the list component
+            alignItems: 'center',
+            flex: 1,
+            flexDirection: 'row',
+            height: 48,
+            marginLeft: 16,
+        },
+        title: {
+            flex: 1,
+            color: theme.sidebarText,
+            opacity: 1,
+            fontSize: 15,
+            fontWeight: '400',
+            letterSpacing: 0.8,
+            lineHeight: 18,
+        },
+        searchContainer: {
+            flex: 1,
+            paddingRight: 10,
+            ...Platform.select({
+                android: {
+                    marginBottom: 1,
+                },
+                ios: {
+                    marginBottom: 3,
+                },
+            }),
+        },
+        divider: {
+            backgroundColor: changeOpacity(theme.sidebarText, 0.1),
+            height: 1,
+        },
+        actionContainer: {
+            alignItems: 'center',
+            height: 48,
+            justifyContent: 'center',
+            width: 50,
+        },
+        action: {
+            color: theme.sidebarText,
+            fontSize: 20,
+            fontWeight: '500',
+            lineHeight: 18,
+        },
+        above: {
+            backgroundColor: theme.mentionBg,
+            top: 9,
+        },
+    };
+});
