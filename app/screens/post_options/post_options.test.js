4ece9eb4dbc6695445f7c0d54bc33dbc89b7c67e
Elias Nahum <nahumhbl@gmail.com>
2019-08-13 17:04:24 -0400

    Fix unit test

--- app/screens/post_options/post_options.test.js
+++ app/screens/post_options/post_options.test.js
@@ -110,8 +110,14 @@ describe('PostOptions', () => {
         expect(Alert.alert).toBeCalled();
 
         // Trigger on press of Delete in the Alert
+        const closeWithAnimation = jest.spyOn(wrapper.instance(), 'closeWithAnimation');
         Alert.alert.mock.calls[0][2][1].onPress();
+        expect(closeWithAnimation).toBeCalled();
 
+        // get the callback that gets called by closeWithAnimation
+        const callback = closeWithAnimation.mock.calls[0][0];
+
+        callback();
         expect(actions.deletePost).toBeCalled();
         expect(actions.removePost).toBeCalled();
     });
