35f72fea80595b75990635e3eb6ea5dfc549058d
Elias Nahum <nahumhbl@gmail.com>
2019-08-13 19:26:38 -0400

    Event constants

---
+++ app/constants/events.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export default {
+    SET_CHANNEL_LOADING: 'set_channel_loading',
+    SWITCH_CHANNEL: 'switch_channel',
+};
--- app/constants/index.js
+++ app/constants/index.js
@@ -3,17 +3,22 @@
 
 import DeepLinkTypes from './deep_linking';
 import DeviceTypes from './device';
+
 import ListTypes from './list';
 import NavigationTypes from './navigation';
 import Posts from './posts';
 import Permissions from './permissions';
 import Roles from './roles';
 import ViewTypes, {UpgradeTypes} from './view';
+import CustomPropTypes from './custom_prop_types';
 
+import Events from './events';
 import General from './general';
 import Preferences from './preferences';
 
 export {
+    CustomPropTypes,
+    Events,
     General,
     Preferences,
     Posts,
