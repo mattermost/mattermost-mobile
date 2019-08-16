d26e35340cec93846ac292cab2e8965c9bd0d86c
Elias Nahum <nahumhbl@gmail.com>
2019-08-15 23:27:11 -0400

    Add comment to explain reducePermissionsToSet

--- app/realm/utils/role.js
+++ app/realm/utils/role.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// Use this function as the callback of an Array.prototype.reduce with an array of roles
 export function reducePermissionsToSet(result, role) {
     if (this.includes(role.name)) {
         role.permissions.forEach((p) => result.add(p));
