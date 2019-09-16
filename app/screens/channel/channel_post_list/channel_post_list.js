19e6b2a00e8d29bbcbd3ed826b228e08ed476490
Elias Nahum <nahumhbl@gmail.com>
2019-09-16 13:17:48 -0300

    [MM-18362] Sidebar improvements (#3223)
    * sidebar lists displays past bottom safe area insets
    
    * Sidebar animation speed
    
    * Sidebar improvements
    
    * Add status icons and avatars to mattermost font
    
    * Bot icon the same size as other channel icons
    
    * improvements to the channel badge
    
    * Badge color and border
    
    * More precision when showing more unread above
    
    * Improve GM icon
    
    * Fix badge on team sidebar
    
    * Align channel sidebar badges
    
    * alignments
    
    * Fix tests
    
    * Remove unnecessary isLandscape in main sidebar connector

--- app/screens/channel/channel_post_list/channel_post_list.js
+++ app/screens/channel/channel_post_list/channel_post_list.js
@@ -6,7 +6,6 @@ import React, {PureComponent} from 'react';
 import {
     Keyboard,
     Platform,
-    StyleSheet,
     View,
 } from 'react-native';
 
@@ -18,6 +17,7 @@ import PostList from 'app/components/post_list';
 import RetryBarIndicator from 'app/components/retry_bar_indicator';
 import {ViewTypes} from 'app/constants';
 import tracker from 'app/utils/time_tracker';
+import {changeOpacity, makeStyleSheetFromTheme} from 'app/utils/theme';
 import telemetry from 'app/telemetry';
 
 let ChannelIntro = null;
@@ -216,8 +216,11 @@ export default class ChannelPostList extends PureComponent {
             );
         }
 
+        const style = getStyleSheet(theme);
+
         return (
             <View style={style.container}>
+                <View style={style.separator}/>
                 {component}
                 <AnnouncementBanner/>
                 <RetryBarIndicator/>
@@ -226,8 +229,12 @@ export default class ChannelPostList extends PureComponent {
     }
 }
 
-const style = StyleSheet.create({
+const getStyleSheet = makeStyleSheetFromTheme((theme) => ({
     container: {
         flex: 1,
     },
-});
+    separator: {
+        backgroundColor: changeOpacity(theme.centerChannelColor, 0.2),
+        height: 1,
+    },
+}));
