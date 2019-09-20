4277116f824bc86ec354e92c349561772cdb5ce2
Miguel Alatzar <this.migbot@gmail.com>
2019-09-19 17:22:51 -0700

    Properly determine if channel is archived

--- app/components/post_body/index.js
+++ app/components/post_body/index.js
@@ -32,7 +32,7 @@ import PostBody from './post_body';
 
 const POST_TIMEOUT = 20000;
 
-function makeMapStateToProps() {
+export function makeMapStateToProps() {
     const memoizeHasEmojisOnly = memoizeResult((message, customEmojis) => hasEmojisOnly(message, customEmojis));
     const getReactionsForPost = makeGetReactionsForPost();
 
@@ -61,9 +61,10 @@ function makeMapStateToProps() {
         const roles = getCurrentUserId(state) ? getCurrentUserRoles(state) : '';
         const isAdmin = checkIsAdmin(roles);
         const isSystemAdmin = checkIsSystemAdmin(roles);
+        const channelIsArchived = ownProps.hasOwnProperty('channelIsArchived') ? ownProps.channelIsArchived : channel?.delete_at !== 0; //eslint-disable-line camelcase
         let canDelete = false;
 
-        if (post && !ownProps.channelIsArchived) {
+        if (post && !channelIsArchived) {
             canDelete = canDeletePost(state, config, license, currentTeamId, currentChannelId, currentUserId, post, isAdmin, isSystemAdmin);
         }
 
