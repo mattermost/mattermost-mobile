b96a38473c0b1c71e4b9d5de6785e7a67a8a8c5c
Miguel Alatzar <migbot@users.noreply.github.com>
2019-09-26 07:51:43 -0700

    [MM-18329] Add (you) suffix to self DM channel title (#3293)
    * Add (you) suffix to self DM channel title
    
    * Use FormattedText component

---
+++ app/screens/channel/channel_nav_bar/channel_title/channel_title.test.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import ChannelTitle from './channel_title';
+
+jest.mock('react-intl');
+
+describe('ChannelTitle', () => {
+    const baseProps = {
+        isGuest: false,
+        hasGuests: false,
+        canHaveSubtitle: false,
+        isSelfDMChannel: false,
+    };
+
+    test('should match snapshot', () => {
+        const wrapper = shallow(
+            <ChannelTitle {...baseProps}/>
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    test('should match snapshot when isSelfDMChannel is true', () => {
+        const props = {
+            ...baseProps,
+            isSelfDMChannel: true,
+        };
+        const wrapper = shallow(
+            <ChannelTitle {...props}/>,
+            {context: {intl: {formatMessage: (intlId) => intlId.defaultMessage}}},
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
