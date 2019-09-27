b349078954d5b46e2e430929265dfe490d942b83
Claude Wild <claude.wild@mattermost.com>
2019-09-27 08:08:36 -0400

    MM-18464 Resolved Snapshot
    Resolved snapshots

--- app/screens/selector_screen/selector_screen.test.js
+++ app/screens/selector_screen/selector_screen.test.js
@@ -76,6 +76,7 @@ describe('SelectorScreen', () => {
         data: [{text: 'text', value: 'value'}],
         dataSource: null,
         theme: Preferences.THEMES.default,
+        isLandscape: false,
     };
 
     test('should match snapshot for explicit options', async () => {
