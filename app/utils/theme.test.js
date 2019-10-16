52aca776b12dee3abe8646d0c90480f8528e86c1
Miguel Alatzar <this.migbot@gmail.com>
2019-10-16 12:01:12 -0700

    Revert "MM-17759 Add code highlighting (#3072)"
    This reverts commit 26b999e8857575da803e68b362a060386c36e6d7.

--- app/utils/theme.test.js
+++ app/utils/theme.test.js
@@ -1,44 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {getHighlightStyleFromTheme, getKeyboardAppearanceFromTheme} from 'app/utils/theme';
-import * as HighlightStyles from 'react-syntax-highlighter/styles/hljs';
-
-describe('getHighlightStyleFromTheme', () => {
-    const themes = [{
-        codeTheme: 'github',
-    }, {
-        codeTheme: 'atom-one-light',
-    }, {
-        codeTheme: 'androidstudio',
-    }, {
-        codeTheme: 'invalid_theme',
-    }];
-
-    it('should return github style for codeTheme="github"', () => {
-        const theme = getHighlightStyleFromTheme(themes[0]);
-        expect(theme).toBeTruthy();
-        expect(theme).toBe(HighlightStyles.github);
-    });
-
-    it('should return atomOneLight style for codeTheme="atom-one-light" (snake-case to camelCase)', () => {
-        const theme = getHighlightStyleFromTheme(themes[1]);
-        expect(theme).toBeTruthy();
-        expect(theme).toBe(HighlightStyles.atomOneLight);
-    });
-
-    it('should return androidstudio style for codeTheme="androidstudio" (no case conversion)', () => {
-        const theme = getHighlightStyleFromTheme(themes[2]);
-        expect(theme).toBeTruthy();
-        expect(theme).toBe(HighlightStyles.androidstudio);
-    });
-
-    it('should return github style by default for codeTheme="invalid_theme"', () => {
-        const theme = getHighlightStyleFromTheme(themes[3]);
-        expect(theme).toBeTruthy();
-        expect(theme).toBe(HighlightStyles.github);
-    });
-});
+import {getKeyboardAppearanceFromTheme} from 'app/utils/theme';
 
 describe('getKeyboardAppearanceFromTheme', () => {
     const themes = [{
