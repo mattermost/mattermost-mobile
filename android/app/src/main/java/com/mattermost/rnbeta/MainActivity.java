8546be731c689b11607f22417c3ed92cf455f9bc
Miguel Alatzar <this.migbot@gmail.com>
2019-10-17 14:52:54 -0700

    Fix share extension

--- android/app/src/main/java/com/mattermost/rnbeta/MainActivity.java
+++ android/app/src/main/java/com/mattermost/rnbeta/MainActivity.java
@@ -10,19 +10,5 @@ public class MainActivity extends NavigationActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_screen);
-
-        /**
-         * Reference: https://stackoverflow.com/questions/7944338/resume-last-activity-when-launcher-icon-is-clicked
-         * 1. Open app from launcher/appDrawer
-         * 2. Go home
-         * 3. Send notification and open
-         * 4. It creates a new Activity and Destroys the old
-         * 5. Causing an unnecessary app restart
-         * 6. This solution short-circuits the restart
-         */
-        if (!isTaskRoot()) {
-            finish();
-            return;
-        }
     }
 }
