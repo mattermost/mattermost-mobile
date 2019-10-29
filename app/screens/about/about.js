e7542454f213b397a46682d679eb43dae5bf5ddf
marianunez <maria.nunez@mattermost.com>
2019-10-29 15:03:55 -0400

    Added default links for TOC and Privacy policy for About Screen

--- app/screens/about/about.js
+++ app/screens/about/about.js
@@ -18,6 +18,7 @@ import {changeOpacity, makeStyleSheetFromTheme, setNavigatorStyles} from 'app/ut
 import {paddingHorizontal as padding} from 'app/components/safe_area_view/iphone_x_spacing';
 import AppIcon from 'app/components/app_icon';
 import Config from 'assets/config';
+import AboutLinks from 'app/constants/about_links';
 
 const MATTERMOST_BUNDLE_IDS = ['com.mattermost.rnbeta', 'com.mattermost.rn'];
 
@@ -53,11 +54,11 @@ export default class About extends PureComponent {
     };
 
     handleTermsOfService = () => {
-        Linking.openURL(this.props.config.TermsOfServiceLink);
+        Linking.openURL(AboutLinks.TERMS_OF_SERVICE);
     };
 
     handlePrivacyPolicy = () => {
-        Linking.openURL(this.props.config.PrivacyPolicyLink);
+        Linking.openURL(AboutLinks.PRIVACY_POLICY);
     }
 
     render() {
