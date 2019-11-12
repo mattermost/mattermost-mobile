55a7f62b9f2dde12468b7bdabba84733135a97a4
Miguel Alatzar <migbot@users.noreply.github.com>
2019-11-12 15:45:11 -0700

    [MM-19968] Ensure SELECT_CHANNEL_WITH_MEMBER is dispatched when app opened from a push notification (#3533)
    * Dispatch SELECT_CHANNEL_WITH_MEMBER
    
    * Call handleSelectChannel with true when from any push notification
    
    * Always dispatch SELECT_CHANNEL_WITH_MEMBER with passed channelId
    
    * Add missing comma
    
    * Update unit tests
    
    * Fix makrChannelViewedAndRead call

---
+++ app/screens/notification/notification.test.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import {InteractionManager} from 'react-native';
+
+import Preferences from 'mattermost-redux/constants/preferences';
+
+import Notification from './notification.js';
+
+jest.mock('react-native-navigation', () => ({
+    Navigation: {
+        events: jest.fn(() => ({
+            registerComponentDidDisappearListener: jest.fn(),
+        })),
+    },
+}));
+
+InteractionManager.runAfterInteractions = jest.fn((cb) => cb());
+
+describe('Notification', () => {
+    const baseProps = {
+        actions: {
+            loadFromPushNotification: jest.fn(),
+        },
+        componentId: 'component-id',
+        deviceWidth: 100,
+        notification: {},
+        theme: Preferences.THEMES.default,
+    };
+
+    test('should match snapshot', () => {
+        const wrapper = shallow(
+            <Notification {...baseProps}/>,
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    test('should call loadFromPushNotification on notification tap for non-local notification', () => {
+        const props = {
+            ...baseProps,
+            notification: {
+                localNotification: true,
+            },
+        };
+        const wrapper = shallow(
+            <Notification {...props}/>,
+        );
+        const instance = wrapper.instance();
+
+        instance.notificationTapped();
+        expect(baseProps.actions.loadFromPushNotification).not.toHaveBeenCalled();
+
+        const notification = {
+            localNotification: false,
+        };
+        wrapper.setProps({notification});
+        instance.notificationTapped();
+        expect(baseProps.actions.loadFromPushNotification).toHaveBeenCalledWith(notification);
+    });
+});
