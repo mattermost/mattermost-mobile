dc484eb586046b0f05f86dbd2be02646b0f3a404
Scott MacGregor <smacgregor@zynga.com>
2019-11-18 22:43:16 -0800

    MM-18766 - fix linter error

--- app/screens/image_preview/downloader.ios.js
+++ app/screens/image_preview/downloader.ios.js
@@ -48,7 +48,7 @@ export default class Downloader extends PureComponent {
         intl: intlShape,
     };
 
-    static getDerivedStateFromProps(props, state) {
+    static getDerivedStateFromProps(props, _) {
         if (!props.show) {
             return {
                 didCancel: false,
