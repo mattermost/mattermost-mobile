92c20a3e7c4eee90d09f2b1adc51eef6c715221b
Claude Wild <claude.wild@mattermost.com>
2019-11-22 09:14:32 -0500

    MM-19834 Added Unit Tests
    Added unit tests for the rendering of highlighted/non-highlighted displayname.

---
+++ app/components/at_mention/at_mention.test.js
@@ -0,0 +1,43 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import AtMention from './at_mention.js';
+
+describe('AtMention', () => {
+    const baseProps = {
+        usersByUsername: {},
+        mentionKeys: [{key: 'John.Smith'}, {key: 'Jane.Doe'}],
+        teammateNameDisplay: '',
+        mentionName: 'John.Smith',
+        mentionStyle: {color: '#ff0000'},
+        theme: {},
+    };
+
+    test('should match snapshot, no highlight', () => {
+        const wrapper = shallow(
+            <AtMention {...baseProps}/>
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    test('should match snapshot, with highlight', () => {
+        const wrapper = shallow(
+            <AtMention {...baseProps}/>
+        );
+
+        wrapper.setState({user: {username: 'John.Smith'}});
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    test('should match snapshot, without highlight', () => {
+        const wrapper = shallow(
+            <AtMention {...baseProps}/>
+        );
+
+        wrapper.setState({user: {username: 'Victor.Welch'}});
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+});
