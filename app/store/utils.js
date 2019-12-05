f54e460f0286d9505fdd1a21d330f9cd624aa966
Miguel Alatzar <this.migbot@gmail.com>
2019-12-05 15:36:33 -0700

    Pass callback to waitForHydration

--- app/store/utils.js
+++ app/store/utils.js
@@ -42,23 +42,14 @@ export function transformSet(incoming, setTransforms, toStorage = true) {
 }
 
 export function waitForHydration(store, callback) {
-    let unsubscribeFromStore;
-    return new Promise((resolve) => {
-        if (__DEV__) {
-            // when in DEV mode resetting the app can get into a white screen
-            resolve();
-            return;
-        }
-        const subscription = () => {
-            if (store.getState().views.root.hydrationComplete) {
-                unsubscribeFromStore();
-                if (callback && typeof callback === 'function') {
-                    callback();
-                }
-                resolve();
+    const subscription = () => {
+        if (store.getState().views.root.hydrationComplete) {
+            unsubscribeFromStore();
+            if (callback && typeof callback === 'function') {
+                callback();
             }
-        };
+        }
+    };
 
-        unsubscribeFromStore = store.subscribe(subscription);
-    });
+    const unsubscribeFromStore = store.subscribe(subscription);
 }
