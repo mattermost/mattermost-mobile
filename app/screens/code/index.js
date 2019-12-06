189040ee525304195380de3317311dc64ae3365f
Elias Nahum <nahumhbl@gmail.com>
2019-12-06 20:46:22 -0300

    MM-20875 migrate code screen to realm (#3672)

--- app/screens/code/index.js
+++ app/screens/code/index.js
@@ -1,16 +1,25 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {connect} from 'react-redux';
+import {realmConnect} from 'realm-react-redux';
 
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {General, Preferences} from 'app/constants';
+import {getTheme} from 'app/realm/selectors/preference';
+import options from 'app/store/realm_options';
 
 import Code from './code';
 
-function mapStateToProps(state) {
+function mapPropsToQueries(realm) {
+    const general = realm.objectForPrimaryKey('General', General.REALM_SCHEMA_ID) || General.REALM_EMPTY_OBJECT;
+    const themePreferences = realm.objects('Preference').filtered('category = $0', Preferences.CATEGORY_THEME);
+
+    return [general, themePreferences];
+}
+
+function mapQueriesToProps([general, themePreferences]) {
     return {
-        theme: getTheme(state),
+        theme: getTheme([general], themePreferences),
     };
 }
 
-export default connect(mapStateToProps)(Code);
+export default realmConnect(mapPropsToQueries, mapQueriesToProps, null, null, options)(Code);
