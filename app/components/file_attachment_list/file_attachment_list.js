4d3a28cb2475a3121cf5b709589c34a26dd9467f
lebedev.vladimir <lebedev.vladimir3@gmail.com>
2020-01-09 15:13:43 +0300

    remove redundant state initialization

--- app/components/file_attachment_list/file_attachment_list.js
+++ app/components/file_attachment_list/file_attachment_list.js
@@ -48,8 +48,6 @@ export default class FileAttachmentList extends PureComponent {
 
         this.items = [];
         this.filesForGallery = this.getFilesForGallery(props);
-        this.state = {
-        };
 
         this.buildGalleryFiles().then((results) => {
             this.galleryFiles = results;
