39012c35963bffb8ba34c587faa69fb6aa38b0e0
lebedev.vladimir <lebedev.vladimir3@gmail.com>
2020-01-09 15:40:28 +0300

    restore state

--- app/components/file_attachment_list/file_attachment_list.js
+++ app/components/file_attachment_list/file_attachment_list.js
@@ -48,6 +48,7 @@ export default class FileAttachmentList extends PureComponent {
 
         this.items = [];
         this.filesForGallery = this.getFilesForGallery(props);
+        this.state = {};
 
         this.buildGalleryFiles().then((results) => {
             this.galleryFiles = results;
