aa9e2aeafb2fa48edecba5dd5a3ecb054bbd3d45
Miguel Alatzar <this.migbot@gmail.com>
2020-01-17 17:25:39 -0700

    Emit server version changed event

--- app/init/fetch.test.js
+++ app/init/fetch.test.js
@@ -3,6 +3,8 @@
 
 import {Client4} from 'mattermost-redux/client';
 import {HEADER_X_VERSION_ID} from 'mattermost-redux/client/client4';
+import EventEmitter from 'mattermost-redux/utils/event_emitter';
+import {General} from 'mattermost-redux/constants';
 
 import {
     HEADER_X_CLUSTER_ID,
@@ -60,5 +62,26 @@ describe('Fetch', () => {
         expect(Client4.serverVersion).toEqual(headers[HEADER_X_VERSION_ID.toLowerCase()]);
         expect(Client4.clusterId).toEqual(headers[HEADER_X_CLUSTER_ID.toLowerCase()]);
         expect(setToken).toHaveBeenCalledWith(headers[HEADER_TOKEN.toLowerCase()]);
+    });
+
+    test('doFetchWithResponse handles server version change', async () => {
+        const emit = jest.spyOn(EventEmitter, 'emit');
+
+        const serverVersion1 = 'version1';
+        const serverVersion2 = 'version2';
+
+        const response = {
+            json: () => Promise.resolve('data'),
+            ok: true,
+            headers: {
+                [HEADER_X_VERSION_ID]: serverVersion1,
+            },
+        };
+        global.fetch.mockReturnValueOnce(response);
+
+        expect(Client4.serverVersion).not.toEqual(serverVersion1);
+        await Client4.doFetchWithResponse('https://mattermost.com', {method: 'GET'});
+        expect(Client4.serverVersion).toEqual(serverVersion1);
+        expect(emit).toHaveBeenCalledWith(General.SERVER_VERSION_CHANGED, serverVersion1);
     });
 });
