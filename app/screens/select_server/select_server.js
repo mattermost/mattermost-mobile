cde12ae44732a90d1be1d336747c1eec89a04a68
Ryan Wang <qwang1993@gmail.com>
2020-01-23 17:46:35 -0500

    MM-17649 - fix VoiceOver on server select & login

--- app/screens/select_server/select_server.js
+++ app/screens/select_server/select_server.js
@@ -425,7 +425,10 @@ export default class SelectServer extends PureComponent {
                     enabled={Platform.OS === 'ios'}
                 >
                     <StatusBar barStyle={statusStyle}/>
-                    <TouchableWithoutFeedback onPress={this.blur}>
+                    <TouchableWithoutFeedback
+                        onPress={this.blur}
+                        accessible={false}
+                    >
                         <View style={[GlobalStyles.container, GlobalStyles.signupContainer]}>
                             <Image
                                 source={require('assets/images/logo.png')}
@@ -439,6 +442,10 @@ export default class SelectServer extends PureComponent {
                                 />
                             </View>
                             <TextInput
+                                accessibilityLabel={formatMessage({
+                                    id: 'mobile.components.select_server_view.enterServerUrl',
+                                    defaultMessage: 'Enter Server URL',
+                                })}
                                 ref={this.inputRef}
                                 value={url}
                                 editable={!inputDisabled}
