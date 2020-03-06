f751b552b43fb4f49c04c81105ee38aca51caa5e
Harrison Healey <harrisonmhealey@gmail.com>
2020-03-06 17:07:20 -0500

    Add custom rule to prevent passing getState into dispatch (#4010)
    * Add custom rule to prevent passing getState into dispatch
    
    * Update package.json

--- app/actions/views/search.js
+++ app/actions/views/search.js
@@ -4,10 +4,8 @@
 import {ViewTypes} from 'app/constants';
 
 export function handleSearchDraftChanged(text) {
-    return async (dispatch, getState) => {
-        dispatch({
-            type: ViewTypes.SEARCH_DRAFT_CHANGED,
-            text,
-        }, getState);
+    return {
+        type: ViewTypes.SEARCH_DRAFT_CHANGED,
+        text,
     };
 }
