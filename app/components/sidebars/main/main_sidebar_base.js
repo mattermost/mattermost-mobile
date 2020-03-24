142f16384416eb246d38385457b1d3c49e2309ff
Elias Nahum <nahumhbl@gmail.com>
2020-03-23 21:57:47 -0300

    Set switchToChannel flag back to false

--- app/components/sidebars/main/main_sidebar_base.js
+++ app/components/sidebars/main/main_sidebar_base.js
@@ -151,7 +151,7 @@ export default class MainSidebarBase extends Component {
             return;
         }
 
-        this.selectChannel(result.data.channel || result.data, currentChannelId, true);
+        this.selectChannel(result.data.channel || result.data, currentChannelId, false);
     };
 
     onSearchEnds = () => {
