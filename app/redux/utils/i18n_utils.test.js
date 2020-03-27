563afc56a8c66367686d7ec12e29ec41fd0ce9b4
Miguel Alatzar <this.migbot@gmail.com>
2020-03-27 13:15:32 -0700

    Fix i18n_utils test

--- app/redux/utils/i18n_utils.test.js
+++ app/redux/utils/i18n_utils.test.js
@@ -4,6 +4,8 @@
 import assert from 'assert';
 
 import {setLocalizeFunction, localizeMessage} from '@redux/utils/i18n_utils';
+const en = require('assets/base/i18n/en.json');
+const [testKey, testValue] = Object.entries(en)[0];
 
 describe('i18n utils', () => {
     afterEach(() => {
@@ -11,15 +13,17 @@ describe('i18n utils', () => {
     });
 
     it('should return default message', () => {
-        assert.equal(localizeMessage('someting.string', 'defaultString'), 'defaultString');
+        assert.equal(localizeMessage(testKey, testValue), testValue);
     });
 
     it('should return previously set Localized function return value', () => {
+        const prevValue = 'previous-value';
         function mockFunc() {
-            return 'test';
+            return prevValue;
         }
 
         setLocalizeFunction(mockFunc);
-        assert.equal(localizeMessage('someting.string', 'defaultString'), 'test');
+        assert.not.equal(testValue, prevValue);
+        assert.equal(localizeMessage(testKey, testValue), prevValue);
     });
 });
