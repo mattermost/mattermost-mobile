da1c67f3be1c76ea7aba2ad7b6761ecbb93f86de
Elias Nahum <nahumhbl@gmail.com>
2020-04-06 16:47:47 -0400

    Rename post_textbox constant

--- app/constants/post_draft.js
+++ app/constants/post_draft.js
@@ -6,3 +6,6 @@ export const IS_REACTION_REGEX = /(^\+:([^:\s]*):)$/i;
 export const INSERT_TO_DRAFT = 'insert_to_draft';
 export const INSERT_TO_COMMENT = 'insert_to_comment';
 export const ICON_SIZE = 24;
+export const ACCESSORIES_CONTAINER_NATIVE_ID = 'channelAccessoriesContainer';
+export const CHANNEL_POST_TEXTBOX_CURSOR_CHANGE = 'onChannelTextBoxCursorChange';
+export const CHANNEL_POST_TEXTBOX_VALUE_CHANGE = 'onChannelTextBoxValueChange';
