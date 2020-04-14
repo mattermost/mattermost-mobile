711521098581f86c0e2d803929181ee247ab65c6
Miguel Alatzar <migbot@users.noreply.github.com>
2020-04-14 09:55:54 -0700

    Disable camelcase check

--- app/components/post_body/post_body.js
+++ app/components/post_body/post_body.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+/* eslint-disable camelcase */
+
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {
