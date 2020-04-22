cb4657c75b2606e7fbb9d739b1053731cd07a2ae
Miguel Alatzar <this.migbot@gmail.com>
2020-04-22 13:49:33 -0700

    Set previous version on logout instead

--- app/reducers/app/previousVersion.js
+++ app/reducers/app/previousVersion.js
@@ -1,14 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import DeviceInfo from 'react-native-device-info';
-
-import {UserTypes} from '@mm-redux/action_types';
-
 export default function previousVersion(state = '', action) {
-    switch (action.type) {
-    case UserTypes.LOGIN:
-        return state || DeviceInfo.getVersion();
-    }
     return state;
 }
