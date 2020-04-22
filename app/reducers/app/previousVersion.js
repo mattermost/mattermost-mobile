9aa165e02781ee3c9f6aa734b2eff99497ae410d
Miguel Alatzar <this.migbot@gmail.com>
2020-04-22 13:05:14 -0700

    Handle setting previousVersion on login and clearing data

--- app/reducers/app/previousVersion.js
+++ app/reducers/app/previousVersion.js
@@ -1,6 +1,14 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export default function previousVersion(state = '') {
+import DeviceInfo from 'react-native-device-info';
+
+import {UserTypes} from '@mm-redux/action_types';
+
+export default function previousVersion(state = '', action) {
+    switch (action.type) {
+    case UserTypes.LOGIN:
+        return state || DeviceInfo.getVersion();
+    }
     return state;
 }
