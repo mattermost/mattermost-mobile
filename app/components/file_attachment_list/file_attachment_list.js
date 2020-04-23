9434acd79ac80e6f54287ca0a7e14fa3656401cf
Mattermost Build <build@mattermost.com>
2020-04-23 13:56:14 +0200

    Automated cherry pick of #4192 (#4216)
    * Fix infinite loop retrieving post files
    
    Right now if all the attachments of a post are removed the component
    update code enters into an infinite loop becuase the comparison between
    arrays always return false.
    
    * Move all the code inside the file changing detector condition
    
    Co-authored-by: Mario de Frutos <mario@defrutos.org>

--- app/components/file_attachment_list/file_attachment_list.js
+++ app/components/file_attachment_list/file_attachment_list.js
@@ -69,13 +69,11 @@ export default class FileAttachmentList extends PureComponent {
     }
 
     componentDidUpdate(prevProps) {
-        if (prevProps.files !== this.props.files) {
+        if (prevProps.files.length !== this.props.files.length) {
             this.filesForGallery = this.getFilesForGallery(this.props);
             this.buildGalleryFiles().then((results) => {
                 this.galleryFiles = results;
             });
-        }
-        if (this.props.files !== prevProps.files && this.props.files.length === 0) {
             this.loadFilesForPost();
         }
     }
