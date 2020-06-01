941d532df11f904a0519e927ae0e71f043d1196e
Miguel Alatzar <this.migbot@gmail.com>
2020-06-01 16:00:12 -0700

    Remove obsolete rn-placeholder mocks

--- app/screens/pinned_posts/pinned_posts.test.js
+++ app/screens/pinned_posts/pinned_posts.test.js
@@ -9,10 +9,6 @@ import {shallowWithIntl} from 'test/intl-test-helper';
 
 import PinnedPosts from './pinned_posts';
 
-jest.mock('rn-placeholder', () => ({
-    ImageContent: () => null,
-}));
-
 describe('PinnedPosts', () => {
     const baseProps = {
         actions: {
