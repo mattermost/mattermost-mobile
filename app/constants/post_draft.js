446f95ff6b2033fc205acbfa884f72a502582cb7
Amit Uttam <changingrainbows@gmail.com>
2020-06-10 13:01:40 -0300

    MM-21368 Notify user when editing message that's too long (#4371)
    * MM-21368 Notify user when editing message that's too long
    
    * Disable 'Save' button to prevent edits from being posted
    * Display error message specific to the message size and allows max, instead of the current, default "Invalid Message" error.
    
    * Split message length error into text (left justified) and counts (right justified)
    
    UX guidance from PR review
    
    * UX Review: Wrap for longer error text + spacing adjustment
    
    * UX Review: Align split messages to top + add spacing in-between
    
    * UX Review: Align error message box padding with content

--- app/constants/post_draft.js
+++ app/constants/post_draft.js
@@ -1,11 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const MAX_FILE_COUNT = 5;
-export const IS_REACTION_REGEX = /(^\+:([^:\s]*):)$/i;
-export const INSERT_TO_DRAFT = 'insert_to_draft';
-export const INSERT_TO_COMMENT = 'insert_to_comment';
-export const ICON_SIZE = 24;
 export const ACCESSORIES_CONTAINER_NATIVE_ID = 'channelAccessoriesContainer';
 export const CHANNEL_POST_TEXTBOX_CURSOR_CHANGE = 'onChannelTextBoxCursorChange';
 export const CHANNEL_POST_TEXTBOX_VALUE_CHANGE = 'onChannelTextBoxValueChange';
+export const ICON_SIZE = 24;
+export const INSERT_TO_COMMENT = 'insert_to_comment';
+export const INSERT_TO_DRAFT = 'insert_to_draft';
+export const IS_REACTION_REGEX = /(^\+:([^:\s]*):)$/i;
+export const MAX_FILE_COUNT = 5;
+export const MAX_MESSAGE_LENGTH_FALLBACK = 4000;
