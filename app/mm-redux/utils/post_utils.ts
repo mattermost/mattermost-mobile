b6a9bb45246c4925353463da57e4f93f7c372ec8
Martin Kraft <martin@upspin.org>
2020-06-10 14:13:30 -0400

    MM-11713: Default to false expclicitly.

--- app/mm-redux/utils/post_utils.ts
+++ app/mm-redux/utils/post_utils.ts
@@ -87,7 +87,7 @@ export function canEditPost(state: GlobalState, config: any, license: any, teamI
 
     if (hasNewPermissions(state)) {
         const permission = isOwner ? Permissions.EDIT_POST : Permissions.EDIT_OTHERS_POSTS;
-        canEdit = haveIChannelPermission(state, {team: teamId, channel: channelId, permission});
+        canEdit = haveIChannelPermission(state, {team: teamId, channel: channelId, permission, default: false});
         if (license.IsLicensed === 'true' && config.PostEditTimeLimit !== '-1' && config.PostEditTimeLimit !== -1) {
             const timeLeft = (post.create_at + (config.PostEditTimeLimit * 1000)) - Date.now();
             if (timeLeft <= 0) {
