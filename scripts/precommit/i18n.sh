12a16a91ed4bd117c337332355eeeded434e318b
Elisabeth Kulzer <elikul@elikul.de>
2020-08-29 16:15:28 +0200

    Keep consistent

--- scripts/precommit/i18n.sh
+++ scripts/precommit/i18n.sh
@@ -4,9 +4,11 @@ mkdir -p tmp
 cp assets/base/i18n/en.json tmp/en.json
 mkdir -p tmp/fake-webapp-dir/i18n/
 echo '{}' > tmp/fake-webapp-dir/i18n/en.json
+
 npm run mmjstool -- i18n extract-mobile --webapp-dir tmp/fake-webapp-dir --mobile-dir .
 diff tmp/en.json assets/base/i18n/en.json
+# Address weblate behavior which does not remove whole translation item when translation string is set to empty
+npm run mmjstool -- i18n clean-empty --webapp-dir tmp/fake-webapp-dir --mobile-dir . --check
+
 rm -rf tmp
 
-# Address weblate behavior which does not remove whole translation item when translation string is set to empty
-npm run mmjstool -- i18n clean-empty --mobile-dir . --check
