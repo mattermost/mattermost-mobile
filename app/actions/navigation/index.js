a9870bd93c77fba9f109190e5f9f15dc20f9e137
Elias Nahum <nahumhbl@gmail.com>
2020-09-09 10:17:37 -0300

    Do not remove the Channel screen from the stack tracking

--- app/actions/navigation/index.js
+++ app/actions/navigation/index.js
@@ -13,8 +13,6 @@ import EphemeralStore from '@store/ephemeral_store';
 import Store from '@store/store';
 import {NavigationTypes} from '@constants';
 
-const CHANNEL_SCREEN = 'Channel';
-
 function getThemeFromState() {
     const state = Store.redux?.getState() || {};
 
@@ -29,8 +27,8 @@ export function resetToChannel(passProps = {}) {
     const stack = {
         children: [{
             component: {
-                id: CHANNEL_SCREEN,
-                name: CHANNEL_SCREEN,
+                id: NavigationTypes.CHANNEL_SCREEN,
+                name: NavigationTypes.CHANNEL_SCREEN,
                 passProps,
                 options: {
                     layout: {
@@ -427,7 +425,7 @@ export function closeMainSideMenu() {
     }
 
     Keyboard.dismiss();
-    Navigation.mergeOptions(CHANNEL_SCREEN, {
+    Navigation.mergeOptions(NavigationTypes.CHANNEL_SCREEN, {
         sideMenu: {
             left: {visible: false},
         },
@@ -439,7 +437,7 @@ export function enableMainSideMenu(enabled, visible = true) {
         return;
     }
 
-    Navigation.mergeOptions(CHANNEL_SCREEN, {
+    Navigation.mergeOptions(NavigationTypes.CHANNEL_SCREEN, {
         sideMenu: {
             left: {enabled, visible},
         },
@@ -452,7 +450,7 @@ export function openSettingsSideMenu() {
     }
 
     Keyboard.dismiss();
-    Navigation.mergeOptions(CHANNEL_SCREEN, {
+    Navigation.mergeOptions(NavigationTypes.CHANNEL_SCREEN, {
         sideMenu: {
             right: {visible: true},
         },
@@ -465,7 +463,7 @@ export function closeSettingsSideMenu() {
     }
 
     Keyboard.dismiss();
-    Navigation.mergeOptions(CHANNEL_SCREEN, {
+    Navigation.mergeOptions(NavigationTypes.CHANNEL_SCREEN, {
         sideMenu: {
             right: {visible: false},
         },
