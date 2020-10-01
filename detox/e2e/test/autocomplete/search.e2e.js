a8906d919780a188befad23d71c336c4e3971cba
Andre Vasconcelos <andre.onogoro@gmail.com>
2020-10-01 22:42:52 +0900

    Added comments to search E2E test

--- detox/e2e/test/autocomplete/search.e2e.js
+++ detox/e2e/test/autocomplete/search.e2e.js
@@ -13,23 +13,31 @@ describe('Autocomplete', () => {
 
     beforeEach(async () => {
         await device.reloadReactNative();
+
+        // # Enter search screen
         await element(by.id('channel.search.button')).tap();
     });
 
     it('should render at_mention component', async () => {
         await expect(element(by.id('autocomplete.at_mention.list'))).not.toExist();
+
+        // # Tap "from:" modifier
         await element(by.id('search_from')).tap();
         await expect(element(by.id('autocomplete.at_mention.list'))).toExist();
     });
 
     it('should render channel_mention component', async () => {
         await expect(element(by.id('autocomplete.channel_mention.list'))).not.toExist();
+
+        // # Tap "in:" modifier
         await element(by.id('search_in')).tap();
         await expect(element(by.id('autocomplete.channel_mention.list'))).toExist();
     });
 
     it('should render date_suggestion component', async () => {
         await expect(element(by.id('autocomplete_date_suggestion'))).not.toExist();
+
+        // # Tap "before:" modifier
         await element(by.id('search_before')).tap();
         await expect(element(by.id('autocomplete_date_suggestion'))).toExist();
     });
