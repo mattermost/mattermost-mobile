66a75fca487882d94312aca994420425fefa5f47
hectorgabucio <hectorcompangabucio@gmail.com>
2020-10-04 12:20:03 +0200

    MM-T3201 spaces and more comments

--- detox/e2e/test/channels/create_public_channel.e2e.js
+++ detox/e2e/test/channels/create_public_channel.e2e.js
@@ -13,19 +13,25 @@ describe('Channels', () => {
     });
 
     it('MM-T3201 Create public channel', async () => {
+        // go to channel sidebar list
         await element(by.id('channel_drawer_button')).tap();
+
+        // tap on + public channels
         await element(by.id('action_button_sidebar.channels')).tap();
 
         // expect a list of public channels, initially empty
         await expect(element(by.text('No more channels to join'))).toBeVisible();
 
+        // tap to create new channel
         await element(by.text('CREATE')).tap();
 
         // expect a new screen to create a new public channel
         await expect(element(by.text('New Public Channel'))).toBeVisible();
 
+        // fill data
         await element(by.id('input_channel_name')).typeText('a');
         await element(by.text('CREATE')).tap();
+
         // the `Create` button is not tappable until at least 2 characters have been entered
         await expect(element(by.id('input_channel_name'))).toBeVisible();
 
@@ -33,9 +39,7 @@ describe('Channels', () => {
         await element(by.id('input_channel_purpose')).typeText('This sentence has');
         await element(by.id('input_channel_purpose')).tapReturnKey();
         await element(by.id('input_channel_purpose')).typeText('multiple lines');
-
         await element(by.id('channel_scroll')).scroll(200, 'down');
-
         await expect(element(by.id('input_channel_header'))).toBeVisible();
         const expectedChannelHeader = 'I 🌮 love 🌮 tacos 🌮';
         await element(by.id('input_channel_header')).replaceText(expectedChannelHeader);
@@ -44,6 +48,7 @@ describe('Channels', () => {
 
         const expectedChannelName = 'abc';
         const expectedPurpose = 'This sentence has\nmultiple lines';
+
         // expect a redirection to the created channel
         await expect(element(by.text('Beginning of ' + expectedChannelName))).toBeVisible();
         await element(by.text(expectedChannelName)).tap();
