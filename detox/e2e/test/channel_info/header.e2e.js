042405d0ab06ad1f6a46110bdddbb29150a0ac2d
Elias Nahum <nahumhbl@gmail.com>
2020-10-13 19:14:43 -0300

    Apply suggestions from code review
    Co-authored-by: Joseph Baylon <joseph.baylon@mattermost.com>

--- detox/e2e/test/channel_info/header.e2e.js
+++ detox/e2e/test/channel_info/header.e2e.js
@@ -16,32 +16,33 @@ describe('Channel Info Header', () => {
         await logoutUser();
     });
 
-    it('MM-T28539 should render autocomplete in channel header edit screen', async () => {
-        // Open drawer
+    it('MM-T3406 should render correct GM member count in channel info header', async () => {
+        // # Open drawer
         await element(by.id('channel_drawer.button')).tap();
         await expect(element(by.text('DIRECT MESSAGES'))).toBeVisible();
 
-        // Open Direct Channels screen
+        // # Open Direct Channels screen
         await element(by.id('sidebar.channel_list.create_direct')).tap();
         await expect(element(by.id('direct_channels_screen'))).toBeVisible();
 
-        // Wait for some profiles to load
+        // # Wait for some profiles to load
         await wait(timeouts.ONE_SEC);
 
-        // Select two profiles
+        // # Select 3 profiles
         await element(by.id('more_dms.user').withAncestor(by.id('more_dms.list'))).atIndex(0).tap();
         await element(by.id('more_dms.user').withAncestor(by.id('more_dms.list'))).atIndex(1).tap();
         await element(by.id('more_dms.user').withAncestor(by.id('more_dms.list'))).atIndex(2).tap();
 
-        // Create a GM with selected profiles
+        // # Create a GM with selected profiles
         await element(by.id('start-conversation')).tap();
 
         // # Open channel info modal
         await element(by.id('channel.title.button')).tap();
 
+        // * Verify GM member count is 3
         await expect(element(by.id('channel_icon.gm_member_count'))).toHaveText('3');
 
-        // close channel info screen
+        // # Close channel info screen
         await element(by.id('screen.channel_info.close')).tap();
     });
 });
