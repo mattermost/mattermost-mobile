1a554c3e57321b7f0e616d90a6a1cc2b058e4da2
edtrist <edtrist@users.noreply.github.com>
2020-11-07 20:56:17 +0000

    Select first ordered team in user preference on login

--- app/utils/teams.js
+++ app/utils/teams.js
@@ -2,14 +2,16 @@
 // See LICENSE.txt for license information.
 // @flow
 
-export function selectFirstAvailableTeam(teams, primaryTeamName) {
+import {sortTeamsByUserPreference} from '@mm-redux/utils/team_utils';
+
+export function selectFirstAvailableTeam(teams, locale, teamsOrder = '', primaryTeamName) {
     let defaultTeam;
     if (primaryTeamName) {
         defaultTeam = teams.find((t) => t?.name === primaryTeamName.toLowerCase());
     }
 
     if (!defaultTeam) {
-        defaultTeam = Object.values(teams).sort((a, b) => a.display_name.localeCompare(b.display_name))[0];
+        defaultTeam = sortTeamsByUserPreference(teams, locale, teamsOrder)[0];
     }
 
     return defaultTeam;
