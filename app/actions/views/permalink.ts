fda66be10c995d0f29db056bc0bd8f078ec6cb40
Anurag Shivarathri <anurag6713@gmail.com>
2020-12-02 18:42:36 +0530

    Added error messages

--- app/actions/views/permalink.ts
+++ app/actions/views/permalink.ts
@@ -23,64 +23,92 @@ export let showingPermalink = false;
 export function showPermalink(intl: typeof intlShape, teamName: string, postId: string, openAsPermalink = true) {
     return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
         const loadTeam = await dispatch(loadChannelsByTeamName(teamName, permalinkBadTeam.bind(null, intl)));
-        if (!loadTeam.error) {
-            Keyboard.dismiss();
+        if (loadTeam.error) {
+            Alert.alert(
+                intl.formatMessage({
+                    id: 'mobile.failed_network_action.teams_channel_description',
+                    defaultMessage: 'Channels could not be loaded for {teamName}.',
+                }, {teamName})
+            );
+            return null;
+        }
+        Keyboard.dismiss();
 
-            // Warn before joining private channels through permalink
-            const state = getState();
-            const currentUserId = getCurrentUserId(state);
-            const roles = currentUserId ? getCurrentUserRoles(state) : '';
-            if (isSystemAdmin(roles)) {
-                const postData = await dispatch(getPost(postId));
-                if (postData.error) {
-                    return {error: postData.error};
-                }
-                const channelId = postData.data?.channel_id;
-                if (channelId) {
-                    const {data: isMember} = isChannelMember(state, channelId);
-                    if (!isMember) {
-                        const {data: channelData, error: channelError} = await dispatch(getChannel(channelId));
-                        if (channelError) {
-                            return {error: channelError};
-                        }
-                        if (channelData.type === General.PRIVATE_CHANNEL) {
-                            Alert.alert(
-                                intl.formatMessage({
-                                    id: 'permalink.show_dialog_warn.title',
-                                    defaultMessage: 'Private Channel',
-                                }),
-                                intl.formatMessage({
-                                    id: 'permalink.show_dialog_warn.description',
-                                    defaultMessage: 'You must join "{channel}" to view this post.',
-                                }, {
-                                    channel: channelData.name,
-                                }),
-                                [
-                                    {
-                                        text: intl.formatMessage({
-                                            id: 'permalink.show_dialog_warn.cancel',
-                                            defaultMessage: 'Cancel',
-                                        }),
-                                    },
-                                    {
-                                        text: intl.formatMessage({
-                                            id: 'permalink.show_dialog_warn.join',
-                                            defaultMessage: 'Join',
-                                        }),
-                                        onPress: async () => {
-                                            await dispatch(joinChannel(currentUserId, teamName, channelId, channelData.name));
+        // Warn before joining private channels through permalink
+        const state = getState();
+        const currentUserId = getCurrentUserId(state);
+        const roles = currentUserId ? getCurrentUserRoles(state) : '';
+        if (isSystemAdmin(roles)) {
+            const postData = await dispatch(getPost(postId));
+            if (postData.error) {
+                Alert.alert(
+                    intl.formatMessage({
+                        id: 'permalink.unable_to_get_post',
+                        defaultMessage: 'Unable to get the post data.',
+                    }, {teamName})
+                );
+                return {error: postData.data};
+            }
+            const channelId = postData.data?.channel_id;
+            if (channelId) {
+                const {data: isMember} = isChannelMember(state, channelId);
+                if (!isMember) {
+                    const {data: channelData, error: channelError} = await dispatch(getChannel(channelId));
+                    if (channelError) {
+                        Alert.alert(
+                            intl.formatMessage({
+                                id: 'permalink.unable_to_get_channel',
+                                defaultMessage: 'Unable to get the channel data.',
+                            }, {teamName})
+                        );
+                        return {error: channelError};
+                    }
+                    if (channelData.type === General.PRIVATE_CHANNEL) {
+                        Alert.alert(
+                            intl.formatMessage({
+                                id: 'permalink.show_dialog_warn.title',
+                                defaultMessage: 'Private Channel',
+                            }),
+                            intl.formatMessage({
+                                id: 'permalink.show_dialog_warn.description',
+                                defaultMessage: 'You must join "{channel}" to view this post.',
+                            }, {
+                                channel: channelData.name,
+                            }),
+                            [
+                                {
+                                    text: intl.formatMessage({
+                                        id: 'permalink.show_dialog_warn.cancel',
+                                        defaultMessage: 'Cancel',
+                                    }),
+                                },
+                                {
+                                    text: intl.formatMessage({
+                                        id: 'permalink.show_dialog_warn.join',
+                                        defaultMessage: 'Join',
+                                    }),
+                                    onPress: async () => {
+                                        const joinResponse = await dispatch(joinChannel(currentUserId, teamName, channelId, channelData.name));
+                                        if(joinResponse.error) {
+                                            Alert.alert(
+                                                intl.formatMessage({
+                                                    id: 'permalink.unable_to_jon_channel',
+                                                    defaultMessage: 'Unable to join the channel',
+                                                })
+                                            );
+                                        } else {
                                             dispatch(processShowPermaLink(postId, openAsPermalink));
-                                        },
+                                        }
                                     },
-                                ],
-                            );
-                            return null;
-                        }
+                                },
+                            ],
+                        );
+                        return null;
                     }
                 }
             }
-            dispatch(processShowPermaLink(postId, openAsPermalink));
         }
+        dispatch(processShowPermaLink(postId, openAsPermalink));
         return null;
     };
 }
