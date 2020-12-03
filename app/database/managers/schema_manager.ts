1feb8f1aeef650df5839e0de7dbb8f03f624d854
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-03 22:42:25 +0400

    MM_30478 : Returning an sqliteAdapter instead of an object

--- app/database/managers/schema_manager.ts
+++ app/database/managers/schema_manager.ts
@@ -1,23 +1,27 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {SQLiteAdapterOptions} from '@nozbe/watermelondb/adapters/sqlite';
-import {AppSchema} from '@nozbe/watermelondb';
-import {Migration, schemaMigrations} from '@nozbe/watermelondb/Schema/migrations';
-import {MigrationEvents} from '@typings/database';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+import {schemaMigrations} from '@nozbe/watermelondb/Schema/migrations';
+import {defaultSchema} from '../default/schema';
 
 /**
  * Creates an adapter options object that will be used by the database manager.
  * @param {string} dbPath - The dbName or filePath (e.g. AppGroup path) for the database
- * @param {AppSchema} schema - The schema to be used for this connection
+ * @param {import type {AppSchema} from '@nozbe/watermelondb'} schema - The schema to be used for this connection
  * @param {Migration} migrationSteps - The migration steps if any for this database
  * @param {MigrationEvents} migrationEvents - The migrationEvent object housing all the callbacks for the migration status onSuccess, onFailure and onStarted.
  */
-export const createSqliteAdaptorOptions = (dbPath: string, schema: AppSchema, migrationSteps?: Migration [], migrationEvents?: MigrationEvents):SQLiteAdapterOptions => {
-    return {
+export const createSqliteAdaptor = ({
+    dbPath = '',
+    migrationEvents = undefined,
+    migrationSteps = [],
+    schema = defaultSchema,
+}) => {
+    return new SQLiteAdapter({
         schema,
         dbName: dbPath,
-        ...(migrationSteps && migrationSteps.length > 0 && {migrations: schemaMigrations({migrations: migrationSteps})}),
-        ...(migrationEvents && {migrationEvents}),
-    };
+        ...(Boolean(migrationEvents) && {migrationEvents}),
+        ...(Boolean(migrationSteps) && migrationSteps!.length > 0 && {migrations: schemaMigrations({migrations: migrationSteps!})}),
+    });
 };
