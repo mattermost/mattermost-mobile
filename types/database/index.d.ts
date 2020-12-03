a40210f685e753fd82077732c673e8da4978fe79
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-03 21:30:16 +0400

    MM_30482 : Added tests for schema_manager

--- types/database/index.d.ts
+++ types/database/index.d.ts
@@ -1,7 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+import {Migration} from '@nozbe/watermelondb/Schema/migrations';
+import {AppSchema} from '@nozbe/watermelondb';
+
 export type MigrationEvents = {
     onSuccess?: () => void,
     onStarted?: () => void,
     onFailure?: (error: string) => void,
+}
+
+export type MMAdaptorOptions = {
+    dbPath : string,
+    schema: AppSchema,
+    migrationSteps?: Migration [],
+    migrationEvents?: MigrationEvents
 }
