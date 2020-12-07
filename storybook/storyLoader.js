794a556c2caf14216434fcc04c16351f98eef2cb
Ashish Bhate <ashish.bhate@mattermost.com>
2020-12-07 16:34:22 +0530

    fix storyloader formatting

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -1,19 +1,17 @@
-// Auto-generated file created by react-native-storybook-loader
-// Do not edit.
-//
-// https://github.com/elderfo/react-native-storybook-loader.git
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
 function loadStories() {
-  require("../app/components/avatars/avatars.stories");
-  require("../app/components/loading.stories");
+    require('../app/components/avatars/avatars.stories');
+    require('../app/components/loading.stories');
 }
 
 const stories = [
-  "../app/components/avatars/avatars.stories",
-  "../app/components/loading.stories",
+    '../app/components/avatars/avatars.stories',
+    '../app/components/loading.stories',
 ];
 
 module.exports = {
-  loadStories,
-  stories,
+    loadStories,
+    stories,
 };
