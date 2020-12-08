e84deb59a94100f121e5b69ae05ae239c4e5b13d
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-08 20:44:43 +0400

    MM_30476 : ADDED documentations for default schema

--- app/database/default/models/app.ts
+++ app/database/default/models/app.ts
@@ -7,10 +7,21 @@ import {field} from '@nozbe/watermelondb/decorators';
 import {MM_TABLES} from '@constants/database';
 
 const {APP} = MM_TABLES.DEFAULT;
+
+/**
+ * The App model will hold information - such as the version number, build number and creation date -
+ * for the Mattermost mobile app.
+ */
 export default class App extends Model {
+    // table (entity name) : app
     static table = APP
 
+    // build_number : Build number for the app
     @field('build_number') buildNumber!: string
+
+    // created_at : Date of creation for this version
     @field('created_at') createdAt!: number
+
+    // version_number : Version number for the app
     @field('version_number') versionNumber!: string
 }
--- app/database/default/models/global.ts
+++ app/database/default/models/global.ts
@@ -8,13 +8,20 @@ import {MM_TABLES} from '@constants/database';
 
 const {GLOBAL} = MM_TABLES.DEFAULT;
 
+// TODO : add TS definitions to sanitizer function signature.
+// TODO : atm, the return type for 'value' is string[].  However, this return type can change to string/number/etc.  A broader definition will need to be applied and this return type updated accordingly.
+
+/**
+ * The Global model will act as a dictionary of name-value pairs.  The value field can be a JSON object or any other
+ * data type.
+ */
 export default class Global extends Model {
+    // table (entity name) : global
     static table = GLOBAL
 
+    // name : The label/key to use to retrieve the special 'value'
     @field('name') name!: string
 
-    // TODO : add TS definitions to sanitizer function signature.
-
-    // TODO : atm, the return type for 'value' is string[].  However, this return type can change to string/number/etc.  A broader definition will need to be applied and this return type updated accordingly.
+    // value : The value part of the key-value combination
     @json('value', (rawJson) => rawJson) value!: string[]
 }
