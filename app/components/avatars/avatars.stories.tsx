c3444ccf72547cd10b712609b299b9fe6f7d83e0
Ashish Bhate <ashish.bhate@mattermost.com>
2020-12-18 17:01:53 +0530

    use state instead of hardcoding profiles in story

--- app/components/avatars/avatars.stories.tsx
+++ app/components/avatars/avatars.stories.tsx
@@ -1,19 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react';
-
 import {storiesOf} from '@storybook/react-native';
-import {withKnobs, array, number, select} from '@storybook/addon-knobs';
+import {withKnobs, number, select} from '@storybook/addon-knobs';
+
 import {Preferences} from '@mm-redux/constants';
+import {UserProfile} from '@mm-redux/types/users';
+import {getProfiles} from '@mm-redux/selectors/entities/users';
+import Store from '@store/store';
 
 import Avatars from './avatars';
+
+const state = Store.redux?.getState();
+const users = getProfiles(state, {});
+const userIds = users.map((user:UserProfile) => user.id);
 
 storiesOf('Avatars', module).
     addDecorator(withKnobs).
     add('Avatars', () => (
         <Avatars
             size={number('size', 24, {range: true, min: 16, max: 80, step: 8})}
-            userIds={array('userIds', ['cb5wff5ah3y8ifijsgwrdhnn9o', 'psd3g9p9sjykuxqijh9qzmh86w', 'ue6q4zzgntykikm6f6fy5tju8r', 'iq164nrrmjff5mtbafr4jpukcy'], ',')}
+            userIds={userIds.slice(0, number(`number of named participants (max ${userIds.length})`, userIds.length))}
             theme={select('theme', Preferences.THEMES, Preferences.THEMES.default)}
         />
     ));
