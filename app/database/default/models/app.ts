cc02c9b8cb4c8b700db1a0fbaeecb409425199c5
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-29 23:01:54 +0400

    MM_30476 : Updated all models and typings for the default server (#5069)

--- app/database/default/models/app.ts
+++ app/database/default/models/app.ts
@@ -2,13 +2,26 @@
 // See LICENSE.txt for license information.
 
 import {Model} from '@nozbe/watermelondb';
+import {field} from '@nozbe/watermelondb/decorators';
+
 import {MM_TABLES} from '@constants/database';
-import field from '@nozbe/watermelondb/decorators/field';
 
+const {APP} = MM_TABLES.DEFAULT;
+
+/**
+ * The App model will hold information - such as the version number, build number and creation date -
+ * for the Mattermost mobile app.
+ */
 export default class App extends Model {
-    static table = MM_TABLES.DEFAULT.APP
+    /** table (entity name) : app */
+    static table = APP;
 
-    @field('build_number') buildNumber!: string
-    @field('created_at') createdAt!: number
-    @field('version_number') versionNumber!: string
+    /** build_number : Build number for the app */
+    @field('build_number') buildNumber!: string;
+
+    /** created_at : Date of creation for this version */
+    @field('created_at') createdAt!: number;
+
+    /** version_number : Version number for the app */
+    @field('version_number') versionNumber!: string;
 }
---
+++ types/database/servers.d.ts
@@ -0,0 +1,28 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {Model} from '@nozbe/watermelondb';
+
+/**
+ * The Server model will help us to identify the various servers a user will log in; in the context of
+ * multi-server support system.  The dbPath field will hold the App-Groups file-path
+ */
+export default class Servers extends Model {
+    /** table (entity name) : servers */
+    static table: string;
+
+    /** db_path : The file path where the database is stored */
+    dbPath: string;
+
+    /** display_name : The server display name */
+    displayName: string;
+
+    /** mention_count : The number of mention on this server */
+    mentionCount: number;
+
+    /** unread_count : The number of unread messages on this server */
+    unreadCount: number;
+
+    /** url : The online address for the Mattermost server */
+    url: string;
+}
