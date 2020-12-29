0d2868e4f2b65f61c0f4cdfd5f38d259d1c885f9
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-29 22:49:07 +0400

    MM_30476 : Updated Babel configs (#5068)

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,36 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: [
+        'module:metro-react-native-babel-preset',
+        '@babel/preset-typescript',
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+    ],
     env: {
         production: {
             plugins: ['transform-remove-console'],
         },
     },
     plugins: [
-        '@babel/plugin-transform-runtime',
         ['@babel/plugin-proposal-decorators', {legacy: true}],
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime',
         ['module-resolver', {
             root: ['.'],
             alias: {
-                '@assets': './dist/assets',
                 '@actions': './app/actions',
+                '@assets': './dist/assets',
                 '@components': './app/components',
                 '@constants': './app/constants',
                 '@i18n': './app/i18n',
                 '@init': './app/init',
                 '@notifications': './app/notifications',
-                '@share': './share_extension',
                 '@screens': './app/screens',
                 '@selectors': './app/selectors',
+                '@share': './share_extension',
                 '@store': './app/store',
                 '@telemetry': './app/telemetry',
+                '@typings': './types',
                 '@utils': './app/utils',
                 '@websocket': './app/client/websocket',
             },
