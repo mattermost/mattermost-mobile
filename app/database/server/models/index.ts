96a36225f4a0c0830401dd8fea110995d20083f6
Avinash Lingaloo <avinashlng1080@gmail.com>
2020-12-30 14:31:26 +0400

    MM_30476 : Added the models index back

---
+++ app/database/server/models/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export {default as ChannelMembership} from './channel_membership';
+export {default as CustomEmoji} from './custom_emoji';
+export {default as Preference} from './preference';
+export {default as Reaction} from './reaction';
+export {default as Role} from './role';
+export {default as System} from './system';
+export {default as TermsOfService} from './terms_of_service';
+export {default as User} from './user';
