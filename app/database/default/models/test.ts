f5592e8f1930c948a7ba214f377a0f9ea20eacc6
Avinash Lingaloo <avinashlng1080@gmail.com>
2021-01-05 18:34:51 +0400

    MM_30476 : ADDED tests for models and schema

--- app/database/default/models/test.ts
+++ app/database/default/models/test.ts
@@ -13,7 +13,6 @@ const {APP, GLOBAL, SERVERS} = MM_TABLES.DEFAULT;
 function makeDatabase({actionsEnabled = false} = {}) {
     return new Database({
 
-        // @ts-ignore
         adapter: {schema: defaultSchema},
         modelClasses: [App, Global, Servers],
         actionsEnabled,
@@ -24,8 +23,6 @@ function genericModelTests(model: Model, tableName: string, database: Database) 
     describe(`=> Testing Model : ${model.constructor.name}`, () => {
         it(`the value of the properties "db" and "database" should be similar for model ${model.constructor.name}`, async () => {
             expect(model.database).toBe(database);
-
-            // @ts-ignore
             expect(model.db).toBe(database);
         });
         it(`the database collection and the model's collection should be similar for model ${model.constructor.name}`, () => {
@@ -38,14 +35,10 @@ function genericModelTests(model: Model, tableName: string, database: Database) 
 describe('*** Testing DEFAULT Models ***', () => {
     const database = makeDatabase();
 
-    // @ts-ignore
     const appModel = new App(database.get(APP), {});
-
-    // @ts-ignore
     const globalModel = new Global(database.get(GLOBAL), {});
-
-    // @ts-ignore
     const serverModel = new Servers(database.get(SERVERS), {});
+
     genericModelTests(appModel, APP, database);
     genericModelTests(globalModel, GLOBAL, database);
     genericModelTests(serverModel, SERVERS, database);
