4fe1000e17aca6c6441383539e6e056e5e4e3ac8
Elias Nahum <nahumhbl@gmail.com>
2021-01-13 11:16:06 -0300

    MM-31764 Fix image attachment layout when permanent sidebar is visible

--- app/init/device.ts
+++ app/init/device.ts
@@ -4,10 +4,12 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import {DeviceTypes} from 'app/constants';
 
-if (DeviceTypes.IS_TABLET) {
-    AsyncStorage.getItem(DeviceTypes.PERMANENT_SIDEBAR_SETTINGS).then((value) => {
+export async function setupPermanentSidebar() {
+    if (DeviceTypes.IS_TABLET) {
+        const value = await AsyncStorage.getItem(DeviceTypes.PERMANENT_SIDEBAR_SETTINGS);
+
         if (!value) {
             AsyncStorage.setItem(DeviceTypes.PERMANENT_SIDEBAR_SETTINGS, 'true');
         }
-    });
+    }
 }
