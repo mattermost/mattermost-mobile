a182cab48fe68fb9c911342dc4a60ddda90ffce6
Anurag Shivarathri <anurag6713@gmail.com>
2021-01-28 21:07:05 +0530

    Moved code to tapping_channel_url_link_joins_channel and refactored code

--- detox/e2e/support/ui/component/alert.js
+++ detox/e2e/support/ui/component/alert.js
@@ -6,10 +6,12 @@ import {isAndroid} from '@support/utils';
 class Alert {
     // alert titles
     deletePostTitle = isAndroid() ? element(by.text('Delete Post')) : element(by.label('Delete Post')).atIndex(0);
+    joinPrivateChannelTitle = isAndroid() ? element(by.text('Join private channel')) : element(by.label('Join private channel')).atIndex(0);
 
     // alert buttons
     cancelButton = isAndroid() ? element(by.text('CANCEL')) : element(by.label('Cancel')).atIndex(0);
     deleteButton = isAndroid() ? element(by.text('DELETE')) : element(by.label('Delete')).atIndex(0);
+    joinButton = isAndroid() ? element(by.text('JOIN')) : element(by.label('Join')).atIndex(0);
 }
 
 const alert = new Alert();
