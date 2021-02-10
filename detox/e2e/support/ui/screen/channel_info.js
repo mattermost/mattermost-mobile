acd2b09fa8ea0f6dd290f58e41ad7c154fdc88f8
Joseph Baylon <joseph.baylon@mattermost.com>
2021-02-10 13:50:44 -0800

    MM-30698 Detox/E2E: Add e2e for MM-T3191 (#5137)
    * MM-30698 Detox/E2E: Add e2e for MM-T3191
    
    * Fixed comment

--- detox/e2e/support/ui/screen/channel_info.js
+++ detox/e2e/support/ui/screen/channel_info.js
@@ -10,8 +10,14 @@ class ChannelInfoScreen {
         closeChannelInfoButton: 'close.channel_info.button',
         channelIconGMMemberCount: 'channel_icon.gm_member_count',
         favoritePreferenceAction: 'channel_info.favorite.action',
+        favoriteSwitchFalse: 'channel_info.favorite.action.switch.false',
+        favoriteSwitchTrue: 'channel_info.favorite.action.switch.true',
         mutePreferenceAction: 'channel_info.mute.action',
+        muteSwitchFalse: 'channel_info.mute.action.switch.false',
+        muteSwitchTrue: 'channel_info.mute.action.switch.true',
         ignoreMentionsPreferenceAction: 'channel_info.ignore_mentions.action',
+        ignoreMentionsSwitchFalse: 'channel_info.ignore_mentions.switch.false',
+        ignoreMentionsSwitchTrue: 'channel_info.ignore_mentions.switch.true',
         notificationPreferenceAction: 'channel_info.notification_preference.action',
         pinnedMessagesAction: 'channel_info.pinned_messages.action',
         manageMembersAction: 'channel_info.manage_members.action',
@@ -24,8 +30,14 @@ class ChannelInfoScreen {
     closeChannelInfoButton = element(by.id(this.testID.closeChannelInfoButton));
     channelIconGMMemberCount = element(by.id(this.testID.channelIconGMMemberCount));
     favoritePreferenceAction = element(by.id(this.testID.favoritePreferenceAction));
+    favoriteSwitchFalse = element(by.id(this.testID.favoriteSwitchFalse));
+    favoriteSwitchTrue = element(by.id(this.testID.favoriteSwitchTrue));
     mutePreferenceAction = element(by.id(this.testID.mutePreferenceAction));
+    muteSwitchFalse = element(by.id(this.testID.muteSwitchFalse));
+    muteSwitchTrue = element(by.id(this.testID.muteSwitchTrue));
     ignoreMentionsPreferenceAction = element(by.id(this.testID.ignoreMentionsPreferenceAction));
+    ignoreMentionsSwitchTrue = element(by.id(this.testID.ignoreMentionsSwitchTrue));
+    muteSwitchTrue = element(by.id(this.testID.muteSwitchTrue));
     notificationPreferenceAction = element(by.id(this.testID.notificationPreferenceAction));
     pinnedMessagesAction = element(by.id(this.testID.pinnedMessagesAction));
     manageMembersAction = element(by.id(this.testID.manageMembersAction));
