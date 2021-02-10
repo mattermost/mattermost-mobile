b716a9ca8d22923217a728551f2b7a7a4eb4b6c0
Joseph Baylon <joseph.baylon@mattermost.com>
2021-02-10 13:55:43 -0800

    MM-31256 Detox/E2E: Add e2e for MM-T3618 (#5144)

--- detox/e2e/support/ui/component/alert.js
+++ detox/e2e/support/ui/component/alert.js
@@ -5,11 +5,14 @@ import {isAndroid} from '@support/utils';
 
 class Alert {
     // alert titles
+    archivePublicChannelTitle = isAndroid() ? element(by.text('Archive Public Channel')) : element(by.label('Archive Public Channel')).atIndex(0);
     deletePostTitle = isAndroid() ? element(by.text('Delete Post')) : element(by.label('Delete Post')).atIndex(0);
 
     // alert buttons
     cancelButton = isAndroid() ? element(by.text('CANCEL')) : element(by.label('Cancel')).atIndex(0);
     deleteButton = isAndroid() ? element(by.text('DELETE')) : element(by.label('Delete')).atIndex(0);
+    noButton = isAndroid() ? element(by.text('NO')) : element(by.label('No')).atIndex(0);
+    yesButton = isAndroid() ? element(by.text('YES')) : element(by.label('Yes')).atIndex(0);
 }
 
 const alert = new Alert();
