6ad405e975e5d7a7a8f35135a4a62ca7854e34d6
Elias Nahum <nahumhbl@gmail.com>
2021-02-17 11:31:30 -0300

    android: Set the app to run at the highest available fps

--- android/app/src/main/java/com/mattermost/rnbeta/MainActivity.java
+++ android/app/src/main/java/com/mattermost/rnbeta/MainActivity.java
@@ -1,12 +1,22 @@
 package com.mattermost.rnbeta;
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+
+import android.view.Display;
 import android.view.KeyEvent;
 import android.content.res.Configuration;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.reactnativenavigation.NavigationActivity;
 import com.github.emilioicai.hwkeyboardevent.HWKeyboardEventModule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
 
 public class MainActivity extends NavigationActivity {
     private boolean HWKeyboardConnected = false;
@@ -14,6 +24,7 @@ public class MainActivity extends NavigationActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFrameRate();
         setContentView(R.layout.launch_screen);
         setHWKeyboardConnected();
     }
@@ -46,5 +57,49 @@ public class MainActivity extends NavigationActivity {
 
     private void setHWKeyboardConnected() {
         HWKeyboardConnected = getResources().getConfiguration().keyboard == Configuration.KEYBOARD_QWERTY;
+    }
+
+    protected void setFrameRate() {
+        ArrayList<HashMap<String, Object>> supported = getSupportedModes();
+        boolean seen = false;
+        HashMap<String, Object> best = null;
+        for (HashMap<String, Object> stringObjectHashMap : supported) {
+            if (!seen || (Float)stringObjectHashMap.getOrDefault("refreshRate", 0f) > (Float)best.getOrDefault("refreshRate", 0f)) {
+                seen = true;
+                best = stringObjectHashMap;
+            }
+        }
+
+        if (best != null && best.get("id") != null) {
+            final Window window = this.getWindow();
+            final WindowManager.LayoutParams params = window.getAttributes();
+            params.preferredDisplayModeId =(int)best.get("id");
+            window.setAttributes(params);
+        }
+    }
+
+    protected ArrayList<HashMap<String, Object>> getSupportedModes() {
+        final ArrayList<HashMap<String, Object>> ret = new ArrayList<>();
+        final WindowManager windowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        final Display display = windowManager.getDefaultDisplay();
+        final Display.Mode[] modes = display.getSupportedModes();
+        if (modes == null) {
+            return ret;
+        }
+
+        final Window window = this.getWindow();
+        final WindowManager.LayoutParams params = window.getAttributes();
+        final int selectedMode = params.preferredDisplayModeId;
+
+        for (final Display.Mode mode : modes) {
+            final HashMap<String, Object> item = new HashMap<>();
+            item.put("id", mode.getModeId());
+            item.put("width", mode.getPhysicalWidth());
+            item.put("height", mode.getPhysicalHeight());
+            item.put("refreshRate", mode.getRefreshRate());
+            item.put("selected", mode.getModeId() == selectedMode);
+            ret.add(item);
+        }
+        return ret;
     }
 }
