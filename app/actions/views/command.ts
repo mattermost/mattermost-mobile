84d844dfc80b0f7ee9df7a04389600b1266e3813
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2021-03-08 10:43:58 -0500

    typescript actions/command.ts

--- app/actions/views/command.ts
+++ app/actions/views/command.ts
@@ -1,18 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {intlShape} from 'react-intl';
+
 import {IntegrationTypes} from '@mm-redux/action_types';
 import {executeCommand as executeCommandService} from '@mm-redux/actions/integrations';
 import {getCurrentTeamId} from '@mm-redux/selectors/entities/teams';
 import {AppCallTypes} from '@mm-redux/constants/apps';
+import {DispatchFunc, GetStateFunc, ActionFunc} from '@mm-redux/types/actions';
 
 import {AppCommandParser} from '@components/autocomplete/slash_suggestion/app_command_parser/app_command_parser';
 
 import {doAppCall} from '@actions/apps';
 import {appsEnabled} from '@utils/apps';
 
-export function executeCommand(message, channelId, rootId) {
-    return async (dispatch, getState) => {
+export function executeCommand(message: string, channelId: string, rootId: string, intl: typeof intlShape): ActionFunc {
+    return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
         const state = getState();
 
         const teamId = getCurrentTeamId(state);
@@ -34,9 +37,9 @@ export function executeCommand(message, channelId, rootId) {
         const cmd = msg.substring(0, cmdLength).toLowerCase();
         msg = cmd + msg.substring(cmdLength, msg.length);
 
-        const appsAreEnabled = appsEnabled(getState());
+        const appsAreEnabled = appsEnabled(state);
         if (appsAreEnabled) {
-            const parser = new AppCommandParser(null, args.root_id, args.channel_id);
+            const parser = new AppCommandParser({dispatch, getState}, args.channel_id, args.root_id);
             if (parser.isAppCommand(msg)) {
                 const call = await parser.composeCallFromCommand(message);
                 if (!call) {
@@ -44,7 +47,7 @@ export function executeCommand(message, channelId, rootId) {
                 }
 
                 call.type = AppCallTypes.SUBMIT;
-                const res = await dispatch(doAppCall(call));
+                const res = await dispatch(doAppCall(call, intl));
                 if (res?.data?.error) {
                     return {error: {message: res.data.error}};
                 }
