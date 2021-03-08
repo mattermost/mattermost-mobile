832790bfca84cfdbddf5d019ef455093550810bf
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2021-03-08 09:54:18 -0500

    wrap appsEnabled

--- app/actions/views/command.js
+++ app/actions/views/command.js
@@ -9,6 +9,7 @@ import {AppCallTypes} from '@mm-redux/constants/apps';
 import {AppCommandParser} from '@components/autocomplete/slash_suggestion/app_command_parser/app_command_parser';
 
 import {doAppCall} from '@actions/apps';
+import {appsEnabled} from '@utils/apps';
 
 export function executeCommand(message, channelId, rootId) {
     return async (dispatch, getState) => {
@@ -33,19 +34,22 @@ export function executeCommand(message, channelId, rootId) {
         const cmd = msg.substring(0, cmdLength).toLowerCase();
         msg = cmd + msg.substring(cmdLength, msg.length);
 
-        const parser = new AppCommandParser(null, args.root_id, args.channel_id);
-        if (parser.isAppCommand(msg)) {
-            const call = await parser.composeCallFromCommand(message);
-            if (!call) {
-                return {error: {message: 'Error submitting command'}};
-            }
+        const appsAreEnabled = appsEnabled(getState());
+        if (appsAreEnabled) {
+            const parser = new AppCommandParser(null, args.root_id, args.channel_id);
+            if (parser.isAppCommand(msg)) {
+                const call = await parser.composeCallFromCommand(message);
+                if (!call) {
+                    return {error: {message: 'Error submitting command'}};
+                }
 
-            call.type = AppCallTypes.SUBMIT;
-            const res = await dispatch(doAppCall(call));
-            if (res?.data?.error) {
-                return {error: {message: res.data.error}};
+                call.type = AppCallTypes.SUBMIT;
+                const res = await dispatch(doAppCall(call));
+                if (res?.data?.error) {
+                    return {error: {message: res.data.error}};
+                }
+                return res;
             }
-            return res;
         }
 
         const {data, error} = await dispatch(executeCommandService(msg, args));
