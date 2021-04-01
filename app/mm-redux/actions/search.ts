1de2c927b5e8266b375de81319754c4905e2e8b1
Elias Nahum <nahumhbl@gmail.com>
2021-04-01 17:27:46 -0300

    Update dependencies

--- app/mm-redux/actions/search.ts
+++ app/mm-redux/actions/search.ts
@@ -13,7 +13,7 @@ import {ActionResult, batchActions, DispatchFunc, GetStateFunc, ActionFunc} from
 import {RelationOneToOne} from '@mm-redux/types/utilities';
 import {Post} from '@mm-redux/types/posts';
 import {SearchParameter} from '@mm-redux/types/search';
-import {analytics} from '@init/analytics.ts';
+import {analytics} from '@init/analytics';
 
 const WEBAPP_SEARCH_PER_PAGE = 20;
 export function getMissingChannelsFromPosts(posts: RelationOneToOne<Post, Post>): ActionFunc {
