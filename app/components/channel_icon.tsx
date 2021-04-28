066329a4d12547e1ec721f4c527983cd0f1465c4
Elias Nahum <nahumhbl@gmail.com>
2021-04-28 18:54:54 -0400

    MM-34407 Use Avatars for Sidebar DMs (#5334)
    * MM-34407 Use Avatars for Sidebar DMs
    
    * Update app/components/channel_icon.js
    
    Co-authored-by: Joseph Baylon <joseph.baylon@mattermost.com>
    
    * UI feedback
    
    * Align the private channel icon
    
    Co-authored-by: Joseph Baylon <joseph.baylon@mattermost.com>
    (cherry picked from commit 1b36529853b2c0f75d44e47153b51e63ae2c0e9d)

---
+++ app/components/channel_icon.tsx
@@ -0,0 +1,184 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {StyleProp, Text, View, ViewStyle} from 'react-native';
+
+import CompassIcon from '@components/compass_icon';
+import ProfilePicture from '@components/profile_picture';
+import {General} from '@mm-redux/constants';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
+
+import type {Theme} from '@mm-redux/types/preferences';
+
+type Props = {
+    hasDraft: boolean;
+    isActive: boolean;
+    isArchived: boolean;
+    isInfo: boolean;
+    isUnread: boolean;
+    membersCount: number;
+    size: number;
+    statusStyle?: StyleProp<ViewStyle>;
+    testID?: string;
+    theme: Theme;
+    type: string;
+    userId?: string;
+};
+
+const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
+    return {
+        container: {
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        icon: {
+            color: changeOpacity(theme.sidebarText, 0.4),
+        },
+        iconActive: {
+            color: theme.sidebarTextActiveColor,
+        },
+        iconUnread: {
+            color: theme.sidebarUnreadText,
+        },
+        iconInfo: {
+            color: theme.centerChannelColor,
+        },
+        groupBox: {
+            alignItems: 'center',
+            backgroundColor: changeOpacity(theme.sidebarText, 0.16),
+            borderRadius: 4,
+            justifyContent: 'center',
+        },
+        groupBoxActive: {
+            backgroundColor: changeOpacity(theme.sidebarTextActiveColor, 0.3),
+        },
+        groupBoxUnread: {
+            backgroundColor: changeOpacity(theme.sidebarUnreadText, 0.3),
+        },
+        groupBoxInfo: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.3),
+        },
+        group: {
+            color: theme.sidebarText,
+            fontSize: 10,
+            fontWeight: '600',
+        },
+        groupActive: {
+            color: theme.sidebarTextActiveColor,
+        },
+        groupUnread: {
+            color: theme.sidebarUnreadText,
+        },
+        groupInfo: {
+            color: theme.centerChannelColor,
+        },
+    };
+});
+
+const ChannelIcon = (props: Props) => {
+    const style = getStyleSheet(props.theme);
+
+    let activeIcon;
+    let unreadIcon;
+    let activeGroupBox;
+    let unreadGroupBox;
+    let activeGroup;
+    let unreadGroup;
+
+    if (props.isUnread) {
+        unreadIcon = style.iconUnread;
+        unreadGroupBox = style.groupBoxUnread;
+        unreadGroup = style.groupUnread;
+    }
+
+    if (props.isActive) {
+        activeIcon = style.iconActive;
+        activeGroupBox = style.groupBoxActive;
+        activeGroup = style.groupActive;
+    }
+
+    if (props.isInfo) {
+        activeIcon = style.iconInfo;
+        activeGroupBox = style.groupBoxInfo;
+        activeGroup = style.groupInfo;
+    }
+
+    let icon;
+    let extraStyle;
+    if (props.isArchived) {
+        icon = (
+            <CompassIcon
+                name='archive-outline'
+                style={[style.icon, unreadIcon, activeIcon, {fontSize: props.size, left: 1}]}
+                testID={`${props.testID}.archive`}
+            />
+        );
+    } else if (props.hasDraft) {
+        icon = (
+            <CompassIcon
+                name='pencil-outline'
+                style={[style.icon, unreadIcon, activeIcon, {fontSize: props.size, left: 2}]}
+                testID={`${props.testID}.draft`}
+            />
+        );
+    } else if (props.type === General.OPEN_CHANNEL) {
+        icon = (
+            <CompassIcon
+                name='globe'
+                style={[style.icon, unreadIcon, activeIcon, {fontSize: props.size, left: 1}]}
+                testID={`${props.testID}.public`}
+            />
+        );
+    } else if (props.type === General.PRIVATE_CHANNEL) {
+        icon = (
+            <CompassIcon
+                name='lock-outline'
+                style={[style.icon, unreadIcon, activeIcon, {fontSize: props.size, left: 0.5}]}
+                testID={`${props.testID}.private`}
+            />
+        );
+    } else if (props.type === General.GM_CHANNEL) {
+        const fontSize = (props.size - 12);
+        const boxSize = (props.size - 4);
+        icon = (
+            <View style={[style.groupBox, unreadGroupBox, activeGroupBox, {width: boxSize, height: boxSize}]}>
+                <Text
+                    style={[style.group, unreadGroup, activeGroup, {fontSize}]}
+                    testID={`${props.testID}.gm_member_count`}
+                >
+                    {props.membersCount}
+                </Text>
+            </View>
+        );
+    } else if (props.type === General.DM_CHANNEL) {
+        // extraStyle = {marginRight: 6};
+        icon = (
+            <ProfilePicture
+                size={props.size}
+                statusSize={12}
+                userId={props.userId}
+                testID={props.testID}
+                statusStyle={props.statusStyle}
+            />
+        );
+    }
+
+    return (
+        <View style={[style.container, extraStyle, {width: props.size, height: props.size}]}>
+            {icon}
+        </View>
+    );
+};
+
+ChannelIcon.defaultProps = {
+    hasDraft: false,
+    isActive: false,
+    isArchived: false,
+    isInfo: false,
+    isUnread: false,
+    membersCount: 0,
+    size: 12,
+};
+
+export default ChannelIcon;
--- app/screens/channel_info/channel_info_header.js
+++ app/screens/channel_info/channel_info_header.js
@@ -12,18 +12,17 @@ import {
 import {intlShape} from 'react-intl';
 import Clipboard from '@react-native-community/clipboard';
 
+import {popToRoot} from '@actions/navigation';
+import ChannelIcon from '@components/channel_icon';
+import FormattedDate from '@components/formatted_date';
+import FormattedText from '@components/formatted_text';
+import Markdown from '@components/markdown';
 import {General} from '@mm-redux/constants';
-
-import ChannelIcon from 'app/components/channel_icon';
-import FormattedDate from 'app/components/formatted_date';
-import FormattedText from 'app/components/formatted_text';
-import Markdown from 'app/components/markdown';
+import BottomSheet from '@utils/bottom_sheet';
+import {t} from '@utils/i18n';
+import {getMarkdownTextStyles, getMarkdownBlockStyles} from '@utils/markdown';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
 import mattermostManaged from 'app/mattermost_managed';
-import BottomSheet from 'app/utils/bottom_sheet';
-import {getMarkdownTextStyles, getMarkdownBlockStyles} from 'app/utils/markdown';
-import {changeOpacity, makeStyleSheetFromTheme} from 'app/utils/theme';
-import {t} from 'app/utils/i18n';
-import {popToRoot} from 'app/actions/navigation';
 
 export default class ChannelInfoHeader extends React.PureComponent {
     static propTypes = {
@@ -34,11 +33,10 @@ export default class ChannelInfoHeader extends React.PureComponent {
         header: PropTypes.string,
         onPermalinkPress: PropTypes.func,
         purpose: PropTypes.string,
-        status: PropTypes.string,
+        teammateId: PropTypes.string,
         theme: PropTypes.object.isRequired,
         type: PropTypes.string.isRequired,
         isArchived: PropTypes.bool.isRequired,
-        isBot: PropTypes.bool.isRequired,
         isTeammateGuest: PropTypes.bool.isRequired,
         hasGuests: PropTypes.bool.isRequired,
         isGroupConstrained: PropTypes.bool,
@@ -135,11 +133,10 @@ export default class ChannelInfoHeader extends React.PureComponent {
             memberCount,
             onPermalinkPress,
             purpose,
-            status,
+            teammateId,
             theme,
             type,
             isArchived,
-            isBot,
             isGroupConstrained,
             testID,
             timeZone,
@@ -148,9 +145,10 @@ export default class ChannelInfoHeader extends React.PureComponent {
         const style = getStyleSheet(theme);
         const textStyles = getMarkdownTextStyles(theme);
         const blockStyles = getMarkdownBlockStyles(theme);
-        const baseTextStyle = Platform.OS === 'ios' ?
-            {...style.detail, lineHeight: 20} :
-            style.detail;
+        const baseTextStyle = Platform.select({
+            ios: {...style.detail, lineHeight: 20},
+            android: style.detail,
+        });
 
         return (
             <View style={style.container}>
@@ -159,11 +157,10 @@ export default class ChannelInfoHeader extends React.PureComponent {
                         isInfo={true}
                         membersCount={memberCount}
                         size={24}
-                        status={status}
+                        userId={teammateId}
                         theme={theme}
                         type={type}
                         isArchived={isArchived}
-                        isBot={isBot}
                         testID={`${testID}.channel_icon`}
                     />
                     <Text
@@ -268,6 +265,7 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
             fontSize: 15,
             fontWeight: '600',
             color: theme.centerChannelColor,
+            marginLeft: 13,
         },
         channelNameContainer: {
             flexDirection: 'row',
