052fba01e6868da53094cac29704bab7997dc214
Elias Nahum <nahumhbl@gmail.com>
2021-05-18 18:57:50 -0400

    MM-35384 Add country variant localization support

---
+++ app/i18n/languages.ts
@@ -0,0 +1,28 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import keyMirror from '@mm-redux/utils/key_mirror';
+
+const languages: Record<string, string> = keyMirror({
+    bg: null,
+    de: null,
+    en: null,
+    es: null,
+    fr: null,
+    hu: null,
+    it: null,
+    ja: null,
+    ko: null,
+    nl: null,
+    pl: null,
+    'pt-BR': null,
+    ro: null,
+    ru: null,
+    sv: null,
+    tr: null,
+    uk: null,
+    'zh-CN': null,
+    'zh-TW': null,
+});
+
+export default languages;
