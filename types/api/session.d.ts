0f0cb39aa7333127410b4fbd11551655af01e053
Elias Nahum <nahumhbl@gmail.com>
2021-07-03 18:51:36 -0400

    session type

--- types/api/session.d.ts
+++ types/api/session.d.ts
@@ -3,8 +3,8 @@
 
 interface Session {
     id: string,
-    create_at: number;
+    create_at: string|number;
     device_id?: string;
-    expires_at: number;
+    expires_at: string|number;
     user_id: string;
 }
