81ca9db5b3033b8323077a33f786b0b2e9d0f287
Elias Nahum <nahumhbl@gmail.com>
2021-07-03 18:56:16 -0400

    add missing launch props from server screen

--- app/screens/server/index.tsx
+++ app/screens/server/index.tsx
@@ -75,6 +75,9 @@ const Server: NavigationFunctionComponent = ({componentId, extra, launchType, la
 
         const passProps = {
             config,
+            extra,
+            launchError,
+            launchType,
             license,
             theme,
             serverUrl,
