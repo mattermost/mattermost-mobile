e3b08dd532df99c71e23c7cd0919079136c2f9b9
Avinash Lingaloo <>
2021-07-20 16:29:55 +0400

    Revert "Fix Android crashes as it is looking for MainSidebar and SettingsSidebar"
    This reverts commit 62ea11ae691e83bbe3c5e81c243b8ed89fa96083.

--- app/screens/channel/channel_nav_bar/index.tsx
+++ app/screens/channel/channel_nav_bar/index.tsx
@@ -47,8 +47,7 @@ const ChannelNavBar = ({currentUserId, channel, onPress, config}: ChannelNavBar)
             height = layoutHeight;
         }
 
-        //todo: Enable after installing MainSidebar and SettingsSidebar
-        // DeviceEventEmitter.emit(VIEWS.CHANNEL_NAV_BAR_CHANGED, layoutHeight);
+        DeviceEventEmitter.emit(VIEWS.CHANNEL_NAV_BAR_CHANGED, layoutHeight);
     };
 
     switch (Platform.OS) {
