cb31e80ee1e6e4d61341d7d603394e7540be616d
Avinash Lingaloo <>
2021-07-21 15:02:25 +0400

    Fix ts issue

--- app/constants/device.ts
+++ app/constants/device.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {Dimensions, Platform} from 'react-native';
+import {Platform} from 'react-native';
 import {FileSystem} from 'react-native-unimodules';
 import DeviceInfo from 'react-native-device-info';
 
