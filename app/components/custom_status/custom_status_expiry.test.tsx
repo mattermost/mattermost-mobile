7d6a48d8cbe357f68006428b4b890463c8b641ab
Saturnino Abril <saturnino.abril@gmail.com>
2021-07-23 05:40:23 +0800

    quick fix on failing unit test with custom expiry status (#5566)

--- app/components/custom_status/custom_status_expiry.test.tsx
+++ app/components/custom_status/custom_status_expiry.test.tsx
@@ -5,11 +5,15 @@ import React from 'react';
 import moment from 'moment-timezone';
 
 import Preferences from '@mm-redux/constants/preferences';
-import * as PreferenceSelectors from '@mm-redux/selectors/entities/preferences';
 import {renderWithReduxIntl} from 'test/testing_library';
 import CustomStatusExpiry from './custom_status_expiry';
 
 jest.mock('@mm-redux/selectors/entities/preferences');
+
+jest.mock('@mm-redux/selectors/entities/preferences', () => ({
+    getBool: jest.fn().mockReturnValue(false),
+    isCollapsedThreadsEnabled: jest.fn(),
+}));
 
 describe('components/custom_status/custom_status_expiry', () => {
     const date = moment().endOf('day');
@@ -19,7 +23,6 @@ describe('components/custom_status/custom_status_expiry', () => {
         time: date.toDate(),
     };
 
-    (PreferenceSelectors.getBool as jest.Mock).mockReturnValue(false);
     it('should match snapshot', () => {
         const wrapper = renderWithReduxIntl(
             <CustomStatusExpiry
