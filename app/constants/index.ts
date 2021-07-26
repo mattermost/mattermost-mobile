55e36f5700ac2962c5228ae371714310ffd503ef
Avinash Lingaloo <>
2021-07-26 13:30:26 +0400

    Added deep_linking constant

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -4,6 +4,7 @@
 import ActionType from './action_type';
 import Attachment from './attachment';
 import Database from './database';
+import DeepLink from './deep_linking';
 import Device from './device';
 import Files from './files';
 import General from './general';
@@ -12,8 +13,8 @@ import Navigation from './navigation';
 import Network from './network';
 import Permissions from './permissions';
 import Preferences from './preferences';
-import Screens from './screens';
 import SSO, {REDIRECT_URL_SCHEME, REDIRECT_URL_SCHEME_DEV} from './sso';
+import Screens from './screens';
 import View, {Upgrade} from './view';
 import WebsocketEvents from './websocket';
 
@@ -21,6 +22,7 @@ export {
     ActionType,
     Attachment,
     Database,
+    DeepLink,
     Device,
     Files,
     General,
@@ -31,8 +33,8 @@ export {
     Preferences,
     REDIRECT_URL_SCHEME,
     REDIRECT_URL_SCHEME_DEV,
-    Screens,
     SSO,
+    Screens,
     Upgrade,
     View,
     WebsocketEvents,
