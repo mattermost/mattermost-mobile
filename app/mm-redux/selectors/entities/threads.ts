a6f0629fd3faa5434e26f7a03bf134ebec151437
Anurag Shivarathri <anurag6713@gmail.com>
2021-07-27 19:08:48 +0530

    Added conditions to check for post & thread existence

--- app/mm-redux/selectors/entities/threads.ts
+++ app/mm-redux/selectors/entities/threads.ts
@@ -52,7 +52,7 @@ export function getThread(state: GlobalState, threadId: $ID<UserThread>, fallbac
     if (!thread || !thread?.id) {
         if (fallbackFromPosts) {
             const post = getPost(state, threadId);
-            if (!post?.root_id) {
+            if (post && !post?.root_id) {
                 const {id, is_following, reply_count, last_reply_at, participants} = post;
                 return {
                     id,
