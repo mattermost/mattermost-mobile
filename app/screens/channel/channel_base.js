02f8d5c999e185bd979c326c08e19530c0c4aa60
Elias Nahum <nahumhbl@gmail.com>
2021-08-05 22:56:36 -0400

    MM-37660 Fix keep mention for channel A when opening a push notification on channel B (#5597)
    * Fix keep mention for channel A when opening a push notification on channel B
    
    * properly fix race condition
    
    * Load channels and channel members when opening the app from a push notification
    
    (cherry picked from commit a7b604e8c95cc47ff2ca24a44a081ea5cbe55461)

--- app/screens/channel/channel_base.js
+++ app/screens/channel/channel_base.js
@@ -9,7 +9,6 @@ import {Alert, Animated, Keyboard, StyleSheet} from 'react-native';
 import {showModal, showModalOverCurrentContext} from '@actions/navigation';
 import CompassIcon from '@components/compass_icon';
 import {TYPING_VISIBLE} from '@constants/post_draft';
-import PushNotifications from '@init/push_notifications';
 import {General} from '@mm-redux/constants';
 import EventEmitter from '@mm-redux/utils/event_emitter';
 import EphemeralStore from '@store/ephemeral_store';
@@ -87,7 +86,6 @@ export default class ChannelBase extends PureComponent {
         }
 
         if (currentChannelId) {
-            this.clearChannelNotifications();
             requestAnimationFrame(() => {
                 actions.getChannelStats(currentChannelId);
             });
@@ -125,8 +123,6 @@ export default class ChannelBase extends PureComponent {
         }
 
         if (this.props.currentChannelId && this.props.currentChannelId !== prevProps.currentChannelId) {
-            this.clearChannelNotifications();
-
             requestAnimationFrame(() => {
                 this.props.actions.getChannelStats(this.props.currentChannelId);
             });
@@ -137,13 +133,6 @@ export default class ChannelBase extends PureComponent {
         EventEmitter.off('leave_team', this.handleLeaveTeam);
         EventEmitter.off(TYPING_VISIBLE, this.runTypingAnimations);
         EventEmitter.off(General.REMOVED_FROM_CHANNEL, this.handleRemovedFromChannel);
-    }
-
-    clearChannelNotifications = () => {
-        const clearNotificationsTimeout = setTimeout(() => {
-            clearTimeout(clearNotificationsTimeout);
-            PushNotifications.clearChannelNotifications(this.props.currentChannelId);
-        }, 1000);
     }
 
     registerTypingAnimation = (animation) => {
