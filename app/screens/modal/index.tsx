c31cde7fc44b7ce4cc5b3c5b44fd87737326224d
Avinash Lingaloo <>
2021-08-12 15:28:36 +0400

    Updated modal screen

--- app/screens/modal/index.tsx
+++ app/screens/modal/index.tsx
@@ -3,41 +3,13 @@
 
 import React from 'react';
 import {Text, View} from 'react-native';
-import {Navigation} from 'react-native-navigation';
 
-export default class Modal extends React.Component {
-    // Set a dismiss button in static options
-    static options() {
-        return {
-            topBar: {
-                leftButtons: [{
-                    id: 'dismiss',
-                    icon: require('@assets/images/autocomplete/slash_command.png'),
-                }],
-            },
-        };
-    }
+const Modal = () => {
+    return (
+        <View style={{backgroundColor: '#ffffff', flex: 1}}>
+            <Text>{'Modal screen'}</Text>
+        </View>
+    );
+};
 
-    constructor(props) {
-        super(props);
-
-        // Register to events
-        Navigation.events().bindComponent(this);
-    }
-
-    // Handle the button press event and dismiss the modal if needed
-    navigationButtonPressed({buttonId}) {
-        if (buttonId === 'dismiss') {
-            console.log('>>>>>>>>>>>>>>> pressed');
-            Navigation.dismissModal(this.props.componentId);
-        }
-    }
-
-    render() {
-        return (
-            <View style={{backgroundColor: '#ffffff', flex: 1}}>
-                <Text>{'Modal screen'}</Text>
-            </View>
-        );
-    }
-}
+export default Modal;
