b1a9be734ff9f96008fb79d5b7498a29ae1493af
Kyriakos Ziakoulis <cooxlee@gmail.com>
2021-08-15 18:33:53 +0300

    Fixes bad JSON and intl ids

--- app/screens/settings/notification_settings_email/notification_settings_email.android.js
+++ app/screens/settings/notification_settings_email/notification_settings_email.android.js
@@ -112,13 +112,13 @@ class NotificationSettingsEmailAndroid extends NotificationSettingsEmailBase {
                 <SectionItem
                     label={(
                         <FormattedText
-                            id='mobile.notification_settings.push_threads.title'
+                            id='user.settings.notifications.push_threads.title'
                             defaultMessage='Thread reply notifications'
                         />
                     )}
                     description={(
                         <FormattedText
-                            id='mobile.notification_settings.push_threads.description'
+                            id='user.settings.notifications.push_threads.description'
                             defaultMessage={'Notify me about all replies to threads I\'m following'}
                         />
                     )}
