276b26100c1881eb784e631c8d1734fd1d481c17
Elias Nahum <nahumhbl@gmail.com>
2021-08-19 16:42:26 -0400

    Add constants

--- app/constants/screens.ts
+++ app/constants/screens.ts
@@ -2,12 +2,16 @@
 // See LICENSE.txt for license information.
 
 export const ABOUT = 'About';
+export const APP_FORM = 'AppForm';
 export const CHANNEL = 'Channel';
 export const FORGOT_PASSWORD = 'ForgotPassword';
+export const INTEGRATION_SELECTOR = 'IntegrationSelector';
 export const LOGIN = 'Login';
 export const LOGIN_OPTIONS = 'LoginOptions';
 export const MAIN_SIDEBAR = 'MainSidebar';
 export const MFA = 'MFA';
+export const PERMALINK = 'Permalink';
+export const SEARCH = 'Search';
 export const SERVER = 'Server';
 export const SETTINGS_SIDEBAR = 'SettingsSidebar';
 export const SSO = 'SSO';
@@ -15,12 +19,16 @@ export const THREAD = 'Thread';
 
 export default {
     ABOUT,
+    APP_FORM,
     CHANNEL,
     FORGOT_PASSWORD,
+    INTEGRATION_SELECTOR,
     LOGIN,
     LOGIN_OPTIONS,
     MAIN_SIDEBAR,
     MFA,
+    PERMALINK,
+    SEARCH,
     SERVER,
     SETTINGS_SIDEBAR,
     THREAD,
