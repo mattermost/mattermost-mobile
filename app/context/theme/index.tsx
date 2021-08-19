c0450cab0c35cba15389d2bdf18c31b2b8a1a5e2
Elias Nahum <nahumhbl@gmail.com>
2021-08-19 16:44:27 -0400

    Add test helper with theme & database

--- app/context/theme/index.tsx
+++ app/context/theme/index.tsx
@@ -26,14 +26,14 @@ type WithThemeProps = {
 
 const {SERVER: {PREFERENCE, SYSTEM}} = MM_TABLES;
 
-function getDefaultThemeByAppearance(): Theme {
+export function getDefaultThemeByAppearance(): Theme {
     if (Appearance.getColorScheme() === 'dark') {
         return Preferences.THEMES.windows10;
     }
     return Preferences.THEMES.default;
 }
 
-const ThemeContext = createContext(getDefaultThemeByAppearance());
+export const ThemeContext = createContext(getDefaultThemeByAppearance());
 const {Consumer, Provider} = ThemeContext;
 
 const ThemeProvider = ({currentTeamId, children, themes}: Props) => {
