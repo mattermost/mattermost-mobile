d02d1922c6d0e5254e179a8695d1b6a53bb44c33
Daniel Espino García <larkox@gmail.com>
2021-08-24 14:16:38 +0200

    Remove duplicated code on cleanUrlForLogging (#5642)

--- app/utils/client_error.ts
+++ app/utils/client_error.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {cleanUrlForLogging} from '@utils/logging';
+import {cleanUrlForLogging} from '@utils/url';
 
 export class ClientError extends Error {
     details: Error;
