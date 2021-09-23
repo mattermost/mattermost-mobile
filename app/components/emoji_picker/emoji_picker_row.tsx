264c35b68af9e67cf904d56d9850a68da05f4f09
Avinash Lingaloo <>
2021-09-23 16:18:14 +0400

    Added EmojiPickerRow

---
+++ app/components/emoji_picker/emoji_picker_row.tsx
@@ -0,0 +1,96 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useCallback, useMemo} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+
+import Emoji from '@components/emoji';
+
+type EmojiItem = {
+    aliases: string[];
+    name: string;
+    short_name: string;
+};
+
+type EmojiPickerRowProps = {
+    emojiGutter: number;
+    emojiSize: number;
+    items: EmojiItem[];
+    onEmojiPress: (emojiName: string) => void;
+};
+
+const EmojiPickerRow = ({emojiGutter, emojiSize, items, onEmojiPress}: EmojiPickerRowProps) => {
+    const memoizedEmojis = useMemo(() => items, [items]);
+
+    const renderEmojis = useCallback(
+        () => (emoji: EmojiItem, index: number, emojis: EmojiItem[]) => {
+            const size = emojiSize + 7;
+            const style: any = [
+                styles.emoji,
+                {width: size, height: size, marginHorizontal: emojiGutter},
+            ];
+
+            if (index === 0) {
+                style.push(styles.emojiLeft);
+            } else if (index === emojis.length - 1) {
+                style.push(styles.emojiRight);
+            }
+
+            if (!emoji) {
+                return (
+                    <View
+                        key={index}
+                        style={style}
+                    />);
+            }
+
+            const name = 'short_name' in emoji ? emoji.short_name : emoji.name;
+
+            return (
+                <TouchableOpacity
+                    key={name}
+                    style={style}
+                    onPress={() => onEmojiPress(name)}
+                >
+                    <Emoji
+                        emojiName={name}
+                        textStyle={styles.emojiText}
+                        size={emojiSize}
+                    />
+                </TouchableOpacity>
+            );
+        },
+        [],
+    );
+
+    return (
+        <View style={[styles.columnStyle, {marginVertical: emojiGutter}]}>
+            {memoizedEmojis.map(renderEmojis)}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    columnStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    emoji: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+    emojiText: {
+        color: '#000',
+        fontWeight: 'bold',
+    },
+    emojiLeft: {
+        marginLeft: 0,
+    },
+    emojiRight: {
+        marginRight: 0,
+    },
+});
+
+export default EmojiPickerRow;
