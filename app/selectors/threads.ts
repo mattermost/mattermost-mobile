fe2e5c45ea0806bf935970f620144c8a95bf0abc
Anurag Shivarathri <anurag6713@gmail.com>
2021-09-30 00:58:21 +0530

    Reverted comparision check

--- app/selectors/threads.ts
+++ app/selectors/threads.ts
@@ -7,7 +7,11 @@ import type {UserThread} from '@mm-redux/types/threads';
 import type {$ID} from '@mm-redux/types/utilities';
 
 export function getThreadLastViewedAt(state: GlobalState, threadId: $ID<UserThread>): number {
-    return state.views.threads.lastViewedAt[threadId] || getThread(state, threadId)?.last_viewed_at;
+    if (state.views.threads.lastViewedAt[threadId]) {
+        // timestamp - 1, to properly mark "new messages" in threads when manually unread as lastViewedAt is set to the post's created timestamp
+        return state.views.threads.lastViewedAt[threadId] - 1;
+    }
+    return getThread(state, threadId)?.last_viewed_at || 0;
 }
 
 export function getViewingGlobalThreads(state: GlobalState): boolean {
