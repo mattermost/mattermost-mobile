0561e9a78ba028560150a804218195ba9d3e8908
Mattermost Build <build@mattermost.com>
2021-10-15 14:08:59 +0200

    MM-39337 set allowFontScaling to TextInput components (#5751) (#5752)
    (cherry picked from commit f8baaf8505433e46334e27c28ecd68752cf9f53a)
    
    Co-authored-by: Elias Nahum <nahumhbl@gmail.com>

--- app/components/post_draft/post_input/post_input.js
+++ app/components/post_draft/post_input/post_input.js
@@ -298,6 +298,7 @@ export default class PostInput extends PureComponent {
 
         return (
             <PasteableTextInput
+                allowFontScaling={true}
                 testID={testID}
                 ref={this.input}
                 disableCopyPaste={this.state.disableCopyAndPaste}
