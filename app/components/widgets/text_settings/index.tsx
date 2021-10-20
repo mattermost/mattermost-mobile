93156577f0a9f353d4e039a64af2f7d0f2d4881a
Avinash Lingaloo <>
2021-10-20 16:14:11 +0400

    clean up text settings component

--- app/components/widgets/text_settings/index.tsx
+++ app/components/widgets/text_settings/index.tsx
@@ -2,7 +2,10 @@
 // See LICENSE.txt for license information.
 
 import React, {memo, useCallback} from 'react';
-import {View, TextInput, Platform} from 'react-native';
+import {useIntl} from 'react-intl';
+import {View, Platform} from 'react-native';
+import {backgroundColor} from 'react-native-calendars/src/style';
+import FloatingTextInput from 'react-native-reanimated-text-input';
 
 import {useTheme} from '@context/theme';
 import {getMarkdownBlockStyles, getMarkdownTextStyles} from '@utils/markdown';
@@ -11,8 +14,6 @@ import {changeOpacity, makeStyleSheetFromTheme, getKeyboardAppearanceFromTheme} 
 import DisableContent from './components/disabled_content';
 import ErrorContent from './components/error_content';
 import HelpContent from './components/help_content';
-import LabelContent from './components/label_content';
-import OptionalContent from './components/optional_content';
 
 export interface IntlText {
     id: string;
@@ -39,6 +40,7 @@ type TextSettingProps = {
 
 const TextSetting = (props: TextSettingProps) => {
     const theme = useTheme();
+    const intl = useIntl();
     const {disabled, disabledText, errorText, helpText, id, keyboardType, label, maxLength, multiline, onChange, optional, placeholder, secureTextEntry, testID, value} = props;
 
     const onChangeText = useCallback((text: string) => {
@@ -51,41 +53,42 @@ const TextSetting = (props: TextSettingProps) => {
 
     const keyboard = Platform.OS === 'android' && keyboardType === 'url' ? 'default' : keyboardType;
 
-    const inputStyle = multiline ? style.multiline : style.input;
-    const noEditing = disabled ? style.disabled : null;
+    const labelText = typeof label === 'string' ? label : intl.formatMessage(label);
+    const optionalText = optional ? intl.formatMessage({id: 'channel_modal.optional', defaultMessage: '(optional)'}) : ' *';
+
+    const formattedLabel = labelText + optionalText;
 
     return (
-        <View testID={testID}>
-            <View style={style.titleContainer}>
-                <LabelContent
-                    label={label}
-                    testID={testID}
-                />
-                <OptionalContent optional={Boolean(optional)}/>
-            </View>
-            <View style={[style.inputContainer, noEditing]}>
-                <View>
-                    <TextInput
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        disableFullscreenUI={true}
-                        editable={!disabled}
-                        keyboardAppearance={getKeyboardAppearanceFromTheme(theme)}
-                        keyboardType={keyboard}
-                        maxLength={maxLength}
-                        multiline={multiline}
-                        onChangeText={onChangeText}
-                        placeholder={placeholder}
-                        placeholderTextColor={changeOpacity(theme.centerChannelColor, 0.5)}
-                        secureTextEntry={secureTextEntry}
-                        style={inputStyle}
-                        testID={`${testID}.input`}
-                        underlineColorAndroid='transparent'
-                        value={value}
-                    />
-                </View>
-            </View>
-            <View>
+        <View
+            testID={testID}
+            style={style.viewContainer}
+        >
+            <FloatingTextInput
+                activeColor={changeOpacity(theme.centerChannelColor, 0.64)}
+                activeLabelColor={changeOpacity(theme.centerChannelColor, 0.64)}
+                textInputColorStyles={{
+                    borderColor: changeOpacity(theme.centerChannelColor, 0.16),
+                }}
+                autoCapitalize='none'
+                autoCorrect={false}
+
+                containerStyle={{width: '84%', marginLeft: 14}}
+                disableFullscreenUI={true}
+                editable={!disabled}
+                keyboardAppearance={getKeyboardAppearanceFromTheme(theme)}
+                keyboardType={keyboard}
+                label={formattedLabel}
+                maxLength={maxLength}
+                multiline={multiline}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                placeholderTextColor={changeOpacity(theme.centerChannelColor, 0.5)}
+                secureTextEntry={secureTextEntry}
+                testID={`${testID}.input`}
+                underlineColorAndroid='transparent'
+                value={value}
+            />
+            <View style={{width: '90%'}}>
                 {disabled && disabledText && (
                     <DisableContent
                         blockStyles={blockStyles}
@@ -108,51 +111,15 @@ const TextSetting = (props: TextSettingProps) => {
                     />
                 )}
             </View>
+
         </View>
     );
 };
 
-const getStyleSheet = makeStyleSheetFromTheme((theme) => {
-    const input = {
-        color: theme.centerChannelColor,
-        fontSize: 14,
-        paddingHorizontal: 15,
-    };
-
+const getStyleSheet = makeStyleSheetFromTheme(() => {
     return {
-        inputContainer: {
-            borderTopWidth: 1,
-            borderBottomWidth: 1,
-            borderTopColor: changeOpacity(theme.centerChannelColor, 0.1),
-            borderBottomColor: changeOpacity(theme.centerChannelColor, 0.1),
-            backgroundColor: theme.centerChannelBg,
-        },
-        input: {
-            ...input,
-            height: 40,
-        },
-        multiline: {
-            ...input,
-            paddingTop: 10,
-            paddingBottom: 13,
-            height: 125,
-        },
-        disabled: {
-            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
-        },
-        titleContainer: {
-            flexDirection: 'row',
-            marginTop: 15,
-            marginBottom: 10,
-        },
-        optional: {
-            color: changeOpacity(theme.centerChannelColor, 0.5),
-            fontSize: 14,
-            marginLeft: 5,
-        },
-        asterisk: {
-            color: theme.errorTextColor,
-            fontSize: 14,
+        viewContainer: {
+            marginVertical: 7,
         },
     };
 });
