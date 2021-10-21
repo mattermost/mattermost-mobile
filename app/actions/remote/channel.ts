aa7797ddfcd7e801d0682a4d2a2c6b7805610196
Elias Nahum <nahumhbl@gmail.com>
2021-10-21 20:43:48 -0300

    Fix channel remote & local actions + added actions for markChannelAsViewed & fetchMyChannel

--- app/actions/remote/channel.ts
+++ app/actions/remote/channel.ts
@@ -1,18 +1,29 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {Model} from '@nozbe/watermelondb';
+import {IntlShape} from 'react-intl';
+
+import {switchToChannel} from '@actions/local/channel';
 import {General} from '@constants';
 import DatabaseManager from '@database/manager';
+import {privateChannelJoinPrompt} from '@helpers/api/channel';
 import NetworkManager from '@init/network_manager';
 import {prepareMyChannelsForTeam, queryMyChannel} from '@queries/servers/channel';
+import {queryCommonSystemValues} from '@queries/servers/system';
+import {prepareMyTeams, queryMyTeamById, queryTeamById, queryTeamByName} from '@queries/servers/team';
+import MyChannelModel from '@typings/database/models/servers/my_channel';
+import MyTeamModel from '@typings/database/models/servers/my_team';
+import TeamModel from '@typings/database/models/servers/team';
+import {PERMALINK_GENERIC_TEAM_NAME_REDIRECT} from '@utils/url';
 import {displayGroupMessageName, displayUsername} from '@utils/user';
 
 import {fetchRolesIfNeeded} from './role';
 import {forceLogoutIfNecessary} from './session';
+import {addUserToTeam, fetchTeamByName, removeUserFromTeam} from './team';
 import {fetchProfilesPerChannels, fetchUsersByIds} from './user';
 
 import type {Client} from '@client/rest';
-import type {Model} from '@nozbe/watermelondb';
 
 export type MyChannelsRequest = {
     channels?: Channel[];
@@ -132,11 +143,58 @@ export const fetchMyChannelsForTeam = async (serverUrl: string, teamId: string, 
     }
 };
 
-export const fetchMissingSidebarInfo = async (serverUrl: string, directChannels: Channel[], locale?: string, teammateDisplayNameSetting?: string, exludeUserId?: string) => {
+export const fetchMyChannel = async (serverUrl: string, teamId: string, channelId: string, fetchOnly = false): Promise<MyChannelsRequest> => {
+    let client: Client;
+    try {
+        client = NetworkManager.getClient(serverUrl);
+    } catch (error) {
+        return {error};
+    }
+
+    try {
+        const [channel, member] = await Promise.all([
+            client.getChannel(channelId),
+            client.getChannelMember(channelId, 'me'),
+        ]);
+
+        if (!fetchOnly) {
+            const operator = DatabaseManager.serverDatabases[serverUrl]?.operator;
+            const modelPromises: Array<Promise<Model[]>> = [];
+            if (operator) {
+                const prepare = await prepareMyChannelsForTeam(operator, teamId, [channel], [member]);
+                if (prepare) {
+                    modelPromises.push(...prepare);
+                }
+                if (modelPromises.length) {
+                    const models = await Promise.all(modelPromises);
+                    const flattenedModels = models.flat() as Model[];
+                    if (flattenedModels?.length > 0) {
+                        try {
+                            await operator.batchRecords(flattenedModels);
+                        } catch {
+                            // eslint-disable-next-line no-console
+                            console.log('FAILED TO BATCH CHANNELS');
+                        }
+                    }
+                }
+            }
+        }
+
+        return {
+            channels: [channel],
+            memberships: [member],
+        };
+    } catch (error) {
+        forceLogoutIfNecessary(serverUrl, error as ClientErrorProps);
+        return {error};
+    }
+};
+
+export const fetchMissingSidebarInfo = async (serverUrl: string, directChannels: Channel[], locale?: string, teammateDisplayNameSetting?: string, exludeUserId?: string, fetchOnly = false) => {
     const channelIds = directChannels.map((dc) => dc.id);
     const result = await fetchProfilesPerChannels(serverUrl, channelIds, exludeUserId, false);
     if (result.error) {
-        return;
+        return {error: result.error};
     }
 
     const displayNameByChannel: Record<string, string> = {};
@@ -160,10 +218,14 @@ export const fetchMissingSidebarInfo = async (serverUrl: string, directChannels:
         }
     });
 
-    const operator = DatabaseManager.serverDatabases[serverUrl]?.operator;
-    if (operator) {
-        operator.handleChannel({channels: directChannels, prepareRecordsOnly: false});
+    if (!fetchOnly) {
+        const operator = DatabaseManager.serverDatabases[serverUrl]?.operator;
+        if (operator) {
+            await operator.handleChannel({channels: directChannels, prepareRecordsOnly: false});
+        }
     }
+
+    return {directChannels};
 };
 
 export const joinChannel = async (serverUrl: string, userId: string, teamId: string, channelId?: string, channelName?: string, fetchOnly = false) => {
@@ -227,57 +289,173 @@ export const joinChannel = async (serverUrl: string, userId: string, teamId: str
     return {channel, member};
 };
 
-export const markChannelAsViewed = async (serverUrl: string, channelId: string, previousChannelId = '', markOnServer = true) => {
+export const markChannelAsRead = async (serverUrl: string, channelId: string) => {
+    try {
+        const client = NetworkManager.getClient(serverUrl);
+        await client.viewMyChannel(channelId);
+
+        return {};
+    } catch (error) {
+        return {error};
+    }
+};
+
+export const switchToChannelByName = async (serverUrl: string, channelName: string, teamName: string, errorHandler: (intl: IntlShape) => void, intl: IntlShape) => {
     const database = DatabaseManager.serverDatabases[serverUrl]?.database;
     if (!database) {
         return {error: `${serverUrl} database not found`};
     }
 
-    const member = await queryMyChannel(database, channelId);
-    const prevMember = await queryMyChannel(database, previousChannelId);
-    if (markOnServer) {
-        try {
-            const client = NetworkManager.getClient(serverUrl);
-            client.viewMyChannel(channelId, prevMember?.manuallyUnread ? '' : previousChannelId).catch(() => {
-                // do nothing just adding the handler to avoid the warning
-            });
-        } catch (error) {
-            return {error};
-        }
-    }
-
-    const models = [];
-    const lastViewedAt = Date.now();
-    if (member) {
-        member.prepareUpdate((m) => {
-            m.messageCount = 0;
-            m.mentionsCount = 0;
-            m.manuallyUnread = false;
-            m.lastViewedAt = lastViewedAt;
-        });
-
-        models.push(member);
-    }
-
-    if (prevMember && !prevMember.manuallyUnread) {
-        prevMember.prepareUpdate((m) => {
-            m.messageCount = 0;
-            m.mentionsCount = 0;
-            m.manuallyUnread = false;
-            m.lastViewedAt = lastViewedAt;
-        });
-
-        models.push(prevMember);
-    }
-
     try {
-        if (models.length) {
-            const {operator} = DatabaseManager.serverDatabases[serverUrl];
-            await operator.batchRecords(models);
+        let myChannel: MyChannelModel | ChannelMembership | undefined;
+        let team: TeamModel | Team | undefined;
+        let myTeam: MyTeamModel | TeamMembership | undefined;
+        let name = teamName;
+        const roles: string [] = [];
+        const system = await queryCommonSystemValues(database);
+        const currentTeam = await queryTeamById(database, system.currentTeamId);
+
+        if (name === PERMALINK_GENERIC_TEAM_NAME_REDIRECT) {
+            name = currentTeam!.name;
+        } else {
+            team = await queryTeamByName(database, teamName);
         }
 
-        return {data: true};
+        if (!team) {
+            const fetchTeam = await fetchTeamByName(serverUrl, name, true);
+            if (fetchTeam.error) {
+                errorHandler(intl);
+                return {error: fetchTeam.error};
+            }
+
+            team = fetchTeam.team!;
+        }
+
+        let joinedNewTeam = false;
+        myTeam = await queryMyTeamById(database, team.id);
+        if (!myTeam) {
+            const added = await addUserToTeam(serverUrl, team.id, system.currentUserId, true);
+            if (added.error) {
+                errorHandler(intl);
+                return {error: added.error};
+            }
+            myTeam = added.member!;
+            roles.push(...myTeam.roles.split(' '));
+            joinedNewTeam = true;
+        }
+
+        if (!myTeam) {
+            errorHandler(intl);
+            return {error: 'Could not fetch team member'};
+        }
+
+        let isArchived = false;
+        const chReq = await fetchChannelByName(serverUrl, team.id, channelName);
+        if (chReq.error) {
+            errorHandler(intl);
+            return {error: chReq.error};
+        }
+        const channel = chReq.channel;
+        if (!channel) {
+            errorHandler(intl);
+            return {error: 'Could not fetch channel'};
+        }
+
+        isArchived = channel.delete_at > 0;
+        if (isArchived && system.config.ExperimentalViewArchivedChannels !== 'true') {
+            errorHandler(intl);
+            return {error: 'Channel is archived'};
+        }
+
+        myChannel = await queryMyChannel(database, channel.id);
+
+        if (!myChannel) {
+            if (channel.type === General.PRIVATE_CHANNEL) {
+                const displayName = channel.display_name;
+                const {join} = await privateChannelJoinPrompt(displayName, intl);
+                if (!join) {
+                    if (joinedNewTeam) {
+                        await removeUserFromTeam(serverUrl, team.id, system.currentUserId, true);
+                    }
+                    errorHandler(intl);
+                    return {error: 'Refused to join Private channel'};
+                }
+                console.log('joining channel', displayName, channel.id); //eslint-disable-line
+                const result = await joinChannel(serverUrl, system.currentUserId, team.id, channel.id, undefined, true);
+                if (result.error || !result.channel) {
+                    if (joinedNewTeam) {
+                        await removeUserFromTeam(serverUrl, team.id, system.currentUserId, true);
+                    }
+
+                    errorHandler(intl);
+                    return {error: result.error};
+                }
+
+                myChannel = result.member!;
+                roles.push(...myChannel.roles.split(' '));
+            }
+        }
+
+        if (!myChannel) {
+            errorHandler(intl);
+            return {error: 'could not fetch channel member'};
+        }
+
+        const modelPromises: Array<Promise<Model[]>> = [];
+        const {operator} = DatabaseManager.serverDatabases[serverUrl];
+        if (!(team instanceof Model)) {
+            const prepT = prepareMyTeams(operator, [team], [(myTeam as TeamMembership)]);
+            if (prepT) {
+                modelPromises.push(...prepT);
+            }
+        } else if (!(myTeam instanceof Model)) {
+            const mt: MyTeam[] = [{
+                id: myTeam.team_id,
+                roles: myTeam.roles,
+            }];
+            modelPromises.push(
+                operator.handleMyTeam({myTeams: mt, prepareRecordsOnly: true}),
+                operator.handleTeamMemberships({teamMemberships: [myTeam], prepareRecordsOnly: true}),
+            );
+        }
+
+        if (!(myChannel instanceof Model)) {
+            const prepCh = await prepareMyChannelsForTeam(operator, team.id, [channel], [myChannel]);
+            if (prepCh) {
+                modelPromises.push(...prepCh);
+            }
+        }
+
+        let teamId;
+        if (team.id !== system.currentTeamId) {
+            teamId = team.id;
+        }
+
+        let channelId;
+        if (channel.id !== system.currentChannelId) {
+            channelId = channel.id;
+        }
+
+        if (modelPromises.length) {
+            const models = await Promise.all(modelPromises);
+            await operator.batchRecords(models.flat());
+        }
+
+        if (teamId) {
+            fetchMyChannelsForTeam(serverUrl, teamId, true, 0, false, true);
+        }
+
+        if (teamId && channelId) {
+            await switchToChannel(serverUrl, channelId, teamId);
+        }
+
+        if (roles.length) {
+            fetchRolesIfNeeded(serverUrl, roles);
+        }
+
+        return {error: undefined};
     } catch (error) {
+        errorHandler(intl);
         return {error};
     }
 };
