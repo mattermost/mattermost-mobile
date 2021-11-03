4afa762df573ca141e51a0887a7bd55feb733112
Jason Frerich <jason.frerich@mattermost.com>
2021-11-02 21:58:02 -0500

    wip

--- app/screens/login_options/sso_option.tsx
+++ app/screens/login_options/sso_option.tsx
@@ -8,12 +8,13 @@ import Button from 'react-native-button';
 import LocalConfig from '@assets/config.json';
 import FormattedText from '@components/formatted_text';
 import {Sso} from '@constants';
+import {t} from '@i18n';
 import {buttonBackgroundStyle, buttonTextStyle} from '@utils/buttonStyles';
 import {isMinimumServerVersion} from '@utils/helpers';
 import {makeStyleSheetFromTheme, changeOpacity} from '@utils/theme';
 
 const SsoOption = ({ssoType, config, license, onPress, theme}: LoginOptionWithConfigAndLicenseProps) => {
-    const styleButtonText = buttonTextStyle(theme, 'lg', 'primary', 'default');
+    const styleButtonText = buttonTextStyle(theme, 'lg', 'secondary', 'default');
     const styleButtonBackground = buttonBackgroundStyle(theme, 'lg', 'primary');
     const styles = getStyleSheet(theme);
 
@@ -26,38 +27,49 @@ const SsoOption = ({ssoType, config, license, onPress, theme}: LoginOptionWithCo
     switch (ssoType) {
         case Sso.SAML:
             enabled = config.EnableSaml === 'true' && isLicensed && license!.SAML === 'true';
+            enabled = true;
             forceHideFromLocal = LocalConfig.HideSAMLLoginExperimental;
             defaultMessage = config.SamlLoginButtonText;
+            id = t('mobile.login_options.saml');
             break;
 
         case Sso.GITLAB:
             imageSrc = require('@assets/images/gitlab.png');
             enabled = config.EnableSignUpWithGitLab === 'true';
+            enabled = true;
             forceHideFromLocal = LocalConfig.HideGitLabLoginExperimental;
             defaultMessage = 'GitLab';
+            id = t('mobile.login_options.gitlab');
             break;
 
         case Sso.GOOGLE:
             enabled = config.EnableSignUpWithGoogle === 'true';
+
+            // enabled = true;
             imageSrc = require('@assets/images/google.png');
-            id = 'signup.google';
             defaultMessage = 'Google';
+            id = t('mobile.login_options.google');
             break;
 
         case Sso.OPENID:
             enabled = config.EnableSignUpWithOpenId === 'true' && isLicensed && isMinimumServerVersion(config.Version, 5, 33, 0);
+
+            // enabled = true;
             defaultMessage = 'Open ID';
+            id = t('mobile.login_options.openid');
             break;
 
         case Sso.OFFICE365:
             defaultMessage = 'Office 365';
             enabled = config.EnableSignUpWithOffice365 === 'true' && isLicensed && license!.Office365OAuth === 'true';
+
+            // enabled = true;
             forceHideFromLocal = LocalConfig.HideO365LoginExperimental;
+            id = t('mobile.login_options.offic365');
             break;
 
         default:
     }
-    enabled = true;
 
     const logoStyle = {
         height: 18,
@@ -71,6 +83,7 @@ const SsoOption = ({ssoType, config, license, onPress, theme}: LoginOptionWithCo
     };
 
     if (!forceHideFromLocal && enabled) {
+        console.log('<><><> ssoType NOT null', ssoType);
         return (
             <Button
                 key={ssoType}
@@ -85,19 +98,22 @@ const SsoOption = ({ssoType, config, license, onPress, theme}: LoginOptionWithCo
                     />
                 )}
                 <FormattedText
-                    id={ssoType}
+                    id={id}
                     style={[styleButtonText, styles.buttonText]}
                     defaultMessage={defaultMessage}
+                    testID={id}
                 />
             </Button>
         );
     }
+    console.log('<><><> ssoType null', ssoType);
 
     return null;
 };
 
 const getStyleSheet = makeStyleSheetFromTheme((theme) => ({
     button: {
+        marginVertical: 4,
         backgroundColor: 'transparent',
         borderWidth: 1,
         borderColor: changeOpacity(theme.centerChannelColor, 0.64),
--- app/screens/login_options/sso_options.tsx
+++ app/screens/login_options/sso_options.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import {useIntl} from 'react-intl';
+import {View} from 'react-native';
 
 import {Sso} from '@constants';
 import {SSO} from '@constants/screens';
@@ -14,6 +15,7 @@ import SsoOption from './sso_option';
 
 // LoginOptionWithConfigAndLicenseProps
 const SsoOptions = ({config, extra, launchType, launchError, license, theme, serverDisplayName, serverUrl}: LoginOptionsProps) => {
+    const styles = getStyleSheet(theme);
     const intl = useIntl();
     const displaySSO = preventDoubleTap((ssoType: string) => {
         const screen = SSO;
@@ -21,45 +23,83 @@ const SsoOptions = ({config, extra, launchType, launchError, license, theme, ser
         goToScreen(screen, title, {config, extra, launchError, launchType, license, theme, ssoType, serverDisplayName, serverUrl});
     });
 
+    const ssoArray = [Sso.SAML, Sso.OFFICE365, Sso.GOOGLE, Sso.GITLAB];
+    const ssoComponents = ssoArray.map((ssoType) => (
+        <SsoOption
+            key={ssoType.toString()}
+            ssoType={ssoType}
+            config={config}
+            license={license}
+            onPress={displaySSO}
+            theme={theme}
+        />),
+    );
+
+    const numComps = ssoComponents.length;
+    console.log('<><><><><><><><>');
+    console.log('numComps', numComps);
+    console.log('<><><><><><><><>');
+
+    // console.log('ssoComponents', ssoComponents);
+
+    // console.log('props.children', props.children);
     return (
-        <>
-            <SsoOption
-                ssoType={Sso.SAML}
-                config={config}
-                license={license}
-                onPress={displaySSO}
-                theme={theme}
-            />
-            <SsoOption
-                ssoType={Sso.OFFICE365}
-                config={config}
-                license={license}
-                onPress={displaySSO}
-                theme={theme}
-            />
-            <SsoOption
-                ssoType={Sso.GOOGLE}
-                config={config}
-                license={license}
-                onPress={displaySSO}
-                theme={theme}
-            />
-            <SsoOption
-                ssoType={Sso.GITLAB}
-                config={config}
-                license={license}
-                onPress={displaySSO}
-                theme={theme}
-            />
-            <SsoOption
-                ssoType={Sso.OPENID}
-                config={config}
-                license={license}
-                onPress={displaySSO}
-                theme={theme}
-            />
-        </>
+        ssoComponents
     );
 };
 
+const getStyleSheet = () => ({
+    container: {
+        flexDirection: 'row',
+        marginVertical: 24,
+        alignItems: 'center',
+    },
+    separatorContainer: {
+        width: '48%',
+        marginRight: 8,
+    },
+});
+
 export default SsoOptions;
+
+{/* <View style={styles.container}> */}
+{/*     <View style={styles.separatorContainer}> */}
+{/*         <SsoOption */}
+{/*             ssoType={Sso.SAML} */}
+{/*             config={config} */}
+{/*             license={license} */}
+{/*             onPress={displaySSO} */}
+{/*             theme={theme} */}
+{/*         /> */}
+{/*     </View> */}
+{/*     <View style={styles.separatorContainer}> */}
+{/*         <SsoOption */}
+{/*             ssoType={Sso.OFFICE365} */}
+{/*             config={config} */}
+{/*             license={license} */}
+{/*             onPress={displaySSO} */}
+{/*             theme={theme} */}
+{/*         /> */}
+{/*     </View> */}
+{/* </View> */}
+{/* <SsoOption */}
+{/*     ssoType={Sso.GOOGLE} */}
+{/*     config={config} */}
+{/*     license={license} */}
+{/*     onPress={displaySSO} */}
+{/*     theme={theme} */}
+{/* /> */}
+{/* <SsoOption */}
+{/*     ssoType={Sso.GITLAB} */}
+{/*     config={config} */}
+{/*     license={license} */}
+{/*     onPress={displaySSO} */}
+{/*     theme={theme} */}
+{/* /> */}
+{/* <SsoOption */}
+{/*     ssoType={Sso.OPENID} */}
+{/*     config={config} */}
+{/*     license={license} */}
+{/*     onPress={displaySSO} */}
+{/*     theme={theme} */}
+{/* /> */}
