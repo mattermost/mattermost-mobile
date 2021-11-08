4ffc898ca834211421faa39c823790a05b4a2f81
Shaz Amjad <shaz.amjad@me.com>
2021-11-09 08:50:42 +1100

    Snapshot fail fixed

--- app/components/channel_list/index.test.tsx
+++ app/components/channel_list/index.test.tsx
@@ -3,16 +3,25 @@
 import Database from '@nozbe/watermelondb/Database';
 import React from 'react';
 
+import {MM_TABLES} from '@app/constants/database';
+import {TeamModel} from '@app/database/models/server';
 import {renderWithEverything} from '@test/intl-test-helper';
 import TestHelper from '@test/test_helper';
 
 import ChannelsList from './index';
 
 describe('components/channel_list', () => {
-    let database: Database | undefined;
+    let database: Database;
     beforeAll(async () => {
         const server = await TestHelper.setupServerDatabase();
         database = server.database;
+
+        await database.write(async () => {
+            const team = (await database.get<TeamModel>(MM_TABLES.SERVER.TEAM).query().fetch()).pop();
+            await team?.update(() => {
+                team.displayName = 'Test Team!';
+            });
+        });
     });
 
     it('should match snapshot', () => {
