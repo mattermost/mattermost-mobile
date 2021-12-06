921d81b0b97bcf20c956c5171db6e67e7fdf5dab
Avinash Lingaloo <avinashlng1080@gmail.com>
2021-12-06 21:51:00 +0400

    renamed valid_mime_types to  valid_image_mime_types

--- app/constants/files.ts
+++ app/constants/files.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const VALID_MIME_TYPES = [
+export const VALID_IMAGE_MIME_TYPES = [
     'image/jpeg',
     'image/jpeg',
     'image/jpg',
