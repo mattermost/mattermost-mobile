860b9f81ed67f29f149ed23aa887acd11fefbaba
Jason Frerich <jason.frerich@mattermost.com>
2021-12-08 10:56:59 -0600

    copy directly from v1. will get working and then convert class to functional components

---
+++ app/screens/create_channel/index.tsx
@@ -0,0 +1,29 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {getTheme} from '@mm-redux/selectors/entities/preferences';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {handleCreateChannel} from '@actions/views/create_channel';
+
+import CreateChannel from './create_channel';
+
+function mapStateToProps(state) {
+    const {createChannel: createChannelRequest} = state.requests.channels;
+
+    return {
+        createChannelRequest,
+        theme: getTheme(state),
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            handleCreateChannel,
+        }, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateChannel);
