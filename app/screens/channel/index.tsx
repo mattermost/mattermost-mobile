0efa4680006c4087c071d5e0282ccb3e3775b30f
Elias Nahum <nahumhbl@gmail.com>
2021-12-16 23:36:13 +0200

    Prevent channel screen from crashing when no channel id is selected

--- app/screens/channel/index.tsx
+++ app/screens/channel/index.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {Q} from '@nozbe/watermelondb';
 import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
 import withObservables from '@nozbe/with-observables';
 import {combineLatest, of as of$} from 'rxjs';
@@ -33,16 +34,34 @@ const enhanced = withObservables([], ({database}: WithDatabaseArgs) => {
     );
 
     const channel = channelId.pipe(
-        switchMap((id) => database.get<ChannelModel>(CHANNEL).findAndObserve(id)),
+        switchMap((id) => database.get<ChannelModel>(CHANNEL).query(Q.where('id', id)).observe().pipe(
+            // eslint-disable-next-line max-nested-callbacks
+            switchMap((channels) => {
+                if (channels.length) {
+                    return channels[0].observe();
+                }
+
+                return of$(null);
+            }),
+        )),
     );
 
     const channelInfo = channelId.pipe(
-        switchMap((id) => database.get<ChannelInfoModel>(CHANNEL_INFO).findAndObserve(id)),
+        switchMap((id) => database.get<ChannelInfoModel>(CHANNEL_INFO).query(Q.where('id', id)).observe().pipe(
+            // eslint-disable-next-line max-nested-callbacks
+            switchMap((infos) => {
+                if (infos.length) {
+                    return infos[0].observe();
+                }
+
+                return of$(null);
+            }),
+        )),
     );
 
     const isOwnDirectMessage = combineLatest([currentUserId, channel]).pipe(
         switchMap(([userId, ch]) => {
-            if (ch.type === General.DM_CHANNEL) {
+            if (ch?.type === General.DM_CHANNEL) {
                 const teammateId = getUserIdFromChannelName(userId, ch.name);
                 return of$(userId === teammateId);
             }
@@ -51,21 +70,21 @@ const enhanced = withObservables([], ({database}: WithDatabaseArgs) => {
         }),
     );
 
-    const displayName = channel.pipe(switchMap((c) => of$(c.displayName)));
+    const displayName = channel.pipe(switchMap((c) => of$(c?.displayName)));
     const name = combineLatest([currentUserId, channel]).pipe(switchMap(([userId, c]) => {
-        if (c.type === General.DM_CHANNEL) {
+        if (c?.type === General.DM_CHANNEL) {
             const teammateId = getUserIdFromChannelName(userId, c.name);
             return database.get<UserModel>(USER).findAndObserve(teammateId).pipe(
                 // eslint-disable-next-line max-nested-callbacks
                 switchMap((u) => of$(`@${u.username}`)),
             );
-        } else if (c.type === General.GM_CHANNEL) {
+        } else if (c?.type === General.GM_CHANNEL) {
             return of$(`@${c.name}`);
         }
 
-        return of$(c.name);
+        return of$(c?.name);
     }));
-    const memberCount = channelInfo.pipe(switchMap((ci) => of$(ci.memberCount)));
+    const memberCount = channelInfo.pipe(switchMap((ci) => of$(ci?.memberCount || 0)));
 
     return {
         channelId,
