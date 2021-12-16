f33e270338e9bb59683743ef162e14d6b89829d1
Elias Nahum <nahumhbl@gmail.com>
2021-12-16 21:02:14 +0200

    Decouple post list from db and perf tweaks

---
+++ app/components/post_list/refresh_control.tsx
@@ -0,0 +1,41 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {Platform, RefreshControl, StyleProp, ViewStyle} from 'react-native';
+
+type Props = {
+    children: React.ReactElement;
+    enabled: boolean;
+    onRefresh: () => void;
+    refreshing: boolean;
+    style?: StyleProp<ViewStyle>;
+}
+
+const PostListRefreshControl = ({children, enabled, onRefresh, refreshing, style}: Props) => {
+    const props = {
+        onRefresh,
+        refreshing,
+    };
+
+    if (Platform.OS === 'android') {
+        return (
+            <RefreshControl
+                {...props}
+                enabled={enabled}
+                style={style}
+            >
+                {children}
+            </RefreshControl>
+        );
+    }
+
+    const refreshControl = <RefreshControl {...props}/>;
+
+    return React.cloneElement(
+        children,
+        {refreshControl, inverted: true},
+    );
+};
+
+export default PostListRefreshControl;
