98fa1b0a55fc60298420fb571329c61da5d8009e
Elias Nahum <nahumhbl@gmail.com>
2022-02-08 16:16:30 -0300

    [Gekidou] compass icons (#5940)

--- app/components/compass_icon/index.tsx
+++ app/components/compass_icon/index.tsx
@@ -1,9 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import fontelloConfig from '@mattermost/compass-icons/config.json';
 import {createIconSetFromFontello} from 'react-native-vector-icons';
-
-import fontelloConfig from '@assets/compass-icons.json';
 
 const CompassIcon = createIconSetFromFontello(fontelloConfig, 'compass-icons',
     'compass-icons.ttf');
