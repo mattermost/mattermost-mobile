9b89893fc4ff5c930780465377bb8aa4dcf0648d
Jason Frerich <jason.frerich@mattermost.com>
2022-02-16 14:57:29 -0600

    make empty test files pass

--- app/database/operator/server_data_operator/transformers/group.test.ts
+++ app/database/operator/server_data_operator/transformers/group.test.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 export {};
+test.skip('skip', () => undefined);
 
 // import {
 //     transformGroupMembershipRecord,
