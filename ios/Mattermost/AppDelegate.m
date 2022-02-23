bd107b20c89f473fbb24fd40aaf750d24daa5140
Daniel Espino García <larkox@gmail.com>
2022-02-23 11:30:26 +0100

    Handle test notifications on iOS

--- ios/Mattermost/AppDelegate.m
+++ ios/Mattermost/AppDelegate.m
@@ -25,6 +25,7 @@
 NSString* const NOTIFICATION_MESSAGE_ACTION = @"message";
 NSString* const NOTIFICATION_CLEAR_ACTION = @"clear";
 NSString* const NOTIFICATION_UPDATE_BADGE_ACTION = @"update_badge";
+NSString* const NOTIFICATION_TEST_ACTION = @"test";
 MattermostBucket* bucket = nil;
 
 -(void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)(void))completionHandler {
@@ -90,6 +91,7 @@ MattermostBucket* bucket = nil;
   NSString* action = [userInfo objectForKey:@"type"];
   NSString* channelId = [userInfo objectForKey:@"channel_id"];
   BOOL isClearAction = (action && [action isEqualToString: NOTIFICATION_CLEAR_ACTION]);
+  BOOL isTestAction = (action && [action isEqualToString: NOTIFICATION_TEST_ACTION]);
 
   if (isClearAction) {
     // If received a notification that a channel was read, remove all notifications from that channel (only with app in foreground/background)
@@ -99,6 +101,8 @@ MattermostBucket* bucket = nil;
   
   if (state != UIApplicationStateActive || isClearAction) {
     [RNNotifications didReceiveBackgroundNotification:userInfo withCompletionHandler:completionHandler];
+  } else if (isTestAction) {
+    completionHandler(UIBackgroundFetchResultNoData);
   } else {
     completionHandler(UIBackgroundFetchResultNewData);
   }
