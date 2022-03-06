3f9ca96b9154a9025a6b1c92bbfc0c8cbe85627f
Elias Nahum <nahumhbl@gmail.com>
2022-03-06 07:40:30 -0300

    Add animation for reaction counter

--- app/components/post_list/post/body/reactions/reaction.tsx
+++ app/components/post_list/post/body/reactions/reaction.tsx
@@ -2,9 +2,11 @@
 // See LICENSE.txt for license information.
 
 import React, {useCallback} from 'react';
-import {Platform, Text} from 'react-native';
+import {View} from 'react-native';
+import AnimatedNumbers from 'react-native-animated-numbers';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
+import {typography} from '@app/utils/typography';
 import Emoji from '@components/emoji';
 import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
 
@@ -20,23 +22,30 @@ type ReactionProps = {
 const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
     return {
         count: {
-            color: theme.linkColor,
-            marginLeft: 6,
+            color: changeOpacity(theme.centerChannelColor, 0.56),
+            ...typography('Body', 100, 'SemiBold'),
         },
-        customEmojiStyle: {marginHorizontal: 3},
-        highlight: {backgroundColor: changeOpacity(theme.linkColor, 0.1)},
+        countHighlight: {
+            color: theme.buttonBg,
+        },
+        customEmojiStyle: {color: '#000'},
+        emoji: {marginRight: 4},
+        highlight: {
+            backgroundColor: changeOpacity(theme.buttonBg, 0.08),
+            borderColor: theme.buttonBg,
+            borderWidth: 1,
+        },
         reaction: {
             alignItems: 'center',
-            borderRadius: 2,
-            borderColor: changeOpacity(theme.linkColor, 0.4),
-            borderWidth: 1,
+            borderRadius: 4,
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.08),
             flexDirection: 'row',
-            height: 30,
-            marginRight: 6,
-            marginBottom: 5,
-            marginTop: 10,
-            paddingHorizontal: 6,
-            paddingBottom: Platform.select({android: 2}),
+            height: 32,
+            justifyContent: 'center',
+            marginBottom: 12,
+            marginRight: 8,
+            minWidth: 48,
+            paddingHorizontal: 8,
         },
     };
 });
@@ -55,19 +64,20 @@ const Reaction = ({count, emojiName, highlight, onPress, onLongPress, theme}: Re
             delayLongPress={350}
             style={[styles.reaction, (highlight && styles.highlight)]}
         >
-            <Emoji
-                emojiName={emojiName}
-                size={20}
-                textStyle={{color: '#000'}}
-                customEmojiStyle={styles.customEmojiStyle}
-                testID={`reaction.emoji.${emojiName}`}
+            <View style={styles.emoji}>
+                <Emoji
+                    emojiName={emojiName}
+                    size={20}
+                    textStyle={styles.customEmojiStyle}
+                    testID={`reaction.emoji.${emojiName}`}
+                />
+            </View>
+            <AnimatedNumbers
+                includeComma={false}
+                fontStyle={[styles.count, (highlight && styles.countHighlight)]}
+                animateToNumber={count}
+                animationDuration={450}
             />
-            <Text
-                style={styles.count}
-                testID={`reaction.emoji.${emojiName}.count`}
-            >
-                {count}
-            </Text>
         </TouchableOpacity>
     );
 };
