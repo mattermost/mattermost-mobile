e50986791f7802405fba68765a2e7dff80925148
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2022-03-07 14:50:06 -0500

    MM-41988 Updated links to legacy domain about.mm.com (#5962)
    * Updated links to legacy domain about.mm.com
    
    * about.mm.com URL updates
    
    * Fixed URL identified by QA
    
    * mm.org and forum URL updates
    
    * forum URL update
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- app/constants/about_links.js
+++ app/constants/about_links.js
@@ -2,6 +2,6 @@
 // See LICENSE.txt for license information.
 
 export default {
-    TERMS_OF_SERVICE: 'https://about.mattermost.com/default-terms/',
-    PRIVACY_POLICY: 'https://about.mattermost.com/default-privacy-policy/',
+    TERMS_OF_SERVICE: 'https://mattermost.com/terms-of-use/',
+    PRIVACY_POLICY: 'https://mattermost.com/privacy-policy/',
 };
