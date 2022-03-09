2ea48cc337260299cff0867706bb529c6319c3e3
Jason Frerich <jason.frerich@mattermost.com>
2022-03-09 16:52:42 -0600

    wip

--- app/actions/remote/channel.ts
+++ app/actions/remote/channel.ts
@@ -254,8 +254,6 @@ export const fetchMyChannel = async (serverUrl: string, teamId: string, channelI
             client.getChannelMember(channelId, 'me'),
         ]);
 
-        // console.log('member', member);
-
         if (!fetchOnly) {
             storeMyChannelsForTeam(serverUrl, channel.team_id || teamId, [channel], [member]);
         }
