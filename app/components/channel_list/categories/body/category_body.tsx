8ca85eb66a6d6973ee2ca8d9e872086d4e3eef03
Shaz MJ <s@shaz.com.au>
2022-03-11 10:16:02 +1100

    Adds category body channel item collapsing

--- app/components/channel_list/categories/body/category_body.tsx
+++ app/components/channel_list/categories/body/category_body.tsx
@@ -6,23 +6,23 @@ import {FlatList} from 'react-native';
 
 import ChannelListItem from './channel';
 
+import type CategoryModel from '@typings/database/models/servers/category';
+
 type Props = {
     currentChannelId: string;
     sortedIds: string[];
-    collapsed: boolean;
+    category: CategoryModel;
 };
 
 const extractKey = (item: any) => item;
-const itemLayout = (d: any, index: number) => (
-    {length: 40, offset: 40 * index, index}
-);
 
-const CategoryBody = ({currentChannelId, sortedIds}: Props) => {
+const CategoryBody = ({currentChannelId, sortedIds, category}: Props) => {
     const ChannelItem = useCallback(({item}: {item: string}) => {
         return (
             <ChannelListItem
                 channelId={item}
                 isActive={item === currentChannelId}
+                collapsed={category.collapsed}
             />
         );
     }, [currentChannelId]);
@@ -36,7 +36,6 @@ const CategoryBody = ({currentChannelId, sortedIds}: Props) => {
             initialNumToRender={20}
             windowSize={15}
             updateCellsBatchingPeriod={10}
-            getItemLayout={itemLayout}
         />
     );
 };
