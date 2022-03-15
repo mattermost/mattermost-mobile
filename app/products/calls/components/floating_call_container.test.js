545dd3d61d33536a15e6f136abed99a08da946eb
Christopher Poile <cpoile@gmail.com>
2022-03-15 16:28:03 -0400

    tests

--- app/products/calls/components/floating_call_container.test.js
+++ app/products/calls/components/floating_call_container.test.js
@@ -4,12 +4,28 @@
 import {shallow} from 'enzyme';
 import React from 'react';
 import {Text} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import FloatingCallContainer from './floating_call_container';
 
+function testSafeAreaProvider(children) {
+    return (
+        <SafeAreaProvider
+            initialMetrics={{
+                frame: {x: 0, y: 0, width: 0, height: 0},
+                insets: {top: 0, left: 0, right: 0, bottom: 0},
+            }}
+        >
+            {children}
+        </SafeAreaProvider>
+    );
+}
+
 describe('FloatingCallContainer', () => {
     test('should match snapshot', () => {
-        const wrapper = shallow(<FloatingCallContainer><Text>{'test'}</Text></FloatingCallContainer>);
+        const wrapper = shallow(testSafeAreaProvider(
+            <FloatingCallContainer><Text>{'test'}</Text></FloatingCallContainer>,
+        ));
 
         expect(wrapper.getElement()).toMatchSnapshot();
     });
