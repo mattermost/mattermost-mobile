ff225a6200a27961eac8d8a2adea50a5b5e6b9dd
Joseph Baylon <joseph.baylon@mattermost.com>
2022-03-16 19:03:17 -0700

    Revert changes on path_builder

--- detox/e2e/detox.pathbuilder.android.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-const CustomPathBuilder = require('./detox.pathbuilder');
-module.exports = ({rootDir}) => {
-    return new CustomPathBuilder({rootDir, platform: 'android'});
-};
--- detox/e2e/path_builder.js
+++ detox/e2e/path_builder.js
@@ -9,17 +9,17 @@ const SANITIZE_OPTIONS = {replacement: '_'};
 const sanitize = (filename) => sanitizeFilename(filename, SANITIZE_OPTIONS);
 
 class CustomPathBuilder {
-    constructor({platform, rootDir}) {
-        this.platform = platform;
+    constructor({rootDir}) {
         this.rootDir = rootDir;
     }
 
     buildPathForTestArtifact(artifactName, testSummary = null) {
         const fullName = (testSummary && testSummary.fullName) || '';
-        const segments = [this.rootDir, this.platform, sanitize(fullName), sanitize(artifactName)];
-
+        const segments = [this.rootDir, sanitize(fullName), sanitize(artifactName)];
         return path.join(...segments.filter(Boolean));
     }
 }
 
-module.exports = CustomPathBuilder;
+module.exports = ({rootDir}) => {
+    return new CustomPathBuilder({rootDir});
+};
