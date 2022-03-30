30a16d0bc1917bdf40e3fd17ed202761914eb2e9
Elias Nahum <nahumhbl@gmail.com>
2022-03-30 12:13:37 -0300

    [v1] Skip LoginOptions if only one SSO method is enabled (#6097)
    * Skip LoginOptions if only one SSO method is enabled
    
    * feedback review

---
+++ app/constants/sso.ts
@@ -0,0 +1,21 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import keyMirror from '@mm-redux/utils/key_mirror';
+
+export const REDIRECT_URL_SCHEME = 'mmauth://';
+export const REDIRECT_URL_SCHEME_DEV = 'mmauthbeta://';
+
+const constants = keyMirror({
+    SAML: null,
+    GITLAB: null,
+    GOOGLE: null,
+    OFFICE365: null,
+    OPENID: null,
+});
+
+export default {
+    ...constants,
+    REDIRECT_URL_SCHEME,
+    REDIRECT_URL_SCHEME_DEV,
+};
