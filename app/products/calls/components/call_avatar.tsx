2a56b6810b0bb8e63ad7ab86eab6b2218d8ab434
Christopher Poile <cpoile@gmail.com>
2022-03-30 17:45:53 -0400

    MM-42790 MM-42791 - UI Polish (#6103)
    * polish UI
    
    * update snapshots
    
    * add the '(you)' to the end of the user's own name

--- app/products/calls/components/call_avatar.tsx
+++ app/products/calls/components/call_avatar.tsx
@@ -25,7 +25,7 @@ const getStyleSheet = (props: Props) => {
 
     return StyleSheet.create({
         pictureHalo: {
-            backgroundColor: 'rgba(255,255,255,' + (0.16 * props.volume) + ')',
+            backgroundColor: 'rgba(61, 184, 135,' + (0.24 * props.volume) + ')',
             height: baseSize + 16,
             width: baseSize + 16,
             padding: 4,
@@ -33,16 +33,23 @@ const getStyleSheet = (props: Props) => {
             borderRadius: (baseSize + 16) / 2,
         },
         pictureHalo2: {
-            backgroundColor: 'rgba(255,255,255,' + (0.24 * props.volume) + ')',
+            backgroundColor: 'rgba(61, 184, 135,' + (0.32 * props.volume) + ')',
             height: baseSize + 8,
             width: baseSize + 8,
-            padding: 4,
+            padding: 3,
             borderRadius: (baseSize + 8) / 2,
         },
         picture: {
             borderRadius: baseSize / 2,
             height: baseSize,
             width: baseSize,
+            marginBottom: 5,
+        },
+        voiceShadow: {
+            shadowColor: 'rgb(61, 184, 135)',
+            shadowOffset: {width: 0, height: 0},
+            shadowOpacity: 1,
+            shadowRadius: 10,
         },
         mute: {
             position: 'absolute',
@@ -59,6 +66,13 @@ const getStyleSheet = (props: Props) => {
             textAlign: 'center',
             textAlignVertical: 'center',
             overflow: 'hidden',
+            ...Platform.select(
+                {
+                    ios: {
+                        padding: 2,
+                    },
+                },
+            ),
         },
         raisedHand: {
             position: 'absolute',
@@ -76,7 +90,8 @@ const getStyleSheet = (props: Props) => {
             ...Platform.select(
                 {
                     android: {
-                        padding: 4,
+                        paddingLeft: 4,
+                        paddingTop: 2,
                         color: 'rgb(255, 188, 66)',
                     },
                 },
@@ -105,6 +120,7 @@ const CallAvatar = (props: Props) => {
     const style = getStyleSheet(props);
     const profileSize = props.size === 'm' || !props.size ? 40 : 72;
     const iconSize = props.size === 'm' || !props.size ? 12 : 16;
+    const styleShadow = props.volume > 0 ? style.voiceShadow : {};
 
     // Only show one or the other.
     let topRightIcon: JSX.Element | null = null;
@@ -124,34 +140,42 @@ const CallAvatar = (props: Props) => {
         );
     }
 
-    return (
-        <View style={style.pictureHalo}>
-            <View style={style.pictureHalo2}>
-                <View style={style.picture}>
-                    {
-                        props.userId ?
-                            <ProfilePicture
-                                userId={props.userId}
-                                size={profileSize}
-                                showStatus={false}
-                            /> :
-                            <CompassIcon
-                                name='account-outline'
-                                size={profileSize}
-                            />
-                    }
-                    {
-                        props.muted !== undefined &&
-                        <CompassIcon
-                            name={props.muted ? 'microphone-off' : 'microphone'}
-                            size={iconSize}
-                            style={style.mute}
-                        />
-                    }
-                    {topRightIcon}
-                </View>
-            </View>
+    const view = (
+        <View style={[style.picture, styleShadow]}>
+            {
+                props.userId ?
+                    <ProfilePicture
+                        userId={props.userId}
+                        size={profileSize}
+                        showStatus={false}
+                    /> :
+                    <CompassIcon
+                        name='account-outline'
+                        size={profileSize}
+                    />
+            }
+            {
+                props.muted !== undefined &&
+                <CompassIcon
+                    name={props.muted ? 'microphone-off' : 'microphone'}
+                    size={iconSize}
+                    style={style.mute}
+                />
+            }
+            {topRightIcon}
         </View>
     );
+
+    if (Platform.OS === 'android') {
+        return (
+            <View style={style.pictureHalo}>
+                <View style={style.pictureHalo2}>
+                    {view}
+                </View>
+            </View>
+        );
+    }
+
+    return view;
 };
 export default CallAvatar;
