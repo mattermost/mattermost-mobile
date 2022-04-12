bf1bd42aefe71f9188ae697f12d084accdc64ac7
Joseph Baylon <joseph.baylon@mattermost.com>
2022-04-12 13:53:38 -0700

    Fix testIDs

--- app/screens/post_options/options/pin_channel_option.tsx
+++ app/screens/post_options/options/pin_channel_option.tsx
@@ -44,7 +44,7 @@ const PinChannelOption = ({isPostPinned, postId}: PinChannelProps) => {
             defaultMessage={defaultMessage}
             iconName='pin-outline'
             onPress={onPress}
-            testID={`post.options.${key}.channel.option`}
+            testID={`post_options.${key}.channel.option`}
         />
     );
 };
