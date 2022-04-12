2b3d84ee61939c2828815aa938894dee8233d4f2
Elias Nahum <nahumhbl@gmail.com>
2022-04-12 09:03:43 -0400

    Fix create DM when searching profile (#6162)

--- app/screens/create_direct_message/create_direct_message.tsx
+++ app/screens/create_direct_message/create_direct_message.tsx
@@ -149,7 +149,7 @@ export default function CreateDirectMessage({
     }, [selectedIds]);
 
     const createDirectChannel = useCallback(async (id: string): Promise<boolean> => {
-        const user = profiles.find((u) => u.id === id);
+        const user = selectedIds[id];
 
         const displayName = displayUsername(user, intl.locale, teammateNameDisplay);
 
@@ -170,7 +170,7 @@ export default function CreateDirectMessage({
         }
 
         return !result.error;
-    }, [profiles, intl.locale, teammateNameDisplay, serverUrl]);
+    }, [selectedIds, intl.locale, teammateNameDisplay, serverUrl]);
 
     const createGroupChannel = useCallback(async (ids: string[]): Promise<boolean> => {
         const result = await makeGroupChannel(serverUrl, ids);
