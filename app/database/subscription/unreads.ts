f131372d17787608b2c8bd1092bf4c2785445d13
Elias Nahum <nahumhbl@gmail.com>
2022-04-22 14:27:43 -0400

    set badge count on cancel notifications

--- app/database/subscription/unreads.ts
+++ app/database/subscription/unreads.ts
@@ -7,9 +7,9 @@ import {combineLatestWith} from 'rxjs/operators';
 
 import {MM_TABLES} from '@constants/database';
 import DatabaseManager from '@database/manager';
-import {observeThreadMentionCount} from '@queries/servers/thread';
-
-import type MyChannelModel from '@typings/database/models/servers/my_channel';
+import {queryMyTeams} from '@queries/servers/team';
+import {getIsCRTEnabled, observeThreadMentionCount, queryThreads} from '@queries/servers/thread';
+import MyChannelModel from '@typings/database/models/servers/my_channel';
 
 const {SERVER: {CHANNEL, MY_CHANNEL}} = MM_TABLES;
 
@@ -71,4 +71,38 @@ export const subscribeUnreadAndMentionsByServer = (serverUrl: string, observer: 
     }
 
     return subscription;
+};
+
+export const getTotalMentionsForServer = async (serverUrl: string) => {
+    const server = DatabaseManager.serverDatabases[serverUrl];
+    let count = 0;
+    if (server?.database) {
+        const {database} = server;
+        const myChannels = await database.get<MyChannelModel>(MY_CHANNEL).
+            query(
+                Q.on(CHANNEL, Q.where('delete_at', Q.eq(0))),
+                Q.where('mentions_count', Q.gt(0)),
+            ).fetch();
+
+        for (const mc of myChannels) {
+            count += mc.mentionsCount;
+        }
+
+        const isCRTEnabled = await getIsCRTEnabled(database);
+        if (isCRTEnabled) {
+            let includeDmGm = true;
+            const myTeamIds = await queryMyTeams(database).fetchIds();
+            for await (const teamId of myTeamIds) {
+                const threads = await queryThreads(database, teamId, false, includeDmGm).extend(
+                    Q.where('unread_mentions', Q.gt(0)),
+                ).fetch();
+                includeDmGm = false;
+                for (const t of threads) {
+                    count += t.unreadMentions;
+                }
+            }
+        }
+    }
+
+    return count;
 };
