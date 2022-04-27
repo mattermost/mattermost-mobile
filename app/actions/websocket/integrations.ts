ba4685c554baf79685ef7df9c7128094e8e4563f
Daniel Espino García <larkox@gmail.com>
2022-04-27 10:57:55 +0200

    Only set the dialog if we are in the same server

--- app/actions/websocket/integrations.ts
+++ app/actions/websocket/integrations.ts
@@ -1,15 +1,25 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+import DatabaseManager from '@database/manager';
 import IntegrationsManager from '@managers/integrations_manager';
+import {getActiveServerUrl} from '@queries/app/servers';
 
-export function handleOpenDialogEvent(serverUrl: string, msg: WebSocketMessage) {
+export async function handleOpenDialogEvent(serverUrl: string, msg: WebSocketMessage) {
     const data: string = msg.data?.dialog;
     if (!data) {
         return;
     }
+    const appDatabase = DatabaseManager.appDatabase?.database;
+    if (!appDatabase) {
+        return;
+    }
+
     try {
         const dialog: InteractiveDialogConfig = JSON.parse(data);
-        IntegrationsManager.getManager(serverUrl).setDialog(dialog);
+        const currentServer = await getActiveServerUrl(appDatabase);
+        if (currentServer === serverUrl) {
+            IntegrationsManager.getManager(serverUrl).setDialog(dialog);
+        }
     } catch {
         // Do nothing
     }
