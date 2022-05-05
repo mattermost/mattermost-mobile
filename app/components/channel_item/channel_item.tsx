5620a50975869701400ff06fdf763cf5bced8cbe
Elias Nahum <nahumhbl@gmail.com>
2022-05-05 08:53:02 -0400

    Fix channel item with long name and badge (#6222)

--- app/components/channel_item/channel_item.tsx
+++ app/components/channel_item/channel_item.tsx
@@ -55,7 +55,7 @@ export const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
     text: {
         marginTop: -1,
         color: changeOpacity(theme.sidebarText, 0.72),
-        paddingLeft: 12,
+        paddingHorizontal: 12,
     },
     highlight: {
         color: theme.sidebarText,
@@ -70,8 +70,9 @@ export const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
     badge: {
         position: 'relative',
         left: 0,
-        top: 0,
+        top: -2,
         alignSelf: undefined,
+        borderColor: theme.sidebarBg,
     },
     infoBadge: {
         color: theme.buttonColor,
