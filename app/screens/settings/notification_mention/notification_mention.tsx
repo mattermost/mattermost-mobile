2c8c714b5fd9e3d08722ffec5b9b758474ce857f
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-05-25 11:07:37 +0400

    Refactored to ReplySettings component

--- app/screens/settings/notification_mention/notification_mention.tsx
+++ app/screens/settings/notification_mention/notification_mention.tsx
@@ -1,25 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState} from 'react';
-import {ScrollView, View} from 'react-native';
+import React from 'react';
+import {ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-import FormattedText from '@components/formatted_text';
-import Section from '@components/section';
-import SectionItem from '@components/section_item';
 import {useTheme} from '@context/theme';
-import {t} from '@i18n';
 import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
 
 import MentionSettings from './mention_settings';
+import ReplySettings from './reply_settings';
 
 import type UserModel from '@typings/database/models/servers/user';
-
-const replyHeaderText = {
-    id: t('notification_settings.mention.reply'),
-    defaultMessage: 'SEND REPLY NOTIFICATIONS FOR',
-};
 
 const getStyleSheet = makeStyleSheetFromTheme((theme) => {
     return {
@@ -48,61 +40,8 @@ type NotificationMentionProps = {
     mentionKeys: string;
 }
 const NotificationMention = ({currentUser, mentionKeys, isCRTEnabled}: NotificationMentionProps) => {
-    const [replyNotificationType, setReplyNotificationType] = useState('any'); //todo: initialize with value from db/api
     const theme = useTheme();
     const styles = getStyleSheet(theme);
-
-    const setReplyNotifications = (notifType: string) => {
-        setReplyNotificationType(notifType);
-    };
-
-    const renderReplySection = () => {
-        return (
-            <Section
-                headerText={replyHeaderText}
-                containerStyles={styles.area}
-            >
-                <SectionItem
-                    label={(
-                        <FormattedText
-                            id='notification_settings.threads_start_participate'
-                            defaultMessage='Threads that I start or participate in'
-                        />
-                    )}
-                    action={setReplyNotifications}
-                    actionType='select'
-                    actionValue='any'
-                    selected={replyNotificationType === 'any'}
-                />
-                <View style={styles.separator}/>
-                <SectionItem
-                    label={(
-                        <FormattedText
-                            id='notification_settings.threads_start'
-                            defaultMessage='Threads that I start'
-                        />
-                    )}
-                    action={setReplyNotifications}
-                    actionType='select'
-                    actionValue='root'
-                    selected={replyNotificationType === 'root'}
-                />
-                <View style={styles.separator}/>
-                <SectionItem
-                    label={(
-                        <FormattedText
-                            id='notification_settings.threads_mentions'
-                            defaultMessage='Mentions in threads'
-                        />
-                    )}
-                    action={setReplyNotifications}
-                    actionType='select'
-                    actionValue='never'
-                    selected={replyNotificationType === 'never'}
-                />
-            </Section>
-        );
-    };
 
     return (
         <SafeAreaView
@@ -120,7 +59,7 @@ const NotificationMention = ({currentUser, mentionKeys, isCRTEnabled}: Notificat
                     currentUser={currentUser}
                 />
                 {!isCRTEnabled && (
-                    renderReplySection()
+                    <ReplySettings/>
                 )}
             </ScrollView>
         </SafeAreaView>
---
+++ app/screens/settings/notification_mention/reply_settings.tsx
@@ -0,0 +1,89 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useState} from 'react';
+import {View} from 'react-native';
+
+import FormattedText from '@components/formatted_text';
+import Section from '@components/section';
+import SectionItem from '@components/section_item';
+import {useTheme} from '@context/theme';
+import {t} from '@i18n';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
+
+const replyHeaderText = {
+    id: t('notification_settings.mention.reply'),
+    defaultMessage: 'SEND REPLY NOTIFICATIONS FOR',
+};
+
+const getStyleSheet = makeStyleSheetFromTheme((theme) => {
+    return {
+        area: {
+            paddingHorizontal: 16,
+        },
+        separator: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
+            flex: 1,
+            height: 1,
+            marginLeft: 15,
+        },
+    };
+});
+
+const ReplySettings = () => {
+    const [replyNotificationType, setReplyNotificationType] = useState('any'); //todo: initialize with value from db/api
+    const theme = useTheme();
+    const styles = getStyleSheet(theme);
+
+    const setReplyNotifications = (notifType: string) => {
+        setReplyNotificationType(notifType);
+    };
+
+    return (
+        <Section
+            headerText={replyHeaderText}
+            containerStyles={styles.area}
+        >
+            <SectionItem
+                label={(
+                    <FormattedText
+                        id='notification_settings.threads_start_participate'
+                        defaultMessage='Threads that I start or participate in'
+                    />
+                )}
+                action={setReplyNotifications}
+                actionType='select'
+                actionValue='any'
+                selected={replyNotificationType === 'any'}
+            />
+            <View style={styles.separator}/>
+            <SectionItem
+                label={(
+                    <FormattedText
+                        id='notification_settings.threads_start'
+                        defaultMessage='Threads that I start'
+                    />
+                )}
+                action={setReplyNotifications}
+                actionType='select'
+                actionValue='root'
+                selected={replyNotificationType === 'root'}
+            />
+            <View style={styles.separator}/>
+            <SectionItem
+                label={(
+                    <FormattedText
+                        id='notification_settings.threads_mentions'
+                        defaultMessage='Mentions in threads'
+                    />
+                )}
+                action={setReplyNotifications}
+                actionType='select'
+                actionValue='never'
+                selected={replyNotificationType === 'never'}
+            />
+        </Section>
+    );
+};
+
+export default ReplySettings;
