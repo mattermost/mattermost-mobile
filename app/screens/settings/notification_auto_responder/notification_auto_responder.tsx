8a2db8510710bee9bdd7a24172bc26812122d1c8
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-06-08 15:02:43 +0400

    implemented auto responder functionality

--- app/screens/settings/notification_auto_responder/notification_auto_responder.tsx
+++ app/screens/settings/notification_auto_responder/notification_auto_responder.tsx
@@ -1,17 +1,22 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {useIntl} from 'react-intl';
 import {View} from 'react-native';
 import {Edge, SafeAreaView} from 'react-native-safe-area-context';
 
+import {updateMe} from '@actions/remote/user';
 import FloatingTextInput from '@components/floating_text_input_label';
 import FormattedText from '@components/formatted_text';
 import OptionItem from '@components/option_item';
 import {General} from '@constants';
+import {useServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
+import useAndroidHardwareBackHandler from '@hooks/android_back_handler';
+import useNavButtonPressed from '@hooks/navigation_button_pressed';
 import {t} from '@i18n';
+import {popTopScreen, setButtons} from '@screens/navigation';
 import {changeOpacity, makeStyleSheetFromTheme, getKeyboardAppearanceFromTheme} from '@utils/theme';
 import {typography} from '@utils/typography';
 import {getNotificationProps} from '@utils/user';
@@ -27,6 +32,9 @@ const OOO = {
     id: t('notification_settings.auto_responder.default_message'),
     defaultMessage: 'Hello, I am out of office and unable to respond to messages.',
 };
+const SAVE_OOO_BUTTON_ID = 'notification_settings.auto_responder.save.button';
+
+const edges: Edge[] = ['left', 'right'];
 
 const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
     return {
@@ -67,13 +75,13 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
     };
 });
 
-const edges: Edge[] = ['left', 'right'];
 type NotificationAutoResponderProps = {
+    componentId: string;
     currentUser: UserModel;
 }
-const NotificationAutoResponder = ({currentUser}: NotificationAutoResponderProps) => {
+const NotificationAutoResponder = ({currentUser, componentId}: NotificationAutoResponderProps) => {
     const theme = useTheme();
-    const autoresponderRef = useRef(null);
+    const serverUrl = useServerUrl();
     const intl = useIntl();
     const userNotifyProps = useMemo(() => getNotificationProps(currentUser), [currentUser.notifyProps]);
 
@@ -85,19 +93,68 @@ const NotificationAutoResponder = ({currentUser}: NotificationAutoResponderProps
 
     const styles = getStyleSheet(theme);
 
+    const close = useCallback(() => popTopScreen(componentId), [componentId]);
+
+    const saveButton = useMemo(() => {
+        return {
+            id: SAVE_OOO_BUTTON_ID,
+            enabled: false,
+            showAsAction: 'always' as const,
+            testID: 'notification_settings.auto_res.save.button',
+            color: theme.sidebarHeaderTextColor,
+            text: intl.formatMessage({id: 'settings.save', defaultMessage: 'Save'}),
+        };
+    }, [theme.sidebarHeaderTextColor]);
+
+    const canSave = useCallback((enabled: boolean) => {
+        const buttons = {
+            rightButtons: [{
+                ...saveButton,
+                enabled,
+            }],
+        };
+        setButtons(componentId, buttons);
+    }, [componentId, saveButton]);
+
+    //fixme: review all imported code
+    const shouldSaveAutoResponder = useCallback((notify: UserNotifyProps) => {
+        const {auto_responder_active: autoResponderActive} = notify;
+        const prevProps = getNotificationProps(currentUser);
+        const enabling = currentUser.status !== General.OUT_OF_OFFICE && autoResponderActive === 'true'; // ?? still valid ??
+        const disabling = currentUser.status === General.OUT_OF_OFFICE && autoResponderActive === 'false'; // ?? still valid ??
+        const updatedMsg = prevProps.auto_responder_message !== notify.auto_responder_message; // ?? still valid ??
+
+        canSave(enabling || disabling || updatedMsg);
+    }, [currentUser.status, canSave]);
+
+    //todo: compress onAutoResponseToggle and onAutoResponseChangeText
+
     const onAutoResponseToggle = useCallback((active: boolean) => {
-        setNotifyProps({
-            ...notifyProps,
-            auto_responder_active: `${active}`,
-        });
+        const notify = {...notifyProps, auto_responder_active: `${active}`} as unknown as UserNotifyProps;
+        setNotifyProps({...notifyProps, auto_responder_active: `${active}`});
+        shouldSaveAutoResponder(notify);
     }, [notifyProps]);
 
     const onAutoResponseChangeText = useCallback((message: string) => {
-        setNotifyProps({
-            ...notifyProps,
-            auto_responder_message: message,
-        });
+        const notify = {...notifyProps, auto_responder_message: message} as unknown as UserNotifyProps;
+        setNotifyProps(notify);
+        shouldSaveAutoResponder(notify);
     }, [notifyProps]);
+
+    const saveAutoResponder = useCallback(() => {
+        updateMe(serverUrl, {notify_props: notifyProps as unknown as UserNotifyProps});
+        close();
+    }, [serverUrl, notifyProps]);
+
+    useEffect(() => {
+        setButtons(componentId, {
+            rightButtons: [saveButton],
+        });
+    }, []);
+
+    useNavButtonPressed(SAVE_OOO_BUTTON_ID, componentId, () => saveAutoResponder(), [notifyProps]);
+
+    useAndroidHardwareBackHandler(componentId, close);
 
     return (
         <SafeAreaView
@@ -127,7 +184,6 @@ const NotificationAutoResponder = ({currentUser}: NotificationAutoResponderProps
                         onChangeText={onAutoResponseChangeText}
                         placeholder={intl.formatMessage(headerText)}
                         placeholderTextColor={changeOpacity(theme.centerChannelColor, 0.4)}
-                        ref={autoresponderRef}
                         returnKeyType='done'
                         style={styles.input}
                         textAlignVertical='top'
