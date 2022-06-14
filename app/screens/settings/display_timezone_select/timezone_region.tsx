edbb42e55eb1cea80888ad3b89e856be2db30627
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-06-14 21:37:45 +0400

    gets all supported tz

--- app/screens/settings/display_timezone_select/timezone_region.tsx
+++ app/screens/settings/display_timezone_select/timezone_region.tsx
@@ -1,16 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useIntl} from 'react-intl';
 import {FlatList, View} from 'react-native';
 import {Edge, SafeAreaView} from 'react-native-safe-area-context';
 
 import Search from '@components/search';
 import {List} from '@constants';
+import {useServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
+import NetworkManager from '@managers/network_manager';
 import {popTopScreen} from '@screens/navigation';
 import {changeOpacity, getKeyboardAppearanceFromTheme, makeStyleSheetFromTheme} from '@utils/theme';
+import {typography} from '@utils/typography';
 import {getTimezoneRegion} from '@utils/user';
 
 import TimezoneRow from './timezone_row';
@@ -21,13 +24,11 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
             flex: 1,
             backgroundColor: theme.centerChannelBg,
         },
-
         searchBarInput: {
             backgroundColor: changeOpacity(theme.centerChannelColor, 0.2),
             color: theme.centerChannelColor,
-            fontSize: 15,
+            ...typography('Body', 100, 'Regular'),
         },
-
         searchBar: {
             height: 38,
             marginVertical: 5,
@@ -35,22 +36,30 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
     };
 });
 
+const EDGES: Edge[] = ['left', 'right'];
+const EMPTY_TIMEZONES: string[] = [];
 const ITEM_HEIGHT = 45;
 const VIEWABILITY_CONFIG = List.VISIBILITY_CONFIG_DEFAULTS;
-const edges: Edge[] = ['left', 'right'];
+
 const keyExtractor = (item: string) => item;
+const getItemLayout = (_data: string[], index: number) => ({
+    length: ITEM_HEIGHT,
+    offset: ITEM_HEIGHT * index,
+    index,
+});
 
 type SelectTimezonesProps = {
     selectedTimezone: string;
-    initialScrollIndex: number;
-    allTimezones: string[];
+    onBack: (tz: string) => void;
 }
-const SelectTimezones = ({allTimezones, selectedTimezone, initialScrollIndex}: SelectTimezonesProps) => {
+const SelectTimezones = ({selectedTimezone, onBack}: SelectTimezonesProps) => {
     const intl = useIntl();
+    const serverUrl = useServerUrl();
     const theme = useTheme();
     const styles = getStyleSheet(theme);
 
-    const [timezones, setTimezones] = useState(allTimezones);
+    const [timezones, setTimezones] = useState<string[]>(EMPTY_TIMEZONES);
+    const [initialScrollIndex, setInitialScrollIndex] = useState<number>(0);
     const [value, setValue] = useState('');
 
     const filteredTimezones = (timezonePrefix: string) => {
@@ -66,8 +75,8 @@ const SelectTimezones = ({allTimezones, selectedTimezone, initialScrollIndex}: S
         ));
     };
 
-    const timezoneSelected = (tzne: string) => {
-        //fixme:  props.onBack(timezone);
+    const onPressTimezone = (tzne: string) => {
+        onBack(tzne);
         popTopScreen();
     };
 
@@ -75,27 +84,41 @@ const SelectTimezones = ({allTimezones, selectedTimezone, initialScrollIndex}: S
         setValue(text);
     };
 
-    const getItemLayout = (data: string[], index: number) => ({
-        length: ITEM_HEIGHT,
-        offset: ITEM_HEIGHT * index,
-        index,
-    });
-
     const renderItem = ({item: timezone}: {item: string}) => {
         return (
             <TimezoneRow
-                timezone={timezone}
+                onPressTimezone={onPressTimezone}
                 selectedTimezone={selectedTimezone}
-                onPressTimezone={timezoneSelected}
+                timezone={timezone}
             />
         );
     };
 
+    useEffect(() => {
+        // let's get all supported timezones
+        const getSupportedTimezones = async () => {
+            try {
+                const client = NetworkManager.getClient(serverUrl);
+                const allTzs = await client.getTimezones();
+                if (Array.isArray(allTzs)) {
+                    setTimezones(allTzs);
+                    const timezoneIndex = allTzs.findIndex((timezone) => timezone === selectedTimezone);
+                    if (timezoneIndex > 0) {
+                        setInitialScrollIndex(timezoneIndex);
+                    }
+                }
+            } catch (error) {
+                setTimezones(EMPTY_TIMEZONES);
+            }
+        };
+        getSupportedTimezones();
+    }, []);
+
     return (
         <SafeAreaView
-            testID='settings.select_timezone.screen'
-            edges={edges}
+            edges={EDGES}
             style={styles.container}
+            testID='settings.select_timezone.screen'
         >
             <View style={styles.searchBar}>
                 <Search
@@ -113,14 +136,14 @@ const SelectTimezones = ({allTimezones, selectedTimezone, initialScrollIndex}: S
             </View>
             <FlatList
                 data={filteredTimezones(value)}
+                getItemLayout={getItemLayout}
+                initialScrollIndex={initialScrollIndex}
+                keyExtractor={keyExtractor}
+                keyboardDismissMode='on-drag'
+                keyboardShouldPersistTaps='always'
+                maxToRenderPerBatch={15}
                 removeClippedSubviews={true}
                 renderItem={renderItem}
-                keyExtractor={keyExtractor}
-                getItemLayout={getItemLayout}
-                keyboardShouldPersistTaps='always'
-                keyboardDismissMode='on-drag'
-                maxToRenderPerBatch={15}
-                initialScrollIndex={initialScrollIndex}
                 viewabilityConfig={VIEWABILITY_CONFIG}
             />
         </SafeAreaView>
