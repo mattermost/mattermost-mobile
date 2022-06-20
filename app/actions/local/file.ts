105b704d504d73acaf4e9aeaa5ffc40f5412b41e
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-06-20 11:45:50 +0400

    adv settings delete

--- app/actions/local/file.ts
+++ app/actions/local/file.ts
@@ -1,8 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import FileSystem from 'react-native-fs';
+
 import DatabaseManager from '@database/manager';
+import {queryAllServers} from '@queries/app/servers';
 import {getFileById} from '@queries/servers/file';
+import {hashCode} from '@utils/security';
 
 export const updateLocalFile = async (serverUrl: string, file: FileInfo) => {
     try {
@@ -31,6 +35,36 @@ export const updateLocalFilePath = async (serverUrl: string, fileId: string, loc
     } catch (error) {
         // eslint-disable-next-line no-console
         console.log('Failed updateLocalFilePath', error);
+        return {error};
+    }
+};
+
+export const getAllFilesInCachesDirectory = async () => {
+    try {
+        const appDatabase = DatabaseManager.appDatabase;
+        const servers = await queryAllServers(appDatabase!.database);
+        if (!servers.length) {
+            return {error: 'No servers'};
+        }
+
+        const cachesDirectories = servers.map((server) => `${FileSystem.CachesDirectoryPath}/${hashCode(server.url)}`);
+
+        const files: FileSystem.ReadDirItem[][] = [];
+
+        for await (const directory of cachesDirectories) {
+            const directoryFiles = await FileSystem.readDir(directory);
+            files.push(directoryFiles);
+        }
+        const flattenedFiles = files.flat();
+        const totalSize = flattenedFiles.reduce((acc, file) => acc + file.size, 0);
+
+        return {
+            files: flattenedFiles,
+            totalSize,
+        };
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('Failed getAllFilesInCachesDirectory', error);
         return {error};
     }
 };
--- app/screens/settings/advanced/index.tsx
+++ app/screens/settings/advanced/index.tsx
@@ -1,5 +1,159 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import AdvancedSettings from './advanced_settings';
+
+import CameraRoll from '@react-native-community/cameraroll';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import FileSystem, {ReadDirItem} from 'react-native-fs';
+import {Edge, SafeAreaView} from 'react-native-safe-area-context';
+
+import {getAllFilesInCachesDirectory} from '@actions/local/file';
+import MenuItem from '@components/menu_item';
+import {useTheme} from '@context/theme';
+import useAndroidHardwareBackHandler from '@hooks/android_back_handler';
+import {popTopScreen} from '@screens/navigation';
+import {getFormattedFileSize} from '@utils/file';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
+import {typography} from '@utils/typography';
+
+const getStyleSheet = makeStyleSheetFromTheme((theme) => {
+    return {
+        container: {
+            flex: 1,
+            backgroundColor: theme.centerChannelBg,
+        },
+        wrapper: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.06),
+            flex: 1,
+            paddingTop: 35,
+        },
+        divider: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
+            height: 1,
+        },
+        containerStyle: {
+            backgroundColor: theme.centerChannelBg,
+            height: 48,
+        },
+        fileSize: {
+            color: theme.centerChannelColor,
+            ...typography('Body', 100, 'Regular'),
+        },
+        iconContainerStyle: {
+            height: '100%',
+        },
+        rightComponent: {
+            justifyContent: 'center',
+            height: '100%',
+            marginRight: 10,
+        },
+    };
+});
+const EMPTY_FILES: ReadDirItem[] = [];
+const PHOTO_URIS: string[] = [];
+const EDGES: Edge[] = ['left', 'right'];
+
+const getAppPhotoUris = async (applicationName: string, endCursor?: string) => {
+    const photos = await CameraRoll.getPhotos({
+        first: 200,
+        ...(endCursor && {after: endCursor}),
+        assetType: 'All',
+        groupName: applicationName,
+        groupTypes: 'Album',
+    });
+
+    for (const photo of photos.edges) {
+        PHOTO_URIS.push(photo.node.image.uri);
+    }
+
+    if (photos.page_info.has_next_page) {
+        await getAppPhotoUris(applicationName, photos.page_info.end_cursor);
+    }
+};
+
+type AdvancedSettingsProps = {
+    componentId: string;
+}
+const AdvancedSettings = ({componentId}: AdvancedSettingsProps) => {
+    const theme = useTheme();
+    const [dataSize, setDataSize] = useState<number|undefined>(0);
+    const [files, setFiles] = useState<ReadDirItem[]>(EMPTY_FILES);
+
+    const styles = getStyleSheet(theme);
+    const applicationName = DeviceInfo.getApplicationName();
+
+    const getAllCachedFiles = useCallback(async () => {
+        const {totalSize, files: cachedFiles} = await getAllFilesInCachesDirectory();
+        setDataSize(totalSize);
+        setFiles(cachedFiles || EMPTY_FILES);
+    }, []);
+
+    const onPressDeleteData = useCallback(async () => {
+        try {
+            const delFilePromises = [];
+            for (const file of files) {
+                delFilePromises.push(FileSystem.unlink(file.path));
+            }
+            await Promise.all(delFilePromises);
+            await getAppPhotoUris(applicationName);
+
+            if (PHOTO_URIS.length > 0) {
+                await CameraRoll.deletePhotos(PHOTO_URIS);
+            }
+            await getAllCachedFiles();
+        } catch (e) {
+            //todo: show toast if error ?
+        }
+    }, [files]);
+
+    useEffect(() => {
+        getAllCachedFiles();
+    }, []);
+
+    const renderFileSize = useCallback(() => {
+        return (
+            <View
+                style={styles.rightComponent}
+            >
+                <Text
+                    style={styles.fileSize}
+                >
+                    {getFormattedFileSize(dataSize || 0)}
+                </Text>
+            </View>
+        );
+    }, [dataSize, theme.centerChannelColor]);
+
+    const close = () => popTopScreen(componentId);
+    useAndroidHardwareBackHandler(componentId, close);
+
+    return (
+        <SafeAreaView
+            edges={EDGES}
+            style={styles.container}
+            testID='settings_display.screen'
+        >
+            <View
+                style={styles.wrapper}
+            >
+                <MenuItem
+                    containerStyle={styles.containerStyle}
+                    defaultMessage='Delete Documents & Data'
+                    i18nId='advanced_settings.delete_data'
+                    iconContainerStyle={styles.iconContainerStyle}
+                    iconName='trash-can-outline'
+                    isDestructor={true}
+                    onPress={onPressDeleteData}
+                    rightComponent={renderFileSize()}
+                    separator={false}
+                    showArrow={false}
+                    testID='advanced_settings.delete_data'
+                    theme={theme}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
 export default AdvancedSettings;
