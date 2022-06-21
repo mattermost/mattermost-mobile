8518d780c256ea83729a10b22ba5ba28c1ce13cf
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-06-21 17:55:00 +0400

    Update app/screens/settings/index.tsx
    Co-authored-by: Daniel Espino García <larkox@gmail.com>

--- app/screens/settings/index.tsx
+++ app/screens/settings/index.tsx
@@ -15,8 +15,8 @@ import type {WithDatabaseArgs} from '@typings/database/database';
 
 const enhanced = withObservables([], ({database}: WithDatabaseArgs) => {
     const siteName = observeConfigValue(database, 'SiteName');
-    const showHelp = observeConfigValue(database, 'HelpLink').pipe(switchMap((link) => of$(link ? isValidUrl(link) : false)));
     const helpLink = observeConfigValue(database, 'HelpLink');
+    const showHelp = helpLink.pipe(switchMap((link) => of$(link ? isValidUrl(link) : false)));
 
     return {
         showHelp,
