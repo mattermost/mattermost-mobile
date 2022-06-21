8ecbd40427ab31b7272e15bdede86408f12932af
Jason Frerich <jason.frerich@mattermost.com>
2022-06-21 15:58:26 -0500

    use a useMemo instead of useCallback

--- app/screens/home/search/results/results.tsx
+++ app/screens/home/search/results/results.tsx
@@ -64,7 +64,7 @@ const SearchResults = ({
     const paddingTop = useMemo(() => ({paddingTop: scrollPaddingTop, flexGrow: 1}), [scrollPaddingTop]);
     const orderedPosts = useMemo(() => selectOrderedPosts(posts, 0, false, '', '', false, isTimezoneEnabled, currentTimezone, false).reverse(), [posts]);
 
-    const getContainerStyle = useCallback(() => {
+    const getContainerStyle = useMemo(() => {
         let padding = 0;
         if (selectedTab === TabTypes.MESSAGES) {
             padding = posts.length ? 4 : 8;
@@ -153,7 +153,7 @@ const SearchResults = ({
             onScroll={onScroll}
             removeClippedSubviews={true}
             ref={scrollRef}
-            style={getContainerStyle()}
+            style={getContainerStyle}
         />
     );
 };
