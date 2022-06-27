9c85021f3f3536e5ba0a1711ed2ce40ce18d16c4
Jason Frerich <jason.frerich@mattermost.com>
2022-06-26 22:58:34 -0500

    create useImageAttachments hook and share with files component and results

--- app/screens/home/search/results/results.tsx
+++ app/screens/home/search/results/results.tsx
@@ -5,17 +5,16 @@ import React, {useCallback, useMemo} from 'react';
 import {StyleSheet, FlatList, ListRenderItemInfo, NativeScrollEvent, NativeSyntheticEvent, Text, StyleProp, View, ViewStyle} from 'react-native';
 import Animated, {useDerivedValue} from 'react-native-reanimated';
 
-import {buildFilePreviewUrl, buildFileUrl} from '@actions/remote/file';
 import File from '@components/files/file';
 import NoResultsWithTerm from '@components/no_results_with_term';
 import DateSeparator from '@components/post_list/date_separator';
 import PostWithChannelInfo from '@components/post_with_channel_info';
 import {Screens} from '@constants';
-import {useServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
 import {PostModel, ChannelModel} from '@database/models/server';
 import {useIsTablet} from '@hooks/device';
-import {isGif, isImage, isVideo} from '@utils/file';
+import {useImageAttachments} from '@hooks/files';
+import {isImage, isVideo} from '@utils/file';
 import {fileToGalleryItem, openGalleryAtIndex} from '@utils/gallery';
 import {getViewPortWidth} from '@utils/images';
 import {getDateForDateLine, isDateLine, selectOrderedPosts} from '@utils/post_list';
@@ -82,9 +81,10 @@ const Results = ({
     const theme = useTheme();
     const paddingTop = useMemo(() => ({paddingTop: scrollPaddingTop, flexGrow: 1}), [scrollPaddingTop]);
     const orderedPosts = useMemo(() => selectOrderedPosts(posts, 0, false, '', '', false, isTimezoneEnabled, currentTimezone, false).reverse(), [posts]);
-    const serverUrl = useServerUrl();
     const isTablet = useIsTablet();
     const galleryIdentifier = 'search-files-location';
+
+    const {images: imageAttachments, nonImages: nonImageAttachments} = useImageAttachments(fileInfos, publicLinkEnabled);
 
     const getContainerStyle = useMemo(() => {
         let padding = 0;
@@ -112,30 +112,6 @@ const Results = ({
         /* eslint-disable no-console */
         console.log('Options button pressed for..  ', idx);
     });
-
-    const {images: imageAttachments, nonImages: nonImageAttachments} = useMemo(() => {
-        return fileInfos.reduce(({images, nonImages}: {images: FileInfo[]; nonImages: FileInfo[]}, file) => {
-            const imageFile = isImage(file);
-            const videoFile = isVideo(file);
-            if (imageFile || (videoFile && publicLinkEnabled)) {
-                let uri;
-                if (file.localPath) {
-                    uri = file.localPath;
-                } else {
-                    uri = (isGif(file) || videoFile) ? buildFileUrl(serverUrl, file.id!) : buildFilePreviewUrl(serverUrl, file.id!);
-                }
-                images.push({...file, uri});
-            } else {
-                if (videoFile) {
-                    // fallback if public links are not enabled
-                    file.uri = buildFileUrl(serverUrl, file.id!);
-                }
-
-                nonImages.push(file);
-            }
-            return {images, nonImages};
-        }, {images: [], nonImages: []});
-    }, [fileInfos, publicLinkEnabled, serverUrl]);
 
     const filesForGallery = useDerivedValue(() => imageAttachments.concat(nonImageAttachments),
         [imageAttachments, nonImageAttachments]);
