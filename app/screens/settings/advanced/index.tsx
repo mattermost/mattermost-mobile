5cd44043174499d44cdc1868647a9076a1abb49e
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-07-08 14:33:14 +0400

    ts fix

--- app/screens/settings/advanced/index.tsx
+++ app/screens/settings/advanced/index.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useIntl} from 'react-intl';
 import {View, TouchableOpacity} from 'react-native';
 import {Edge, SafeAreaView} from 'react-native-safe-area-context';
