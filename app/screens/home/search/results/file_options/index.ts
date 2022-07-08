77e86c79fc2f26cd8410f42f59e5e4598a893874
Jason Frerich <jason.frerich@mattermost.com>
2022-07-08 12:42:29 -0500

    use observeConfigBooleanValue to get config values

--- app/screens/home/search/results/file_options/index.ts
+++ app/screens/home/search/results/file_options/index.ts
@@ -5,17 +5,17 @@ import withObservables from '@nozbe/with-observables';
 import {combineLatest, of as of$} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
 
-import {observeConfig, observeLicense} from '@queries/servers/system';
+import {observeConfigBooleanValue, observeLicense} from '@queries/servers/system';
 
 import FileOptions from './file_options';
 
 import type {WithDatabaseArgs} from '@typings/database/database';
 
 const enhanced = withObservables([], ({database}: WithDatabaseArgs) => {
-    const config = observeConfig(database);
     const license = observeLicense(database);
-    const enablePublicLink = config.pipe(switchMap((c) => of$(c?.EnablePublicLink === 'true')));
-    const enableMobileFileDownload = config.pipe(switchMap((c) => of$(c?.EnableMobileFileDownload !== 'false')));
+    const enablePublicLink = observeConfigBooleanValue(database, 'EnablePublicLink');
+    const enableMobileFileDownload = observeConfigBooleanValue(database, 'EnableMobileFileDownload');
+
     const complianceDisabled = license.pipe(switchMap((l) => of$(l?.IsLicensed === 'false' || l?.Compliance === 'false')));
     const canDownloadFiles = combineLatest([enableMobileFileDownload, complianceDisabled]).pipe(
         switchMap(([download, compliance]) => of$(compliance || download)),
