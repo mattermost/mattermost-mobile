102789bbd9c7a557096ff77aee4332ba81c0dcd3
Anurag Shivarathri <anurag6713@gmail.com>
2022-07-22 17:28:14 +0530

    [Gekidou MM-40089 MM-39318] CRT New Messages Line (#6489)
    * New Messages Line + More Messages
    
    * Misc
    
    * Update app/actions/local/thread.ts
    
    Co-authored-by: Avinash Lingaloo <avinashlng1080@gmail.com>
    
    Co-authored-by: Avinash Lingaloo <avinashlng1080@gmail.com>

--- app/components/post_list/more_messages/more_messages.tsx
+++ app/components/post_list/more_messages/more_messages.tsx
@@ -19,11 +19,13 @@ import type PostModel from '@typings/database/models/servers/post';
 
 type Props = {
     channelId: string;
-    isManualUnread: boolean;
+    isCRTEnabled?: boolean;
+    isManualUnread?: boolean;
     newMessageLineIndex: number;
     posts: Array<string | PostModel>;
     registerScrollEndIndexListener: (fn: (endIndex: number) => void) => () => void;
     registerViewableItemsListener: (fn: (viewableItems: ViewToken[]) => void) => () => void;
+    rootId?: string;
     scrollToIndex: (index: number, animated?: boolean, applyOffset?: boolean) => void;
     unreadCount: number;
     theme: Theme;
@@ -91,11 +93,13 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
 
 const MoreMessages = ({
     channelId,
+    isCRTEnabled,
     isManualUnread,
     newMessageLineIndex,
     posts,
     registerViewableItemsListener,
     registerScrollEndIndexListener,
+    rootId,
     scrollToIndex,
     unreadCount,
     testID,
@@ -110,7 +114,7 @@ const MoreMessages = ({
     const [remaining, setRemaining] = useState(0);
     const underlayColor = useMemo(() => `hsl(${hexToHue(theme.buttonBg)}, 50%, 38%)`, [theme]);
     const top = useSharedValue(0);
-    const shownTop = isTablet ? 5 : SHOWN_TOP;
+    const shownTop = isTablet || isCRTEnabled ? 5 : SHOWN_TOP;
     const BARS_FACTOR = Math.abs((1) / (HIDDEN_TOP - SHOWN_TOP));
     const styles = getStyleSheet(theme);
     const animatedStyle = useAnimatedStyle(() => ({
@@ -134,8 +138,18 @@ const MoreMessages = ({
         }],
     }), [isTablet, shownTop]);
 
+    // Due to the implementation differences "unreadCount" gets updated for a channel on reset but not for a thread.
+    // So we maintain a localUnreadCount to hide the indicator when the count is reset.
+    // If we don't maintain the local counter, in the case of a thread, the indicator will be shown again once we scroll down after we reach the top.
+    const localUnreadCount = useRef(unreadCount);
+    useEffect(() => {
+        localUnreadCount.current = unreadCount;
+    }, [unreadCount]);
+
     const resetCount = async () => {
-        if (resetting.current) {
+        localUnreadCount.current = 0;
+
+        if (resetting.current || (isCRTEnabled && rootId)) {
             return;
         }
 
@@ -165,7 +179,7 @@ const MoreMessages = ({
         }
 
         const readCount = posts.slice(0, lastViewableIndex).filter((v) => typeof v !== 'string').length;
-        const totalUnread = unreadCount - readCount;
+        const totalUnread = localUnreadCount.current - readCount;
         if (lastViewableIndex >= newMessageLineIndex) {
             resetCount();
             top.value = 0;
