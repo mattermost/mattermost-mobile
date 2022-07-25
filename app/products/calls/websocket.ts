9c2b5133ab1941339075243e3e4c91cc4561f34b
Claudio Costa <cstcld91@gmail.com>
2022-07-25 18:19:55 +0200

    [MM-43838] Calls: WebSocket reconnection support (#6497)
    * WebSocket reconnection support
    
    * Simplify
    
    * Reposition code
    
    * Return early if already closed

--- app/products/calls/websocket.ts
+++ app/products/calls/websocket.ts
@@ -5,26 +5,55 @@ import {EventEmitter} from 'events';
 import Calls from '@constants/calls';
 import {encode} from '@msgpack/msgpack/dist';
 
-export default class WebSocketClient extends EventEmitter {
-    private ws: WebSocket | null;
+const wsMinReconnectRetryTimeMs = 1000; // 1 second
+const wsReconnectionTimeout = 30000; // 30 seconds
+const wsReconnectTimeIncrement = 500; // 0.5 seconds
+export const wsReconnectionTimeoutErr = new Error('max disconnected time reached');
+
+export class WebSocketClient extends EventEmitter {
+    private ws: WebSocket | null = null;
+    private wsURL: string;
+    private authToken: string;
     private seqNo = 1;
+    private serverSeqNo = 0;
     private connID = '';
+    private originalConnID = '';
     private eventPrefix = `custom_${Calls.PluginId}`;
+    private lastDisconnect = 0;
+    private reconnectRetryTime = wsMinReconnectRetryTimeMs;
+    private closed = false;
 
-    constructor(connURL: string, authToken: string) {
+    constructor(wsURL: string, authToken: string) {
         super();
+        this.wsURL = wsURL;
+        this.authToken = authToken;
+        this.init(false);
+    }
 
-        this.ws = new WebSocket(connURL, [], {headers: {authorization: `Bearer ${authToken}`}});
+    private init(isReconnect: boolean) {
+        if (this.closed) {
+            return;
+        }
+
+        this.ws = new WebSocket(`${this.wsURL}?connection_id=${this.connID}&sequence_number=${this.serverSeqNo}`, [], {headers: {authorization: `Bearer ${this.authToken}`}});
+
+        if (isReconnect) {
+            this.ws.onopen = () => {
+                this.lastDisconnect = 0;
+                this.reconnectRetryTime = wsMinReconnectRetryTimeMs;
+                this.emit('open', this.originalConnID, this.connID, true);
+            };
+        }
 
         this.ws.onerror = (err) => {
             this.emit('error', err);
-            this.ws = null;
-            this.close();
         };
 
         this.ws.onclose = () => {
             this.ws = null;
-            this.close();
+            if (!this.closed) {
+                this.close();
+            }
         };
 
         this.ws.onmessage = ({data}) => {
@@ -35,7 +64,12 @@ export default class WebSocketClient extends EventEmitter {
             try {
                 msg = JSON.parse(data);
             } catch (err) {
-                console.log(err); // eslint-disable-line no-console
+                console.log('calls: ws msg parse error', err); // eslint-disable-line no-console
+                return;
+            }
+
+            if (msg) {
+                this.serverSeqNo = msg.seq + 1;
             }
 
             if (!msg || !msg.event || !msg.data) {
@@ -43,14 +77,22 @@ export default class WebSocketClient extends EventEmitter {
             }
 
             if (msg.event === 'hello') {
-                this.connID = msg.data.connection_id;
-                this.emit('open');
+                if (msg.data.connection_id !== this.connID) {
+                    this.connID = msg.data.connection_id;
+                    this.serverSeqNo = 0;
+                    if (this.originalConnID === '') {
+                        this.originalConnID = this.connID;
+                    }
+                }
+                if (!isReconnect) {
+                    this.emit('open', this.originalConnID, this.connID, false);
+                }
                 return;
             } else if (!this.connID) {
                 return;
             }
 
-            if (msg.data.connID !== this.connID) {
+            if (msg.data.connID !== this.connID && msg.data.connID !== this.originalConnID) {
                 return;
             }
 
@@ -74,7 +116,6 @@ export default class WebSocketClient extends EventEmitter {
             seq: this.seqNo++,
             data,
         };
-
         if (this.ws && this.ws.readyState === WebSocket.OPEN) {
             if (binary) {
                 this.ws.send(encode(msg));
@@ -86,12 +127,35 @@ export default class WebSocketClient extends EventEmitter {
 
     close() {
         if (this.ws) {
+            this.closed = true;
             this.ws.close();
             this.ws = null;
+            this.seqNo = 1;
+            this.serverSeqNo = 0;
+            this.connID = '';
+            this.originalConnID = '';
+        } else {
+            this.emit('close');
+
+            const now = Date.now();
+            if (this.lastDisconnect === 0) {
+                this.lastDisconnect = now;
+            }
+
+            if ((now - this.lastDisconnect) >= wsReconnectionTimeout) {
+                this.closed = true;
+                this.emit('error', wsReconnectionTimeoutErr);
+                return;
+            }
+
+            setTimeout(() => {
+                if (!this.ws && !this.closed) {
+                    this.init(true);
+                }
+            }, this.reconnectRetryTime);
+
+            this.reconnectRetryTime += wsReconnectTimeIncrement;
         }
-        this.seqNo = 1;
-        this.connID = '';
-        this.emit('close');
     }
 
     state() {
