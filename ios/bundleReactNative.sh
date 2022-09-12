ff2df3559d1ad6bc049841a4022bc4091612300c
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-09-12 15:34:33 +0400

    ios setup

--- ios/bundleReactNative.sh
+++ ios/bundleReactNative.sh
@@ -12,7 +12,8 @@ if [[ "${SENTRY_ENABLED}" = "true" ]]; then
 	./makeSentryProperties.sh
 
 	export SENTRY_PROPERTIES=sentry.properties
-	../node_modules/@sentry/cli/bin/sentry-cli react-native xcode ./react-native-xcode.sh
+	../node_modules/@sentry/cli/bin/sentry-cli react-native xcode \
+    ../node_modules/react-native/scripts/react-native-xcode.sh
 else
 	echo "Sentry native integration is not enabled"
 	../node_modules/react-native/scripts/react-native-xcode.sh
