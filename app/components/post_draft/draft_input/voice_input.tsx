4e265ec42b39831e1f79e04b3c63f03ff60975bb
Daniel Espino García <larkox@gmail.com>
2022-09-30 10:55:17 +0200

    Improve wiring after test with the recording library

--- app/components/post_draft/draft_input/voice_input.tsx
+++ app/components/post_draft/draft_input/voice_input.tsx
@@ -1,6 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react';
+import {Button, View} from 'react-native';
+
+import {extractFileInfo} from '@utils/file';
 
 type Props = {
     setRecording: (v: boolean) => void;
@@ -11,7 +14,27 @@ function VoiceInput({
     setRecording,
     addFiles,
 }: Props) {
-    return null;
+    // const recorder = useRef(new AudioRecorderPlayer());
+    // useEffect(() => {
+    //     recorder.current?.startRecorder();
+    //     return () => {
+    //         recorder.current?.stopRecorder();
+    //     }
+    // }, [])
+    return (
+        <View>
+            <Button
+                title='record'
+                onPress={async () => {
+                    const url = ''; //await recorder.current?.stopRecorder()
+                    const fi = await extractFileInfo([{uri: url}]);
+                    fi[0].is_voice_recording = true;
+                    addFiles(fi as FileInfo[]);
+                    setRecording(false);
+                }}
+            />
+        </View>
+    );
 }
 
 export default VoiceInput;
