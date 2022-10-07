043c2d84c6bd891365b7f17a78c8ad8d6813a9c5
Elias Nahum <nahumhbl@gmail.com>
2022-10-07 09:41:53 -0300

    feedback review 2

--- types/global/managed_config.d.ts
+++ types/global/managed_config.d.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-type ManagedConfig = JSX.IntrinsicAttributes & {
+type ManagedConfig = {
     allowOtherServers?: string;
     blurApplicationScreen?: string;
     copyAndPasteProtection?: string;
