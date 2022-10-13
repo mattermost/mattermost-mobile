fa923a7dfdbee1bd6f9cfc9cdd4b3b8f8c523322
Joseph Baylon <joseph.baylon@mattermost.com>
2022-10-13 15:59:42 -0700

    Detox/E2E: Modify reporting according to sharding (#6663)

--- detox/e2e/config.js
+++ detox/e2e/config.js
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 const platform = process.env.IOS === 'true' ? 'ios' : 'android';
+const shard = process.env.CI_NODE_INDEX ? process.env.CI_NODE_INDEX : '';
 
 module.exports = {
     setupFilesAfterEnv: ['./test/setup.ts'],
@@ -19,14 +20,20 @@ module.exports = {
         ['jest-junit', {
             suiteName: 'Mobile App E2E with Detox and Jest',
             outputDirectory: './artifacts',
-            outputName: `${platform}-junit.xml`,
+            outputName: `${platform}-junit${shard}.xml`,
             uniqueOutputName: false,
         }],
         ['jest-html-reporters', {
             pageTitle: 'Mobile App E2E with Detox and Jest',
             publicPath: './artifacts',
-            filename: `${platform}-report.html`,
+            filename: `${platform}-report${shard}.html`,
             expand: false,
+        }],
+        ['jest-stare', {
+            reportHeadline: 'Mobile App E2E with Detox and Jest',
+            resultDir: './artifacts/jest-stare',
+            resultJson: `${platform}-data${shard}.json`,
+            resultHtml: `${platform}-main${shard}.html`,
         }],
     ],
     verbose: true,
