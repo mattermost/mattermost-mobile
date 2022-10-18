c1e7c04e17a2aa6fa84abba7127656cdd5581731
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-10-18 16:54:01 +0400

    Update servers.ts

--- app/database/subscription/servers.ts
+++ app/database/subscription/servers.ts
@@ -7,7 +7,6 @@ import {MM_TABLES} from '@constants/database';
 import DatabaseManager from '@database/manager';
 
 import type ServersModel from '@typings/database/models/app/servers';
-import type TeamModel from '@typings/database/models/servers/team';
 
 const {SERVERS} = MM_TABLES.APP;
 
@@ -28,3 +27,4 @@ export const subscribeAllServers = (observer: (servers: ServersModel[]) => void)
         observeWithColumns(['last_active_at']).
         subscribe(observer);
 };
+
