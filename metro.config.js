13b1cdfee67416663ae34a8399fdb85626df1234
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-10-30 16:49:59 +0400

    maxWorker 2

--- metro.config.js
+++ metro.config.js
@@ -21,5 +21,6 @@ module.exports = (async () => {
             assetExts: assetExts.filter((ext) => ext !== 'svg'),
             sourceExts: [...sourceExts, 'svg'],
         },
+        maxWorkers: 2,
     };
 })();
