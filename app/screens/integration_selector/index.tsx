e180635e5ecbd7a82ed05302365092e8dc61ee28
Javier Aguirre <contacto@javaguirre.net>
2022-10-31 10:20:25 +0100

    Activating screens

---
+++ app/screens/integration_selector/index.tsx
@@ -0,0 +1,17 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import { Text } from 'react-native-elements';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+function IntegrationSelector() {
+
+    return (
+        <SafeAreaView testID='integration_selector.screen'>
+            <Text>HELLO WORLD!</Text>
+        </SafeAreaView>
+    );
+}
+
+export default IntegrationSelector;
