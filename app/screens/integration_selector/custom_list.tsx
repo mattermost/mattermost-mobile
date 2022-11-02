00a92171e25c6367fbb1b17a9614a744d9c9c591
Javier Aguirre <contacto@javaguirre.net>
2022-11-02 11:13:19 +0100

    Adding default themes for components

---
+++ app/screens/integration_selector/custom_list.tsx
@@ -0,0 +1,78 @@
+import { Text, Platform } from 'react-native';
+
+import { makeStyleSheetFromTheme, changeOpacity } from '@utils/theme';
+import { useTheme } from '@app/context/theme';
+
+const getStyleFromTheme = makeStyleSheetFromTheme((theme) => {
+    return {
+        list: {
+            backgroundColor: theme.centerChannelBg,
+            flex: 1,
+            ...Platform.select({
+                android: {
+                    marginBottom: 20,
+                },
+            }),
+        },
+        container: {
+            flexGrow: 1,
+        },
+        separator: {
+            height: 1,
+            flex: 1,
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
+        },
+        listView: {
+            flex: 1,
+            backgroundColor: theme.centerChannelBg,
+            ...Platform.select({
+                android: {
+                    marginBottom: 20,
+                },
+            }),
+        },
+        loadingText: {
+            color: changeOpacity(theme.centerChannelColor, 0.6),
+        },
+        searching: {
+            backgroundColor: theme.centerChannelBg,
+            height: '100%',
+            position: 'absolute',
+            width: '100%',
+        },
+        sectionContainer: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.07),
+            paddingLeft: 10,
+            paddingVertical: 2,
+        },
+        sectionWrapper: {
+            backgroundColor: theme.centerChannelBg,
+        },
+        sectionText: {
+            fontWeight: '600',
+            color: theme.centerChannelColor,
+        },
+        noResultContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        noResultText: {
+            fontSize: 26,
+            color: changeOpacity(theme.centerChannelColor, 0.5),
+        },
+    };
+});
+
+function CustomList() {
+    const theme = useTheme();
+    const style = getStyleFromTheme(theme);
+
+    return (
+        <Text>Custom List</Text>
+    )
+}
+
+
+export default CustomList;
--- app/screens/integration_selector/index.tsx
+++ app/screens/integration_selector/index.tsx
@@ -2,14 +2,100 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import { Text } from 'react-native-elements';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import SearchBar from '@components/search';
+import { changeOpacity, getKeyboardAppearanceFromTheme, makeStyleSheetFromTheme } from '@utils/theme';
+import { useTheme } from '@context/theme';
+
+import CustomList from './custom_list';
+
+const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
+    return {
+        container: {
+            flex: 1,
+        },
+        searchBar: {
+            marginVertical: 5,
+            height: 38,
+        },
+        loadingContainer: {
+            alignItems: 'center',
+            backgroundColor: theme.centerChannelBg,
+            height: 70,
+            justifyContent: 'center',
+        },
+        loadingText: {
+            color: changeOpacity(theme.centerChannelColor, 0.6),
+        },
+        noResultContainer: {
+            flexGrow: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        noResultText: {
+            fontSize: 26,
+            color: changeOpacity(theme.centerChannelColor, 0.5),
+        },
+        separator: {
+            height: 1,
+            flex: 0,
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.1),
+        },
+    };
+});
 
 function IntegrationSelector() {
+    const theme = useTheme();
+    const style = getStyleSheet(theme);
+    const searchBarInput = ""
+    const term = ""
+    const listType = "FLATLIST"
 
     return (
         <SafeAreaView testID='integration_selector.screen'>
-            <Text>HELLO WORLD!</Text>
+            <View
+                testID='selector.screen'
+                style={style.searchBar}
+            >
+                <SearchBar
+                    testID='selector.search_bar'
+                    // ref={this.searchBarRef}
+                    // placeholder={formatMessage({ id: 'search_bar.search', defaultMessage: 'Search' })}
+                    placeholder="Search"
+                    // cancelTitle={formatMessage({ id: 'mobile.post.cancel', defaultMessage: 'Cancel' })}
+                    // backgroundColor='transparent'
+                    // inputHeight={33}
+                    // inputStyle={searchBarInput}
+                    placeholderTextColor={changeOpacity(theme.centerChannelColor, 0.5)}
+                    // tintColorSearch={changeOpacity(theme.centerChannelColor, 0.5)}
+                    // tintColorDelete={changeOpacity(theme.centerChannelColor, 0.5)}
+                    // titleCancelColor={theme.centerChannelColor}
+                    // onChangeText={this.onSearch}
+                    // onSearchButtonPress={this.onSearch}
+                    // onCancelButtonPress={this.clearSearch}
+                    autoCapitalize='none'
+                    keyboardAppearance={getKeyboardAppearanceFromTheme(theme)}
+                    value={term}
+                />
+            </View>
+
+            {/* {selectedOptionsComponent} */}
+
+            <CustomList
+            // data={data}
+            // key='custom_list'
+            // listType={listType}
+            // loading={loading}
+            // loadingComponent={this.renderLoading()}
+            // noResults={this.renderNoResults()}
+            // onLoadMore={this.loadMore}
+            // onRowPress={this.handleSelectItem}
+            // renderItem={rowComponent}
+            // theme={theme}
+            />
         </SafeAreaView>
     );
 }
