4d293707eb4021fc2947fedd6ce3bd0711977494
Jason Frerich <jason.frerich@mattermost.com>
2022-11-07 09:43:20 -0600

    fix post merge

--- app/screens/home/search/results/results.tsx
+++ app/screens/home/search/results/results.tsx
@@ -89,100 +89,6 @@ const Results = ({
                     containerStyle={[styles.loading, paddingTop]}
                 />
             }
-<<<<<<< HEAD
-            return null;
-        }
-
-        if ('message' in item) {
-            return (
-                <PostWithChannelInfo
-                    location={Screens.SEARCH}
-                    post={item}
-                    searchPatterns={searchTermsToPatterns(searchValue)}
-                    testID='search_results.post_list'
-                />
-            );
-        }
-
-        const updateFileForGallery = (idx: number, file: FileInfo) => {
-            'worklet';
-            orderedFilesForGallery[idx] = file;
-        };
-
-        const container: StyleProp<ViewStyle> = fileInfos.length > 1 ? styles.container : undefined;
-        const isSingleImage = orderedFilesForGallery.length === 1 && (isImage(orderedFilesForGallery[0]) || isVideo(orderedFilesForGallery[0]));
-        const isReplyPost = false;
-
-        return (
-            <View
-                style={container}
-                key={item.id}
-            >
-                <File
-                    channelName={channelNames[item.channel_id!]}
-                    galleryIdentifier={galleryIdentifier}
-                    key={item.id}
-                    canDownloadFiles={canDownloadFiles}
-                    file={item}
-                    index={filesForGalleryIndexes[item.id!] || 0}
-                    onPress={handlePreviewPress}
-                    onOptionsPress={handleOptionsPress}
-                    isSingleImage={isSingleImage}
-                    showDate={true}
-                    publicLinkEnabled={publicLinkEnabled}
-                    updateFileForGallery={updateFileForGallery}
-                    inViewPort={true}
-                    wrapperWidth={(getViewPortWidth(isReplyPost, isTablet) - 6)}
-                    nonVisibleImagesCount={0}
-                    asCard={true}
-                />
-            </View>
-        );
-    }, [
-        theme,
-        (orderedFilesForGallery.length === 1) && orderedFilesForGallery[0].mime_type,
-        handleOptionsPress,
-        channelNames,
-        filesForGalleryIndexes,
-        canDownloadFiles,
-        handlePreviewPress,
-        publicLinkEnabled,
-        isTablet,
-        fileInfos.length > 1,
-    ]);
-
-    const noResults = useMemo(() => {
-        return (
-            <NoResultsWithTerm
-                term={searchValue}
-                type={selectedTab}
-            />
-        );
-    }, [searchValue, selectedTab]);
-
-    let data;
-    if (loading) {
-        data = ['loading'];
-    } else {
-        data = selectedTab === TabTypes.MESSAGES ? orderedPosts : orderedFilesForGallery;
-    }
-
-    return (
-        <AnimatedFlatList
-            ListEmptyComponent={noResults}
-            data={data}
-            scrollToOverflowEnabled={true}
-            showsVerticalScrollIndicator={true}
-            scrollEventThrottle={16}
-            indicatorStyle='black'
-            refreshing={false}
-            renderItem={renderItem}
-            nestedScrollEnabled={true}
-            removeClippedSubviews={true}
-            style={containerStyle}
-            testID='search_results.post_list.flat_list'
-        />
-=======
             {!loading &&
             <Animated.View style={[styles.container, transform]}>
                 <View style={styles.result} >
@@ -207,7 +113,6 @@ const Results = ({
             </Animated.View>
             }
         </>
->>>>>>> gekidou
     );
 };
 
