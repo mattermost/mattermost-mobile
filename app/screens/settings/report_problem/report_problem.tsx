272674e496eb520d978dba05db11ea0d15129d45
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-11-29 22:13:37 +0400

    Update report_problem.tsx

--- app/screens/settings/report_problem/report_problem.tsx
+++ app/screens/settings/report_problem/report_problem.tsx
@@ -3,14 +3,12 @@
 
 import TurboLogger from '@mattermost/react-native-turbo-log';
 import React from 'react';
-import {Platform, Alert} from 'react-native';
+import {Alert, Platform} from 'react-native';
 import DeviceInfo from 'react-native-device-info';
-import FileSystem from 'react-native-fs';
-import Mailer from 'react-native-mail';
+import TurboMailer from 'react-native-turbo-mailer';
 
 import {preventDoubleTap} from '@app/utils/tap';
 import {useTheme} from '@context/theme';
-import {logError} from '@utils/log';
 
 import SettingItem from '../setting_item';
 
@@ -21,9 +19,9 @@ type ReportProblemProps = {
     supportEmail: string;
     version: string;
     siteName: string;
-}
+};
 
-const ReportProblem = ({currentTeamId, currentUserId, supportEmail, version, buildNumber, siteName}: ReportProblemProps) => {
+const ReportProblem = ({buildNumber, currentTeamId, currentUserId, siteName, supportEmail, version}: ReportProblemProps) => {
     const theme = useTheme();
 
     const openEmailClient = preventDoubleTap(async () => {
@@ -33,66 +31,29 @@ const ReportProblem = ({currentTeamId, currentUserId, supportEmail, version, bui
 
         const logPaths = await TurboLogger.getLogPaths();
 
-        // /data/user/0/com.mattermost.rnbeta/cache/logs/com.mattermost.rnbeta-latest.log
-        const paths: string[] = [];
-        const fileNames: string[] = [];
-        const logDir = `${FileSystem.TemporaryDirectoryPath}/mattermost-logs/`;
+        const attachments = logPaths.map((path) => ({
+            path,
+            mimeType: 'message/rfc822',
+        }));
+
         try {
-            const mvPromises = logPaths.map(async (logPath) => {
-                const pathParts = logPath.split('/');
-                const logFileName = pathParts[pathParts.length - 1];
-                fileNames.push(logFileName);
-
-                await FileSystem.moveFile(logPath, `${logDir}`);
-                paths.push(`${logDir}/${logFileName}`);
+            await TurboMailer.sendMail({
+                subject: `Problem with ${siteName} React Native app`,
+                recipients: [supportEmail],
+                body: [
+                    'Please share a description of the problem:\n\n',
+                    `Current User Id: ${currentUserId}`,
+                    `Current Team Id: ${currentTeamId}`,
+                    `Server Version: ${version} (Build ${buildNumber})`,
+                    `App Version: ${appVersion} (Build ${appBuild})`,
+                    `App Platform: ${Platform.OS}`,
+                    `Device Model: ${deviceId}`, // added this one
+                ].join('\n'),
+                attachments,
             });
-            const copiedFiles = await Promise.all(mvPromises);
-        } catch (e) {
-            logError(`An error occurred while copying logs to ${logDir}`, e);
+        } catch (e: any) {
+            Alert.alert('Error', e.message);
         }
-
-        const attachments: any = [];
-        try {
-            const fileStatsPromises = paths.map(async (wp) => {
-                return attachments.push({
-                    uri: `content://${wp}`,
-                    mimeType: 'text/plain',
-                });
-            });
-
-            const stats = await Promise.all(fileStatsPromises);
-        } catch (e) {
-            logError(`An error occurred while reading logs from ${logDir} folder`, e);
-        }
-
-        Mailer.mail({
-            subject: `Problem with ${siteName} React Native app`,
-
-            // recipients: [supportEmail],
-            recipients: ['avinash.lingaloo@mattermost.com'], //fixme:
-            body: [
-                'Please share a description of the problem:\n\n',
-                `Current User Id: ${currentUserId}`,
-                `Current Team Id: ${currentTeamId}`,
-                `Server Version: ${version} (Build ${buildNumber})`,
-                `App Version: ${appVersion} (Build ${appBuild})`,
-                `App Platform: ${Platform.OS}`,
-                `Device Model: ${deviceId}`, // added this one
-            ].join('\n'),
-            attachments,
-        }, (error, event) => {
-            //fixme: error : not_available  || not_found => verify if the default email client has been configured or ask the user to do so and to try again later
-
-            Alert.alert(
-                error,
-                event,
-                [
-                    {text: 'Ok', onPress: () => console.log('OK: Email Error Response', {error, event})},
-                    {text: 'Cancel', onPress: () => console.log('CANCEL: Email Error Response')},
-                ],
-                {cancelable: true},
-            );
-        });
     });
 
     return (
