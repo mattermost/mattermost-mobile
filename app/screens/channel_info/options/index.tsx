d85af5cf305c87015b0071c26a6242aec8684e7b
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-11-30 21:34:17 +0400

    MM-44652 : Temporarily hides NotificationPreferences

--- app/screens/channel_info/options/index.tsx
+++ app/screens/channel_info/options/index.tsx
@@ -10,7 +10,6 @@ import {isTypeDMorGM} from '@utils/channel';
 import EditChannel from './edit_channel';
 import IgnoreMentions from './ignore_mentions';
 import Members from './members';
-import NotificationPreference from './notification_preference';
 import PinnedMessages from './pinned_messages';
 
 type Props = {
@@ -27,7 +26,7 @@ const Options = ({channelId, type, callsEnabled}: Props) => {
             {type !== General.DM_CHANNEL &&
                 <IgnoreMentions channelId={channelId}/>
             }
-            <NotificationPreference channelId={channelId}/>
+            {/*<NotificationPreference channelId={channelId}/>*/}
             <PinnedMessages channelId={channelId}/>
             {type !== General.DM_CHANNEL &&
                 <Members channelId={channelId}/>
