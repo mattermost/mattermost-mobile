439e275b2f4c7eb0211c2d6d2fad9418f8164580
Javier Aguirre <contacto@javaguirre.net>
2022-12-01 11:04:40 +0100

    Fix lint

--- app/screens/integration_selector/integration_selector.tsx
+++ app/screens/integration_selector/integration_selector.tsx
@@ -8,6 +8,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {fetchChannels, searchChannels} from '@actions/remote/channel';
 import ServerUserList from '@app/components/server_user_list';
+import {t} from '@app/i18n';
 import FormattedText from '@components/formatted_text';
 import SearchBar from '@components/search';
 import {General, View as ViewConstants} from '@constants';
