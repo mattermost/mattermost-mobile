09c6e8e0365f001d95521eb75a9885f5938dee8e
Jason Frerich <jason.frerich@mattermost.com>
2022-12-07 18:01:01 -0600

    replace View with a virtualized flatlist

--- app/screens/integration_selector/selected_options/index.tsx
+++ app/screens/integration_selector/selected_options/index.tsx
@@ -1,13 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {View, ScrollView} from 'react-native';
+import React, {useCallback} from 'react';
+import {FlatList, ListRenderItemInfo, View} from 'react-native';
 
 import {View as ViewConstants} from '@constants';
 import {makeStyleSheetFromTheme} from '@utils/theme';
 
 import SelectedOption from '../selected_option';
+
+type SelectedOption = DialogOption | UserProfile | Channel
 
 type Props = {
     theme: Theme;
@@ -19,10 +21,12 @@ type Props = {
 const getStyleFromTheme = makeStyleSheetFromTheme(() => {
     return {
         container: {
-            marginLeft: 5,
+            marginHorizontal: 5,
+            marginRight: 5,
             marginBottom: 5,
             maxHeight: 100,
             flexGrow: 0,
+            flexDirection: 'row',
         },
         users: {
             alignItems: 'flex-start',
@@ -36,43 +40,47 @@ const SelectedOptions = ({
     theme, selectedOptions, onRemove, dataSource,
 }: Props) => {
     const style = getStyleFromTheme(theme);
-    const options: React.ReactNode[] = selectedOptions.map((optionItem) => {
-        let key: string;
 
-        switch (dataSource) {
-            case ViewConstants.DATA_SOURCE_USERS:
-                key = (optionItem as UserProfile).id;
-                break;
-            case ViewConstants.DATA_SOURCE_CHANNELS:
-                key = (optionItem as Channel).id;
-                break;
-            default:
-                key = (optionItem as DialogOption).value;
-                break;
-        }
-
+    const renderItem = useCallback(({item}: ListRenderItemInfo<SelectedOption>) => {
         return (
             <SelectedOption
-                key={key}
-                option={optionItem}
+                option={item}
                 theme={theme}
                 dataSource={dataSource}
                 onRemove={onRemove}
             />);
-    });
+    }, [dataSource, onRemove, theme]);
 
-    // eslint-disable-next-line no-warning-comments
-    // TODO Consider using a Virtualized List since the number of elements is potentially unbounded.
-    // https://mattermost.atlassian.net/browse/MM-48420
+    const extractKey = useCallback((item: SelectedOption) => {
+        let key: string;
+
+        switch (dataSource) {
+            case ViewConstants.DATA_SOURCE_USERS:
+                key = (item as UserProfile)?.id;
+                break;
+            case ViewConstants.DATA_SOURCE_CHANNELS:
+                key = (item as Channel).id;
+                break;
+            default:
+                key = (item as DialogOption).value;
+                break;
+        }
+        return key;
+    }, [dataSource]);
+
     return (
-        <ScrollView
-            style={style.container}
-            contentContainerStyle={style.scrollViewContent}
-        >
-            <View style={style.users}>
-                {options}
-            </View>
-        </ScrollView>
+        <View style={[style.container]}>
+            <FlatList
+                numColumns={3}
+                data={selectedOptions}
+                initialNumToRender={3}
+                renderItem={renderItem}
+                keyExtractor={extractKey}
+            />
+
+        </View>
+
+    // </>
     );
 };
 
