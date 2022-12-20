3de85b2bd1454a4608477158ac6ecf02c84fe45f
Elias Nahum <nahumhbl@gmail.com>
2022-12-20 11:43:30 +0200

    iOS notifications badge and posts in channel/thread (#6885)

--- ios/Gekidou/Sources/Gekidou/Storage/Database+Posts.swift
+++ ios/Gekidou/Sources/Gekidou/Storage/Database+Posts.swift
@@ -181,8 +181,9 @@ extension Database {
     public func handlePostData(_ db: Connection, _ postData: PostData, _ channelId: String, _ usedSince: Bool = false, _ receivingThreads: Bool = false) throws {
         let sortedChainedPosts = chainAndSortPosts(postData)
         try insertOrUpdatePosts(db, sortedChainedPosts, channelId)
-        let earliest = sortedChainedPosts.first!.create_at
-        let latest = sortedChainedPosts.last!.create_at
+        let sortedAndNotDeletedPosts = sortedChainedPosts.filter({$0.delete_at == 0})
+        let earliest = sortedAndNotDeletedPosts.first!.create_at
+        let latest = sortedAndNotDeletedPosts.last!.create_at
         if (!receivingThreads) {
             try handlePostsInChannel(db, channelId, earliest, latest, usedSince)
 
@@ -564,7 +565,7 @@ extension Database {
         var postsInThread = [String: [Post]]()
         
         for post in posts {
-            if !post.root_id.isEmpty {
+            if !post.root_id.isEmpty && post.delete_at == 0 {
                 var threadPosts = postsInThread[post.root_id] ?? [Post]()
                 threadPosts.append(post)
                 
