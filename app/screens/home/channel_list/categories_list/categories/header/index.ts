0f1a29e2da8f9e168837fa59b3df57639c41b799
Elias Nahum <nahumhbl@gmail.com>
2022-12-20 11:42:50 +0200

    Do not show favorites category if it only contain archived channels (#6881)

--- app/screens/home/channel_list/categories_list/categories/header/index.ts
+++ app/screens/home/channel_list/categories_list/categories/header/index.ts
@@ -2,14 +2,23 @@
 // See LICENSE.txt for license information.
 
 import withObservables from '@nozbe/with-observables';
+import {switchMap} from 'rxjs/operators';
+
+import {observeConfigBooleanValue} from '@queries/servers/system';
 
 import CategoryHeader from './header';
 
 import type CategoryModel from '@typings/database/models/servers/category';
 
-const enhanced = withObservables(['category'], ({category}: {category: CategoryModel}) => ({
-    category,
-    hasChannels: category.hasChannels,
-}));
+const enhanced = withObservables(['category'], ({category}: {category: CategoryModel}) => {
+    const canViewArchived = observeConfigBooleanValue(category.database, 'ExperimentalViewArchivedChannels');
+
+    return {
+        category,
+        hasChannels: canViewArchived.pipe(
+            switchMap((canView) => category.observeHasChannels(canView)),
+        ),
+    };
+});
 
 export default enhanced(CategoryHeader);
