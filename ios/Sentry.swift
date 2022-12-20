9316814e69aa6b52eba2a0eff4328bc6950d23d2
Avinash Lingaloo <avinashlng1080@gmail.com>
2022-12-20 22:29:55 +0400

    Update Sentry.swift

--- ios/Sentry.swift
+++ ios/Sentry.swift
@@ -11,20 +11,18 @@ import Foundation
 import Sentry
 
 func initSentryAppExt(){
-if let SENTRY_ENABLED = Bundle.main.infoDictionary?["SENTRY_ENABLED"] as? String,
-   let SENTRY_DSN_IOS = Bundle.main.infoDictionary?["SENTRY_DSN_IOS"] as? String {
-    if(SENTRY_ENABLED=="true"){
-      SentrySDK.start { options in
-        options.dsn = SENTRY_DSN_IOS
-
-        // Features turned off by default, but worth checking out
-        options.enableAppHangTracking = true
-        options.enableFileIOTracking = true
-        options.enableCoreDataTracking = true
-        options.enableCaptureFailedRequests = true
-      }
-    }
-  }
+   if let SENTRY_ENABLED = Bundle.main.infoDictionary?["SENTRY_ENABLED"] as? String,
+      let SENTRY_DSN_IOS = Bundle.main.infoDictionary?["SENTRY_DSN_IOS"] as? String {
+        if(SENTRY_ENABLED=="true"){
+          SentrySDK.start { options in
+            options.dsn = SENTRY_DSN_IOS
+            options.enableAppHangTracking = true
+            options.enableFileIOTracking = true
+            options.enableCoreDataTracking = true
+            options.enableCaptureFailedRequests = true
+          }
+        }
+   }
 }
 
 func testSentry(msg: String){
