c2aaef2e14db019b93257dc089514c6d75a440bd
Elias Nahum <nahumhbl@gmail.com>
2023-01-06 11:38:40 +0200

    Preserve new message line on WS reconnect (#6934)

--- app/actions/websocket/index.ts
+++ app/actions/websocket/index.ts
@@ -444,7 +444,7 @@ async function fetchPostDataIfNeeded(serverUrl: string) {
             await fetchPostsForChannel(serverUrl, currentChannelId);
             markChannelAsRead(serverUrl, currentChannelId);
             if (!EphemeralStore.wasNotificationTapped()) {
-                markChannelAsViewed(serverUrl, currentChannelId);
+                markChannelAsViewed(serverUrl, currentChannelId, true);
             }
             EphemeralStore.setNotificationTapped(false);
         }
