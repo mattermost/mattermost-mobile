14c8757ffabc322851ea7e792c62ee72414a9915
Jason Frerich <jason.frerich@mattermost.com>
2023-01-11 20:25:46 -0600

    Do not enable the Keyboard Search button unless there is text in the search bar

--- app/components/search/index.tsx
+++ app/components/search/index.tsx
@@ -166,6 +166,7 @@ const Search = forwardRef<SearchRef, SearchProps>((props: SearchProps, ref) => {
 
             // @ts-expect-error clearIcon definition does not include a ReactElement
             clearIcon={clearIcon}
+            enablesReturnKeyAutomatically={true}
             containerStyle={[styles.containerStyle, props.containerStyle]}
             inputContainerStyle={[styles.inputContainerStyle, props.inputContainerStyle]}
             inputStyle={[styles.inputStyle, props.inputStyle]}
@@ -189,6 +190,6 @@ const Search = forwardRef<SearchRef, SearchProps>((props: SearchProps, ref) => {
     );
 });
 
-Search.displayName = 'SeachBar';
+Search.displayName = 'SearchBar';
 
 export default Search;
