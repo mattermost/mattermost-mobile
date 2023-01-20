fc16af723aa813d97e98a6f2655bc0f347be4c23
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-01-20 15:05:19 +0400

    Update channel_notification_preference.tsx

--- app/screens/channel_notification_preference/channel_notification_preference.tsx
+++ app/screens/channel_notification_preference/channel_notification_preference.tsx
@@ -3,9 +3,12 @@
 
 import React, {useCallback, useState} from 'react';
 import {useIntl} from 'react-intl';
+import {Text, TouchableOpacity, View} from 'react-native';
 
+import CompassIcon from '@components/compass_icon';
 import {NotificationLevel} from '@constants';
 import {useServerUrl} from '@context/server';
+import {useTheme} from '@context/theme';
 import useAndroidHardwareBackHandler from '@hooks/android_back_handler';
 import useBackNavigation from '@hooks/navigate_back';
 import {t} from '@i18n';
@@ -14,6 +17,8 @@ import SettingBlock from '@screens/settings/setting_block';
 import SettingContainer from '@screens/settings/setting_container';
 import SettingOption from '@screens/settings/setting_option';
 import SettingSeparator from '@screens/settings/settings_separator';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
+import {typography} from '@utils/typography';
 
 type NotifPrefOptions = {
     defaultMessage: string;
@@ -51,6 +56,55 @@ const NOTIFY_OPTIONS: Record<string, NotifPrefOptions> = {
 const NOTIFY_ABOUT = {id: t('channel_notification_preference.notify_about'), defaultMessage: 'Notify me about...'};
 const THREAD_REPLIES = {id: t('channel_notification_preference.thread_replies'), defaultMessage: 'Thread replies'};
 
+const MUTED_TITLE = {id: t('channel_notification_preference.muted_title'), defaultMessage: 'This channel is muted'};
+const MUTED_CONTENT = {id: t('channel_notification_preference.muted_content'), defaultMessage: 'You can change the notification settings, but you will not receive notifications until the channel is unmuted.'};
+
+const UNMUTE_CONTENT = {id: t('channel_notification_preference.unmute_content'), defaultMessage: 'Unmute channel'};
+
+const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
+    return {
+        mutedBanner: {
+            width: '90%',
+            minHeight: 200,
+            backgroundColor: changeOpacity(theme.sidebarTextActiveBorder, 0.16),
+            alignSelf: 'center',
+            marginVertical: 20,
+            paddingHorizontal: 16,
+            borderRadius: 4,
+        },
+        mutedBannerTitle: {
+            flexDirection: 'row',
+            marginTop: 16,
+        },
+        mutedTextTitle: {
+            ...typography('Heading', 200),
+            color: theme.centerChannelColor,
+            marginLeft: 10,
+            paddingTop: 5,
+        },
+        mutedText: {
+            ...typography('Body', 200),
+            color: theme.centerChannelColor,
+            marginTop: 12,
+            marginBottom: 16,
+        },
+        unMuteButton: {
+            flexDirection: 'row',
+            backgroundColor: theme.buttonBg,
+            marginBottom: 20,
+            borderRadius: 4,
+            paddingVertical: 12,
+            width: '55%',
+            paddingHorizontal: 20,
+        },
+        unMuteText: {
+            ...typography('Heading', 100),
+            color: theme.buttonColor,
+            marginLeft: 7,
+        },
+    };
+});
+
 type ChannelNotificationPreferenceProps = {
     componentId: string;
     notifyLevel?: NotificationLevel;
@@ -61,6 +115,8 @@ type ChannelNotificationPreferenceProps = {
 const ChannelNotificationPreference = ({componentId, notifyLevel, isCRTEnabled}: ChannelNotificationPreferenceProps) => {
     const serverUrl = useServerUrl();
     const intl = useIntl();
+    const theme = useTheme();
+    const styles = getStyleSheet(theme);
 
     const [notifyAbout, setNotifyAbout] = useState<UserNotifyPropsPush>(notifyLevel);
     const [threadReplies, setThreadReplies] = useState<boolean>(false); // TODO: get from server
@@ -77,12 +133,37 @@ const ChannelNotificationPreference = ({componentId, notifyLevel, isCRTEnabled}:
         close();
     }, [canSaveSettings, close, serverUrl]);
 
+    const renderMutedBanner = useCallback(() => {
+        return (
+            <View style={styles.mutedBanner}>
+                <View style={styles.mutedBannerTitle}>
+                    <CompassIcon
+                        name='bell-off-outline'
+                        size={24}
+                        color={theme.linkColor}
+                    />
+                    <Text style={styles.mutedTextTitle}>{intl.formatMessage(MUTED_TITLE)}</Text>
+                </View>
+                <Text style={styles.mutedText}>{intl.formatMessage(MUTED_CONTENT)}</Text>
+                <TouchableOpacity style={styles.unMuteButton}>
+                    <CompassIcon
+                        name='bell-outline'
+                        size={18}
+                        color={theme.buttonColor}
+                    />
+                    <Text style={styles.unMuteText}>{intl.formatMessage(UNMUTE_CONTENT)}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }, []);
+
     useBackNavigation(saveNotificationSettings);
 
     useAndroidHardwareBackHandler(componentId, saveNotificationSettings);
 
     return (
         <SettingContainer testID='push_notification_settings'>
+            {renderMutedBanner()}
             <SettingBlock
                 headerText={NOTIFY_ABOUT}
             >
@@ -99,7 +180,7 @@ const ChannelNotificationPreference = ({componentId, notifyLevel, isCRTEnabled}:
                                 type='select'
                                 value={value}
                             />
-                            <SettingSeparator/>
+                            <SettingSeparator key={`notif_pref_option_separator${k}`}/>
                         </>
                     );
                 })
