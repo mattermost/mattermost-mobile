342c5131945c778754d3b24059922274ee1c4a63
Christopher Poile <cpoile@gmail.com>
2023-02-07 17:13:07 -0500

    "fix" the wrong start time

--- app/products/calls/components/join_call_banner/index.ts
+++ app/products/calls/components/join_call_banner/index.ts
@@ -3,6 +3,7 @@
 
 import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
 import withObservables from '@nozbe/with-observables';
+import moment from 'moment-timezone';
 import {of as of$} from 'rxjs';
 import {distinctUntilChanged, switchMap} from 'rxjs/operators';
 
@@ -24,16 +25,19 @@ const enhanced = withObservables(['serverUrl', 'channelId'], ({
     channelId,
     database,
 }: OwnProps & WithDatabaseArgs) => {
-    const callsState = observeCallsState(serverUrl);
-    const participants = callsState.pipe(
+    const callsState = observeCallsState(serverUrl).pipe(
         switchMap((state) => of$(state.calls[channelId])),
+    );
+    const participants = callsState.pipe(
         distinctUntilChanged((prev, curr) => prev?.participants === curr?.participants), // Did the participants object ref change?
         switchMap((call) => (call ? of$(Object.keys(call.participants)) : of$([]))),
         distinctUntilChanged((prev, curr) => idsAreEqual(prev, curr)), // Continue only if we have a different set of participant ids
         switchMap((ids) => (ids.length > 0 ? queryUsersById(database, ids).observeWithColumns(['last_picture_update']) : of$([]))),
     );
     const channelCallStartTime = callsState.pipe(
-        switchMap((cs) => of$(cs.calls[channelId]?.startTime || 0)),
+
+        // if for some reason we don't have a startTime, use 'a few seconds ago' instead of '53 years ago'
+        switchMap((state) => of$(state && state.startTime ? state.startTime : moment.now())),
         distinctUntilChanged(),
     );
 
