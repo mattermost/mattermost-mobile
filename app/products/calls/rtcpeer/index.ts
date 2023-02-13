4cb20a4bec9c38f2bf6b71d76787dd3950962f6e
Christopher Poile <cpoile@gmail.com>
2023-02-13 11:03:49 -0500

    MM-49065 - Calls: Implement glare free negotiation (#7111)
    * glare-free negotiation
    
    * update to latest with fix
    
    * remove unneeded sessionConstraints
    
    * update to latest react-native-webrtc

--- app/products/calls/rtcpeer/index.ts
+++ app/products/calls/rtcpeer/index.ts
@@ -25,15 +25,9 @@ export default class RTCPeer extends EventEmitter {
     private pc: RTCPeerConnection | null;
     private readonly senders: { [key: string]: RTCRtpSender };
     private candidates: RTCIceCandidate[] = [];
+    private makingOffer = false;
 
     public connected: boolean;
-
-    private readonly sessionConstraints = {
-        mandatory: {
-            OfferToReceiveAudio: true,
-            OfferToReceiveVideo: true,
-        },
-    };
 
     constructor(config: RTCPeerConfig) {
         super();
@@ -91,11 +85,13 @@ export default class RTCPeer extends EventEmitter {
 
     private async onNegotiationNeeded() {
         try {
-            const desc = await this.pc?.createOffer(this.sessionConstraints) as RTCSessionDescription;
-            await this.pc?.setLocalDescription(desc);
+            this.makingOffer = true;
+            await this.pc?.setLocalDescription();
             this.emit('offer', this.pc?.localDescription);
         } catch (err) {
             this.emit('error', err);
+        } finally {
+            this.makingOffer = false;
         }
     }
 
@@ -114,6 +110,10 @@ export default class RTCPeer extends EventEmitter {
 
         const msg = JSON.parse(data);
 
+        if (msg.type === 'offer' && (this.makingOffer || this.pc?.signalingState !== 'stable')) {
+            logDebug('signaling conflict, we are polite, proceeding...');
+        }
+
         try {
             switch (msg.type) {
                 case 'candidate':
@@ -131,7 +131,7 @@ export default class RTCPeer extends EventEmitter {
                     break;
                 case 'offer':
                     await this.pc.setRemoteDescription(new RTCSessionDescription(msg));
-                    await this.pc.setLocalDescription(await this.pc.createAnswer() as RTCSessionDescription);
+                    await this.pc.setLocalDescription();
                     this.emit('answer', this.pc.localDescription);
                     break;
                 case 'answer':
