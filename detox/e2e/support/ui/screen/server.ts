0ecdf44e35465f21852dc5778023a1a8b68214bf
Elisabeth Kulzer <9913860+metanerd@users.noreply.github.com>
2023-03-01 15:47:30 +0100

    Fix login smoke test again.

--- detox/e2e/support/ui/screen/server.ts
+++ detox/e2e/support/ui/screen/server.ts
@@ -47,6 +47,7 @@ class ServerScreen {
         await this.serverUrlInput.replaceText(serverUrl);
         await this.serverUrlInput.tapReturnKey();
         await this.serverDisplayNameInput.replaceText(serverDisplayName);
+        await this.serverUrlInput.tapReturnKey();
         await this.tapConnectButton();
     };
 
