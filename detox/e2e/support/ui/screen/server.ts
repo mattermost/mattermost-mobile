dfdbce70e8606881a865f4268a6eca260ff129fd
Elisabeth Kulzer <9913860+metanerd@users.noreply.github.com>
2023-03-02 18:02:33 +0100

    Fix connect on android

--- detox/e2e/support/ui/screen/server.ts
+++ detox/e2e/support/ui/screen/server.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {isAndroid, timeouts, wait} from '@support/utils';
+import {isIos, timeouts, wait} from '@support/utils';
 import {expect} from 'detox';
 
 class ServerScreen {
@@ -48,7 +48,9 @@ class ServerScreen {
         await this.serverUrlInput.tapReturnKey();
         await this.serverDisplayNameInput.replaceText(serverDisplayName);
         await this.serverDisplayNameInput.tapReturnKey();
-        await this.tapConnectButton();
+        if (isIos()) {
+            await this.tapConnectButton();
+        }
     };
 
     close = async () => {
@@ -57,10 +59,6 @@ class ServerScreen {
     };
 
     tapConnectButton = async () => {
-        if (isAndroid()) {
-            await device.pressBack();
-            await wait(timeouts.ONE_SEC);
-        }
         await this.connectButton.tap();
         await wait(timeouts.ONE_SEC);
     };
