ab8a43032ef87c8a52d06ae03b91e8fbdf4f6dd3
Elias Nahum <nahumhbl@gmail.com>
2023-03-03 15:53:29 +0200

    Refactor category channels to react to setting changes and apply the correct order (#7170)
    * Refactor category channels to react to setting changes and apply the correct order
    
    * feedback review

--- app/screens/home/channel_list/categories_list/categories/body/category_body.test.tsx
+++ app/screens/home/channel_list/categories_list/categories/body/category_body.test.tsx
@@ -12,28 +12,8 @@ import TestHelper from '@test/test_helper';
 import CategoryBody from '.';
 
 import type CategoryModel from '@typings/database/models/servers/category';
-import type CategoryChannelModel from '@typings/database/models/servers/category_channel';
-import type ChannelModel from '@typings/database/models/servers/channel';
 
 const {SERVER: {CATEGORY}} = MM_TABLES;
-
-jest.mock('@queries/servers/categories', () => {
-    const Queries = jest.requireActual('@queries/servers/categories');
-    const switchMap = jest.requireActual('rxjs/operators').switchMap;
-    const mQ = jest.requireActual('@nozbe/watermelondb').Q;
-
-    return {
-        ...Queries,
-        observeChannelsByCategoryChannelSortOrder: (database: Database, category: CategoryModel, excludeIds?: string[]) => {
-            return category.categoryChannelsBySortOrder.observeWithColumns(['sort_order']).pipe(
-                switchMap((categoryChannels: CategoryChannelModel[]) => {
-                    const ids = categoryChannels.filter((cc) => excludeIds?.includes(cc.channelId)).map((cc) => cc.channelId);
-                    return database.get<ChannelModel>('Channel').query(mQ.where('id', mQ.oneOf(ids))).observe();
-                }),
-            );
-        },
-    };
-});
 
 describe('components/channel_list/categories/body', () => {
     let database: Database;
