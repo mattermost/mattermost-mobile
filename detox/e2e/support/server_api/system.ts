1cc2ce98f7b0c3ceafd89a90810b1eef6560eddd
Elisabeth Kulzer <9913860+metanerd@users.noreply.github.com>
2023-03-03 15:53:17 +0100

    Remove config modifications.

--- detox/e2e/support/server_api/system.ts
+++ detox/e2e/support/server_api/system.ts
@@ -153,29 +153,6 @@ export const apiRequireSMTPServer = async (baseUrl: string) => {
 };
 
 /**
- * Update configuration.
- * See https://api.mattermost.com/#operation/UpdateConfig
- * @param {string} baseUrl - the base server URL
- * @param {Object} newConfig - specific config to update
- * @return {Object} returns {config} on success or {error, status} on error
- */
-export const apiUpdateConfig = async (baseUrl: string, newConfig: any = {}): Promise<any> => {
-    try {
-        const {config: currentConfig} = await apiGetConfig(baseUrl);
-        const config = merge.all([currentConfig, getDefaultConfig(baseUrl), newConfig]);
-
-        const response = await client.put(
-            `${baseUrl}/api/v4/config`,
-            config,
-        );
-
-        return {config: response.data};
-    } catch (err) {
-        return getResponseFromError(err);
-    }
-};
-
-/**
  * Upload server license with file expected at "/detox/e2e/support/fixtures/mattermost-license.txt"
  * See https://api.mattermost.com/#operation/UploadLicenseFile
  * @param {string} baseUrl - the base server URL
@@ -209,18 +186,6 @@ export const getClientLicense = async (baseUrl: string): Promise<any> => {
     return {license: out.license};
 };
 
-export const getDefaultConfig = (siteUrl: string) => {
-    const fromEnv = {
-        LdapSettings: {
-            LdapServer: ldapServer,
-            LdapPort: ldapPort,
-        },
-        ServiceSettings: {SiteURL: siteUrl},
-    };
-
-    return merge(defaultServerConfig, fromEnv);
-};
-
 export const System = {
     apiCheckSystemHealth,
     apiEmailTest,
@@ -230,10 +195,8 @@ export const System = {
     apiRequireLicense,
     apiRequireLicenseForFeature,
     apiRequireSMTPServer,
-    apiUpdateConfig,
     apiUploadLicense,
     getClientLicense,
-    getDefaultConfig,
 };
 
 export default System;
