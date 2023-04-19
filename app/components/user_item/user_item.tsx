ef1c30599ec785bcfc8e55b0d2fa0dd938dc145c
Elias Nahum <nahumhbl@gmail.com>
2023-04-19 13:15:41 -0400

    update RN

--- app/components/user_item/user_item.tsx
+++ app/components/user_item/user_item.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import {useIntl} from 'react-intl';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import CompassIcon from '@components/compass_icon';
 import CustomStatusEmoji from '@components/custom_status/custom_status_emoji';
