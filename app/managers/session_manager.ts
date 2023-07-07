4fc9138cee2d432df328681c84372125b5385889
Mattermost Build <build@mattermost.com>
2023-07-07 12:21:23 +0300

    Fix ios cache (#7412) (#7439)
    * Fix Keychain cache
    
    * Fix
    
    * Fix import
    
    (cherry picked from commit 5ab2f86b6636e6aa6d98337735582877d5f91094)
    
    Co-authored-by: Daniel Espino García <larkox@gmail.com>

--- app/managers/session_manager.ts
+++ app/managers/session_manager.ts
@@ -22,6 +22,7 @@ import {getThemeFromState} from '@screens/navigation';
 import EphemeralStore from '@store/ephemeral_store';
 import {deleteFileCache, deleteFileCacheByDir} from '@utils/file';
 import {isMainActivity} from '@utils/helpers';
+import {invalidateKeychainCache} from '@utils/mattermost_managed';
 import {addNewServer} from '@utils/server';
 
 import type {LaunchType} from '@typings/launch';
@@ -115,6 +116,7 @@ class SessionManager {
     private terminateSession = async (serverUrl: string, removeServer: boolean) => {
         cancelSessionNotification(serverUrl);
         await removeServerCredentials(serverUrl);
+        invalidateKeychainCache(serverUrl);
         PushNotifications.removeServerNotifications(serverUrl);
 
         NetworkManager.invalidateClient(serverUrl);
