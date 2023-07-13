de8eac3710fc213f83e6d20e469aaf700080f457
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2023-07-13 08:36:09 -0400

    Update app/screens/login/form.tsx
    Co-authored-by: Elias Nahum <nahumhbl@gmail.com>

--- app/screens/login/form.tsx
+++ app/screens/login/form.tsx
@@ -8,7 +8,7 @@ import {Keyboard, TextInput, TouchableOpacity, View} from 'react-native';
 import Button from 'react-native-button';
 
 import {login} from '@actions/remote/session';
-import {tryOpenURL} from '@app/utils/url';
+import {tryOpenURL} from '@utils/url';
 import CompassIcon from '@components/compass_icon';
 import FloatingTextInput from '@components/floating_text_input_label';
 import FormattedText from '@components/formatted_text';
