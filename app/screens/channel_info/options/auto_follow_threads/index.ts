a870b65949931fddc5593a870c0b10b802b4ad77
Tanmay Thole <tanmaythole@gmail.com>
2023-07-21 22:04:16 +0530

    unused code removed

--- app/screens/channel_info/options/auto_follow_threads/index.ts
+++ app/screens/channel_info/options/auto_follow_threads/index.ts
@@ -4,16 +4,10 @@
 import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
 import withObservables from '@nozbe/with-observables';
 import {of as of$} from 'rxjs';
-import {
-
-    // combineLatestWith,
-    switchMap,
-} from 'rxjs/operators';
+import {switchMap} from 'rxjs/operators';
 
 import {Channel} from '@constants';
 import {observeChannelSettings} from '@queries/servers/channel';
-
-// import {observeCurrentUser} from '@queries/servers/user';
 
 import AutoFollowThreads from './auto_follow_threads';
 
@@ -23,27 +17,10 @@ type Props = WithDatabaseArgs & {
     channelId: string;
 }
 
-// const isChannelMentionsIgnored = (channelNotifyProps?: Partial<ChannelNotifyProps>, userNotifyProps?: UserNotifyProps | null) => {
-//     let autoFollowThreadsDefault = Channel.IGNORE_CHANNEL_MENTIONS_OFF;
-
-//     if (userNotifyProps?.channel && userNotifyProps.channel === 'false') {
-//         autoFollowThreadsDefault = Channel.IGNORE_CHANNEL_MENTIONS_ON;
-//     }
-
-//     let ignoreChannelMentions = channelNotifyProps?.ignore_channel_mentions;
-//     if (!ignoreChannelMentions || ignoreChannelMentions === Channel.IGNORE_CHANNEL_MENTIONS_DEFAULT) {
-//         ignoreChannelMentions = autoFollowThreadsDefault as any;
-//     }
-
-//     return ignoreChannelMentions !== Channel.IGNORE_CHANNEL_MENTIONS_OFF;
-// };
-
 const enhanced = withObservables(['channelId'], ({channelId, database}: Props) => {
-    // const currentUser = observeCurrentUser(database);
     const settings = observeChannelSettings(database, channelId);
     const followedStatus = settings.pipe(
         switchMap((s) => {
-            console.log(s?.notifyProps);
             return of$(s?.notifyProps?.channel_auto_follow_threads === Channel.CHANNEL_AUTO_FOLLOW_THREADS_TRUE);
         }),
     );
