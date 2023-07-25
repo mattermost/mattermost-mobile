61231c6df7bdcce8d106bfd390596b58d784bbeb
panoramix360 <lucasreis3000@gmail.com>
2023-07-25 10:39:22 -0300

    feat: adding the scheme_admin info on the channel members user_list sections

--- app/components/user_list/index.tsx
+++ app/components/user_list/index.tsx
@@ -67,12 +67,12 @@ export function createProfilesSections(intl: IntlShape, profiles: UserProfile[],
         return [];
     }
 
-    const sections = new Map<string, UserProfile[]>();
+    const sections = new Map<string, UserProfileWithChannelAdmin[]>();
 
     if (members?.length) {
         // when channel members are provided, build the sections by admins and members
         const membersDictionary = new Map<string, ChannelMembership>();
-        const membersSections = new Map<string, UserProfile[]>();
+        const membersSections = new Map<string, UserProfileWithChannelAdmin[]>();
         const {formatMessage} = intl;
         members.forEach((m) => membersDictionary.set(m.user_id, m));
         profiles.forEach((p) => {
@@ -80,7 +80,7 @@ export function createProfilesSections(intl: IntlShape, profiles: UserProfile[],
             if (member) {
                 const sectionKey = sectionRoleKeyExtractor(member.scheme_admin!).id;
                 const section = membersSections.get(sectionKey) || [];
-                section.push(p);
+                section.push({...p, scheme_admin: member.scheme_admin});
                 membersSections.set(sectionKey, section);
             }
         });
@@ -337,7 +337,7 @@ export default function UserList({
     };
 
     if (term) {
-        return renderFlatList(data as UserProfile[]);
+        return renderFlatList(data as UserProfileWithChannelAdmin[]);
     }
-    return renderSectionList(data as Array<SectionListData<UserProfile>>);
+    return renderSectionList(data as Array<SectionListData<UserProfileWithChannelAdmin>>);
 }
