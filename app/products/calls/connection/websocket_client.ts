703894178ec753851d7abcefa1f92536b2109383
streamer45 <cstcld91@gmail.com>
2023-08-21 15:15:28 -0600

    Calls: fix potential websocket client leak

--- app/products/calls/connection/websocket_client.ts
+++ app/products/calls/connection/websocket_client.ts
@@ -60,9 +60,9 @@ export class WebSocketClient extends EventEmitter {
         };
 
         this.ws.onclose = () => {
-            this.ws = null;
+            this.emit('close');
             if (!this.closed) {
-                this.close();
+                this.reconnect();
             }
         };
 
@@ -143,40 +143,39 @@ export class WebSocketClient extends EventEmitter {
     }
 
     close() {
-        if (this.ws) {
-            this.closed = true;
-            this.ws.close();
-            this.ws = null;
-            this.seqNo = 1;
-            this.serverSeqNo = 0;
-            this.connID = '';
-            this.originalConnID = '';
-        } else {
-            this.emit('close');
+        this.closed = true;
+        this.ws?.close();
+        this.ws = null;
+        this.seqNo = 1;
+        this.serverSeqNo = 0;
+        this.connID = '';
+        this.originalConnID = '';
+    }
 
-            const now = Date.now();
-            if (this.lastDisconnect === 0) {
-                this.lastDisconnect = now;
-            }
-
-            if ((now - this.lastDisconnect) >= wsReconnectionTimeout) {
-                this.closed = true;
-                this.emit('error', wsReconnectionTimeoutErr);
-                return;
-            }
-
-            setTimeout(() => {
-                if (!this.ws && !this.closed) {
-                    this.init(true);
-                }
-            }, this.reconnectRetryTime);
-
-            this.reconnectRetryTime += wsReconnectTimeIncrement;
+    reconnect() {
+        const now = Date.now();
+        if (this.lastDisconnect === 0) {
+            this.lastDisconnect = now;
         }
+
+        if ((now - this.lastDisconnect) >= wsReconnectionTimeout) {
+            this.closed = true;
+            this.emit('error', wsReconnectionTimeoutErr);
+            return;
+        }
+
+        setTimeout(() => {
+            if (!this.closed) {
+                logDebug('reconnecting');
+                this.init(true);
+            }
+        }, this.reconnectRetryTime);
+
+        this.reconnectRetryTime += wsReconnectTimeIncrement;
     }
 
     state() {
-        if (!this.ws) {
+        if (this.closed || !this.ws) {
             return WebSocket.CLOSED;
         }
         return this.ws.readyState;
