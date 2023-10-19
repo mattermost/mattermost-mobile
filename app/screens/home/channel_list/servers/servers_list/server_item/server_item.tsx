ec0d87eb5ade4612091f1674b741683660b203ae
Elias Nahum <nahumhbl@gmail.com>
2023-10-19 10:34:21 +0400

    Fix race condition when showing the turorial (#7599)
    (cherry picked from commit 12a29fe33e8abed246adbebc4f54576a4b0901f0)

--- app/screens/home/channel_list/servers/servers_list/server_item/server_item.tsx
+++ app/screens/home/channel_list/servers/servers_list/server_item/server_item.tsx
@@ -213,9 +213,11 @@ const ServerItem = ({
     };
 
     const onLayout = useCallback(() => {
-        swipeable.current?.close();
-        startTutorial();
-    }, []);
+        if (showTutorial) {
+            swipeable.current?.close();
+            startTutorial();
+        }
+    }, [showTutorial]);
 
     const containerStyle = useMemo(() => {
         const style: StyleProp<ViewStyle> = [styles.container];
@@ -390,6 +392,7 @@ const ServerItem = ({
                     style={containerStyle}
                     ref={viewRef}
                     testID={serverItemTestId}
+                    onLayout={onLayout}
                 >
                     <RectButton
                         onPress={onServerPressed}
@@ -474,13 +477,14 @@ const ServerItem = ({
                 itemBounds={itemBounds}
                 onDismiss={handleDismissTutorial}
                 onShow={handleShowTutorial}
-                onLayout={onLayout}
                 itemBorderRadius={8}
             >
+                {Boolean(itemBounds.endX) &&
                 <TutorialSwipeLeft
                     message={intl.formatMessage({id: 'server.tutorial.swipe', defaultMessage: 'Swipe left on a server to see more actions'})}
                     style={isTablet ? styles.tutorialTablet : styles.tutorial}
                 />
+                }
             </TutorialHighlight>
             }
         </>
