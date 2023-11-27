54f63aafd1620439ff69a7e223e50d879e870188
Mario Vitale <mvitale1989@hotmail.com>
2023-11-27 18:11:31 +0100

    Start implementing version bumping

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -3,6 +3,17 @@ set -eu -o pipefail
 cd "$(dirname "$0")"/..
 
 log () { echo "[$(date +%Y-%m-%dT%H:%M:%S%Z)]" "$@"; }
+is_false () { grep -qiE '^(no?|false)$' <<<$1; }
+
+: ${BRANCH_TO_BUILD:=main}
+: ${PR_REVIEWERS:=mattermost/core-build-engineers}
+: ${BUMP_BUILD_NUMBER:=true}
+: ${BUMP_VERSION:=false}     # If enabled, you must populate the VERSION variable as well
+: ${LOCAL_MODE:=yes}         # Disable LOCAL_MODE to push the commit to origin, and create a corresponding PR
+LATEST_BUILD_NUMBER=$(./scripts/get_latest_build_number.sh)
+BUILD_NUMBER=$(($LATEST_BUILD_NUMBER + 1))
+GIT_LOCAL_BRANCH=bump-build-${BRANCH_TO_BUILD}-${BUILD_NUMBER}
+log "Build number to use for the beta build: $BUILD_NUMBER"
 
 log "Asserting that the workdir is clean"
 if ! git diff --quiet; then
@@ -10,36 +21,37 @@ if ! git diff --quiet; then
   exit 1
 fi
 
-log "Saving the currently checked out branch"
-CURRENT_BRANCH=$(git branch --show-current)
-trap "git checkout $CURRENT_BRANCH" EXIT
-
-: ${BRANCH_TO_BUILD:=main}
-: ${PR_REVIEWERS:=mattermost/core-build-engineers}
-: ${DRY_RUN:=}
-LATEST_BUILD_NUMBER=$(./scripts/get_latest_build_number.sh)
-BUILD_NUMBER=$(($LATEST_BUILD_NUMBER + 1))
-GIT_LOCAL_BRANCH=bump-build-${BRANCH_TO_BUILD}-${BUILD_NUMBER}
-log "Build number to use for the beta build: $BUILD_NUMBER"
-
-log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
-git checkout $BRANCH_TO_BUILD
-git pull
-git checkout -b $GIT_LOCAL_BRANCH
+#log "Saving the currently checked out branch"
+#CURRENT_BRANCH=$(git branch --show-current)
+#trap "git checkout $CURRENT_BRANCH" EXIT
+#
+#log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
+#git checkout $BRANCH_TO_BUILD
+#git pull
+#git checkout -b $GIT_LOCAL_BRANCH
 
 log "Generating env file required by Fastlane..."
 tee .env <<EOF
-export INCREMENT_BUILD_NUMBER=true
-export BUILD_NUMBER=${BUILD_NUMBER}
-export COMMIT_CHANGES_TO_GIT=true
 export BRANCH_TO_BUILD=${BRANCH_TO_BUILD}
 export GIT_LOCAL_BRANCH=${GIT_LOCAL_BRANCH}
+export COMMIT_CHANGES_TO_GIT=true
+$(is_false $BUMP_BUILD_NUMBER || echo "\
+export INCREMENT_BUILD_NUMBER=true
+export BUILD_NUMBER=${BUILD_NUMBER}
+")
+$(is_false $BUMP_VERSION || echo "\
+export INCREMENT_VERSION_NUMBER=true
+export VERSION=${VERSION}
+")
 EOF
+
+echo "DEBUG, exit"
+exit 0
 
 log "Running the fastlane build number bumper script"
 (. .env && cd fastlane && bundle install && bundle exec fastlane set_app_build_number)
 
-if grep -qiE '^(no?|false)$' <<<$DRY_RUN; then
+if is_false $LOCAL_MODE; then
   log "Pushing branch ${GIT_LOCAL_BRANCH}, and creating a corresponding PR to ${BRANCH_TO_BUILD}"
   git push origin ${GIT_LOCAL_BRANCH}
 
@@ -60,6 +72,6 @@ NONE
 \`\`\`
 EOF
 else
-  log "Running in DRY_RUN mode: skipping branch push and PR creation"
-  log "Run with DRY_RUN=false to disable dry run mode"
+  log "Running with LOCAL_MODE enabled: skipping branch push and PR creation"
+  log "Run with LOCAL_MODE=false to disable dry run mode"
 fi
