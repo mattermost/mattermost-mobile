efaeb8f691591b7b74d1a44f07ab61051da1445b
Mario Vitale <mvitale1989@hotmail.com>
2023-11-27 19:08:00 +0100

    Fully implement version bumping

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -3,55 +3,70 @@ set -eu -o pipefail
 cd "$(dirname "$0")"/..
 
 log () { echo "[$(date +%Y-%m-%dT%H:%M:%S%Z)]" "$@"; }
-is_false () { grep -qiE '^(no?|false)$' <<<$1; }
 
 : ${BRANCH_TO_BUILD:=main}
 : ${PR_REVIEWERS:=mattermost/core-build-engineers}
-: ${BUMP_BUILD_NUMBER:=true}
-: ${BUMP_VERSION:=false}     # If enabled, you must populate the VERSION variable as well
-: ${LOCAL_MODE:=yes}         # Disable LOCAL_MODE to push the commit to origin, and create a corresponding PR
-LATEST_BUILD_NUMBER=$(./scripts/get_latest_build_number.sh)
-BUILD_NUMBER=$(($LATEST_BUILD_NUMBER + 1))
-GIT_LOCAL_BRANCH=bump-build-${BRANCH_TO_BUILD}-${BUILD_NUMBER}
-log "Build number to use for the beta build: $BUILD_NUMBER"
+: ${BUMP_BUILD_NUMBER:=}
+: ${BUMP_VERSION_NUMBER:=}  # If enabled, you must populate the VERSION variable as well
+: ${CREATE_PR:=}            # Enable CREATE_PR to push the commit to origin, and create a corresponding PR
+: ${GIT_LOCAL_BRANCH:=chore-bump-${BRANCH_TO_BUILD}-$(date +%s)}
+
+log "Checking that the configuration is sane"
+if [ -n "$BUMP_VERSION_NUMBER" ]; then
+  : ${VERSION:?Setting this variable is required when BUMP_VERSION_NUMBER is set.}
+fi
 
 log "Asserting that the workdir is clean"
 if ! git diff --quiet; then
-  log "Error, workdir is not clean: aborting" >&2
+  log "Error: workdir is not clean. Aborting" >&2
   exit 1
 fi
 
-#log "Saving the currently checked out branch"
-#CURRENT_BRANCH=$(git branch --show-current)
-#trap "git checkout $CURRENT_BRANCH" EXIT
-#
-#log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
-#git checkout $BRANCH_TO_BUILD
-#git pull
-#git checkout -b $GIT_LOCAL_BRANCH
+log "Saving the currently checked out branch"
+CURRENT_BRANCH=$(git branch --show-current)
+trap "git checkout $CURRENT_BRANCH" EXIT
+
+log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
+git checkout $BRANCH_TO_BUILD
+git pull
+git checkout -b $GIT_LOCAL_BRANCH
+
+if [ -n "$BUMP_BUILD_NUMBER" ]; then
+  log "Selecting the next largest build number..."
+  LATEST_BUILD_NUMBER=$(./scripts/get_latest_build_number.sh)
+  BUILD_NUMBER=$(($LATEST_BUILD_NUMBER + 1))
+  log "Build number to use for the beta build: $BUILD_NUMBER"
+fi
 
 log "Generating env file required by Fastlane..."
 tee .env <<EOF
 export BRANCH_TO_BUILD=${BRANCH_TO_BUILD}
 export GIT_LOCAL_BRANCH=${GIT_LOCAL_BRANCH}
 export COMMIT_CHANGES_TO_GIT=true
-$(is_false $BUMP_BUILD_NUMBER || echo "\
+$([ -z "$BUMP_BUILD_NUMBER" ] || echo "\
 export INCREMENT_BUILD_NUMBER=true
 export BUILD_NUMBER=${BUILD_NUMBER}
 ")
-$(is_false $BUMP_VERSION || echo "\
+$([ -z "$BUMP_VERSION_NUMBER" ] || echo "\
 export INCREMENT_VERSION_NUMBER=true
 export VERSION=${VERSION}
 ")
 EOF
 
-echo "DEBUG, exit"
-exit 0
+log "Setting up fastlane environment"
+(. .env && cd fastlane && bundle install)
 
-log "Running the fastlane build number bumper script"
-(. .env && cd fastlane && bundle install && bundle exec fastlane set_app_build_number)
+if [ -n "$BUMP_BUILD_NUMBER" ]; then
+  log "Bumping build number..."
+  (. .env && cd fastlane && bundle exec set_app_build_number)
+fi
 
-if is_false $LOCAL_MODE; then
+if [ -n "$BUMP_VERSION_NUMBER" ]; then
+  log "Bumping version number..."
+  (. .env && cd fastlane && bundle exec set_app_version)
+fi
+
+if [ -n "${CREATE_PR}" ]; then
   log "Pushing branch ${GIT_LOCAL_BRANCH}, and creating a corresponding PR to ${BRANCH_TO_BUILD}"
   git push origin ${GIT_LOCAL_BRANCH}
 
@@ -64,7 +79,12 @@ if is_false $LOCAL_MODE; then
     --title "Bump app build number to $BUILD_NUMBER" \
     --body-file - <<EOF
 #### Summary
+$([ -z "$BUMP_BUILD_NUMBER" ] || echo "\
 Bump app build number to $BUILD_NUMBER
+")
+$([ -z "$BUMP_VERSION_NUMBER" ] || echo "\
+Bump app version number to ${VERSION}
+")
 
 #### Release Note
 \`\`\`release-note
@@ -72,6 +92,5 @@ NONE
 \`\`\`
 EOF
 else
-  log "Running with LOCAL_MODE enabled: skipping branch push and PR creation"
-  log "Run with LOCAL_MODE=false to disable dry run mode"
+  log "Skipping branch push and PR creation. Run with CREATE_PR=yes to perform those operations."
 fi
