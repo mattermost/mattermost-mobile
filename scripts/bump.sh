a9186f27f8960ee1bfeb795abdbea3475196fde7
Mario Vitale <mvitale1989@hotmail.com>
2023-12-06 16:53:57 +0100

    Additional script sanity check, better env and PR templating

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -14,19 +14,23 @@ log () { echo "[$(date +%Y-%m-%dT%H:%M:%S%Z)]" "$@"; }
 : ${GIT_LOCAL_BRANCH:=chore-bump-${BRANCH_TO_BUILD}-$(date +%s)}
 
 log "Checking that the configuration is sane"
+if [ -z "${BUMP_BUILD_NUMBER}" -a -z "${BUMP_VERSION_NUMBER}" ]; then
+  log "Error: you must set at least one between BUMP_BUILD_NUMBER and BUMP_VERSION_NUMBER" >&2
+  exit 1
+fi
 if [ -n "$BUMP_VERSION_NUMBER" ]; then
   : ${VERSION:?Setting this variable is required when BUMP_VERSION_NUMBER is set.}
   VERSION_REGEXP='^[0-9]+\.[0-9]+\.[0-9]+$'
   if ! grep -qE $VERSION_REGEXP"" <<<$VERSION; then
     log "Error: the VERSION variable value should match regexp '$VERSION_REGEXP'. Aborting." >&2
-    exit 1
+    exit 2
   fi
 fi
 
 log "Asserting that the workdir is clean"
 if ! git diff --quiet; then
   log "Error: workdir is not clean. Aborting" >&2
-  exit 1
+  exit 3
 fi
 
 log "Saving the currently checked out branch"
@@ -43,16 +47,16 @@ tee .env <<EOF
 export BRANCH_TO_BUILD=${BRANCH_TO_BUILD}
 export GIT_LOCAL_BRANCH=${GIT_LOCAL_BRANCH}
 export COMMIT_CHANGES_TO_GIT=true
-$([ -z "$BUMP_BUILD_NUMBER" ] || echo "\
+$([ -n "$BUMP_BUILD_NUMBER" ] && echo "\
 export INCREMENT_BUILD_NUMBER=true
-")
-$([ -z "${BUILD_NUMBER:-}" ] || echo "\
+" || :)
+$([ -n "$BUMP_BUILD_NUMBER" -a -n "${BUILD_NUMBER:-}" ] && echo "\
 export BUILD_NUMBER=${BUILD_NUMBER}
-")
-$([ -z "$BUMP_VERSION_NUMBER" ] || echo "\
+" || :)
+$([ -n "$BUMP_VERSION_NUMBER" ] && echo "\
 export INCREMENT_VERSION_NUMBER=true
 export VERSION=${VERSION}
-")
+" || :)
 EOF
 
 log "Setting up fastlane environment"
@@ -73,17 +77,18 @@ if [ -n "${CREATE_PR}" ]; then
   git push origin ${GIT_LOCAL_BRANCH}
 
   log "Creating PR"
+  PR_TITLE="Bump app ${BUMP_BUILD_NUMBER:+build}${BUMP_VERSION_NUMBER:+${BUMP_BUILD_NUMBER:+ and }version} number"
   gh pr create \
     --repo mattermost/mattermost-mobile \
     --base main \
     --head "${GIT_LOCAL_BRANCH}" \
     --reviewer "${PR_REVIEWERS}" \
-    --title "Bump app build number to $BUILD_NUMBER" \
+    --title "$PR_TITLE" \
     $([ -z "$BUMP_VERSION_NUMBER" ] || echo -n "--milestone v${VERSION} --label CherryPick/Approved") \
     --body-file - <<EOF
 #### Summary
 $([ -z "$BUMP_BUILD_NUMBER" ] || echo "\
-Bump app build number to $BUILD_NUMBER
+Bump app build number to ${BUILD_NUMBER:-immediately next available}
 ")
 $([ -z "$BUMP_VERSION_NUMBER" ] || echo "\
 Bump app version number to ${VERSION}
