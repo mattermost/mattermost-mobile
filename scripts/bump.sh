2a55f49ae6c9a1ece94adca5af8c6b05055b9dad
Mario Vitale <mvitale1989@hotmail.com>
2023-12-06 19:21:05 +0100

    Fix BUILD_NUMBER inference order

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -37,17 +37,17 @@ log "Saving the currently checked out branch"
 CURRENT_BRANCH=$(git branch --show-current)
 trap "git checkout $CURRENT_BRANCH" EXIT
 
-log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
-git checkout $BRANCH_TO_BUILD
-git pull
-git checkout -b $GIT_LOCAL_BRANCH
-
 if [ -n "$BUMP_BUILD_NUMBER" ]; then
   log "Selecting the next largest build number..."
   LATEST_BUILD_NUMBER=$(./scripts/get_latest_build_number.sh)
   BUILD_NUMBER=$(($LATEST_BUILD_NUMBER + 1))
   log "Build number to use for the beta build: $BUILD_NUMBER"
 fi
+
+log "Creating branch '${GIT_LOCAL_BRANCH}' based on branch '$BRANCH_TO_BUILD'"
+git checkout $BRANCH_TO_BUILD
+git pull
+git checkout -b $GIT_LOCAL_BRANCH
 
 log "Generating env file required by Fastlane..."
 tee .env <<EOF
