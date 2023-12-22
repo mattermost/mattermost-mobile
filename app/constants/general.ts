77e3ccd4b937f1b999346f33f71a048c9798454c
Miguel de la Cruz <mgdelacroix@gmail.com>
2023-12-22 10:29:49 +0100

    Improves get roles by names requests to batch them following the new endpoint limit (#7650)
    (cherry picked from commit 4095212d86019c1823bb0d0540de112e216919dd)

--- app/constants/general.ts
+++ app/constants/general.ts
@@ -32,6 +32,7 @@ export default {
     MAX_USERS_IN_GM: 7,
     MIN_USERS_IN_GM: 3,
     MAX_GROUP_CHANNELS_FOR_PROFILES: 50,
+    MAX_GET_ROLES_BY_NAMES: 100,
     DEFAULT_AUTOLINKED_URL_SCHEMES: ['http', 'https', 'ftp', 'mailto', 'tel', 'mattermost'],
     PROFILE_CHUNK_SIZE: 100,
     SEARCH_TIMEOUT_MILLISECONDS: 500,
