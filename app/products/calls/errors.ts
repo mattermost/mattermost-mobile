ec29f31187f5019581251223d114a494c7295bec
streamer45 <cstcld91@gmail.com>
2024-01-12 12:42:46 -0600

    Handle user leaving channel with active call

---
+++ app/products/calls/errors.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export const userRemovedFromChannelErr = new Error('user was removed from channel');
+export const userLeftChannelErr = new Error('user has left channel');
