cc96982d24c6675c7ce32992f12245301b7ae3f7
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-01-13 06:56:22 +0000

    MM-53479 : Improve design of keyword that trigger notification in mobile (#7458)

--- app/components/selected_chip/index.tsx
+++ app/components/selected_chip/index.tsx
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React, {useCallback} from 'react';
-import {Text, TouchableOpacity, useWindowDimensions} from 'react-native';
+import {Text, TouchableOpacity, useWindowDimensions, type StyleProp, type ViewStyle} from 'react-native';
 import Animated, {FadeIn, FadeOut} from 'react-native-reanimated';
 
 import CompassIcon from '@components/compass_icon';
@@ -17,6 +17,7 @@ type SelectedChipProps = {
     extra?: React.ReactNode;
     onRemove: (id: string) => void;
     testID?: string;
+    containerStyle?: StyleProp<ViewStyle>;
 }
 
 export const USER_CHIP_HEIGHT = 32;
@@ -54,10 +55,13 @@ export default function SelectedChip({
     extra,
     onRemove,
     testID,
+    containerStyle,
 }: SelectedChipProps) {
     const theme = useTheme();
     const style = getStyleFromTheme(theme);
     const dimensions = useWindowDimensions();
+
+    const containerStyles = [style.container, containerStyle];
 
     const onPress = useCallback(() => {
         onRemove(id);
@@ -67,7 +71,7 @@ export default function SelectedChip({
         <Animated.View
             entering={FadeIn.duration(FADE_DURATION)}
             exiting={FadeOut.duration(FADE_DURATION)}
-            style={style.container}
+            style={containerStyles}
             testID={testID}
         >
             {extra}
