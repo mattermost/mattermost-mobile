1a36fbd9e4bb566ca3d3e0be6b4eedb37de208de
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-03-22 06:52:15 +0530

    fix test

--- detox/e2e/support/test_config.ts
+++ detox/e2e/support/test_config.ts
@@ -3,10 +3,10 @@
 
 export const serverOneUrl = process.env.SITE_1_URL || (process.env.IOS === 'true' ? 'https://mobile-e2e.test.mattermost.cloud' : 'http://10.0.2.2:8065');
 export const siteOneUrl = process.env.SITE_1_URL || 'https://mobile-e2e.test.mattermost.cloud';
-export const serverTwoUrl = process.env.SITE_2_URL || 'https://mobile-e2e-site-2.test.mattermost.cloud/';
-export const siteTwoUrl = process.env.SITE_2_URL || 'https://mobile-e2e-site-2.test.mattermost.cloud/';
-export const serverThreeUrl = process.env.SITE_3_URL || 'https://mobile-e2e-site-3.test.mattermost.cloud/';
-export const siteThreeUrl = process.env.SITE_3_URL || 'https://mobile-e2e-site-3.test.mattermost.cloud/';
+export const serverTwoUrl = process.env.SITE_2_URL || 'https://mobile-e2e-site-2.test.mattermost.cloud';
+export const siteTwoUrl = process.env.SITE_2_URL || 'https://mobile-e2e-site-2.test.mattermost.cloud';
+export const serverThreeUrl = process.env.SITE_3_URL || 'https://mobile-e2e-site-3.test.mattermost.cloud';
+export const siteThreeUrl = process.env.SITE_3_URL || 'https://mobile-e2e-site-3.test.mattermost.cloud';
 export const smtpUrl = process.env.SMTP_URL || 'http://127.0.0.1:9001';
 export const adminEmail = process.env.ADMIN_EMAIL || 'success+sysadmin@simulator.amazonses.com';
 export const adminUsername = process.env.ADMIN_USERNAME || 'admin';
