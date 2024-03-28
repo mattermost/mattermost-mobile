2b1dfff8d4aa41639b165df9d0a63e81cc963855
Mattermost Build <build@mattermost.com>
2024-03-28 14:37:32 +0200

    Allow iOS to connect to servers without SSL (#7881) (#7885)

--- app/screens/server/index.tsx
+++ app/screens/server/index.tsx
@@ -290,31 +290,34 @@ const Server = ({
             cancelPing = undefined;
         };
 
-        const serverUrl = await getServerUrlAfterRedirect(pingUrl, !retryWithHttp);
-        if (!serverUrl) {
+        const ping = await getServerUrlAfterRedirect(pingUrl, !retryWithHttp);
+        if (!ping.url) {
             cancelPing();
+            if (retryWithHttp) {
+                const nurl = pingUrl.replace('https:', 'http:');
+                pingServer(nurl, false);
+            } else {
+                setUrlError(getErrorMessage(ping.error, intl));
+                setButtonDisabled(true);
+                setConnecting(false);
+            }
             return;
         }
-        const result = await doPing(serverUrl, true, managedConfig?.timeout ? parseInt(managedConfig?.timeout, 10) : undefined);
+        const result = await doPing(ping.url, true, managedConfig?.timeout ? parseInt(managedConfig?.timeout, 10) : undefined);
 
         if (canceled) {
             return;
         }
 
         if (result.error) {
-            if (retryWithHttp) {
-                const nurl = serverUrl.replace('https:', 'http:');
-                pingServer(nurl, false);
-            } else {
-                setUrlError(getErrorMessage(result.error, intl));
-                setButtonDisabled(true);
-                setConnecting(false);
-            }
+            setUrlError(getErrorMessage(result.error, intl));
+            setButtonDisabled(true);
+            setConnecting(false);
             return;
         }
 
-        canReceiveNotifications(serverUrl, result.canReceiveNotifications as string, intl);
-        const data = await fetchConfigAndLicense(serverUrl, true);
+        canReceiveNotifications(ping.url, result.canReceiveNotifications as string, intl);
+        const data = await fetchConfigAndLicense(ping.url, true);
         if (data.error) {
             setButtonDisabled(true);
             setUrlError(getErrorMessage(data.error, intl));
@@ -332,7 +335,7 @@ const Server = ({
         }
 
         const server = await getServerByIdentifier(data.config.DiagnosticId);
-        const credentials = await getServerCredentials(serverUrl);
+        const credentials = await getServerCredentials(ping.url);
         setConnecting(false);
 
         if (server && server.lastActiveAt > 0 && credentials?.token) {
@@ -344,7 +347,7 @@ const Server = ({
             return;
         }
 
-        displayLogin(serverUrl, data.config!, data.license!);
+        displayLogin(ping.url, data.config!, data.license!);
     };
 
     const transform = useAnimatedStyle(() => {
