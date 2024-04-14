7b6635b2b1fdb9aa2e2a72f41f29ce1cd02ec18d
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-04-14 19:58:01 +0530

    run android tests

---
+++ detox/test_device_info.js
@@ -0,0 +1,53 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+/* eslint-disable no-process-env */
+/* eslint-disable no-console */
+/* eslint-disable no-process-exit */
+const fs = require('fs');
+
+function getDeviceInfo(platform) {
+    const detoxConfig = JSON.parse(fs.readFileSync('./.detoxrc.json', 'utf-8'));
+
+    let deviceName = '';
+    let deviceOSVersion = '';
+
+    if (platform === 'ios' && detoxConfig.devices.ios) {
+        deviceName = detoxConfig.devices.ios.device.type;
+        deviceOSVersion = detoxConfig.devices.ios.device.os;
+    } else if (platform === 'android' && detoxConfig.devices.android) {
+        deviceName = detoxConfig.devices.android.device.avdName;
+        deviceOSVersion = 'Android';
+    } else {
+        throw new Error(`Unsupported platform: ${platform}`);
+    }
+
+    return {deviceName, deviceOSVersion};
+}
+
+function main(platform) {
+    try {
+        const {deviceName, deviceOSVersion} = getDeviceInfo(platform);
+        const buildID = `${process.env.GITHUB_RUN_ID}-${deviceName}-${deviceOSVersion}`.replace(/ /g, '_');
+
+        // Set environment variables
+        process.env.BUILD_ID = buildID;
+        process.env.DEVICE_NAME = deviceName;
+        process.env.DEVICE_OS_VERSION = deviceOSVersion;
+
+        console.log(`BUILD_ID=${buildID}`);
+        console.log(`DEVICE_NAME=${deviceName}`);
+        console.log(`DEVICE_OS_VERSION=${deviceOSVersion}`);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+const args = process.argv.slice(2);
+if (args.length !== 1 || (args[0] !== 'ios' && args[0] !== 'android')) {
+    console.error('Usage: node script.js <platform>');
+    process.exit(1);
+}
+
+const PLATFORM = args[0];
+main(PLATFORM);
