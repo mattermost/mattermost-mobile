a7bd74380f46e4454e26c09a9e4ad12f09ace740
Aditya Pratap Singh Hada <49001649+apshada@users.noreply.github.com>
2024-04-16 15:59:59 +0530

    [MM-56585]Show only unreads filter stays active after changing the 'Group unreads' setting (#7790)
    * showing unread and threads, of group unread setting in on
    
    * using third variable to maintain state for showing categories
    
    * removed useState from useMemo
    
    * Removed state, return orderctegroies if onlyUnreads & unreadsOnTop is true
    
    * test cases updates
    
    * removed onlyUnreads props from threads
    
    * removed onlyUnreads
    
    * test cases updated
    
    * removed unnecessary changes
    
    * removed onlyUnreads from dep
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>
    (cherry picked from commit 6dcefaf1e4332142d2722123ae6f3ec0842593b9)

--- app/components/threads_button/index.ts
+++ app/components/threads_button/index.ts
@@ -2,31 +2,20 @@
 // See LICENSE.txt for license information.
 
 import {withDatabase, withObservables} from '@nozbe/watermelondb/react';
-import {of as of$} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
 
-import Preferences from '@constants/preferences';
-import {getSidebarPreferenceAsBool} from '@helpers/api/preference';
-import {querySidebarPreferences} from '@queries/servers/preference';
-import {observeCurrentChannelId, observeCurrentTeamId, observeOnlyUnreads} from '@queries/servers/system';
+import {observeCurrentChannelId, observeCurrentTeamId} from '@queries/servers/system';
 import {observeUnreadsAndMentionsInTeam} from '@queries/servers/thread';
 
 import ThreadsButton from './threads_button';
 
 import type {WithDatabaseArgs} from '@typings/database/database';
-import type PreferenceModel from '@typings/database/models/servers/preference';
 
 const enhanced = withObservables([], ({database}: WithDatabaseArgs) => {
     const currentTeamId = observeCurrentTeamId(database);
 
     return {
         currentChannelId: observeCurrentChannelId(database),
-        groupUnreadsSeparately: querySidebarPreferences(database, Preferences.CHANNEL_SIDEBAR_GROUP_UNREADS).
-            observeWithColumns(['value']).
-            pipe(
-                switchMap((prefs: PreferenceModel[]) => of$(getSidebarPreferenceAsBool(prefs, Preferences.CHANNEL_SIDEBAR_GROUP_UNREADS))),
-            ),
-        onlyUnreads: observeOnlyUnreads(database),
         unreadsAndMentions: currentTeamId.pipe(
             switchMap(
                 (teamId) => observeUnreadsAndMentionsInTeam(database, teamId),
