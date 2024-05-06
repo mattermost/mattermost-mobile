b98c184e7cd8ce7132aa83111591eb1e788d858c
Daniel Espino García <larkox@gmail.com>
2024-05-06 12:00:19 +0200

    Fix gallery footer author image showing old image (#7910)
    * Fix gallery footer author image showing old image
    
    * Fix tests
    
    * Address feedback
    
    * Fix tests and minor refactor

--- app/components/team_sidebar/team_list/team_item/team_icon.tsx
+++ app/components/team_sidebar/team_list/team_item/team_icon.tsx
@@ -5,6 +5,8 @@ import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {View, Text, type StyleProp, type TextStyle} from 'react-native';
 import FastImage from 'react-native-fast-image';
 
+import {buildAbsoluteUrl} from '@actions/remote/file';
+import {buildTeamIconUrl} from '@actions/remote/team';
 import {useServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
 import NetworkManager from '@managers/network_manager';
@@ -124,7 +126,7 @@ export default function TeamIcon({
         teamIconContent = (
             <FastImage
                 style={styles.image}
-                source={{uri: `${serverUrl}${client!.getTeamIconUrl(id, lastIconUpdate)}`}}
+                source={{uri: buildAbsoluteUrl(serverUrl, buildTeamIconUrl(serverUrl, id, lastIconUpdate))}}
                 onError={handleImageError}
             />
         );
