27361557674d236170c62d85109e4b497cae31cf
Christopher Poile <cpoile@gmail.com>
2024-05-10 16:43:35 -0400

    [MM-58129] Calls: Host controls remove participant, view profile (#7938)
    * host remove command, host removed feedback
    
    * add view profile control
    
    * i18n; dismissBottomSheet later
    
    * PR comments
    
    * remove confusing setPreviousCall(null)
    
    * our own separator, bit more space for Android
    
    * no need for previousCall due to changes on server
    
    * move host removed alert to an error
    
    * channel_id is not send via the msg.broadcast
    
    * channel_id is still sent via the msg.broadcast
    
    (cherry picked from commit 7fbd2e52e2e6a33cc42682326fec4835e15cbeb9)

--- app/products/calls/errors.ts
+++ app/products/calls/errors.ts
@@ -3,3 +3,4 @@
 
 export const userRemovedFromChannelErr = new Error('user was removed from channel');
 export const userLeftChannelErr = new Error('user has left channel');
+export const hostRemovedErr = new Error('user was removed by host');
