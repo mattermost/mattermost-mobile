d59ea607d7fce1397bc088ef6be3ab2284cd0fe4
Elias Nahum <nahumhbl@gmail.com>
2024-05-23 09:33:22 +0800

    create local library to replace MattermostManaged, Notifications and SplitView modules with new arch support

---
+++ libraries/@mattermost/rnutils/src/NativeRNUtils.ts
@@ -0,0 +1,62 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {type TurboModule, TurboModuleRegistry} from 'react-native';
+
+import type {Int32, UnsafeObject} from 'react-native/Libraries/Types/CodegenTypes';
+
+export type SplitView = Readonly<{
+  isSplit: boolean;
+  isTablet: boolean;
+}>
+
+type DatabaseOperationResult = Readonly<{
+  error?: string | null;
+  success: boolean;
+}>
+
+type Notification = Readonly<{
+  identifier: string;
+  payload?: UnsafeObject;
+  title?: string;
+  body?: string;
+  sound?: string;
+  badge?: Int32;
+  type?: string;
+  thread?: string;
+  channel_id?: string;
+  root_id?: string;
+}>
+
+type Constants = Readonly<{
+  appGroupIdentifier: string;
+  appGroupSharedDirectory: Readonly<{
+    sharedDirectory: string;
+    databasePath: string;
+  }>;
+}>
+
+export interface Spec extends TurboModule {
+    readonly getConstants: () => Constants;
+
+    addListener: (eventType: string) => void;
+    removeListeners: (count: number) => void;
+
+    getRealFilePath: (filePath: string) => Promise<string>;
+    saveFile: (filePath: string) => Promise<string>;
+
+    isRunningInSplitView: () => SplitView;
+    unlockOrientation: () => void;
+    lockPortrait: () => void;
+
+    deleteDatabaseDirectory: (databaseName: string, shouldRemoveDirectory: boolean) => DatabaseOperationResult;
+    renameDatabase: (databaseName: string, newDatabaseName: string) => DatabaseOperationResult;
+    deleteEntitiesFile: () => boolean;
+
+    getDeliveredNotifications(): Promise<Notification[]>;
+    removeChannelNotifications(serverUrl: string, channelId: string): void;
+    removeThreadNotifications(serverUrl: string, threadId: string): void;
+    removeServerNotifications(serverUrl: string): void;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('RNUtils');
