ed90de72892875b1ba34a7a29490b0698fb6bcdd
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-06-15 03:24:24 +0530

    new server

--- detox/e2e/support/test_config.ts
+++ detox/e2e/support/test_config.ts
@@ -1,15 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const serverOneUrl = process.env.SITE_1_URL || (process.env.IOS === 'true' ? 'https://plugins.test.mattermost.cloud' : 'http://10.0.2.2:8065');
-export const siteOneUrl = 'https://plugins.test.mattermost.cloud';
-export const serverTwoUrl = 'https://test-9-9-1-rc1.test.mattermost.cloud';
-export const siteTwoUrl = process.env.SITE_2_URL || 'http://localhost:8065';
-export const serverThreeUrl = process.env.SITE_3_URL || 'http://localhost:8065';
-export const siteThreeUrl = process.env.SITE_3_URL || 'http://localhost:8065';
+export const serverOneUrl = 'https://mobile-e2e-site-1.test.mattermost.cloud'
+export const siteOneUrl = 'https://mobile-e2e-site-1.test.mattermost.cloud'
+export const serverTwoUrl = 'https://mobile-e2e-site-2.test.mattermost.cloud'
+export const siteTwoUrl = 'https://mobile-e2e-site-2.test.mattermost.cloud'
+export const serverThreeUrl = 'https://mobile-e2e-site-3.test.mattermost.cloud'
+export const siteThreeUrl = 'https://mobile-e2e-site-3.test.mattermost.cloud'
 export const smtpUrl = process.env.SMTP_URL || 'http://127.0.0.1:9001';
-export const adminEmail = process.env.ADMIN_EMAIL || 'sysadmin@sample.mattermost.com';
-export const adminUsername = 'admin';
-export const adminPassword = 'admin@xqatw5wyofn4xyr6kmtr99kjqy';
+export const adminEmail = 'success+sysadmin@simulator.amazonses.com'
+export const adminUsername = 'admin'
+export const adminPassword = 'admin@bq8s1bqr6inm8rramf4js8f96a'
 export const ldapServer = process.env.LDAP_SERVER || '127.0.0.1';
 export const ldapPort = process.env.LDAP_PORT || 389;
