6baa04dba3ee4a83c32ec50e5410e536aa9628eb
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-06-21 05:17:13 +0530

    fix eslint

--- detox/utils/generate_detox_config_ci.js
+++ detox/utils/generate_detox_config_ci.js
@@ -1,10 +1,12 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+/* eslint-disable no-process-env */
+/* eslint-disable no-console */
 const fs = require('fs');
-
-const deviceType = process.env.DETOX_DEVICE_TYPE || 'iPhone 14';
-const deviceOS = process.env.DETOX_DEVICE_OS || 'iOS 17.2';
-
+const deviceType = process.env.DETOX_DEVICE_TYPE || 'iPhone 15 Pro';
+const deviceOS = process.env.DETOX_DEVICE_OS || 'iOS 17.4';
 const detoxConfigTemplate = fs.readFileSync('../.detoxrc.json', 'utf8');
-
 const detoxConfig = detoxConfigTemplate
   .replace('__DEVICE_TYPE__', deviceType)
   .replace('__DEVICE_OS__', deviceOS);
