f3ce3b1bcd7c6c23288dbf8804d947e0f23310e5
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-09-08 14:51:46 +0530

    install platform emu

--- detox/create_android_emulator.sh
+++ detox/create_android_emulator.sh
@@ -30,7 +30,8 @@ fi
 SYSTEM_IMAGE="system-images;android-${SDK_VERSION};google_apis;${CPU_ARCH_FAMILY}"
 
 # Install the system image if it's not already installed
-sdkmanager --install "platform-tools" "$SYSTEM_IMAGE"
+sdkmanager --install "$SYSTEM_IMAGE"
+sdkmanager --install "platform-tools"
 
 # Create virtual device in a relative "detox_pixel_4_xl_api_${SDK_VERSION}" folder
 avdmanager create avd -n $NAME -k "$SYSTEM_IMAGE" -p $NAME -d 'pixel'
