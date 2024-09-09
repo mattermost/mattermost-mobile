16beb8309bbfa0d73053b3fe7c6efcecc2590343
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-09-09 23:28:27 +0530

    add emulator to the path

--- detox/create_android_emulator.sh
+++ detox/create_android_emulator.sh
@@ -33,8 +33,6 @@ SYSTEM_IMAGE="system-images;android-${SDK_VERSION};default;${CPU_ARCH_FAMILY}"
 # Install the system image if it's not already installed
 sdkmanager --install "platforms;android-${SDK_VERSION}"
 sdkmanager --install "$SYSTEM_IMAGE"
-sdkmanager --install "platform-tools"
-sdkmanager --install "emulator"
 
 # Create virtual device in a relative "detox_pixel_4_xl_api_${SDK_VERSION}" folder
 avdmanager create avd -n $NAME -k "$SYSTEM_IMAGE" -p $NAME -d 'pixel'
