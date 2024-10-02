3b91c63ffecd7945f004eaaa1185fea24a9bd629
Elias Nahum <nahumhbl@gmail.com>
2024-10-02 14:45:11 +0800

    Add support for Auth Scheme customization

--- app/constants/sso.ts
+++ app/constants/sso.ts
@@ -1,10 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import LocalConfig from '@assets/config.json';
 import keyMirror from '@utils/key_mirror';
 
-export const REDIRECT_URL_SCHEME = 'mmauth://';
-export const REDIRECT_URL_SCHEME_DEV = 'mmauthbeta://';
+export const REDIRECT_URL_SCHEME = LocalConfig.AuthUrlScheme;
+export const REDIRECT_URL_SCHEME_DEV = LocalConfig.AuthUrlSchemeDev;
 
 const constants = keyMirror({
     SAML: null,
