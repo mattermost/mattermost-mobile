f0c6a9d7504d00a496a52075c2732b4b24931a77
Tanmay Thole <tanmaythole@gmail.com>
2024-10-05 19:46:53 +0530

    feat: Add copy button on code view

--- app/screens/code/index.tsx
+++ app/screens/code/index.tsx
@@ -2,13 +2,19 @@
 // See LICENSE.txt for license information.
 
 import {useManagedConfig} from '@mattermost/react-native-emm';
-import React from 'react';
+import Clipboard from '@react-native-clipboard/clipboard';
+import React, {useCallback, useEffect} from 'react';
 import {StyleSheet, type TextStyle} from 'react-native';
 import {SafeAreaView, type Edge} from 'react-native-safe-area-context';
 
+import CompassIcon from '@app/components/compass_icon';
+import {SNACK_BAR_TYPE} from '@app/constants/snack_bar';
+import {useTheme} from '@app/context/theme';
+import useNavButtonPressed from '@app/hooks/navigation_button_pressed';
+import {showSnackBar} from '@app/utils/snack_bar';
 import SyntaxHiglight from '@components/syntax_highlight';
 import useAndroidHardwareBackHandler from '@hooks/android_back_handler';
-import {popTopScreen} from '@screens/navigation';
+import {popTopScreen, setButtons} from '@screens/navigation';
 
 import type {AvailableScreens} from '@typings/screens/navigation';
 
@@ -19,6 +25,8 @@ type Props = {
     textStyle: TextStyle;
 }
 
+const COPY_CODE_BUTTON = 'copy-code';
+
 const edges: Edge[] = ['left', 'right'];
 
 const styles = StyleSheet.create({
@@ -26,8 +34,31 @@ const styles = StyleSheet.create({
 });
 
 const Code = ({code, componentId, language, textStyle}: Props) => {
+    const theme = useTheme();
     const managedConfig = useManagedConfig<ManagedConfig>();
     useAndroidHardwareBackHandler(componentId, popTopScreen);
+
+    const copyToClipboard = useCallback(() => {
+        if (!code) {
+            return;
+        }
+
+        Clipboard.setString(code);
+        showSnackBar({barType: SNACK_BAR_TYPE.CODE_COPIED, sourceScreen: componentId});
+    }, [code, componentId]);
+
+    useNavButtonPressed(COPY_CODE_BUTTON, componentId, copyToClipboard, []);
+
+    useEffect(() => {
+        setButtons(componentId, {
+            rightButtons: [
+                {
+                    id: COPY_CODE_BUTTON,
+                    icon: CompassIcon.getImageSourceSync('content-copy', 24, theme.centerChannelColor),
+                },
+            ],
+        });
+    }, [theme.centerChannelColor]);
 
     return (
         <SafeAreaView
