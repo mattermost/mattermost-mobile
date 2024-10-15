f8bcead69ef340fb900352ad63e03c68f18bfc8c
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-10-15 17:09:43 +0530

    handle lengthy message with show more button

--- app/components/draft/draft_post/draft_message/index.tsx
+++ app/components/draft/draft_post/draft_message/index.tsx
@@ -1,10 +1,13 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {View} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {ScrollView, View, useWindowDimensions, type LayoutChangeEvent} from 'react-native';
+import Animated from 'react-native-reanimated';
 
+import ShowMoreButton from '@app/components/post_list/post/body/message/show_more_button';
 import {useTheme} from '@app/context/theme';
+import {useShowMoreAnimatedStyle} from '@app/hooks/show_more';
 import {getMarkdownBlockStyles, getMarkdownTextStyles} from '@app/utils/markdown';
 import {makeStyleSheetFromTheme} from '@app/utils/theme';
 import {typography} from '@app/utils/typography';
@@ -20,6 +23,7 @@ type Props = {
 }
 
 const EMPTY_MENTION_KEYS: UserMentionKey[] = [];
+const SHOW_MORE_HEIGHT = 54;
 
 const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
     return {
@@ -48,21 +52,52 @@ const DraftMessage: React.FC<Props> = ({
     const style = getStyleSheet(theme);
     const blockStyles = getMarkdownBlockStyles(theme);
     const textStyles = getMarkdownTextStyles(theme);
+    const [height, setHeight] = useState<number|undefined>();
+    const [open, setOpen] = useState(false);
+    const dimensions = useWindowDimensions();
+    const maxHeight = Math.round((dimensions.height * 0.5) + SHOW_MORE_HEIGHT);
+    const animatedStyle = useShowMoreAnimatedStyle(height, maxHeight, open);
+
+    const onLayout = useCallback((event: LayoutChangeEvent) => setHeight(event.nativeEvent.layout.height), []);
+    const onPress = () => setOpen(!open);
+
     return (
-        <View>
-            <Markdown
-                baseTextStyle={style.message}
-                blockStyles={blockStyles}
-                channelId={draft.channelId}
-                layoutWidth={layoutWidth}
-                location={location}
-                postId={draft.id}
-                textStyles={textStyles}
-                value={draft.message}
-                mentionKeys={EMPTY_MENTION_KEYS}
+        <>
+            <Animated.View style={animatedStyle}>
+                <ScrollView
+                    keyboardShouldPersistTaps={'always'}
+                    scrollEnabled={false}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <View
+                        style={[style.messageContainer]}
+                        onLayout={onLayout}
+                    >
+                        <Markdown
+                            baseTextStyle={style.message}
+                            blockStyles={blockStyles}
+                            channelId={draft.channelId}
+                            layoutWidth={layoutWidth}
+                            location={location}
+                            postId={draft.id}
+                            textStyles={textStyles}
+                            value={draft.message}
+                            mentionKeys={EMPTY_MENTION_KEYS}
+                            theme={theme}
+                        />
+                    </View>
+                </ScrollView>
+            </Animated.View>
+            {(height || 0) > maxHeight &&
+            <ShowMoreButton
+                highlight={false}
                 theme={theme}
+                showMore={!open}
+                onPress={onPress}
             />
-        </View>
+            }
+        </>
     );
 };
 
