b78983434f4f2b187baaaad2baa386ae203ee71b
Claudio Costa <cstcld91@gmail.com>
2024-10-24 15:10:49 -0700

    Ensure full validation of Calls post props (#8279)
    (cherry picked from commit 751bb2dfb1b0ff84da152cc0ecfd0c9df224c4f4)

--- app/products/calls/utils.test.ts
+++ app/products/calls/utils.test.ts
@@ -6,7 +6,7 @@ import assert from 'assert';
 import {type CallsConfigState, DefaultCallsConfig} from '@calls/types/calls';
 import {License} from '@constants';
 
-import {getICEServersConfigs} from './utils';
+import {getICEServersConfigs, getCallPropsFromPost} from './utils';
 
 describe('getICEServersConfigs', () => {
     it('backwards compatible case, no ICEServersConfigs present', () => {
@@ -95,5 +95,111 @@ describe('getICEServersConfigs', () => {
                 urls: ['turn:turn.example.com:3478'],
             },
         ]);
+    });
+});
+
+describe('getCallPropsFromPost', () => {
+    test('undefined props', () => {
+        const post = {} as Post;
+
+        const props = getCallPropsFromPost(post);
+
+        expect(props.title).toBe('');
+        expect(props.start_at).toBe(0);
+        expect(props.end_at).toBe(0);
+        expect(props.recordings).toStrictEqual({});
+        expect(props.recording_files.length).toBe(0);
+        expect(props.transcriptions).toStrictEqual({});
+        expect(props.participants.length).toBe(0);
+    });
+
+    test('missing props', () => {
+        const post = {
+            props: {},
+        } as Post;
+
+        const props = getCallPropsFromPost(post);
+
+        expect(props.title).toBe('');
+        expect(props.start_at).toBe(0);
+        expect(props.end_at).toBe(0);
+        expect(props.recordings).toStrictEqual({});
+        expect(props.recording_files.length).toBe(0);
+        expect(props.transcriptions).toStrictEqual({});
+        expect(props.participants.length).toBe(0);
+    });
+
+    test('invalid props', () => {
+        const callProps = {
+            title: {},
+            start_at: 'invalid',
+            end_at: [],
+            recordings: null,
+            transcriptions: 45,
+            participants: 'invalid',
+            recording_files: 45,
+        };
+
+        const post = {
+            props: callProps as unknown,
+        } as Post;
+
+        const props = getCallPropsFromPost(post);
+
+        expect(props.title).toBe('');
+        expect(props.start_at).toBe(0);
+        expect(props.end_at).toBe(0);
+        expect(props.recordings).toStrictEqual({});
+        expect(props.recording_files.length).toBe(0);
+        expect(props.transcriptions).toStrictEqual({});
+        expect(props.participants.length).toBe(0);
+    });
+
+    test('full props', () => {
+        const callProps = {
+            title: 'call title',
+            start_at: 1000,
+            end_at: 1045,
+            recordings: {
+                recA: {
+                    file_id: 'recAFileID',
+                    post_id: 'recAPostID',
+                    tr_id: 'trA',
+                },
+                recB: {
+                    file_id: 'recBFileID',
+                    post_id: 'recBPostID',
+                    tr_id: 'trB',
+                },
+            },
+            recording_files: ['recAFileID', 'recBFileID'],
+            transcriptions: {
+                trA: {
+                    file_id: 'trAFileID',
+                    post_id: 'trAPostID',
+                    rec_id: 'recA',
+                },
+                trB: {
+                    file_id: 'trBFileID',
+                    post_id: 'trBPostID',
+                    rec_id: 'recB',
+                },
+            },
+            participants: ['userA', 'userB'],
+        };
+
+        const post = {
+            props: callProps as unknown,
+        } as Post;
+
+        const props = getCallPropsFromPost(post);
+
+        expect(props.title).toBe(post.props.title);
+        expect(props.start_at).toBe(post.props.start_at);
+        expect(props.end_at).toBe(post.props.end_at);
+        expect(props.recordings).toBe(post.props.recordings);
+        expect(props.recording_files).toBe(post.props.recording_files);
+        expect(props.transcriptions).toBe(post.props.transcriptions);
+        expect(props.participants).toBe(post.props.participants);
     });
 });
