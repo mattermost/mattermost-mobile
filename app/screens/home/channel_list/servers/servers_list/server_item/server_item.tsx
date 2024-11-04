bcd7fdca1affa7c40ffa8092e1f57e1571a1a552
Rahim Rahman <rahim.rahman@mattermost.com>
2024-11-04 14:28:53 -0700

    @asaadmahmood borderWidth 1px

--- app/screens/home/channel_list/servers/servers_list/server_item/server_item.tsx
+++ app/screens/home/channel_list/servers/servers_list/server_item/server_item.tsx
@@ -59,6 +59,7 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
     badge: {
         left: 18,
         top: -5,
+        borderWidth: 1,
     },
     button: {
         borderRadius: 8,
