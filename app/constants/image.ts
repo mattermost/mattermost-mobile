aa48d6d9d0297193f03251d84a77763a5868682c
fxnm <47592197+fxnm@users.noreply.github.com>
2024-11-19 13:17:00 +0100

    fix: fixed image overflow in message attachments

--- app/constants/image.ts
+++ app/constants/image.ts
@@ -6,7 +6,7 @@ export const IMAGE_MIN_DIMENSION = 50;
 export const MAX_GIF_SIZE = 100 * 1024 * 1024;
 export const VIEWPORT_IMAGE_OFFSET = 70;
 export const VIEWPORT_IMAGE_REPLY_OFFSET = 11;
-export const VIEWPORT_IMAGE_ATTACHMENT_OFFSET = 31.5; // (2 * 12) Container Padding + (2,5 + 5) Image Padding
+export const VIEWPORT_IMAGE_ATTACHMENT_OFFSET = 31.5; // (2 * 12) MessageAttachment Padding + (2,5 + 5) AttachmentImage Padding
 export const MAX_RESOLUTION = 7680 * 4320; // 8K, ~33MPX
 
 export default {
