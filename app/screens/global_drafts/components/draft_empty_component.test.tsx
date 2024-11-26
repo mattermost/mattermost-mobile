10ec08b152137d365a51cafc3612352f5c467b4d
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-11-26 18:51:39 +0530

    Added unit test for app/screens/global_drafts/components/draft_empty_component/index.tsx

---
+++ app/screens/global_drafts/components/draft_empty_component.test.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import {renderWithIntlAndTheme} from '@test/intl-test-helper';
+
+import DraftEmptyComponent from '.';
+
+describe('Draft Empty Component', () => {
+    it('should match the snapshot', () => {
+        const wrapper = renderWithIntlAndTheme(
+            <DraftEmptyComponent/>,
+        );
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    it('should render empty draft message', () => {
+        const wrapper = renderWithIntlAndTheme(
+            <DraftEmptyComponent/>,
+        );
+        expect(wrapper.getByText('No drafts at the moment')).toBeTruthy();
+        expect(wrapper.getByText('Any message you have started will show here.')).toBeTruthy();
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+});
