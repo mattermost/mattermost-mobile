85b4105d40631b3e9a85dd9d7a35d6a598b1da82
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-11-26 16:42:43 +0530

    added test for app/components/draft/draft_post/draft_message/index.tsx

---
+++ app/components/draft/draft_post/draft_message/draft_message.test.tsx
@@ -0,0 +1,59 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import {renderWithEverything} from '@test/intl-test-helper';
+import TestHelper from '@test/test_helper';
+
+import DraftMessage from '.';
+
+import type {Database} from '@nozbe/watermelondb';
+import type DraftModel from '@typings/database/models/servers/draft';
+
+describe('Draft Message', () => {
+    let database: Database;
+
+    beforeAll(async () => {
+        const server = await TestHelper.setupServerDatabase();
+        database = server.database;
+    });
+    it('should render the message', () => {
+        const props = {
+            draft: {
+                updateAt: 1633024800000,
+                message: 'Hello, World!',
+                channelId: 'channel_id',
+                rootId: '',
+                files: [],
+                metadata: {},
+            } as unknown as DraftModel,
+            layoutWidth: 100,
+            location: 'draft',
+        };
+        const {getByText} = renderWithEverything(
+            <DraftMessage {...props}/>, {database},
+        );
+        expect(getByText('Hello, World!')).toBeTruthy();
+    });
+
+    it('should match snapshot', () => {
+        const props = {
+            draft: {
+                updateAt: 1633024800000,
+                message: 'Hello, World!',
+                channelId: 'channel_id',
+                rootId: '',
+                files: [],
+                metadata: {},
+            } as unknown as DraftModel,
+            layoutWidth: 100,
+            location: 'draft',
+        };
+        const wrapper = renderWithEverything(
+            <DraftMessage {...props}/>, {database},
+        );
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+});
