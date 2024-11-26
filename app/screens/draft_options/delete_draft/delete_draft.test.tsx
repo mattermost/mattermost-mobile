05785dfc7f636d872607367cbecb6047812d26bd
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-11-26 17:56:17 +0530

    added test for app/screens/draft_options/delete_draft/index.tsx

---
+++ app/screens/draft_options/delete_draft/delete_draft.test.tsx
@@ -0,0 +1,44 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import {Screens} from '@constants';
+import {dismissBottomSheet} from '@screens/navigation';
+import {fireEvent, renderWithIntlAndTheme} from '@test/intl-test-helper';
+
+import DeleteDraft from '.';
+
+jest.mock('@screens/navigation', () => ({
+    dismissBottomSheet: jest.fn(),
+}));
+
+describe('DeleteDraft', () => {
+    it('should render the component', () => {
+        const wrapper = renderWithIntlAndTheme(
+            <DeleteDraft
+                bottomSheetId={Screens.DRAFT_OPTIONS}
+                channelId='channel_id'
+                rootId='root_id'
+            />,
+        );
+        const {getByText, getByTestId} = wrapper;
+        expect(getByText('Delete draft')).toBeTruthy();
+        expect(getByTestId('trash-can-outline-icon')).toBeTruthy();
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    it('calls draftDeleteHandler when pressed', () => {
+        const wrapper = renderWithIntlAndTheme(
+            <DeleteDraft
+                bottomSheetId={Screens.DRAFT_OPTIONS}
+                channelId='channel_id'
+                rootId='root_id'
+            />,
+        );
+        const {getByTestId} = wrapper;
+        fireEvent.press(getByTestId('trash-can-outline-icon'));
+        expect(dismissBottomSheet).toHaveBeenCalledTimes(1);
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+});
