dsafortworth/dsafw-mobile

addressed review comments

app/components/files/files.tsx
@@ -23,8 +23,8 @@ type FilesProps = {
     layoutWidth?: number;
     location: string;
     isReplyPost: boolean;
-    postId: string;
-    postProps: Record<string, any>;
+    postId?: string;
+    postProps?: Record<string, any>;
     publicLinkEnabled: boolean;
 }
 

mattermost/mattermost-mobile

app/components/files/files.tsx
@@ -23,8 +23,8 @@ type FilesProps = {
     layoutWidth?: number;
     location: string;
     isReplyPost: boolean;
-    postId: string;
-    postProps: Record<string, unknown>;
+    postId?: string;
+    postProps?: Record<string, unknown>;
     publicLinkEnabled: boolean;
 }
 