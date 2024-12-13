ce7c5338bde17a804b7871d3340dd8dd5af981f4
Jesse Hallam <jesse.hallam@gmail.com>
2024-12-13 12:31:21 -0400

    refactor: Simplify websocket event handlers for playbooks

--- app/products/playbooks/connection/websocket_event_handlers.ts
+++ app/products/playbooks/connection/websocket_event_handlers.ts
@@ -1,86 +1,32 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type {WebSocketMessage} from '@types/api/websocket';
-import {getPlaybooksConfigIfEnabled} from '@queries/playbooks/servers';
-import {queryClient} from '@queries/servers';
-
-export const handlePlaybookRunUpdated = async (serverUrl: string, msg: WebSocketMessage) => {
+export const handlePlaybookRunUpdated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
     if (!msg.data) {
-        return;
-    }
 
-    const config = await getPlaybooksConfigIfEnabled(serverUrl);
-    if (!config?.pluginEnabled) {
-        return;
     }
-
-    // Invalidate playbook run cache to trigger refetch
-    queryClient.invalidateQueries({
-        queryKey: ['playbook_runs', serverUrl],
-    });
 };
 
-export const handlePlaybookRunCreated = async (serverUrl: string, msg: WebSocketMessage) => {
+export const handlePlaybookRunCreated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
     if (!msg.data) {
-        return;
-    }
 
-    const config = await getPlaybooksConfigIfEnabled(serverUrl);
-    if (!config?.pluginEnabled) {
-        return;
     }
-
-    // Invalidate playbook runs list cache
-    queryClient.invalidateQueries({
-        queryKey: ['playbook_runs', serverUrl],
-    });
 };
 
-export const handlePlaybookCreated = async (serverUrl: string, msg: WebSocketMessage) => {
+export const handlePlaybookCreated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
     if (!msg.data) {
-        return;
-    }
 
-    const config = await getPlaybooksConfigIfEnabled(serverUrl);
-    if (!config?.pluginEnabled) {
-        return;
     }
-
-    // Invalidate playbooks list cache
-    queryClient.invalidateQueries({
-        queryKey: ['playbooks', serverUrl],
-    });
 };
 
-export const handlePlaybookArchived = async (serverUrl: string, msg: WebSocketMessage) => {
+export const handlePlaybookArchived = async (serverUrl: string, msg: WebSocketMessage<any>) => {
     if (!msg.data) {
-        return;
-    }
 
-    const config = await getPlaybooksConfigIfEnabled(serverUrl);
-    if (!config?.pluginEnabled) {
-        return;
     }
-
-    // Invalidate playbooks list cache
-    queryClient.invalidateQueries({
-        queryKey: ['playbooks', serverUrl],
-    });
 };
 
-export const handlePlaybookRestored = async (serverUrl: string, msg: WebSocketMessage) => {
+export const handlePlaybookRestored = async (serverUrl: string, msg: WebSocketMessage<any>) => {
     if (!msg.data) {
-        return;
-    }
 
-    const config = await getPlaybooksConfigIfEnabled(serverUrl);
-    if (!config?.pluginEnabled) {
-        return;
     }
-
-    // Invalidate playbooks list cache
-    queryClient.invalidateQueries({
-        queryKey: ['playbooks', serverUrl],
-    });
 };
