a9577e2ac37b0901fcaaa66dd49b6175f58dc651
Jesse Hallam <jesse.hallam@gmail.com>
2024-12-13 12:39:04 -0400

    refactor: Remove redundant null checks in websocket event handlers

--- app/products/playbooks/connection/websocket_event_handlers.ts
+++ app/products/playbooks/connection/websocket_event_handlers.ts
@@ -2,61 +2,45 @@
 // See LICENSE.txt for license information.
 
 export const handlePostEdited = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-        return;
-    }
+    // TODO: When we support custom post types, we may need to replicate the plugin behaviour here.
 };
 
 export const handlePostDeleted = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-        return;
-    }
+    // TODO: When we support custom post types, we may need to replicate the plugin behaviour here.
 };
 
 export const handleWebsocketUserAdded = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-        return;
-    }
+    // TODO: Fetch playbook runs for the channel to which the user was just added.
 };
 
 export const handleWebsocketUserRemoved = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-        return;
-    }
+    // TODO: Discard playbook runs for the channel to which the user was just removed.
+    // Note this is contingent on our only viewing runs while inside a channel. If a user remains
+    // a participant and we list active runs elsewhere, this behaviour may need to change.
 };
 
 export const handleChannelUpdated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-        return;
-    }
+    // TODO: If the channel has one or more playbook runs, fetch those playbook runs again
+    // since some runs derive their run name from the channel itself. (Is this still true?)
 };
 
 export const handlePlaybookRunUpdated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-
-    }
+    // TODO: If the playbook run in view, update it. We also need to handle a playbook run
+    // being finished and changing our global state.
 };
 
 export const handlePlaybookRunCreated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-
-    }
+    // TODO: Change our global state to reflect the newly created run.
 };
 
 export const handlePlaybookCreated = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-
-    }
+    // No action required on mobile for now.
 };
 
 export const handlePlaybookArchived = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-
-    }
+    // No action required on mobile for now.
 };
 
 export const handlePlaybookRestored = async (serverUrl: string, msg: WebSocketMessage<any>) => {
-    if (!msg.data) {
-
-    }
+    // No action required on mobile for now.
 };
