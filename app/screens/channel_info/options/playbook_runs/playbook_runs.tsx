db333902bbe3c9411b9dbc9983137c63a522928e
Jesse Hallam <jesse.hallam@gmail.com>
2024-12-13 15:48:22 -0400

    feat: Add PlaybookRuns component for channel info screen

---
+++ app/screens/channel_info/options/playbook_runs/playbook_runs.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {useIntl} from 'react-intl';
+import {Platform} from 'react-native';
+
+import OptionItem from '@components/option_item';
+import {useServerUrl} from '@context/server';
+import {preventDoubleTap} from '@utils/tap';
+
+const Props = {
+};
+
+const PlaybookRuns = () => {
+    const {formatMessage} = useIntl();
+    const serverUrl = useServerUrl();
+    const title = formatMessage({id: 'channel_info.playbook runs', defaultMessage: 'Playbook runs'});
+    const count = useActivePlaybookRunsCount(serverUrl, teamId, channelId);
+
+    const goToPlaybookRuns = preventDoubleTap(() => null);
+
+    return (
+        <OptionItem
+            action={goToPlaybookRuns}
+            label={title}
+            icon='product-playbooks'
+            type={Platform.select({ios: 'arrow', default: 'default'})}
+            info={count}
+            testID='channel_info.options.playbook_runs.option'
+        />
+    );
+};
+
+export default PlaybookRuns;
