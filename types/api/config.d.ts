3daad14c9996088e6f705bb8962e9f9f3f33192d
Guillermo Vayá <guivaya@gmail.com>
2024-12-20 15:47:01 +0100

    added feature flag

--- types/api/config.d.ts
+++ types/api/config.d.ts
@@ -121,6 +121,7 @@ interface ClientConfig {
     FeatureFlagCollapsedThreads?: string;
     FeatureFlagPostPriority?: string;
     FeatureFlagChannelBookmarks?: string;
+    FeatureFlagCustomProfileAttributes?: string;
     ForgotPasswordLink?: string;
     GfycatApiKey: string;
     GfycatApiSecret: string;
