a917ae9e609b5791c29366781109371481832ac5
Guillermo Vayá <guivaya@gmail.com>
2025-01-13 15:26:49 +0100

    increase space between label and description

--- app/screens/user_profile/label.tsx
+++ app/screens/user_profile/label.tsx
@@ -28,7 +28,7 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
     title: {
         color: theme.centerChannelColor,
         flex: 1,
-        marginLeft: 5,
+        marginRight: 20,
         height: '100%',
         alignItems: 'flex-start',
         ...typography('Body', 100, 'SemiBold'),
