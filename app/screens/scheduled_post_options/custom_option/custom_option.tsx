7a1370415806c31c66bfbae35d529706509f491e
Harshil Sharma <harshil.sharma@mattermost.com>
2025-01-17 16:12:37 +0530

    WIP

--- app/screens/scheduled_post_options/custom_option/custom_option.tsx
+++ app/screens/scheduled_post_options/custom_option/custom_option.tsx
@@ -3,16 +3,12 @@
 
 import React, {useCallback, useState} from 'react';
 import {useIntl} from 'react-intl';
-import {Button, Platform, View} from 'react-native';
+import {View} from 'react-native';
 
 import {useTheme} from '@context/theme';
 import DateTimeSelector from '@screens/custom_status_clear_after/components/date_time_selector';
 import PickerOption from '@screens/post_priority_picker/components/picker_option';
-import DateTimePicker from "@react-native-community/datetimepicker";
-import {CUSTOM_STATUS_TIME_PICKER_INTERVALS_IN_MINUTES} from "@constants/custom_status";
 import {makeStyleSheetFromTheme} from "@utils/theme";
-
-const SCHEDULED_POST_CUSTOM_TIME_PICKER_BUTTON = 'close-scheduled-post-custom-time-picker';
 
 const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
     return {
@@ -51,7 +47,7 @@ export function ScheduledPostCustomOption({userTimezone}: Props) {
             />
             {showDateTimePicker && (
                 <DateTimeSelector
-                    handleChange={onClick}
+                    handleChange={() => {}}
                     theme={theme}
                     timezone={userTimezone}
                 />
