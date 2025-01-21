e2661a40d30067db140c9318aa4a10b33cdeb884
Harshil Sharma <harshilsharma63@gmail.com>
2025-01-21 12:49:51 +0530

    WIP

--- app/components/scheduled_post_indicator/index.ts
+++ app/components/scheduled_post_indicator/index.ts
@@ -1,6 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {withDatabase, withObservables} from '@nozbe/watermelondb/react';
+
+import {observeCurrentUser} from '@queries/servers/user';
+
 import {ScheduledPostIndicator} from './scheduled_post_indicator';
 
-export default ScheduledPostIndicator;
+import {queryDisplayNamePreferences} from '@queries/servers/preference';
+import {map} from 'rxjs/operators';
+import {getDisplayNamePreferenceAsBool} from '@helpers/api/preference';
+
+const enhance = withObservables([], ({database}) => {
+    const currentUser = observeCurrentUser(database);
+    const preferences = queryDisplayNamePreferences(database).
+        observeWithColumns(['value']);
+    const isMilitaryTime = preferences.pipe(map((prefs) => getDisplayNamePreferenceAsBool(prefs, 'use_military_time')));
+
+    return {
+        currentUser,
+        isMilitaryTime,
+    };
+});
+
+export default withDatabase(enhance(ScheduledPostIndicator));
--- app/components/scheduled_post_indicator/scheduled_post_indicator.tsx
+++ app/components/scheduled_post_indicator/scheduled_post_indicator.tsx
@@ -4,8 +4,33 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {FormattedMessage} from 'react-intl';
+import FormattedTime from '@components/formatted_time';
+import {getUserTimezone} from '@utils/user';
+import type UserModel from '@typings/database/models/servers/user';
+import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
+import {typography} from '@utils/typography';
+import {useTheme} from '@context/theme';
 
-export function ScheduledPostIndicator() {
+const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => {
+    return {
+        container: {
+            backgroundColor: changeOpacity(theme.centerChannelColor, 0.08),
+            paddingVertical: 12,
+            paddingHorizontal: 16,
+            color: changeOpacity(theme.centerChannelColor, 0.72),
+        },
+    };
+});
+
+type Props = {
+    currentUser?: UserModel;
+    isMilitaryTime: boolean;
+}
+
+export function ScheduledPostIndicator({currentUser, isMilitaryTime}: Props) {
+    const theme = useTheme();
+    const styles = getStyleSheet(theme);
+
     const [scheduledPostCount, setScheduledPostCount] = React.useState(1);
 
     if (scheduledPostCount === 0) {
@@ -15,19 +40,37 @@ export function ScheduledPostIndicator() {
     let scheduledPostText: React.ReactNode;
 
     if (scheduledPostCount === 1) {
+        const dateTime = (
+            <FormattedTime
+                timezone={getUserTimezone(currentUser)}
+                isMilitaryTime={isMilitaryTime}
+                value={1768902936000}
+                testID='scheduled_post_indicator_single_time'
+            />
+        );
+
         scheduledPostText = (
             <FormattedMessage
                 id='scheduled_post.channel_indicator.single'
                 defaultMessage='Message scheduled for {dateTime}.'
+                values={{
+                    dateTime,
+                }}
             />
-
         );
     }
 
     return (
-        <View>
+        <View
+            className='ScheduledPostIndicator'
+            style={styles.container}
+        >
             <Text>
-                {'Hello, world!'}
+                {scheduledPostText}
+                <FormattedMessage
+                    id='scheduled_post.channel_indicator.link_to_scheduled_posts.text'
+                    defaultMessage='See all.'
+                />
             </Text>
         </View>
     );
