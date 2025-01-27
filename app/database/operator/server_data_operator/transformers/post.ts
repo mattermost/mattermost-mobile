6887d361515fa51bab316c0aef5924f537471d97
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-01-27 19:52:57 +0530

    Addressed review comments

--- app/database/operator/server_data_operator/transformers/post.ts
+++ app/database/operator/server_data_operator/transformers/post.ts
@@ -176,9 +176,9 @@ export const transformSchedulePostsRecord = ({action, database, value}: Transfor
             scheduledPost.metadata.priority = raw.priority;
         }
         scheduledPost.updateAt = raw.update_at ?? Date.now();
-        scheduledPost.scheduledAt = raw.scheduled_at ?? Date.now();
-        scheduledPost.processedAt = raw.processed_at ?? 0;
-        scheduledPost.errorCode = raw.error_code ?? '';
+        scheduledPost.scheduledAt = raw.scheduled_at;
+        scheduledPost.processedAt = raw.processed_at;
+        scheduledPost.errorCode = raw.error_code;
     };
 
     return prepareBaseRecord({
