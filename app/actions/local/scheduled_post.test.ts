62d44494ce0e6f30f17e22a5a311fe81d215c083
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-03 08:24:09 +0530

    Added test case for handle delete scheduled post

--- app/actions/local/scheduled_post.test.ts
+++ app/actions/local/scheduled_post.test.ts
@@ -5,9 +5,13 @@ import DatabaseManager from '@database/manager';
 
 import {handleCreateOrUpdateScheduledPost, handleDeleteScheduledPost} from './scheduled_post';
 
+import type ServerDataOperator from '@database/operator/server_data_operator';
+import type {Database} from '@nozbe/watermelondb';
 import type ScheduledPostModel from '@typings/database/models/servers/scheduled_post';
 
 const serverUrl = 'baseHandler.test.com';
+let database: Database;
+let operator: ServerDataOperator;
 
 const scheduledPosts = [
     {
@@ -36,6 +40,8 @@ const scheduledPosts = [
 
 beforeEach(async () => {
     await DatabaseManager.init([serverUrl]);
+    operator = DatabaseManager.serverDatabases[serverUrl]!.operator;
+    database = DatabaseManager.serverDatabases[serverUrl]!.database;
 });
 
 afterEach(async () => {
@@ -70,7 +76,24 @@ describe('handleScheduledPosts', () => {
     });
 
     it('handleDeleteScheduledPost - success', async () => {
-        const {models} = await handleDeleteScheduledPost(serverUrl, {data: {scheduled_post: JSON.stringify(scheduledPosts[0])}} as WebSocketMessage) as {models: ScheduledPostModel[]};
-        expect(models).toBeTruthy();
+        const mockScheduledPost = {
+            ...scheduledPosts[0],
+            toApi: jest.fn().mockResolvedValue(scheduledPosts[0]), // Mock `toApi`
+        };
+        jest.spyOn(database, 'get').mockImplementation(() => ({
+            query: jest.fn().mockReturnValue({
+                fetch: jest.fn().mockResolvedValue([mockScheduledPost]),
+            }),
+        }) as any);
+
+        const spyOnProcessRecords = jest.spyOn(operator, 'processRecords');
+
+        await handleDeleteScheduledPost(serverUrl, {data: {scheduled_post: JSON.stringify(scheduledPosts[0])}} as WebSocketMessage) as {models: ScheduledPostModel[]};
+        expect(spyOnProcessRecords).toHaveBeenLastCalledWith({
+            createOrUpdateRawValues: [],
+            deleteRawValues: [scheduledPosts[0]],
+            tableName: 'ScheduledPost',
+            fieldName: 'id',
+        });
     });
 });
