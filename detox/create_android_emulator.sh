5ed87cc6b368075c3a06f6816b78942fe7bd3852
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2025-02-03 22:12:20 +0530

    add more test files

--- detox/create_android_emulator.sh
+++ detox/create_android_emulator.sh
@@ -101,7 +101,7 @@ setup_adb_reverse() {
 run_detox_tests() {
     echo "Running Detox tests..."
     cd detox
-    npm run e2e:android-test -- about.e2e.ts
+    npm run e2e:android-test -- about.e2e.ts account_menu.e2e.ts
 }
 
 main() {
