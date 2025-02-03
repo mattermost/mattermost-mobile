7276f1ffee1e2a5aeb9576ec52ccfb9b75e365f5
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-04 01:37:32 +0530

    instead of mocking adding it to database

--- app/actions/websocket/scheduled_post.test.ts
+++ app/actions/websocket/scheduled_post.test.ts
@@ -1,35 +1,38 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {ActionType} from '@constants';
 import DatabaseManager from '@database/manager';
 
 import {handleCreateOrUpdateScheduledPost, handleDeleteScheduledPost} from './scheduled_post';
 
 import type ServerDataOperator from '@database/operator/server_data_operator';
-import type {Database} from '@nozbe/watermelondb';
 import type ScheduledPostModel from '@typings/database/models/servers/scheduled_post';
 
 const serverUrl = 'baseHandler.test.com';
-let database: Database;
 let operator: ServerDataOperator;
 
 const scheduledPosts = [
     {
-        id: 'scheduled_post_id',
         channel_id: 'channel_id',
-        root_id: '',
-        message: 'test scheduled post',
-        scheduled_at: 123,
-        user_id: 'user_id',
-        processed_at: 0,
-        update_at: 456,
         error_code: '',
+        files: [],
+        id: 'scheduled_post_id',
+        message: 'test scheduled post',
+        metadata: {},
+        processed_at: 0,
+        root_id: '',
+        scheduled_at: 123,
+        update_at: 456,
+        user_id: '',
     },
     {
         id: 'scheduled_post_id_2',
         channel_id: 'channel_id',
         root_id: '',
         message: 'test scheduled post 2',
+        files: [],
+        metadata: {},
         scheduled_at: 123,
         user_id: 'user_id',
         processed_at: 0,
@@ -41,7 +44,6 @@ const scheduledPosts = [
 beforeEach(async () => {
     await DatabaseManager.init([serverUrl]);
     operator = DatabaseManager.serverDatabases[serverUrl]!.operator;
-    database = DatabaseManager.serverDatabases[serverUrl]!.database;
 });
 
 afterEach(async () => {
@@ -78,19 +80,17 @@ describe('handleDeleteScheduledPost', () => {
     });
 
     it('handleDeleteScheduledPost - success', async () => {
-        const mockScheduledPost = {
-            ...scheduledPosts[0],
-            toApi: jest.fn().mockResolvedValue(scheduledPosts[0]), // Mock `toApi`
-        };
-        jest.spyOn(database, 'get').mockImplementation(() => ({
-            query: jest.fn().mockReturnValue({
-                fetch: jest.fn().mockResolvedValue([mockScheduledPost]),
-            }),
-        }) as any);
+        await operator.handleScheduledPosts({
+            actionType: ActionType.SCHEDULED_POSTS.CREATE_OR_UPDATED_SCHEDULED_POST,
+            scheduledPosts: [scheduledPosts[0]],
+            prepareRecordsOnly: false,
+        });
 
         const spyOnProcessRecords = jest.spyOn(operator, 'processRecords');
 
-        await handleDeleteScheduledPost(serverUrl, {data: {scheduled_post: JSON.stringify(scheduledPosts[0])}} as WebSocketMessage) as {models: ScheduledPostModel[]};
+        const scheduledPost = scheduledPosts[0];
+
+        await handleDeleteScheduledPost(serverUrl, {data: {scheduled_post: JSON.stringify(scheduledPost)}} as WebSocketMessage) as {models: ScheduledPostModel[]};
         expect(spyOnProcessRecords).toHaveBeenLastCalledWith({
             createOrUpdateRawValues: [],
             deleteRawValues: [scheduledPosts[0]],
