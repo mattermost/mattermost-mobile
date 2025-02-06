6f29392a2eee49b3ee6ecc7d787b4ebf91f4bd4d
Mattermost Build <build@mattermost.com>
2025-02-06 12:24:20 +0200

    Fix Android keyboard gap using navigation events (#8565) (#8566)
    (cherry picked from commit f2383f8bc5058b294d674e38b1560c5f4b332293)
    
    Co-authored-by: Elias Nahum <nahumhbl@gmail.com>

--- app/constants/screens.ts
+++ app/constants/screens.ts
@@ -199,6 +199,8 @@ export const SCREENS_AS_BOTTOM_SHEET = new Set<string>([
     CALL_HOST_CONTROLS,
 ]);
 
+export const SCREENS_WITH_EXTRA_KEYBOARD = new Set<string>([CHANNEL, THREAD]);
+
 export const NOT_READY = [
     CREATE_TEAM,
 ];
