c8a6dd21c391ef0c4d56b2ef559ddfee25b25845
Harshil Sharma <harshilsharma63@gmail.com>
2025-02-07 16:31:46 +0530

    coverage

--- app/client/rest/scheduled_post.test.ts
+++ app/client/rest/scheduled_post.test.ts
@@ -13,9 +13,68 @@ describe('ClientScheduledPost', () => {
         client.doFetch = jest.fn();
     });
 
+    test('createScheduledPost', async () => {
+        const scheduledPost: ScheduledPost = {
+            id: 'scheduled_post_id',
+            channel_id: 'channel_id',
+            message: 'scheduled post message',
+            scheduled_at: 1738925211,
+            user_id: 'current_user_id',
+            root_id: '',
+            update_at: 1738925211,
+        };
+
+        await client.createScheduledPost(scheduledPost, 'connection_id');
+
+        const expectedUrl = client.getScheduledPostRoute();
+        const expectedOptions = {
+            method: 'post',
+            body: scheduledPost,
+            headers: {'Connection-Id': 'connection_id'},
+        };
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
+    test('createScheduledPost without connection ID', async () => {
+        const scheduledPost: ScheduledPost = {
+            id: 'scheduled_post_id',
+            channel_id: 'channel_id',
+            message: 'scheduled post message',
+            scheduled_at: 1738925211,
+            user_id: 'current_user_id',
+            root_id: '',
+            update_at: 1738925211,
+        };
+
+        await client.createScheduledPost(scheduledPost);
+
+        const expectedUrl = client.getScheduledPostRoute();
+        const expectedOptions = {
+            method: 'post',
+            body: scheduledPost,
+            headers: {'Connection-Id': undefined},
+        };
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
     test('getScheduledPostsForTeam', async () => {
         const teamId = 'team_id';
         const includeDirectChannels = false;
+        const groupLabel = 'WebSocket Reconnect';
+        const expectedUrl = `${client.getTeamAndDirectChannelScheduledPostsRoute()}/team/${teamId}?includeDirectChannels=${includeDirectChannels}`;
+        const expectedOptions = {
+            method: 'get',
+            groupLabel,
+        };
+
+        await client.getScheduledPostsForTeam(teamId, includeDirectChannels, groupLabel);
+
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
+    test('getScheduledPostsForTeam with include direct channels', async () => {
+        const teamId = 'team_id';
+        const includeDirectChannels = true;
         const groupLabel = 'WebSocket Reconnect';
         const expectedUrl = `${client.getTeamAndDirectChannelScheduledPostsRoute()}/team/${teamId}?includeDirectChannels=${includeDirectChannels}`;
         const expectedOptions = {
@@ -38,6 +97,19 @@ describe('ClientScheduledPost', () => {
         };
 
         await client.deleteScheduledPost(scheduledPostId, connectionId);
+
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
+    test('deleteScheduledPost without connection ID', async () => {
+        const scheduledPostId = 'scheduled_post_id';
+        const expectedUrl = `${client.getScheduledPostActionsRoute()}/${scheduledPostId}`;
+        const expectedOptions = {
+            method: 'delete',
+            headers: {'Connection-Id': ''},
+        };
+
+        await client.deleteScheduledPost(scheduledPostId);
 
         expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
     });
