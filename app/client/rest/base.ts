6085b79b6f864d82f5209daba16007f732fa4319
Rajat Dabade <rajatdabade1997@gmail.com>
2025-02-07 16:40:19 +0530

    Added rest client and local/remote/WebSocket handling for Scheduled Post (#8526)
    * added handler for receiving all the scheduled post
    
    * Added migration script for adding schedule post table
    
    * reverted back unnecessary changes
    
    * Addressed review comments
    
    * Added test for handleScheduledPost
    
    * removed undefined properties
    
    * reverted unnecessary changes
    
    * Updated logic and test case for fetching records according to teams
    
    * remove unnecessary changes
    
    * Updated logic for handling scheduled post
    
    * Update test case
    
    * Updated logic when review all the scheduled post
    
    * ci fixes
    
    * Schedule post rest client, local/remote/websocket handling
    
    * review fixes
    
    * Added test for local client
    
    * Added test case for handle delete scheduled post
    
    * Added test for remote client
    
    * Added test for websocket schedule post events
    
    * Added test for scheduled_post rest client
    
    * Added include direct channel post flag for handling schedule post fetch request
    
    * Updated test case
    
    * Addressed review comments
    
    * instead of mocking adding it to database
    
    * Updated test
    
    * import fixes
    
    * addressed review comments
    
    * minor
    
    * removed redundent checks
    
    * coverage
    
    ---------
    
    Co-authored-by: Harshil Sharma <harshilsharma63@gmail.com>

--- app/client/rest/base.ts
+++ app/client/rest/base.ts
@@ -171,6 +171,14 @@ export default class ClientBase extends ClientTracking {
         return `${this.urlVersion}/redirect_location`;
     }
 
+    getScheduledPostActionsRoute() {
+        return `${this.getPostsRoute()}/schedule`;
+    }
+
+    getTeamAndDirectChannelScheduledPostsRoute() {
+        return `${this.getPostsRoute()}/scheduled`;
+    }
+
     getThreadsRoute(userId: string, teamId: string): string {
         return `${this.getUserRoute(userId)}/teams/${teamId}/threads`;
     }
