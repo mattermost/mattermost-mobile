e153825803ae7607d1cb1a7b3b61417bd880530d
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-12 21:27:31 +0530

    Fix the attachment issue for the schedule post

--- app/database/operator/server_data_operator/transformers/post.ts
+++ app/database/operator/server_data_operator/transformers/post.ts
@@ -167,7 +167,7 @@ export const transformSchedulePostsRecord = ({action, database, value}: Transfor
         scheduledPost.rootId = raw?.root_id ?? '';
         scheduledPost.message = raw?.message ?? '';
         scheduledPost.channelId = raw?.channel_id ?? '';
-        scheduledPost.files = raw?.files ?? emptyFileInfo;
+        scheduledPost.files = raw?.metadata?.files ?? emptyFileInfo;
         scheduledPost.metadata = raw?.metadata ?? null;
         if (raw.priority) {
             if (!scheduledPost.metadata) {
