165bd2b43e1426400afad801068f48ff1f50926e
Harshil Sharma <harshilsharma63@gmail.com>
2025-02-17 06:19:35 +0530

    added feature popup

--- app/components/post_draft/send_action/index.tsx
+++ app/components/post_draft/send_action/index.tsx
@@ -1,13 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useMemo} from 'react';
-import {View} from 'react-native';
+import {withDatabase, withObservables} from '@nozbe/watermelondb/react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {InteractionManager, View} from 'react-native';
+import Tooltip from 'react-native-walkthrough-tooltip';
 
+import {storeScheduledPostTutorial} from '@actions/app/global';
 import CompassIcon from '@components/compass_icon';
+import ScheduledPostTooltip from '@components/post_draft/send_action/scheudled_post_tooltip';
 import TouchableWithFeedback from '@components/touchable_with_feedback';
+import {Tutorial} from '@constants';
 import {useTheme} from '@context/theme';
 import {usePreventDoubleTap} from '@hooks/utils';
+import {observeTutorialWatched} from '@queries/app/global';
 import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
 
 type Props = {
@@ -15,6 +21,7 @@ type Props = {
     disabled: boolean;
     sendMessage: () => void;
     showScheduledPostOptions: () => void;
+    scheduledPostFeatureTooltipWatched: boolean;
 }
 
 const getStyleSheet = makeStyleSheetFromTheme((theme) => {
@@ -34,6 +41,14 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
             alignItems: 'center',
             justifyContent: 'center',
         },
+        scheduledPostTooltipStyle: {
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: 2},
+            shadowRadius: 2,
+            shadowOpacity: 0.16,
+            width: 250,
+            height: 140,
+        },
     };
 });
 
@@ -42,10 +57,31 @@ function SendButton({
     disabled,
     sendMessage,
     showScheduledPostOptions,
+    scheduledPostFeatureTooltipWatched,
 }: Props) {
     const theme = useTheme();
     const sendButtonTestID = disabled ? `${testID}.send.button.disabled` : `${testID}.send.button`;
     const style = getStyleSheet(theme);
+
+    const [scheduledPostTooltipVisible, setScheduledPostTooltipVisible] = useState(false);
+
+    useEffect(() => {
+        if (scheduledPostFeatureTooltipWatched) {
+            return;
+        }
+
+        InteractionManager.runAfterInteractions(() => {
+            setScheduledPostTooltipVisible(true);
+        });
+
+        // This effect is intended to run only on the first mount, so dependencies are omitted intentionally.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const onCloseScheduledPostTooltip = useCallback(() => {
+        setScheduledPostTooltipVisible(false);
+        storeScheduledPostTutorial();
+    }, []);
 
     const viewStyle = useMemo(() => {
         if (disabled) {
@@ -67,15 +103,32 @@ function SendButton({
             disabled={disabled}
             onLongPress={showScheduledPostOptions}
         >
-            <View style={viewStyle}>
-                <CompassIcon
-                    name='send'
-                    size={24}
-                    color={buttonColor}
-                />
-            </View>
+            <Tooltip
+                isVisible={scheduledPostTooltipVisible}
+                useInteractionManager={true}
+                placement='top'
+                content={<ScheduledPostTooltip onClose={onCloseScheduledPostTooltip}/>}
+                onClose={onCloseScheduledPostTooltip}
+                tooltipStyle={style.scheduledPostTooltipStyle}
+            >
+                <View style={viewStyle}>
+                    <CompassIcon
+                        name='send'
+                        size={24}
+                        color={buttonColor}
+                    />
+                </View>
+            </Tooltip>
         </TouchableWithFeedback>
     );
 }
 
-export default SendButton;
+const enhanced = withObservables([], () => {
+    const scheduledPostFeatureTooltipWatched = observeTutorialWatched(Tutorial.SCHEDULED_POST);
+
+    return {
+        scheduledPostFeatureTooltipWatched,
+    };
+});
+
+export default withDatabase(enhanced((SendButton)));
