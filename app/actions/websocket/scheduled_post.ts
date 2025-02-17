20a94f50331ff3af7b829a118ecec35fe49c1de7
Harshil Sharma <18575143+harshilsharma63@users.noreply.github.com>
2025-02-17 10:56:38 +0530

    Websocket extension (#8585)
    * WIP
    
    * test: Update scheduled post tests to include connectionId verification
    
    * Updated tests
    
    * Lint fixes
    
    * test: Update WebSocket message handling tests with empty data payload
    
    * test: Add database verification for scheduled post operations
    
    * test imporvements and review fixes

--- app/actions/websocket/scheduled_post.ts
+++ app/actions/websocket/scheduled_post.ts
@@ -6,13 +6,13 @@ import {ActionType} from '@constants';
 import {logError} from '@utils/log';
 
 export type ScheduledPostWebsocketEventPayload = {
-    scheduled_post: string;
+    scheduledPost: string;
 }
 
 export async function handleCreateOrUpdateScheduledPost(serverUrl: string, msg: WebSocketMessage<ScheduledPostWebsocketEventPayload>, prepareRecordsOnly = false) {
     try {
-        const scheduledPost: ScheduledPost = JSON.parse(msg.data.scheduled_post);
-        return handleScheduledPosts(serverUrl, ActionType.SCHEDULED_POSTS.CREATE_OR_UPDATED_SCHEDULED_POST, [scheduledPost], prepareRecordsOnly);
+        const scheduledPost: ScheduledPost[] = msg.data.scheduledPost ? [JSON.parse(msg.data.scheduledPost)] : [];
+        return handleScheduledPosts(serverUrl, ActionType.SCHEDULED_POSTS.CREATE_OR_UPDATED_SCHEDULED_POST, scheduledPost, prepareRecordsOnly);
     } catch (error) {
         logError('handleCreateOrUpdateScheduledPost cannot handle scheduled post added/update websocket event', error);
         return {error};
@@ -21,8 +21,8 @@ export async function handleCreateOrUpdateScheduledPost(serverUrl: string, msg: 
 
 export async function handleDeleteScheduledPost(serverUrl: string, msg: WebSocketMessage<ScheduledPostWebsocketEventPayload>, prepareRecordsOnly = false) {
     try {
-        const scheduledPost: ScheduledPost = JSON.parse(msg.data.scheduled_post);
-        return handleScheduledPosts(serverUrl, ActionType.SCHEDULED_POSTS.DELETE_SCHEDULED_POST, [scheduledPost], prepareRecordsOnly);
+        const scheduledPost: ScheduledPost[] = msg.data.scheduledPost ? [JSON.parse(msg.data.scheduledPost)] : [];
+        return handleScheduledPosts(serverUrl, ActionType.SCHEDULED_POSTS.DELETE_SCHEDULED_POST, scheduledPost, prepareRecordsOnly);
     } catch (error) {
         logError('handleDeleteScheduledPost cannot handle scheduled post deleted websocket event', error);
         return {error};
