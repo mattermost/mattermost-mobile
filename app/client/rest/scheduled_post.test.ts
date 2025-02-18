f92c0d4656b504b1725233679ba2adc9e1423738
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-18 15:28:54 +0530

    test: Add tests for updateScheduledPost method

--- app/client/rest/scheduled_post.test.ts
+++ app/client/rest/scheduled_post.test.ts
@@ -89,6 +89,53 @@ describe('ClientScheduledPost', () => {
         expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
     });
 
+    test('updateScheduledPost', async () => {
+        const scheduledPost: ScheduledPost = {
+            id: 'scheduled_post_id',
+            channel_id: 'channel_id',
+            message: 'updated scheduled post message',
+            scheduled_at: 1738925211,
+            user_id: 'current_user_id',
+            root_id: '',
+            update_at: 1738925211,
+            create_at: 1738925211,
+        };
+        const connectionId = 'connection_id';
+
+        await client.updateScheduledPost(scheduledPost, connectionId);
+
+        const expectedUrl = `${client.getScheduledPostActionsRoute()}/${scheduledPost.id}`;
+        const expectedOptions = {
+            method: 'put',
+            body: scheduledPost,
+            headers: {'Connection-Id': connectionId},
+        };
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
+    test('updateScheduledPost without connection ID', async () => {
+        const scheduledPost: ScheduledPost = {
+            id: 'scheduled_post_id',
+            channel_id: 'channel_id',
+            message: 'updated scheduled post message',
+            scheduled_at: 1738925211,
+            user_id: 'current_user_id',
+            root_id: '',
+            update_at: 1738925211,
+            create_at: 1738925211,
+        };
+
+        await client.updateScheduledPost(scheduledPost);
+
+        const expectedUrl = `${client.getScheduledPostActionsRoute()}/${scheduledPost.id}`;
+        const expectedOptions = {
+            method: 'put',
+            body: scheduledPost,
+            headers: {'Connection-Id': ''},
+        };
+        expect(client.doFetch).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    });
+
     test('deleteScheduledPost', async () => {
         const scheduledPostId = 'scheduled_post_id';
         const connectionId = 'connection_id';
