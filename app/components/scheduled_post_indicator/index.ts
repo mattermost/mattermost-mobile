b1c6b080b954cd90b6830cf705723713b8516d34
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-24 19:56:08 +0530

    scheduled post count for GMs and DMs and updated snapshot

--- app/components/scheduled_post_indicator/index.ts
+++ app/components/scheduled_post_indicator/index.ts
@@ -6,7 +6,7 @@ import {map, switchMap} from 'rxjs/operators';
 
 import {getDisplayNamePreferenceAsBool} from '@helpers/api/preference';
 import {queryDisplayNamePreferences} from '@queries/servers/preference';
-import {observeScheduledPostCountForChannel} from '@queries/servers/scheduled_post';
+import {observeScheduledPostCountForChannel, observeScheduledPostCountForDMsAndGMs} from '@queries/servers/scheduled_post';
 import {observeCurrentTeamId} from '@queries/servers/system';
 import {observeCurrentUser} from '@queries/servers/user';
 
@@ -16,17 +16,22 @@ import type {WithDatabaseArgs} from '@typings/database/database';
 
 type Props = WithDatabaseArgs & {
     channelId: string;
+    channelType: ChannelType;
 }
 
-const enhance = withObservables(['channelId'], ({database, channelId}: Props) => {
+const enhance = withObservables(['channelId', 'channelType'], ({database, channelId, channelType}: Props) => {
     const currentUser = observeCurrentUser(database);
     const currentTeamId = observeCurrentTeamId(database);
     const preferences = queryDisplayNamePreferences(database).
         observeWithColumns(['value']);
     const isMilitaryTime = preferences.pipe(map((prefs) => getDisplayNamePreferenceAsBool(prefs, 'use_military_time')));
 
-    // TODO: fix the count for DM's and GM's and Thread
-    const scheduledPostCount = currentTeamId.pipe(switchMap((teamId) => observeScheduledPostCountForChannel(database, teamId, channelId)));
+    let scheduledPostCount;
+    if (channelType === 'D' || channelType === 'G') {
+        scheduledPostCount = observeScheduledPostCountForDMsAndGMs(database, channelId);
+    } else {
+        scheduledPostCount = currentTeamId.pipe(switchMap((teamId) => observeScheduledPostCountForChannel(database, teamId, channelId)));
+    }
     return {
         currentUser,
         isMilitaryTime,
