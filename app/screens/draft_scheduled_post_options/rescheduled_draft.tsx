c2ae511cf23c481257dded0766ed3272faa8562a
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-02-26 11:44:45 +0530

    added test for network connection for rescheduling post

--- app/screens/draft_scheduled_post_options/rescheduled_draft.tsx
+++ app/screens/draft_scheduled_post_options/rescheduled_draft.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {withObservables} from '@nozbe/watermelondb/react';
 import React, {useCallback} from 'react';
 import {useIntl} from 'react-intl';
 
@@ -9,8 +10,12 @@ import FormattedText from '@components/formatted_text';
 import TouchableWithFeedback from '@components/touchable_with_feedback';
 import {Screens} from '@constants';
 import {ICON_SIZE} from '@constants/post_draft';
+import {SNACK_BAR_TYPE} from '@constants/snack_bar';
+import {withServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
+import websocket_manager from '@managers/websocket_manager';
 import {dismissBottomSheet, showModal} from '@screens/navigation';
+import {showSnackBar} from '@utils/snack_bar';
 import {changeOpacity, makeStyleSheetFromTheme} from '@utils/theme';
 import {typography} from '@utils/typography';
 
@@ -20,6 +25,7 @@ import type {AvailableScreens} from '@typings/screens/navigation';
 type Props = {
     bottomSheetId: AvailableScreens;
     draft: ScheduledPostModel;
+    websocketState: WebsocketConnectedState;
 }
 
 const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
@@ -36,15 +42,25 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
     },
 }));
 
-const RescheduledDraft: React.FC<Props> = ({
+export const RescheduledDraft: React.FC<Props> = ({
     bottomSheetId,
     draft,
+    websocketState,
 }) => {
     const theme = useTheme();
     const style = getStyleSheet(theme);
     const intl = useIntl();
     const rescheduledDraft = useCallback(async () => {
         await dismissBottomSheet(bottomSheetId);
+        if (websocketState !== 'connected') {
+            showSnackBar({
+                barType: SNACK_BAR_TYPE.CONNECTION_ERROR,
+                customMessage: intl.formatMessage({id: 'network_connection.not_connected', defaultMessage: 'No internet connection'}),
+                type: 'error',
+                keepOpen: true,
+            });
+            return;
+        }
         const title = intl.formatMessage({id: 'mobile.reschedule_draft.title', defaultMessage: 'Change Schedule'});
         const closeButton = CompassIcon.getImageSourceSync('close', 24, theme.sidebarHeaderTextColor);
         const closeButtonId = 'close-rescheduled-draft';
@@ -59,7 +75,7 @@ const RescheduledDraft: React.FC<Props> = ({
             },
         };
         showModal(Screens.RESCHEDULE_DRAFT, title, passProps, options);
-    }, []);
+    }, [bottomSheetId, draft, intl, theme.sidebarHeaderTextColor, websocketState]);
 
     return (
         <TouchableWithFeedback
@@ -82,4 +98,8 @@ const RescheduledDraft: React.FC<Props> = ({
     );
 };
 
-export default RescheduledDraft;
+const enhanced = withObservables(['serverUrl'], ({serverUrl}: {serverUrl: string}) => ({
+    websocketState: websocket_manager.observeWebsocketState(serverUrl),
+}));
+
+export default withServerUrl(enhanced(RescheduledDraft));
