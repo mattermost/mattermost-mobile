d536a662a9bfda3b4ee659e5227469376015f0ce
Harshil Sharma <harshilsharma63@gmail.com>
2025-02-27 15:54:48 +0530

    Text update

--- app/screens/global_drafts/components/draft_and_scheduled_post_swipe_actions.test.tsx
+++ app/screens/global_drafts/components/draft_and_scheduled_post_swipe_actions.test.tsx
@@ -115,7 +115,7 @@ describe('DraftAndScheduledPostSwipeActions', () => {
             />,
         );
 
-        const deleteButton = getByText('Delete');
+        const deleteButton = getByText('Delete draft');
         fireEvent.press(deleteButton);
 
         await waitFor(() => {
