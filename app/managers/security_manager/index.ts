81081e1b98515fad668a07c893380626007655ef
Elias Nahum <nahumhbl@gmail.com>
2025-03-05 18:06:29 +0800

    Add screen capture prevention and other small fixes

--- app/managers/security_manager/index.ts
+++ app/managers/security_manager/index.ts
@@ -16,22 +16,25 @@ import {toMilliseconds} from '@utils/datetime';
 import {isMainActivity} from '@utils/helpers';
 import {logError} from '@utils/log';
 
+import type {AvailableScreens} from '@typings/screens/navigation';
+
 type SecurityManagerServerConfig = {
-    siteName?: string;
     Biometrics?: boolean;
     JailbreakProtection?: boolean;
-    AllowScreenCapture?: boolean;
+    PreventScreenCapture?: boolean;
+    authenticated?: boolean;
     lastAccessed?: number;
+    siteName?: string;
 };
 
 type SecurityManagerServersCollection = Record<string, SecurityManagerServerConfig>;
 
-class SecurityManager {
-    initialized: boolean = false;
+class SecurityManagerSingleton {
     activeServer?: string;
     serverConfig: SecurityManagerServersCollection = {};
     backgroundSince = 0;
     previousAppState?: AppStateStatus;
+    initialized = false;
 
     constructor() {
         AppState.addEventListener('change', this.onAppStateChange);
@@ -57,161 +60,13 @@ class SecurityManager {
 
         if (activeServer && (!added.has(activeServer) || !this.activeServer)) {
             this.activeServer = activeServer;
+            this.setScreenCapturePolicy(activeServer);
             const isJailbroken = await this.isDeviceJailbroken(activeServer);
             if (!isJailbroken) {
                 this.authenticateWithBiometricsIfNeeded(activeServer);
             }
         }
     }
-
-    /**
-     * Checks if EMM is already enabled and setup
-     * to handle biometric / passcode authentication.
-     */
-    isAuthenticationHandledByEmm = () => {
-        return ManagedApp.enabled && ManagedApp.inAppPinCode;
-    };
-
-    /**
-     * Checks if EMM is already enabled and setup
-     * to handle jailbreak protection.
-     */
-    isJalbreakProtectionHandledByEmm = () => {
-        return ManagedApp.enabled && ManagedApp.cacheConfig?.jailbreakProtection === 'true';
-    };
-
-    /**
-     * Add the config for a server.
-     */
-    addServer = (server: string, config?: ClientConfig) => {
-        const mobileConfig: SecurityManagerServerConfig = {
-            siteName: config?.SiteName,
-            Biometrics: config?.MobileEnableBiometrics === 'true',
-            JailbreakProtection: config?.MobileJailbreakProtection === 'true',
-            AllowScreenCapture: config ? config.MobileAllowScreenshots === 'true' : true,
-        };
-        this.serverConfig[server] = mobileConfig;
-    };
-
-    /**
-     * Removes a configured server, to be called on logout.
-     */
-    removeServer = (server: string) => {
-        delete this.serverConfig[server];
-    };
-
-    /**
-     * Get the configuration of a server.
-     */
-    getServerConfig = (server: string) => {
-        return this.serverConfig[server];
-    };
-
-    /**
-     * Switches the active server.
-     */
-    setActiveServer = (server: string) => {
-        if (this.activeServer && this.serverConfig[this.activeServer]) {
-            this.serverConfig[this.activeServer].lastAccessed = Date.now();
-        }
-
-        if (this.serverConfig[server]) {
-            this.activeServer = server;
-            this.serverConfig[server].lastAccessed = Date.now();
-        }
-    };
-
-    /**
-     * Checks if the device is Jailbroken or Rooted.
-     */
-    isDeviceJailbroken = async (server: string, siteName?: string) => {
-        if (this.isJalbreakProtectionHandledByEmm()) {
-            return false;
-        }
-
-        const config = this.getServerConfig(server);
-        if (!config && !siteName) {
-            return false;
-        }
-
-        const locale = DEFAULT_LOCALE;
-        const translations = getTranslations(locale);
-
-        if (!this.isJalbreakProtectionHandledByEmm() && (config?.JailbreakProtection || siteName)) {
-            const isRooted = await isRootedExperimentalAsync();
-            if (isRooted) {
-                this.showDeviceNotTrustedAlert(server, siteName, translations);
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    /**
-     * Determines if biometric authentication should be prompted.
-     */
-    authenticateWithBiometricsIfNeeded = async (server: string) => {
-        if (this.isAuthenticationHandledByEmm()) {
-            return true;
-        }
-
-        const config = this.getServerConfig(server);
-        if (!config) {
-            return true;
-        }
-
-        if (config?.Biometrics) {
-            const lastAccessed = config?.lastAccessed || 0;
-            const timeSinceLastAccessed = Date.now() - lastAccessed;
-            if (timeSinceLastAccessed > toMilliseconds({minutes: 5})) {
-                return this.authenticateWithBiometrics(server);
-            }
-        }
-
-        return true;
-    };
-
-    /**
-     * Handles biometric authentication.
-     */
-    authenticateWithBiometrics = async (server: string, siteName?: string) => {
-        if (this.isAuthenticationHandledByEmm()) {
-            return true;
-        }
-
-        const config = this.getServerConfig(server);
-        if (!config && !siteName) {
-            return true;
-        }
-
-        const locale = DEFAULT_LOCALE;
-        const translations = getTranslations(locale);
-
-        const isSecured = await Emm.isDeviceSecured();
-        if (!isSecured) {
-            await this.showNotSecuredAlert(server, siteName, translations);
-            return false;
-        }
-
-        try {
-            const auth = await Emm.authenticate({
-                reason: translations[t('mobile.managed.secured_by')].replace('{vendor}', siteName || config?.siteName || 'Mattermost'),
-                fallback: true,
-                supressEnterPassword: true,
-            });
-
-            if (!auth) {
-                throw new Error('Authorization cancelled');
-            }
-        } catch (err) {
-            logError('Failed to authenticate with biometrics', err);
-            this.showBiometricFailureAlert(server, siteName, translations);
-            return false;
-        }
-
-        return true;
-    };
 
     /**
      * Handles app state changes to prompt authentication when resuming from background.
@@ -245,72 +100,121 @@ class SecurityManager {
         this.previousAppState = appState;
     };
 
-    showDeviceNotTrustedAlert = async (server: string, siteName: string | undefined, translations: Record<string, string>) => {
-        const buttons = await this.buildAlertOptions(server, translations);
-        const securedBy = siteName || this.getServerConfig(server)?.siteName || 'Mattermost';
-
-        Alert.alert(
-            translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
-            translations[t('mobile.managed.jailbreak')].
-                replace('{vendor}', securedBy),
-            buttons,
-            {cancelable: false},
-        );
+    /**
+     * Checks if EMM is already enabled and setup
+     * to handle biometric / passcode authentication.
+     */
+    isAuthenticationHandledByEmm = () => {
+        return ManagedApp.enabled && ManagedApp.inAppPinCode;
     };
 
     /**
-     * Shows an alert when the device does not have biometrics or passcode set.
+     * Checks if EMM is already enabled and setup
+     * to handle jailbreak protection.
      */
-    showNotSecuredAlert = (server: string, siteName: string | undefined, translations: Record<string, string>) => {
-        // eslint-disable-next-line no-async-promise-executor
-        return new Promise(async (resolve) => {
-            const buttons: AlertButton[] = [];
-            const config = this.serverConfig[server];
-            const securedBy = siteName || config?.siteName || 'Mattermost';
-
-            if (Platform.OS === 'android') {
-                buttons.push({
-                    text: translations[t('mobile.managed.settings')],
-                    onPress: () => {
-                        Emm.openSecuritySettings();
-                        resolve(true);
-                    },
-                });
-            }
-
-            const alertButtons = await this.buildAlertOptions(server, translations, resolve);
-            buttons.push(...alertButtons);
-
-            let message;
-            const platform = Platform.select({ios: 'ios', default: 'android'});
-            if (config?.siteName || siteName) {
-                message = translations[t(`mobile.managed.not_secured.${platform}.vendor`)].replace('{vendor}', securedBy);
-            } else {
-                message = translations[t(`mobile.managed.not_secured.${platform}`)];
-            }
-
-            Alert.alert(
-                translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
-                message,
-                buttons,
-                {cancelable: false},
-            );
-        });
+    isJalbreakProtectionHandledByEmm = () => {
+        return ManagedApp.enabled && ManagedApp.cacheConfig?.jailbreakProtection === 'true';
     };
 
     /**
-     * Shows an alert when biometric authentication fails.
+     * Checks if EMM is already enabled and setup
+     * to handle screenshot protection.
      */
-    showBiometricFailureAlert = async (server: string, siteName: string | undefined, translations: Record<string, string>) => {
-        const buttons = await this.buildAlertOptions(server, translations);
-        const securedBy = siteName || this.getServerConfig(server)?.siteName || 'Mattermost';
+    isScreenshotProtectionHandledByEmm = () => {
+        return ManagedApp.enabled && ManagedApp.cacheConfig?.blurApplicationScreen === 'true';
+    };
 
-        Alert.alert(
-            translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
-            translations[t('mobile.managed.biometric_failed')],
-            buttons,
-            {cancelable: false},
-        );
+    /**
+     * Get the configuration of a server to prevent screenshots.
+     */
+    isScreenCapturePrevented = (server: string) => {
+        const config = this.getServerConfig(server);
+        if (!config) {
+            return false;
+        }
+
+        return config.PreventScreenCapture == null ? false : config.PreventScreenCapture;
+    };
+
+    /**
+     * Checks if the device is Jailbroken or Rooted.
+     */
+    isDeviceJailbroken = async (server: string, siteName?: string) => {
+        if (this.isJalbreakProtectionHandledByEmm()) {
+            return false;
+        }
+
+        const config = this.getServerConfig(server);
+        if (!config && !siteName) {
+            return false;
+        }
+
+        const locale = DEFAULT_LOCALE;
+        const translations = getTranslations(locale);
+        if (config?.JailbreakProtection || siteName) {
+            const isRooted = await isRootedExperimentalAsync();
+            if (isRooted) {
+                this.showDeviceNotTrustedAlert(server, siteName, translations);
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    /**
+     * Add the config for a server.
+     */
+    addServer = (server: string, config?: ClientConfig, authenticated = false) => {
+        const mobileConfig: SecurityManagerServerConfig = {
+            siteName: config?.SiteName,
+            Biometrics: config?.MobileEnableBiometrics === 'true',
+            JailbreakProtection: config?.MobileJailbreakProtection === 'true',
+            PreventScreenCapture: config?.MobilePreventScreenCapture === 'true',
+            authenticated,
+        };
+        this.serverConfig[server] = mobileConfig;
+    };
+
+    /**
+     * Removes a configured server, to be called on logout.
+     */
+    removeServer = async (server: string) => {
+        delete this.serverConfig[server];
+        if (server === this.activeServer) {
+            this.initialized = false;
+            this.activeServer = undefined;
+        }
+    };
+
+    /**
+     * Get the configuration of a server.
+     */
+    getServerConfig = (server: string): SecurityManagerServerConfig| undefined => {
+        return this.serverConfig[server];
+    };
+
+    /**
+     * Switches the active server.
+     */
+    setActiveServer = (server: string) => {
+        if (this.activeServer && this.serverConfig[this.activeServer]) {
+            this.serverConfig[this.activeServer].lastAccessed = Date.now();
+        }
+
+        if (this.serverConfig[server]) {
+            this.activeServer = server;
+            this.serverConfig[server].lastAccessed = Date.now();
+            this.setScreenCapturePolicy(server);
+        }
+    };
+
+    /**
+     * Gets the last accessed server.
+     */
+    getLastAccessedServer = (otherServers: string[]) => {
+        const lastAccessed = otherServers.map((s) => this.serverConfig[s].lastAccessed).sort().reverse()[0];
+        return Object.keys(this.serverConfig).find((s) => this.serverConfig[s].lastAccessed === lastAccessed);
     };
 
     /**
@@ -318,8 +222,7 @@ class SecurityManager {
      */
     goToPreviousServer = async (otherServers: string[]) => {
         // Switch to last accessed server
-        const lastAccessed = otherServers.map((s) => this.serverConfig[s].lastAccessed).sort().reverse()[0];
-        const lastAccessedServer = Object.keys(this.serverConfig).find((s) => this.serverConfig[s].lastAccessed === lastAccessed);
+        const lastAccessedServer = this.getLastAccessedServer(otherServers);
         if (lastAccessedServer) {
             const theme = Preferences.THEMES.denim;
             const locale = DEFAULT_LOCALE;
@@ -331,6 +234,99 @@ class SecurityManager {
             });
             switchToServer(lastAccessedServer, theme, intl);
         }
+    };
+
+    /**
+     * Determines if biometric authentication should be prompted.
+     */
+    authenticateWithBiometricsIfNeeded = async (server: string) => {
+        if (this.isAuthenticationHandledByEmm()) {
+            return true;
+        }
+
+        const config = this.getServerConfig(server);
+        if (!config) {
+            return true;
+        }
+
+        if (config?.Biometrics) {
+            const lastAccessed = config?.lastAccessed ?? 0;
+            const timeSinceLastAccessed = Date.now() - lastAccessed;
+            if (timeSinceLastAccessed > toMilliseconds({minutes: 5}) || config.authenticated === false) {
+                return this.authenticateWithBiometrics(server);
+            }
+        }
+
+        return true;
+    };
+
+    /**
+     * Handles biometric authentication.
+     */
+    authenticateWithBiometrics = async (server: string, siteName?: string) => {
+        if (this.isAuthenticationHandledByEmm()) {
+            return true;
+        }
+
+        const config = this.getServerConfig(server);
+        if (!config && !siteName) {
+            return true;
+        }
+
+        const locale = DEFAULT_LOCALE;
+        const translations = getTranslations(locale);
+
+        const isSecured = await Emm.isDeviceSecured();
+        if (!isSecured) {
+            await this.showNotSecuredAlert(server, siteName, translations);
+            return false;
+        }
+        const shouldBlurOnAuthenticate = server === this.activeServer && this.isScreenCapturePrevented(server);
+        try {
+            const auth = await Emm.authenticate({
+                reason: translations[t('mobile.managed.secured_by')].replace('{vendor}', siteName || config?.siteName || 'Mattermost'),
+                fallback: true,
+                supressEnterPassword: true,
+                blurOnAuthenticate: shouldBlurOnAuthenticate,
+            });
+
+            if (config) {
+                config.authenticated = auth;
+            }
+
+            if (!auth) {
+                throw new Error('Authorization cancelled');
+            }
+        } catch (err) {
+            logError('Failed to authenticate with biometrics', err);
+            this.showBiometricFailureAlert(server, shouldBlurOnAuthenticate, siteName, translations);
+            return false;
+        }
+
+        return true;
+    };
+
+    /**
+     * Sets the screen capture policy for the given server.
+     */
+    setScreenCapturePolicy = (server: string) => {
+        if (this.isScreenshotProtectionHandledByEmm()) {
+            return;
+        }
+
+        Emm.enableBlurScreen(this.isScreenCapturePrevented(server));
+    };
+
+    /**
+     * Gets the shielded screen ID for the screen.
+     */
+    getShieldScreenId = (screen: AvailableScreens, force = false, skip = false) => {
+        if ((this.activeServer && this.isScreenCapturePrevented(this.activeServer)) || force) {
+            const name = `${screen}.screen`;
+            return skip ? `${name}.skip.shielded` : `${name}.shielded`;
+        }
+
+        return `${screen}.screen`;
     };
 
     /**
@@ -384,6 +380,75 @@ class SecurityManager {
 
         return buttons;
     };
+
+    showDeviceNotTrustedAlert = async (server: string, siteName: string | undefined, translations: Record<string, string>) => {
+        const buttons = await this.buildAlertOptions(server, translations);
+        const securedBy = siteName || this.getServerConfig(server)?.siteName || 'Mattermost';
+
+        Alert.alert(
+            translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
+            translations[t('mobile.managed.jailbreak')].
+                replace('{vendor}', securedBy),
+            buttons,
+            {cancelable: false},
+        );
+    };
+
+    /**
+     * Shows an alert when the device does not have biometrics or passcode set.
+     */
+    showNotSecuredAlert = async (server: string, siteName: string | undefined, translations: Record<string, string>) => {
+        const buttons: AlertButton[] = [];
+        const config = this.serverConfig[server];
+        const securedBy = siteName || config?.siteName || 'Mattermost';
+
+        if (Platform.OS === 'android') {
+            buttons.push({
+                text: translations[t('mobile.managed.settings')],
+                onPress: () => {
+                    Emm.openSecuritySettings();
+                },
+            });
+        }
+
+        const alertButtons = await this.buildAlertOptions(server, translations);
+        buttons.push(...alertButtons);
+
+        let message;
+        const platform = Platform.select({ios: 'ios', default: 'android'});
+        if (config?.siteName || siteName) {
+            message = translations[t(`mobile.managed.not_secured.${platform}.vendor`)].replace('{vendor}', securedBy);
+        } else {
+            message = translations[t(`mobile.managed.not_secured.${platform}`)];
+        }
+
+        Alert.alert(
+            translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
+            message,
+            buttons,
+            {cancelable: false},
+        );
+    };
+
+    /**
+     * Shows an alert when biometric authentication fails.
+     */
+    showBiometricFailureAlert = async (server: string, blurOnAuthenticate: boolean, siteName: string | undefined, translations: Record<string, string>) => {
+        const buttons = await this.buildAlertOptions(server, translations, () => {
+            if (blurOnAuthenticate) {
+                Emm.removeBlurEffect();
+            }
+        });
+        const securedBy = siteName || this.getServerConfig(server)?.siteName || 'Mattermost';
+
+        Alert.alert(
+            translations[t('mobile.managed.blocked_by')].replace('{vendor}', securedBy),
+            translations[t('mobile.managed.biometric_failed')],
+            buttons,
+            {cancelable: false},
+        );
+    };
 }
 
-export default new SecurityManager();
+const SecurityManager = new SecurityManagerSingleton();
+export default SecurityManager;
