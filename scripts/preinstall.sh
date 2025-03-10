977a043a555e2607d2a50e7ff25222178ea57604
Takuya Noguchi <takninnovationresearch@gmail.com>
2025-03-10 15:50:14 +0000

    chore(deps-dev): skip to explicitly install Bundler 2.5
    Since Ruby 2.6, released in 2018, Bundler has been
    included in the Ruby standard library. So, we don't
    need to install Bundler explicitly. The latest minor
    version can be used with the latest Ruby version.
    
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -11,11 +11,6 @@ function cocoapods() {
 }
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  if !(gem list bundler -i --version 2.5.11) > /dev/null 2>&1; then
-    echo "Installing Bundler"
-    gem install bundler --version 2.5.11  || exit 1
-  fi
-
   if !(gem list cocoapods -i --version 1.16.1) > /dev/null 2>&1; then
     cocoapods
   fi
