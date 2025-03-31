40d9f2fe01be6ef8ca1d8567f342de7ae9a2e00d
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-04-01 00:21:13 +0530

    addressed review comment for time utils

--- app/components/formatted_time/index.tsx
+++ app/components/formatted_time/index.tsx
@@ -2,34 +2,17 @@
 // See LICENSE.txt for license information.
 
 import moment from 'moment';
-import mtz from 'moment-timezone';
 import React from 'react';
 import {useIntl} from 'react-intl';
 import {Text, type TextProps} from 'react-native';
 
 import {getLocaleFromLanguage} from '@i18n';
+import {getFormattedTime} from '@utils/time';
 
 type FormattedTimeProps = TextProps & {
     isMilitaryTime: boolean;
     timezone: UserTimezone | string;
     value: number | string | Date;
-}
-
-export function getFormattedTime(isMilitaryTime: boolean, timezone: UserTimezone | string, value: number | string | Date) {
-    let format = 'H:mm';
-    if (!isMilitaryTime) {
-        const localeFormat = mtz.localeData().longDateFormat('LT');
-        format = localeFormat?.includes('A') ? localeFormat : 'h:mm A';
-    }
-
-    let zone: string;
-    if (typeof timezone === 'object') {
-        zone = timezone.useAutomaticTimezone ? timezone.automaticTimezone : timezone.manualTimezone;
-    } else {
-        zone = timezone;
-    }
-
-    return timezone ? mtz.tz(value, zone).format(format) : mtz(value).format(format);
 }
 
 const FormattedTime = ({isMilitaryTime, timezone, value, ...props}: FormattedTimeProps) => {
