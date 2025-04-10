0319b50830f8954279a846caa2cb7a8c940cf0dc
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2025-04-10 23:04:54 +0530

    remove re-try logic

--- detox/e2e/test/setup.ts
+++ detox/e2e/test/setup.ts
@@ -19,7 +19,6 @@ beforeAll(async () => {
             newInstance: true,
             delete: true,
             permissions: {
-
                 // Set all permissions at once
                 notifications: 'YES',
                 camera: 'NO',
