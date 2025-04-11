6eb4286a499389db08a8b41515387d158d6069a8
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-04-11 16:32:16 +0530

    Revert "only for running the application - revert me"
    This reverts commit 7b4e74ba6666894aa153753849e2759e4f0dc061.

--- app/screens/home/search/search.test.tsx
+++ app/screens/home/search/search.test.tsx
@@ -19,6 +19,7 @@ import type {Database} from '@nozbe/watermelondb';
 jest.mock('@react-native-camera-roll/camera-roll', () => ({}));
 
 jest.mock('@react-navigation/native', () => ({
+    ...jest.requireActual('@react-navigation/native'),
     useNavigation: () => ({
         getState: () => ({
             index: 0,
