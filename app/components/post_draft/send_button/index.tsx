d7245812c0282cd152a803fc5cd2b3bd6f52d16b
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-04-29 14:01:32 +0530

    Added config and license check for fetching and displaying scheduled post

--- app/components/post_draft/send_button/index.tsx
+++ app/components/post_draft/send_button/index.tsx
@@ -2,33 +2,14 @@
 // See LICENSE.txt for license information.
 
 import {withDatabase, withObservables} from '@nozbe/watermelondb/react';
-import {combineLatest, of as of$} from 'rxjs';
-import {switchMap, distinctUntilChanged} from 'rxjs/operators';
 
 import {SendButton} from '@components/post_draft/send_button/send_button';
 import {Tutorial} from '@constants';
 import {observeTutorialWatched} from '@queries/app/global';
-import {observeLicense} from '@queries/servers/system';
 
-import type {WithDatabaseArgs} from '@typings/database/database';
+const enhanced = withObservables([], () => {
+    const scheduledPostFeatureTooltipWatched = observeTutorialWatched(Tutorial.SCHEDULED_POST);
 
-type Props = WithDatabaseArgs & {
-    scheduledPostEnabled: boolean;
-}
-
-const enhanced = withObservables(['scheduledPostEnabled'], ({scheduledPostEnabled, database}: Props) => {
-    const scheduledPostTutorialWatched = observeTutorialWatched(Tutorial.SCHEDULED_POST);
-    const isLicense = observeLicense(database);
-
-    const scheduledPostFeatureTooltipWatched = combineLatest([scheduledPostTutorialWatched, isLicense]).pipe(
-        switchMap(([watched, license]) => {
-            if (license?.IsLicensed === 'true' && scheduledPostEnabled) {
-                return of$(watched);
-            }
-            return of$(true);
-        }),
-        distinctUntilChanged(),
-    );
     return {
         scheduledPostFeatureTooltipWatched,
     };
