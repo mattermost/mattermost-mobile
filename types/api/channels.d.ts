adb7d1fc437c38139cd422570f353d1a8eae9eff
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2025-05-14 11:33:48 +0200

    add channel policy enforced type

--- types/api/channels.d.ts
+++ types/api/channels.d.ts
@@ -45,6 +45,9 @@ type Channel = {
     group_constrained: boolean|null;
     shared: boolean;
     banner_info?: ChannelBannerInfo;
+
+    /** Whether the channel has Attribute-Based Access Control (ABAC) policy enforcement enabled, controlling access based on user attributes */
+    policy_enforced?: boolean;
 };
 type ChannelPatch = {
     name?: string;
