383734f557b5aa2c2a548ef9a29ec094ff3bad0f
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-06-23 22:17:59 +0530

    Added test for upload_remove component when not in edit mode

--- app/components/post_draft/uploads/upload_item/upload_remove.test.tsx
+++ app/components/post_draft/uploads/upload_item/upload_remove.test.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {removeDraftFile} from '@actions/local/draft';
 import {fireEvent, renderWithEverything} from '@test/intl-test-helper';
 import TestHelper from '@test/test_helper';
 
@@ -10,6 +11,10 @@ import type {Database} from '@nozbe/watermelondb';
 
 jest.mock('@context/edit_post', () => ({
     useEditPost: jest.fn(),
+}));
+
+jest.mock('@actions/local/draft', () => ({
+    removeDraftFile: jest.fn(),
 }));
 
 const {useEditPost} = require('@context/edit_post');
@@ -40,5 +45,23 @@ describe('UploadRemove', () => {
         );
         fireEvent.press(getByTestId('remove-button-fileId'));
         expect(onFileRemove).toHaveBeenCalledWith('fileId');
+    });
+
+    it('should call removeDraftFile when isEditMode is false', () => {
+        jest.mocked(useEditPost).mockReturnValue({
+            isEditMode: false,
+        });
+
+        const {getByTestId} = renderWithEverything(
+            <UploadRemove
+                channelId='channelId'
+                rootId='rootId'
+                clientId='clientId'
+                fileId='fileId'
+            />,
+            {database, serverUrl},
+        );
+        fireEvent.press(getByTestId('remove-button-fileId'));
+        expect(removeDraftFile).toHaveBeenCalledWith(serverUrl, 'channelId', 'rootId', 'clientId');
     });
 });
