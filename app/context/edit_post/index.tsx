dae4b75720b33a92765b5be5d55bbe4243f1c3c3
Rajat Dabade <rajatdabade1997@gmail.com>
2025-06-25 16:04:08 +0530

    Ability to show quick action and add files to edit post (#8926)
    * Viewing Files in Edit mode in mobile with ability to delete and save
    
    * Added upload attachment to keyboard tracker view
    
    * using state instread of use ref
    
    * Minor
    
    * Added tests
    
    * intl extract
    
    * new function getFiles
    ById, batch file deletion and tests
    
    * Files fetching in edit options and tests
    
    * Removed DeviceEventEmitter and used React context
    
    * Added support to check minimum required version to show edit file attachments
    
    * resolve forward ref issue
    
    * Minor
    
    * memotized props for context and observe config with value
    
    * Import fixes
    
    * Ability to show quick action and add files to edit post
    
    * type safety for EditPostContext
    
    * Reverted back the post priority props
    
    * constant shift
    
    * Added test for QuickAction to show slashcomand
    
    * Added test for edit_post, upload_item and upload_remove
    
    * Added test for Edit_post_input and edit_post index
    
    * fix the height issue between attachment and keyboard due to safeArea
    
    * Minor: removed debugging border color
    
    * Addressed dev review comments
    
    * Import fixes
    
    * Address UX comments
    
    * Fixed props for UploadItem and remove effective Edit mode
    
    * handled save button disabled state when uploading attachments
    
    * handled newly added and retry file removal without alert message
    
    * Test updated
    
    * Added test for input_quick_action index.tsx
    
    * Added test for not in edit mode for upload_item index
    
    * Added test for upload_remove component when not in edit mode
    
    * added tests for file_upload_error hook
    
    * Added test for calling callback when in edit mode
    
    * Test for edit post input for server version check

--- app/context/edit_post/index.tsx
+++ app/context/edit_post/index.tsx
@@ -5,19 +5,29 @@ import React, {createContext, useContext, useMemo, type ReactNode} from 'react';
 
 type EditPostContextType = {
     onFileRemove?: (fileId: string) => void;
+    updateFileCallback?: (fileInfo: FileInfo) => void;
+    isEditMode: boolean;
 };
 
-const EditPostContext = createContext<EditPostContextType>({});
+const EditPostContext = createContext<EditPostContextType>({
+    onFileRemove: undefined,
+    updateFileCallback: undefined,
+    isEditMode: false,
+});
 
 type EditPostProviderProps = {
     children: ReactNode;
     onFileRemove?: (fileId: string) => void;
+    updateFileCallback?: (fileInfo: FileInfo) => void;
+    isEditMode: boolean;
 };
 
-export const EditPostProvider = ({children, onFileRemove}: EditPostProviderProps) => {
+export const EditPostProvider = ({children, onFileRemove, updateFileCallback, isEditMode}: EditPostProviderProps) => {
     const contextValue = useMemo(() => ({
         onFileRemove,
-    }), [onFileRemove]);
+        updateFileCallback,
+        isEditMode,
+    }), [onFileRemove, updateFileCallback, isEditMode]);
 
     return (
         <EditPostContext.Provider value={contextValue}>
