efb69eb6fdc15a37d54eb966b0a33efa51f39024
Daniel Espino García <larkox@gmail.com>
2025-07-18 11:14:23 +0200

    Fix tables on playbook runs (#8997)

--- app/products/playbooks/screens/playbook_run/playbook_run.tsx
+++ app/products/playbooks/screens/playbook_run/playbook_run.tsx
@@ -144,6 +144,9 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => ({
     markdownContainer: {
         width: '100%',
     },
+    markdownContainer: {
+        width: '100%',
+    },
 }));
 
 type Props = {
