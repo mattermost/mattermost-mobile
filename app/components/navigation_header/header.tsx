4acc227c1b45f36aa69bbed6056e34320016474d
Your Name <larkox@gmail.com>
2025-08-04 15:25:10 +0200

    Fix long name playbooks channel header

--- app/components/navigation_header/header.tsx
+++ app/components/navigation_header/header.tsx
@@ -76,7 +76,6 @@ const getStyleSheet = makeStyleSheetFromTheme((theme: Theme) => ({
         paddingHorizontal: 8,
         ...Platform.select({
             ios: {
-                paddingHorizontal: 60,
                 flex: undefined,
                 width: '100%',
                 position: 'absolute',
@@ -177,9 +176,14 @@ const Header = ({
     const containerStyle = useMemo(() => (
         [styles.container, containerAnimatedStyle]), [styles, containerAnimatedStyle]);
 
-    const additionalTitleStyle = useMemo(() => ({
-        marginLeft: Platform.select({android: showBackButton && !leftComponent ? 20 : 0}),
-    }), [leftComponent, showBackButton]);
+    const additionalTitleStyle = useMemo(() => {
+        return {
+            marginLeft: Platform.select({android: showBackButton && !leftComponent ? 20 : 0}),
+            paddingHorizontal: Platform.select({
+                ios: rightButtons?.length === 2 ? 90: 60,
+            }),
+        };
+    }, [leftComponent, showBackButton, rightButtons]);
 
     return (
         <Animated.View style={containerStyle}>
