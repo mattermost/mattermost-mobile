00e0b9165cb40464d999aa36d98af1df0402a7a5
Your Name <larkox@gmail.com>
2025-08-04 18:55:52 +0200

    Purge leftover information in my channel table when purging playbooks

--- app/products/playbooks/actions/local/version.ts
+++ app/products/playbooks/actions/local/version.ts
@@ -1,9 +1,13 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {SYSTEM_IDENTIFIERS} from '@constants/database';
+import {Q} from '@nozbe/watermelondb';
+
+import {MM_TABLES, SYSTEM_IDENTIFIERS} from '@constants/database';
 import DatabaseManager from '@database/manager';
 import {PLAYBOOK_TABLES} from '@playbooks/constants/database';
+
+import type {MyChannelModel} from '@database/models/server';
 
 export const setPlaybooksVersion = async (serverUrl: string, version: string) => {
     try {
@@ -32,14 +36,23 @@ export const setPlaybooksVersion = async (serverUrl: string, version: string) =>
 const purgePlaybooks = async (serverUrl: string) => {
     try {
         const {database} = DatabaseManager.getServerDatabaseAndOperator(serverUrl);
-        await database.write(() => {
-            return database.adapter.unsafeExecute({
+        const models = await database.collections.get<MyChannelModel>(MM_TABLES.SERVER.MY_CHANNEL).query(Q.where('last_playbook_runs_fetch_at', Q.gt(0)));
+
+        await database.write(async () => {
+            await database.adapter.unsafeExecute({
                 sqls: [
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_RUN}`, []],
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_CHECKLIST}`, []],
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_CHECKLIST_ITEM}`, []],
                 ],
             });
+
+            for await (const model of models) {
+                await model.update((channel) => {
+                    channel.lastPlaybookRunsFetchAt = 0;
+                    return channel;
+                });
+            }
         });
     } catch (error) {
         return {error};
