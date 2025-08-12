71cec5a3e29db9332e25a0404e696b9eec484660
Daniel Espino García <larkox@gmail.com>
2025-08-12 14:27:52 +0200

    Purge leftover information in my channel table when purging playbooks (#9057)
    * Purge leftover information in my channel table when purging playbooks
    
    * Add comment
    
    * Clear the sync ephemeral store on plugin disable

--- app/products/playbooks/actions/local/version.ts
+++ app/products/playbooks/actions/local/version.ts
@@ -1,9 +1,14 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {SYSTEM_IDENTIFIERS} from '@constants/database';
+import {Q} from '@nozbe/watermelondb';
+
+import {MM_TABLES, SYSTEM_IDENTIFIERS} from '@constants/database';
 import DatabaseManager from '@database/manager';
 import {PLAYBOOK_TABLES} from '@playbooks/constants/database';
+import EphemeralStore from '@store/ephemeral_store';
+
+import type {MyChannelModel} from '@database/models/server';
 
 export const setPlaybooksVersion = async (serverUrl: string, version: string) => {
     try {
@@ -21,6 +26,8 @@ export const setPlaybooksVersion = async (serverUrl: string, version: string) =>
             if (error) {
                 return {error};
             }
+
+            EphemeralStore.clearChannelPlaybooksSynced(serverUrl);
         }
 
         return {data: true};
@@ -32,14 +39,25 @@ export const setPlaybooksVersion = async (serverUrl: string, version: string) =>
 const purgePlaybooks = async (serverUrl: string) => {
     try {
         const {database} = DatabaseManager.getServerDatabaseAndOperator(serverUrl);
-        await database.write(() => {
-            return database.adapter.unsafeExecute({
+        const models = await database.collections.get<MyChannelModel>(MM_TABLES.SERVER.MY_CHANNEL).query(Q.where('last_playbook_runs_fetch_at', Q.gt(0)));
+
+        await database.write(async () => {
+            await database.adapter.unsafeExecute({
                 sqls: [
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_RUN}`, []],
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_CHECKLIST}`, []],
                     [`DELETE FROM ${PLAYBOOK_TABLES.PLAYBOOK_CHECKLIST_ITEM}`, []],
                 ],
             });
+
+            // Process each model sequentially to prevent overwhelming the event loop with excessive promises
+            // and to avoid overloading the database with concurrent write operations, especially when handling a large number of channels.
+            for await (const model of models) {
+                await model.update((channel) => {
+                    channel.lastPlaybookRunsFetchAt = 0;
+                    return channel;
+                });
+            }
         });
     } catch (error) {
         return {error};
