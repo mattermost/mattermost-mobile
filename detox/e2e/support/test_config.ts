4dc15ec58e6f06a4dd2a8008c705941730fb22ed
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-09-17 17:41:04 +0530

    E2E test for permalink preview

--- detox/e2e/support/test_config.ts
+++ detox/e2e/support/test_config.ts
@@ -1,15 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const serverOneUrl = process.env.SITE_1_URL || (process.env.IOS === 'true' ? 'http://localhost:8065' : 'http://10.0.2.2:8065');
-export const siteOneUrl = process.env.SITE_1_URL || 'http://localhost:8065';
-export const serverTwoUrl = process.env.SITE_2_URL || 'http://localhost:8065';
-export const siteTwoUrl = process.env.SITE_2_URL || 'http://localhost:8065';
-export const serverThreeUrl = process.env.SITE_3_URL || 'http://localhost:8065';
-export const siteThreeUrl = process.env.SITE_3_URL || 'http://localhost:8065';
-export const smtpUrl = process.env.SMTP_URL || 'http://127.0.0.1:9001';
+const {getLocalIPAddress} = require('./helpers');
+
+const baseUrl = `http://${getLocalIPAddress()}:8065`;
+
+export const serverOneUrl = process.env.SITE_1_URL || process.env.SITE_URL || baseUrl;
+export const siteOneUrl = process.env.SITE_1_URL || process.env.SITE_URL || baseUrl;
+export const serverTwoUrl = process.env.SITE_2_URL || baseUrl;
+export const siteTwoUrl = process.env.SITE_2_URL || baseUrl;
+export const serverThreeUrl = process.env.SITE_3_URL || baseUrl;
+export const siteThreeUrl = process.env.SITE_3_URL || baseUrl;
+export const smtpUrl = process.env.SMTP_URL || `http://${getLocalIPAddress()}:9001`;
 export const adminEmail = process.env.ADMIN_EMAIL || 'sysadmin@sample.mattermost.com';
 export const adminUsername = process.env.ADMIN_USERNAME || 'sysadmin';
 export const adminPassword = process.env.ADMIN_PASSWORD || 'Sys@dmin-sample1';
-export const ldapServer = process.env.LDAP_SERVER || '127.0.0.1';
+export const ldapServer = process.env.LDAP_SERVER || getLocalIPAddress();
 export const ldapPort = process.env.LDAP_PORT || 389;
