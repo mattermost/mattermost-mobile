1cedc7b7e141e040415380d7afbaa4b8dbf593c3
Daniel Espino García <larkox@gmail.com>
2025-09-26 17:33:13 +0200

    Add playbooks finish run (#9144)
    * Add playbooks finish run
    
    * Add missing texts
    
    * Fix tablet reconnect issue
    
    * Add body to put method
    
    * Fix tests

--- app/products/playbooks/actions/websocket/reconnect.test.ts
+++ app/products/playbooks/actions/websocket/reconnect.test.ts
@@ -9,6 +9,7 @@ import {fetchIsPlaybooksEnabled} from '@playbooks/database/queries/version';
 import {getCurrentChannelId} from '@queries/servers/system';
 import EphemeralStore from '@store/ephemeral_store';
 import NavigationStore from '@store/navigation_store';
+import {isTablet} from '@utils/helpers';
 
 import {handlePlaybookReconnect} from './reconnect';
 
@@ -18,6 +19,7 @@ jest.mock('@playbooks/actions/remote/runs');
 jest.mock('@playbooks/actions/remote/version');
 jest.mock('@playbooks/database/queries/version');
 jest.mock('@queries/servers/system');
+jest.mock('@utils/helpers');
 
 describe('handlePlaybookReconnect', () => {
     const mockCurrentChannelId = 'channel-123';
@@ -31,6 +33,7 @@ describe('handlePlaybookReconnect', () => {
         jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(false);
         jest.mocked(getCurrentChannelId).mockResolvedValue(mockCurrentChannelId);
         jest.mocked(fetchPlaybookRunsForChannel).mockResolvedValue({runs: []});
+        jest.mocked(isTablet).mockReturnValue(false);
     });
 
     afterEach(async () => {
@@ -65,39 +68,84 @@ describe('handlePlaybookReconnect', () => {
         expect(updatePlaybooksVersion).toHaveBeenCalledTimes(1);
     });
 
-    it('should not fetch playbook runs when not on channel screen', async () => {
-        const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME, Screens.THREAD]);
+    describe('on phone device', () => {
+        it('should not fetch playbook runs when not on channel screen', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME, Screens.THREAD]);
 
-        await handlePlaybookReconnect(serverUrl);
+            await handlePlaybookReconnect(serverUrl);
 
-        expect(getScreensSpy).toHaveBeenCalled();
-        expect(fetchIsPlaybooksEnabled).not.toHaveBeenCalled();
-        expect(getCurrentChannelId).not.toHaveBeenCalled();
-        expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).not.toHaveBeenCalled();
+            expect(getCurrentChannelId).not.toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
+        });
+
+        it('should not fetch playbook runs when playbooks are disabled', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME, Screens.CHANNEL, Screens.THREAD]);
+            jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(false);
+
+            await handlePlaybookReconnect(serverUrl);
+
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
+            expect(getCurrentChannelId).not.toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
+        });
+
+        it('should fetch playbook runs when on channel screen and playbooks are enabled', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.CHANNEL]);
+            jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(true);
+
+            await handlePlaybookReconnect(serverUrl);
+
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
+            expect(getCurrentChannelId).toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).toHaveBeenCalledWith(serverUrl, mockCurrentChannelId);
+            expect(fetchPlaybookRunsForChannel).toHaveBeenCalledTimes(1);
+        });
     });
 
-    it('should not fetch playbook runs when playbooks are disabled', async () => {
-        const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME, Screens.CHANNEL, Screens.THREAD]);
-        jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(false);
+    describe('on tablet device', () => {
+        it('should fetch playbook runs when playbooks are enabled and there is a channel id', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME]);
+            jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(true);
+            jest.mocked(isTablet).mockReturnValueOnce(true);
 
-        await handlePlaybookReconnect(serverUrl);
+            await handlePlaybookReconnect(serverUrl);
 
-        expect(getScreensSpy).toHaveBeenCalled();
-        expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
-        expect(getCurrentChannelId).not.toHaveBeenCalled();
-        expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
-    });
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
+            expect(getCurrentChannelId).toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).toHaveBeenCalledWith(serverUrl, mockCurrentChannelId);
+            expect(fetchPlaybookRunsForChannel).toHaveBeenCalledTimes(1);
+        });
 
-    it('should fetch playbook runs when on channel screen and playbooks are enabled', async () => {
-        const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.CHANNEL]);
-        jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(true);
+        it('should not fetch playbook runs when playbooks are enabled and there is no channel id', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME]);
+            jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(true);
+            jest.mocked(isTablet).mockReturnValueOnce(true);
+            jest.mocked(getCurrentChannelId).mockResolvedValue('');
 
-        await handlePlaybookReconnect(serverUrl);
+            await handlePlaybookReconnect(serverUrl);
 
-        expect(getScreensSpy).toHaveBeenCalled();
-        expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
-        expect(getCurrentChannelId).toHaveBeenCalled();
-        expect(fetchPlaybookRunsForChannel).toHaveBeenCalledWith(serverUrl, mockCurrentChannelId);
-        expect(fetchPlaybookRunsForChannel).toHaveBeenCalledTimes(1);
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
+            expect(getCurrentChannelId).toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
+        });
+
+        it('should not fetch playbook runs when playbooks are disabled', async () => {
+            const getScreensSpy = jest.spyOn(NavigationStore, 'getScreensInStack').mockReturnValue([Screens.HOME]);
+            jest.mocked(fetchIsPlaybooksEnabled).mockResolvedValue(false);
+            jest.mocked(isTablet).mockReturnValueOnce(true);
+
+            await handlePlaybookReconnect(serverUrl);
+
+            expect(getScreensSpy).toHaveBeenCalled();
+            expect(fetchIsPlaybooksEnabled).toHaveBeenCalled();
+            expect(getCurrentChannelId).not.toHaveBeenCalled();
+            expect(fetchPlaybookRunsForChannel).not.toHaveBeenCalled();
+        });
     });
 });
