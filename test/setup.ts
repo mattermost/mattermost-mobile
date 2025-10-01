42988fddc3b638b9b12728b7ce05160a63c11cdf
Yair Szarf <yair@mac.com>
2025-10-01 08:20:22 -0400

    Add a notification disabled notice to notification settings (#9145)
    * Adding the notification disabled notice
    
    * Change the color of the icon on the section notice to red.
    
    * Fix Linter Issues
    
    * Add new line due to CI failure
    
    * Adressing pull request comments and change requests.
    
    * i18 Strings alphabetical order
    
    * result of `npm run i18n-extract`
    
    * Add a couple more tests for notifications
    
    * Remove two unneeded styles
    
    * fix linter issue

--- test/setup.ts
+++ test/setup.ts
@@ -380,6 +380,7 @@ jest.mock('react-native-notifications', () => {
         Notifications: {
             registerRemoteNotifications: jest.fn(),
             addEventListener: jest.fn(),
+            isRegisteredForRemoteNotifications: jest.fn(),
             setDeliveredNotifications: jest.fn((notifications) => {
                 deliveredNotifications = notifications;
             }),
