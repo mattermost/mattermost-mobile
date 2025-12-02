4fcd934295884706a6c6d8d53bc69af526bc4ee5
Harshil Sharma <harshilsharma63@gmail.com>
2025-12-02 20:26:24 +0530

    Added tests and error handling

--- app/actions/websocket/burn_on_read.ts
+++ app/actions/websocket/burn_on_read.ts
@@ -4,25 +4,33 @@
 import {handleNewPostEvent, handlePostEdited} from '@actions/websocket/posts';
 import DatabaseManager from '@database/manager';
 import {getPostById} from '@queries/servers/post';
+import {logError} from '@utils/log';
 
 export async function handleBoRPostRevealedEvent(serverUrl: string, msg: WebSocketMessage) {
-    const operator = DatabaseManager.serverDatabases[serverUrl]?.operator;
-    if (!operator) {
-        return;
-    }
-
-    const {database} = operator;
-    let post: Post;
     try {
-        post = JSON.parse(msg.data.post);
-    } catch {
-        return;
-    }
+        const operator = DatabaseManager.serverDatabases[serverUrl]?.operator;
+        if (!operator) {
+            return null;
+        }
 
-    const existingPost = await getPostById(database, post.id);
-    if (existingPost) {
-        await handlePostEdited(serverUrl, msg);
-    } else {
-        await handleNewPostEvent(serverUrl, msg);
+        const {database} = operator;
+        let post: Post;
+        try {
+            post = JSON.parse(msg.data.post);
+        } catch {
+            return null;
+        }
+
+        const existingPost = await getPostById(database, post.id);
+        if (existingPost) {
+            await handlePostEdited(serverUrl, msg);
+        } else {
+            await handleNewPostEvent(serverUrl, msg);
+        }
+
+        return {};
+    } catch (error) {
+        logError('handleBoRPostRevealedEvent could not handle websocket event for revealed burn-on-read post', error);
+        return {error};
     }
 }
