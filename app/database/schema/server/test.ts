57b46ee1b46f044048efd7794fa23808d1701cab
Harshil Sharma <harshilsharma63@gmail.com>
2025-12-12 19:01:48 +0530

    fixed a test

--- app/database/schema/server/test.ts
+++ app/database/schema/server/test.ts
@@ -52,7 +52,7 @@ const {PLAYBOOK_RUN, PLAYBOOK_CHECKLIST, PLAYBOOK_CHECKLIST_ITEM, PLAYBOOK_RUN_A
 describe('*** Test schema for SERVER database ***', () => {
     it('=> The SERVER SCHEMA should strictly match', () => {
         expect(serverSchema).toEqual({
-            version: 16,
+            version: 17,
             unsafeSql: undefined,
             tables: {
                 [CATEGORY]: {
