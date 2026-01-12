86eaa8b02a64edb7a72aa0d2d7cb3275d855eaf0
Elias Nahum <nahumhbl@gmail.com>
2026-01-12 16:16:58 +0200

    update intune to use new-arch

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-module.exports = {
-    dependencies: {
-        'react-native-notifications': {
-            platforms: {
-                android: null,
-            },
+// eslint-disable-next-line no-process-env
+const intuneEnabled = process.env.INTUNE_ENABLED === '1';
+
+const dependencies = {
+    'react-native-notifications': {
+        platforms: {
+            android: null,
         },
     },
+};
+
+// Conditionally add Intune dependency when enabled
+if (intuneEnabled) {
+    dependencies['@mattermost/intune'] = {
+        root: './libraries/@mattermost/intune',
+    };
+}
+
+module.exports = {
+    dependencies,
     assets: [
         './assets/fonts',
     ],
