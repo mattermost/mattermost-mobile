d2dc148729881f349b7f90b247a1d5c387050fce
Felipe Martin <me@fmartingr.com>
2026-01-26 12:44:53 +0100

    fix: update dependency array in useMemo for isUnrevealedPost to include post expiration metadata

--- app/components/post_list/post/header/header.tsx
+++ app/components/post_list/post/header/header.tsx
@@ -101,7 +101,8 @@ const Header = (props: HeaderProps) => {
     const userIconOverride = ensureString(post.props?.override_icon_url);
     const usernameOverride = ensureString(post.props?.override_username);
 
-    const isUnrevealedPost = useMemo(() => isUnrevealedBoRPost(post), [post]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const isUnrevealedPost = useMemo(() => isUnrevealedBoRPost(post), [post, post.metadata?.expire_at]);
     const ownBoRPost = useMemo(() => isOwnBoRPost(post, currentUser), [currentUser, post]);
     const showBoRIcon = isUnrevealedPost || ownBoRPost;
     const borExpireAt = post.metadata?.expire_at;
