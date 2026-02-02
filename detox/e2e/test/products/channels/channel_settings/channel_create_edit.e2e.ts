faa86555fe356f3285d3f1603f03b25e455a3f82
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2026-02-02 10:54:19 +0530

    eslint fixes

--- detox/e2e/test/products/channels/channel_settings/channel_create_edit.e2e.ts
+++ detox/e2e/test/products/channels/channel_settings/channel_create_edit.e2e.ts
@@ -16,19 +16,18 @@
  * - MM-T854: RN apps Channel can be created using 2 non-latin characters
  */
 
-import {Channel, Setup} from '@support/server_api';
+import {Setup} from '@support/server_api';
 import {
     serverOneUrl,
     siteOneUrl,
 } from '@support/test_config';
 import {
-    BrowseChannelsScreen,
     ChannelInfoScreen,
     ChannelListScreen,
     ChannelScreen,
     CreateOrEditChannelScreen,
-    HomeScreen,
     LoginScreen,
+    HomeScreen,
     ServerScreen,
 } from '@support/ui/screen';
 import {getRandomId, timeouts, wait} from '@support/utils';
@@ -36,273 +35,183 @@ import {expect} from 'detox';
 
 describe('Channels', () => {
     const serverOneDisplayName = 'Server 1';
+    const channelsCategory = 'channels';
     let testUser: any;
-    let testTeam: any;
+    let publicChannelName: string;
+    let publicChannelDisplayName: string;
+    let privateChannelName: string;
+    let privateChannelDisplayName: string;
 
     beforeAll(async () => {
-        const {user, team} = await Setup.apiInit(siteOneUrl);
+        const {user} = await Setup.apiInit(siteOneUrl);
         testUser = user;
-        testTeam = team;
 
-        // # Log in to server
         await ServerScreen.connectToServer(serverOneUrl, serverOneDisplayName);
         await LoginScreen.login(testUser);
     });
 
     afterAll(async () => {
-        // # Log out
         await HomeScreen.logout();
     });
 
     it('MM-T3201 - RN apps Create public channel', async () => {
-        const channelName = `channel-${getRandomId()}`;
-        const channelDisplayName = channelName.replace(/-/g, ' ');
-        const channelPurpose = 'This is a test purpose\nwith multiple lines\nfor testing';
+        publicChannelName = `channel-${getRandomId()}`;
+        publicChannelDisplayName = publicChannelName.replace(/-/g, ' ');
+        const channelPurpose = 'This is a test purpose for the channel';
         const channelHeader = ':taco:';
 
-        // # Step 1: From the channels list, tap on the "+" displayed to the right of "CHANNELS"
         await ChannelListScreen.headerPlusButton.tap();
+        await ChannelListScreen.createNewChannelItem.tap();
 
-        // * On step 1, a list of existing channels should be displayed.
-        await BrowseChannelsScreen.toBeVisible();
-        await expect(BrowseChannelsScreen.flatChannelList).toBeVisible();
-
-        // # Step 2: Tap on "Create" bottomsheet option (Public Channel type is pre-selected in the Create screen, otherwise choose Public Channel)
-        await BrowseChannelsScreen.createButton.tap();
-
-        // * On step 2, a screen enabling you to name your channel should be displayed.
         await CreateOrEditChannelScreen.toBeVisible();
         await expect(CreateOrEditChannelScreen.displayNameInput).toBeVisible();
-
-        // # Step 3: Type in a name, observing that the Create button is not tappable until at least 2 characters have been entered
-        // * Verify Create button is disabled initially
         await expect(CreateOrEditChannelScreen.createButton).toBeVisible();
 
-        // Type single character and verify button is still disabled
-        await CreateOrEditChannelScreen.displayNameInput.typeText('a');
-        await wait(timeouts.ONE_SEC);
+        await CreateOrEditChannelScreen.displayNameInput.replaceText(publicChannelDisplayName);
 
-        // Clear and type the full channel name (at least 2 characters)
-        await CreateOrEditChannelScreen.displayNameInput.clearText();
-        await CreateOrEditChannelScreen.displayNameInput.typeText(channelDisplayName);
-
-        // # Step 4: Type in a purpose that has multiple returns (new lines)
         await CreateOrEditChannelScreen.purposeInput.tap();
-        await CreateOrEditChannelScreen.purposeInput.typeText(channelPurpose);
+        await CreateOrEditChannelScreen.purposeInput.replaceText(channelPurpose);
 
-        // # Step 5: Type in a header that is an emoji (such as :taco:)
         await CreateOrEditChannelScreen.headerInput.tap();
-        await CreateOrEditChannelScreen.headerInput.typeText(channelHeader);
+        await CreateOrEditChannelScreen.headerInput.replaceText(channelHeader);
 
-        // # Step 6: Tap "Create" in the top right of the screen
         await CreateOrEditChannelScreen.createButton.tap();
+        await ChannelScreen.dismissScheduledPostTooltip();
 
-        // * The new channel should be created and you should be redirected to the new channel
         await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText(channelDisplayName);
+        await expect(ChannelScreen.headerTitle).toHaveText(publicChannelDisplayName);
 
-        // * You can tap the channel name to view and verify channel purpose and header
         await ChannelInfoScreen.open();
-        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(channelDisplayName.toUpperCase());
+        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(publicChannelDisplayName);
         await expect(ChannelInfoScreen.publicPrivateTitlePurpose).toHaveText(channelPurpose);
 
-        // Verify header by opening channel screen and checking if header is visible
         await ChannelInfoScreen.close();
-        await ChannelScreen.toBeVisible();
-
-        // Go back to channel list for cleanup
         await ChannelScreen.back();
     });
 
     it('MM-T3203 - RN apps Create private channel', async () => {
-        const channelName = `private-channel-${getRandomId()}`;
-        const channelDisplayName = channelName.replace(/-/g, ' ');
+        privateChannelName = `private-channel-${getRandomId()}`;
+        privateChannelDisplayName = privateChannelName.replace(/-/g, ' ');
         const channelPurpose = 'This is a private test channel purpose';
         const channelHeader = 'Private channel header';
 
-        // # Step 1: From the channels list, tap on the "+" displayed to the right of "CHANNELS", then choose Create option from bottomsheet
         await ChannelListScreen.headerPlusButton.tap();
-        await BrowseChannelsScreen.toBeVisible();
-        await BrowseChannelsScreen.createButton.tap();
+        await ChannelListScreen.createNewChannelItem.tap();
 
-        // * After #1, a screen enabling you to name your channel should be displayed.
         await CreateOrEditChannelScreen.toBeVisible();
         await expect(CreateOrEditChannelScreen.displayNameInput).toBeVisible();
 
-        // # Step 2: Select Private Channel type, type in a name, purpose and header for the new channel
-        // Toggle to make it private
         await CreateOrEditChannelScreen.toggleMakePrivateOn();
         await expect(CreateOrEditChannelScreen.makePrivateToggledOn).toBeVisible();
 
-        // Type channel name
-        await CreateOrEditChannelScreen.displayNameInput.typeText(channelDisplayName);
+        await CreateOrEditChannelScreen.displayNameInput.replaceText(privateChannelDisplayName);
 
-        // Type channel purpose
         await CreateOrEditChannelScreen.purposeInput.tap();
-        await CreateOrEditChannelScreen.purposeInput.typeText(channelPurpose);
+        await CreateOrEditChannelScreen.purposeInput.replaceText(channelPurpose);
 
-        // Type channel header
         await CreateOrEditChannelScreen.headerInput.tap();
-        await CreateOrEditChannelScreen.headerInput.typeText(channelHeader);
+        await CreateOrEditChannelScreen.headerInput.replaceText(channelHeader);
 
-        // # Step 3: Tap on "Create" in the top right of the screen
         await CreateOrEditChannelScreen.createButton.tap();
 
-        // * After #3, the new channel should be created and you should be redirected to the new channel
         await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText(channelDisplayName);
+        await expect(ChannelScreen.headerTitle).toHaveText(privateChannelDisplayName);
 
-        // Verify it's a private channel by checking channel info
         await ChannelInfoScreen.open();
-        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(channelDisplayName.toUpperCase());
+        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(privateChannelDisplayName);
         await expect(ChannelInfoScreen.publicPrivateTitlePurpose).toHaveText(channelPurpose);
 
-        // Close channel info and go back to channel list
         await ChannelInfoScreen.close();
         await ChannelScreen.back();
     });
 
     it('MM-T3199 - RN apps Edit public channel', async () => {
-        // # Setup: Create a public test channel
-        const channelName = `edit-pub-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'O',
-        });
-
-        // Navigate to the channel
-        await ChannelScreen.open('public', channel.display_name);
-
-        // # Step 1: On a public channel where permissions allow you to edit channel header etc., tap a channel name
+        await ChannelScreen.open(channelsCategory, publicChannelName);
         await ChannelInfoScreen.open();
-
-        // # Step 2: Tap Edit Channel, verify Save button is dimmed until changes are made
-        await expect(ChannelInfoScreen.editChannelOption).toBeVisible();
-        await ChannelInfoScreen.editChannelOption.tap();
-        await CreateOrEditChannelScreen.toBeVisible();
+        await CreateOrEditChannelScreen.openEditChannel();
         await expect(CreateOrEditChannelScreen.saveButton).toBeVisible();
 
-        // # Step 3: Add text to the channel name
-        const additionalText = ' edited';
-        await CreateOrEditChannelScreen.displayNameInput.tapAtPoint({x: 200, y: 10}); // Tap at end
-        await CreateOrEditChannelScreen.displayNameInput.typeText(additionalText);
+        const updatedDisplayName = publicChannelDisplayName + ' edited';
+        await CreateOrEditChannelScreen.displayNameInput.clearText();
+        await CreateOrEditChannelScreen.displayNameInput.replaceText(updatedDisplayName);
 
-        // # Step 4: Type text in Purpose field
         const purposeText = 'Updated purpose for this channel';
         await CreateOrEditChannelScreen.purposeInput.tap();
         await CreateOrEditChannelScreen.purposeInput.clearText();
-        await CreateOrEditChannelScreen.purposeInput.typeText(purposeText);
+        await CreateOrEditChannelScreen.purposeInput.replaceText(purposeText);
 
-        // # Step 5: Type text in Header field and tap Return to get new line
         const headerLine1 = 'First line of header';
         await CreateOrEditChannelScreen.headerInput.tap();
         await CreateOrEditChannelScreen.headerInput.clearText();
-        await CreateOrEditChannelScreen.headerInput.typeText(headerLine1 + '\n');
+        await CreateOrEditChannelScreen.headerInput.replaceText(headerLine1 + '\n');
 
-        // # Step 6: Type a second line of text
         const headerLine2 = 'Second line of header';
-        await CreateOrEditChannelScreen.headerInput.typeText(headerLine2);
+        await CreateOrEditChannelScreen.headerInput.replaceText(headerLine2);
 
-        // # Step 7: Tap Save
         await CreateOrEditChannelScreen.saveButton.tap();
 
-        // * Edit modal closes, channel info is displayed
         await wait(timeouts.TWO_SEC);
         await ChannelInfoScreen.toBeVisible();
-
-        // * The changes you made are displayed
-        const updatedDisplayName = channelName + additionalText;
-        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(updatedDisplayName.toUpperCase());
+        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(updatedDisplayName);
         await expect(ChannelInfoScreen.publicPrivateTitlePurpose).toHaveText(purposeText);
 
-        // Close channel info
         await ChannelInfoScreen.close();
         await ChannelScreen.back();
     });
 
     it('MM-T3206 - RN apps Edit private channel', async () => {
-        // # Setup: Create a private test channel
-        const channelName = `edit-priv-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'P',
-        });
-
-        // Navigate to the channel
-        await ChannelScreen.open('private', channel.display_name);
-
-        // # Step 1: On a private channel where policy allows you to edit channel header etc., tap a channel name
+        await ChannelScreen.open(channelsCategory, privateChannelName);
         await ChannelInfoScreen.open();
-
-        // # Step 2: Tap Edit Channel, verify Save button is dimmed until changes are made
-        await expect(ChannelInfoScreen.editChannelOption).toBeVisible();
-        await ChannelInfoScreen.editChannelOption.tap();
-        await CreateOrEditChannelScreen.toBeVisible();
+        await CreateOrEditChannelScreen.openEditChannel();
         await expect(CreateOrEditChannelScreen.saveButton).toBeVisible();
 
-        // # Step 3: Add text to the channel name
-        const additionalText = ' edited';
-        await CreateOrEditChannelScreen.displayNameInput.tapAtPoint({x: 200, y: 10}); // Tap at end
-        await CreateOrEditChannelScreen.displayNameInput.typeText(additionalText);
+        const updatedDisplayName = privateChannelDisplayName + ' edited';
+        await CreateOrEditChannelScreen.displayNameInput.clearText();
+        await CreateOrEditChannelScreen.displayNameInput.replaceText(updatedDisplayName);
 
-        // # Step 4: Type text in Purpose field
         const purposeText = 'Updated purpose for private channel';
         await CreateOrEditChannelScreen.purposeInput.tap();
         await CreateOrEditChannelScreen.purposeInput.clearText();
-        await CreateOrEditChannelScreen.purposeInput.typeText(purposeText);
+        await CreateOrEditChannelScreen.purposeInput.replaceText(purposeText);
 
-        // # Step 5: Type text in Header field and tap Return to get new line
         const headerLine1 = 'First line of header';
         await CreateOrEditChannelScreen.headerInput.tap();
         await CreateOrEditChannelScreen.headerInput.clearText();
-        await CreateOrEditChannelScreen.headerInput.typeText(headerLine1 + '\n');
+        await CreateOrEditChannelScreen.headerInput.replaceText(headerLine1 + '\n');
 
-        // # Step 6: Tap a second line of text
         const headerLine2 = 'Second line of header';
-        await CreateOrEditChannelScreen.headerInput.typeText(headerLine2);
+        await CreateOrEditChannelScreen.headerInput.replaceText(headerLine2);
 
-        // # Step 7: Tap Save
         await CreateOrEditChannelScreen.saveButton.tap();
 
-        // * Edit modal closes, channel info is displayed
         await wait(timeouts.TWO_SEC);
         await ChannelInfoScreen.toBeVisible();
-
-        // * The changes you made are displayed
-        const updatedDisplayName = channelName + additionalText;
-        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(updatedDisplayName.toUpperCase());
+        await expect(ChannelInfoScreen.publicPrivateTitleDisplayName).toHaveText(updatedDisplayName);
         await expect(ChannelInfoScreen.publicPrivateTitlePurpose).toHaveText(purposeText);
 
-        // Close channel info
         await ChannelInfoScreen.close();
         await ChannelScreen.back();
     });
 
     it('MM-T854 - RN apps Channel can be created using 2 non-latin characters', async () => {
-        // # Step 1: Go to create a channel on the RN apps
         await ChannelListScreen.headerPlusButton.tap();
-        await BrowseChannelsScreen.toBeVisible();
-        await BrowseChannelsScreen.createButton.tap();
+        await ChannelListScreen.createNewChannelItem.tap();
+
         await CreateOrEditChannelScreen.toBeVisible();
 
-        // # Step 2: Enter some non-latin characters as the name (e.g. ÁÜäÊúü)
+        await wait(timeouts.ONE_SEC);
+        await CreateOrEditChannelScreen.toBeVisible();
+
         const nonLatinChannelName = 'ÁÜ';
-        await CreateOrEditChannelScreen.displayNameInput.typeText(nonLatinChannelName);
+        await CreateOrEditChannelScreen.displayNameInput.replaceText(nonLatinChannelName);
         await wait(timeouts.ONE_SEC);
 
-        // # Step 3: Click create
         await CreateOrEditChannelScreen.createButton.tap();
         await wait(timeouts.TWO_SEC);
 
-        // * Channel is created
         await ChannelScreen.toBeVisible();
         await expect(ChannelScreen.headerTitle).toHaveText(nonLatinChannelName);
 
-        // Go back to channel list
         await ChannelScreen.back();
     });
 });
--- detox/e2e/test/products/channels/channel_settings/channel_join_leave.e2e.ts
+++ detox/e2e/test/products/channels/channel_settings/channel_join_leave.e2e.ts
@@ -20,198 +20,104 @@ import {
     serverOneUrl,
     siteOneUrl,
 } from '@support/test_config';
+import { Alert } from '@support/ui/component';
 import {
     BrowseChannelsScreen,
     ChannelInfoScreen,
     ChannelListScreen,
     ChannelScreen,
-    HomeScreen,
     LoginScreen,
     ServerScreen,
+    HomeScreen,
 } from '@support/ui/screen';
-import {getRandomId, timeouts, wait} from '@support/utils';
+import {timeouts, wait} from '@support/utils';
 import {expect} from 'detox';
 
 describe('Channels', () => {
     const serverOneDisplayName = 'Server 1';
+    const channelsCategory = 'channels';
     let testUser: any;
-    let testTeam: any;
+    let testChannel: any;
+    let privateChannel: any;
 
     beforeAll(async () => {
-        const {user, team} = await Setup.apiInit(siteOneUrl);
+        const {user, channel, team} = await Setup.apiInit(siteOneUrl);
         testUser = user;
-        testTeam = team;
+        testChannel = channel;
 
-        // # Log in to server
+        const {channel: privateChannelData} = await Channel.apiCreateChannel(siteOneUrl, {type: 'P', prefix: 'channel', teamId: team.id});
+        privateChannel = privateChannelData;
+
+        await Channel.apiAddUserToChannel(siteOneUrl, testUser.id, privateChannel.id);
         await ServerScreen.connectToServer(serverOneUrl, serverOneDisplayName);
         await LoginScreen.login(testUser);
     });
 
     afterAll(async () => {
-        // # Log out
         await HomeScreen.logout();
     });
 
     it('MM-T3200 - RN apps Join existing channel', async () => {
-        // # Setup: Create a test channel that the user is not a member of
-        const channelName = `join-test-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'O',
-        });
 
-        // Remove user from the channel so they can join it
-        await Channel.apiRemoveUserFromChannel(siteOneUrl, channel.id, testUser.id);
-
-        // # Step 1: From the channels list, tap on the "+" displayed to the right of "PUBLIC CHANNELS"
-        await ChannelListScreen.headerPlusButton.tap();
-        await BrowseChannelsScreen.toBeVisible();
-
-        // # Step 2: Type the first two letters of a channel in the search box
-        const searchTerm = channelName.substring(0, 2);
-        await BrowseChannelsScreen.searchInput.typeText(searchTerm);
-
-        // # Step 3: Observe list filtered by search term
-        await wait(timeouts.ONE_SEC);
-        await expect(BrowseChannelsScreen.getChannelItem(channel.display_name)).toBeVisible();
-
-        // # Step 4: Tap a channel name to select it
-        await BrowseChannelsScreen.getChannelItem(channel.display_name).tap();
-        await wait(timeouts.ONE_SEC);
-
-        // Tap again if needed (first tap may close keyboard)
-        await BrowseChannelsScreen.getChannelItem(channel.display_name).tap();
-
-        // * List filters as expected, and channel is joined and displayed
-        await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText(channel.display_name);
-
-        // * System message shows you have joined the channel
+        await ChannelScreen.open(channelsCategory, testChannel.name);
         await wait(timeouts.TWO_SEC);
         const joinMessage = `${testUser.username} joined the channel`;
         await waitFor(element(by.text(joinMessage).withAncestor(by.id('post_list')))).
-            toBeVisible().
-            whileElement(by.id('post_list.flat_list')).
-            scroll(50, 'down');
+            toBeVisible();
 
-        // Go back to channel list
         await ChannelScreen.back();
     });
 
     it('MM-T3202 - RN apps Leave public channel', async () => {
-        // # Setup: Create a test channel and navigate to it
-        const channelName = `leave-test-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'O', // Open (public) channel
-        });
-
-        await ChannelScreen.open('public', channel.display_name);
-
-        // # Step 1: Tap on "leave channel"
+        await ChannelScreen.open(channelsCategory, testChannel.name);
         await ChannelInfoScreen.open();
+        await ChannelInfoScreen.scrollView.scrollTo('bottom');
         await expect(ChannelInfoScreen.leaveChannelOption).toBeVisible();
         await ChannelInfoScreen.leaveChannelOption.tap();
 
-        // * After #1, a message is displayed on screen asking whether or not you want to leave the channel
         await wait(timeouts.ONE_SEC);
-        const leaveAlertTitle = 'Leave';
+        const leaveAlertTitle = 'Leave channel';
         await expect(element(by.text(leaveAlertTitle))).toBeVisible();
-        await expect(element(by.text(`Are you sure you want to leave the channel ${channel.display_name}?`))).toBeVisible();
+        await expect(element(by.text(`Are you sure you want to leave the public channel ${testChannel.display_name}? You can always rejoin.`))).toBeVisible();
 
-        // # Step 2: Tap on "yes"
-        await element(by.text('Yes')).tap();
-
-        // * After #2, you should be removed from the channel and be redirected to Town Square
+        await Alert.leaveButton.tap();
         await wait(timeouts.TWO_SEC);
-        await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText('Town Square');
+        await ChannelListScreen.toBeVisible();
     });
 
     it('MM-T3207 - RN apps Leave private channel', async () => {
-        // # Setup: Create a private test channel
-        const channelName = `private-leave-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'P', // Private channel
-        });
 
-        // Navigate to the private channel
-        await ChannelScreen.open('private', channel.display_name);
-
-        // # Step 1: On a private channel, tap channel name
+        await ChannelScreen.open(channelsCategory, privateChannel.name);
         await ChannelInfoScreen.open();
+        await ChannelInfoScreen.scrollView.scrollTo('bottom');
 
-        // # Step 2: Tap Leave Channel
         await expect(ChannelInfoScreen.leaveChannelOption).toBeVisible();
         await ChannelInfoScreen.leaveChannelOption.tap();
 
-        // # Step 3: Tap Yes on confirmation modal
         await wait(timeouts.ONE_SEC);
-        await expect(element(by.text('Leave'))).toBeVisible();
-        await element(by.text('Yes')).tap();
+        await expect(element(by.text('Leave channel'))).toBeVisible();
+        await expect(element(by.text(`Are you sure you want to leave the private channel ${privateChannel.display_name}? You cannot rejoin the channel unless you're invited again.`))).toBeVisible();
+        await Alert.leaveButton.tap();
 
-        // * Redirects to Town Square
         await wait(timeouts.TWO_SEC);
-        await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText('Town Square');
-
-        // * Channel no longer listed in channel drawer
-        // The private channel should not be visible in the list anymore
-        try {
-            await expect(ChannelListScreen.getChannelItem('private', channel.display_name)).not.toBeVisible();
-        } catch (e) {
-            // If the element doesn't exist at all, that's also acceptable
-        }
+        await ChannelListScreen.toBeVisible();
     });
 
     it('MM-T3188 - RN apps Search for public channel Cancel search join channel', async () => {
-        // # Setup: Create a test channel that the user is not a member of
-        const channelName = `search-join-${getRandomId()}`;
-        const {channel} = await Channel.apiCreateChannel(siteOneUrl, {
-            teamId: testTeam.id,
-            name: channelName,
-            displayName: channelName,
-            type: 'O',
-        });
+        const channelName = testChannel.name;
 
-        // Remove user from the channel so they can search and join it
-        await Channel.apiRemoveUserFromChannel(siteOneUrl, channel.id, testUser.id);
+        await BrowseChannelsScreen.open();
 
-        // # Step 1: From the channels list, tap on the "+" next to Channels
-        await ChannelListScreen.headerPlusButton.tap();
-        await wait(timeouts.ONE_SEC);
-
-        // # Step 2: Tap More Channels
-        await BrowseChannelsScreen.toBeVisible();
-
-        // # Step 3: Enter the beginning of a channel name you're not a member of into the search text field
-        // * Verify the channel name appears in the search results below
         const searchTerm = channelName.substring(0, 4);
         await BrowseChannelsScreen.searchInput.typeText(searchTerm);
         await wait(timeouts.TWO_SEC);
 
-        await expect(BrowseChannelsScreen.getChannelItem(channel.display_name)).toBeVisible();
-
-        // Tap the channel to join it
-        await BrowseChannelsScreen.getChannelItem(channel.display_name).tap();
+        await expect(BrowseChannelsScreen.getChannelItemDisplayName(testChannel.name)).toHaveText(testChannel.display_name);
+        await BrowseChannelsScreen.getChannelItem(testChannel.name).multiTap(2);
         await wait(timeouts.ONE_SEC);
 
-        // Tap again if needed (first tap may close keyboard)
-        await BrowseChannelsScreen.getChannelItem(channel.display_name).tap();
-        await wait(timeouts.TWO_SEC);
-
-        // Verify we're in the channel
         await ChannelScreen.toBeVisible();
-        await expect(ChannelScreen.headerTitle).toHaveText(channel.display_name);
-
-        // Go back to channel list
+        await expect(ChannelScreen.headerTitle).toHaveText(testChannel.display_name);
         await ChannelScreen.back();
     });
 });
