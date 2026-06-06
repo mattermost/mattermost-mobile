1242c605dee647cc7ca5926b42eddcca66d1193b
echo-echo-chen <1611394830@qq.com>
2026-06-06 23:21:28 +0800

    rebase 练习6

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license informationsss.
+// See LICENSE.txt for license information.
 
 
 import {RUNNING_E2E} from '@env';
