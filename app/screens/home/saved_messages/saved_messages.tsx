bba742b27d716b587d76b8d7e70da1747d45c7a4
Daniel Espino García <larkox@gmail.com>
2026-07-13 17:43:09 +0200

    [MM-69227] Fix back button behavior (#9927)
    * [MM-69227] Fix back button behavior
    
    * Rever unneeded patch change and address coderabbit comment

--- app/screens/home/saved_messages/saved_messages.tsx
+++ app/screens/home/saved_messages/saved_messages.tsx
@@ -19,6 +19,7 @@ import {SCREENS_AS_BOTTOM_SHEET} from '@constants/screens';
 import {PostConfigProvider} from '@context/post_config';
 import {useServerUrl} from '@context/server';
 import {useTheme} from '@context/theme';
+import useAndroidHomeTabBackHandler from '@hooks/android_home_tab_back_handler';
 import {useCollapsibleHeader} from '@hooks/header';
 import {useCurrentScreen} from '@store/navigation_store';
 import {getDateForDateLine, selectOrderedPosts} from '@utils/post_list';
@@ -63,6 +64,8 @@ function SavedMessages({appsEnabled, posts, currentUser, customEmojiNames}: Prop
     const isBottomSheetOpen = currentScreen && SCREENS_AS_BOTTOM_SHEET.has(currentScreen);
     const isPemalinkScreen = currentScreen === Screens.PERMALINK;
     const isGalleryScreen = currentScreen === Screens.GALLERY;
+
+    useAndroidHomeTabBackHandler(Screens.SAVED_MESSAGES);
 
     const params = route.params as {direction: string};
     const toLeft = params.direction === 'left';
